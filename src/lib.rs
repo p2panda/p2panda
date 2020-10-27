bfff374a1b1e9befea9361819dd0f9b819b48831
Andreas Dzialocha <x12@adz.garden>
2020-10-27 20:32:45 +0100

    Small experiment

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+extern crate wasm_bindgen;
+
+use wasm_bindgen::prelude::wasm_bindgen;
+
+#[wasm_bindgen]
+pub fn hello() -> String {
+    "Hallo, hier ist alles schön".into()
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
