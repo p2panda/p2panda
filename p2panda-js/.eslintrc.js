dae4ecdbb4ce6958959ea560f194aaba4e9031f9
Vincent Ahrend <mail@vincentahrend.com>
2020-12-12 20:06:41 +0100

    Replace standard with prettier

--- p2panda-js/.eslintrc.js
+++ p2panda-js/.eslintrc.js
@@ -1,21 +1,14 @@
 module.exports = {
-  // Parser to lint TypeScript code, see:
-  // https://standardjs.com/index.html#typescript
   parser: '@typescript-eslint/parser',
-  plugins: [
-    // Required plugin to lint TypeScript code
-    '@typescript-eslint',
+  extends: [
+    // Applies the recommended rules from @typescript-eslint/eslint-plugin
+    'plugin:@typescript-eslint/recommended',
+    // Keep prettier last to make sure its style changes are not overwritten
+    // by other rules
+    'plugin:prettier/recommended',
   ],
   rules: {
-    // Standard incorrectly emits unused-variable errors, see:
-    // https://github.com/standard/standard/issues/1283
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': 'error',
-    // Always require dangling commas for multiline objects and arrays
-    'comma-dangle': ['error', 'always-multiline'],
-    // Standard does not like semicolons, semistandard likes them, we like
-    // semicolons as well, but we're using standardx, therefore we have to
-    // require them here manually
-    semi: ['error', 'always'],
+    // Warn on prettier violations and continue with build
+    'prettier/prettier': 1,
   },
 };
