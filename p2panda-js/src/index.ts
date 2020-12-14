2b189323ba85b31cf9dcc0ff6d2649cf48d1492b
Vincent Ahrend <mail@vincentahrend.com>
2020-12-14 18:31:53 +0100

    Setup p2panda-js tests (#17)
    * Make Webpack less verbose
    
    * Add basic test 
    
    * Add nodemon
    
    * Let test access library export instead of wasm

--- p2panda-js/src/index.ts
+++ p2panda-js/src/index.ts
@@ -1,3 +1,6 @@
+// This method makes sure we only load the p2panda-rs library once even when it was
+// used multiple times (singleton). Also it sets the panic hook automatically
+// for better debugging.
 const wasm = import('wasm').then(({ setWasmPanicHook, ...rest }) => {
   // Set panic hooks for better logging of wasm errors. See:
   // https://github.com/rustwasm/console_error_panic_hook
@@ -6,9 +9,4 @@ const wasm = import('wasm').then(({ setWasmPanicHook, ...rest }) => {
   return rest;
 });
 
-// This method makes sure we only load the p2panda-rs library once even when it was
-// used multiple times (singleton). Also it sets the panic hook automatically
-// for better debugging.
-wasm.then(({ KeyPair }) => {
-  console.log(new KeyPair());
-});
+export default wasm;
