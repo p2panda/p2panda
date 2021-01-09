944aa891315ef465e84ea4ef812d232287aaf73a
Andreas Dzialocha <x12@adz.garden>
2021-01-09 21:40:50 +0100

    Give wasm chunk its own name in bundle

--- p2panda-js/src/index.ts
+++ p2panda-js/src/index.ts
@@ -1,12 +1,14 @@
 // This method makes sure we only load the p2panda-rs library once even when it was
 // used multiple times (singleton). Also it sets the panic hook automatically
 // for better debugging.
-const wasm = import('wasm').then(({ setWasmPanicHook, ...rest }) => {
-  // Set panic hooks for better logging of wasm errors. See:
-  // https://github.com/rustwasm/console_error_panic_hook
-  setWasmPanicHook();
+const wasm = import(/* webpackChunkName: "wasm" */ 'wasm').then(
+  ({ setWasmPanicHook, ...rest }) => {
+    // Set panic hooks for better logging of wasm errors. See:
+    // https://github.com/rustwasm/console_error_panic_hook
+    setWasmPanicHook();
 
-  return rest;
-});
+    return rest;
+  },
+);
 
 export default wasm;
