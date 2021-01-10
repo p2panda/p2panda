9cd54276793582bed987c8ed7d08fd7b89df1f62
Andreas Dzialocha <x12@adz.garden>
2021-01-10 01:26:28 +0100

    Update comments, make it an actual singleton

--- p2panda-js/src/index.ts
+++ p2panda-js/src/index.ts
@@ -3,13 +3,13 @@
 import wasmBase64 from 'wasm/index_bg.wasm';
 import wasmInit, { setWasmPanicHook, KeyPair } from 'wasm';
 
-// This method makes sure we only load the p2panda-rs library once even when it
+// This value makes sure we only load the p2panda-rs library once even when it
 // was used multiple times (singleton). Also it sets the panic hook
 // automatically for better debugging.
 //
 // The WebAssembly code is encoded to base64 and bundled by Webpack to be able
-// to use this library straight from the browser.
-export default new Promise((resolve) => {
+// to use this library directly in the browser without any further build steps.
+const wasm = new Promise((resolve) => {
   // Decode base64-encoded WebAssembly to bytes and initialize
   const bytes = Uint8Array.from(atob(wasmBase64), (c) => c.charCodeAt(0));
 
@@ -23,3 +23,5 @@ export default new Promise((resolve) => {
     });
   });
 });
+
+export default wasm;
