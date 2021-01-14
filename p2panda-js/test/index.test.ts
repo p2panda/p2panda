b7972b6c1260af0b20fc34a7cb15997b5fefab95
Andreas Dzialocha <x12@adz.garden>
2021-01-15 00:12:01 +0100

    Add one more test for fun

--- p2panda-js/test/index.test.ts
+++ p2panda-js/test/index.test.ts
@@ -5,7 +5,14 @@ import p2panda from '../lib';
 describe('KeyPair', () => {
   it('creates a key pair', async () => {
     const { KeyPair } = await p2panda;
-    const kp = new KeyPair();
-    expect(kp.privateKey().length).to.eq(64);
+    const keyPair = new KeyPair();
+    expect(keyPair.privateKey().length).to.eq(64);
+  });
+
+  it('restores a key pair', async () => {
+    const { KeyPair } = await p2panda;
+    const keyPair = new KeyPair();
+    const keyPairSecond = KeyPair.fromPrivateKey(keyPair.privateKey());
+    expect(keyPair.publicKey()).to.eq(keyPairSecond.publicKey());
   });
 });
