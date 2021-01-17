118c8e4bdac978379aa7249a7bcf9aa5bb4100c3
Andreas Dzialocha <x12@adz.garden>
2021-01-17 01:16:56 +0100

    Extend Message and MessageEncoded structs, add errors, documentation and additional methods

--- p2panda-rs/src/error.rs
+++ p2panda-rs/src/error.rs
@@ -1,7 +1,7 @@
 use validator::ValidationErrors;
 
+// @TODO: Replace this with own `Validation` trait
+pub type ValidationResult = anyhow::Result<(), ValidationErrors>;
+
 /// A specialized `Result` type for p2panda-rs.
 pub type Result<T> = anyhow::Result<T>;
-
-/// A specialized `Result` type for validation errors.
-pub type ValidationResult = anyhow::Result<(), ValidationErrors>;
