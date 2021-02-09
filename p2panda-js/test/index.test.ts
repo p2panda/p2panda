7e2e2c321db27e5a74a024abf6eb38279572cf7c
Andreas Dzialocha <x12@adz.garden>
2021-02-09 23:36:27 +0100

    Bring back private_key methods in KeyPair struct
    I realized we don't need to change the API when changing the fields to
    now wrap the whole Ed25519 keypair in our KeyPair struct.
    
    The old API had the advantage to only work with the private key to
    restore the key pair. Its only a small difference but does not need a
    redundant public part, saves some bytes and is more elegant :-)

--- p2panda-js/test/index.test.ts
+++ p2panda-js/test/index.test.ts
@@ -6,13 +6,13 @@ describe('KeyPair', () => {
   it('creates a key pair', async () => {
     const { KeyPair } = await p2panda;
     const keyPair = new KeyPair();
-    expect(keyPair.toHex().length).to.eq(128);
+    expect(keyPair.privateKey().length).to.eq(64);
   });
 
   it('restores a key pair', async () => {
     const { KeyPair } = await p2panda;
     const keyPair = new KeyPair();
-    const keyPairSecond = KeyPair.fromHex(keyPair.toHex());
+    const keyPairSecond = KeyPair.fromPrivateKey(keyPair.privateKey());
     expect(keyPair.publicKey()).to.eq(keyPairSecond.publicKey());
   });
 });
--- p2panda-rs/src/keypair.rs
+++ p2panda-rs/src/keypair.rs
@@ -1,4 +1,4 @@
-use ed25519_dalek::{Keypair as Ed25519Keypair, Signer};
+use ed25519_dalek::{Keypair as Ed25519Keypair, PublicKey, SecretKey, Signer};
 #[cfg(not(target_arch = "wasm32"))]
 use ed25519_dalek::{Signature, SignatureError};
 use rand::rngs::OsRng;
@@ -24,46 +24,54 @@ impl KeyPair {
     pub fn new() -> Self {
         let mut csprng: OsRng = OsRng {};
         let key_pair = Ed25519Keypair::generate(&mut csprng);
-
         Self(key_pair)
     }
 
-    /// Derives a key pair from hex-encoded serialized representation
+    /// Derives a key pair from a private key (encoded as hex string).
     ///
     /// WARNING: "Absolutely no validation is done on the key. If you give this function bytes
     /// which do not represent a valid point, or which do not represent corresponding parts of the
     /// key, then your Keypair will be broken and it will be your fault."
     /// https://docs.rs/ed25519-dalek/1.0.1/ed25519_dalek/struct.Keypair.html#warning
-    #[cfg_attr(target_arch = "wasm32", wasm_bindgen(js_name = fromHex))]
-    pub fn from_hex(encoded_key_pair: String) -> Self {
-        let bytes = hex::decode(encoded_key_pair).unwrap();
+    #[cfg_attr(target_arch = "wasm32", wasm_bindgen(js_name = fromPrivateKey))]
+    pub fn from_private_key(private_key: String) -> Self {
+        // Decode private key
+        let secret_key_bytes = hex::decode(private_key).unwrap();
+        let secret_key = SecretKey::from_bytes(&secret_key_bytes).unwrap();
+
+        // Derive public part from secret part
+        let public_key: PublicKey = (&secret_key).into();
+
+        // Assemble key pair from both parts
+        let bytes = [secret_key.to_bytes(), public_key.to_bytes()].concat();
+        println!("{:?}", bytes);
         let key_pair = Ed25519Keypair::from_bytes(&bytes).unwrap();
 
         Self(key_pair)
     }
 
-    /// Returns the key pair, including secret key, encoded as a hex string.
-    #[cfg_attr(target_arch = "wasm32", wasm_bindgen(js_name = toHex))]
-    pub fn to_hex(&self) -> String {
-        hex::encode(self.0.to_bytes())
-    }
-
-    /// Returns the key pair as a byte representation
-    #[cfg_attr(target_arch = "wasm32", wasm_bindgen(js_name = toBytes))]
-    pub fn to_bytes(&self) -> Box<[u8]> {
-        Box::from(self.0.to_bytes())
-    }
-
     /// Returns the public half of the key pair, encoded as a hex string.
     #[cfg_attr(target_arch = "wasm32", wasm_bindgen(js_name = publicKey))]
     pub fn public_key(&self) -> String {
-        hex::encode(self.0.public.as_bytes())
+        hex::encode(self.0.public.to_bytes())
+    }
+
+    /// Returns the private half of the key pair, encoded as a hex string.
+    #[cfg_attr(target_arch = "wasm32", wasm_bindgen(js_name = privateKey))]
+    pub fn private_key(&self) -> String {
+        hex::encode(self.0.secret.to_bytes())
     }
 
     /// Returns the public half of the key pair.
     #[cfg_attr(target_arch = "wasm32", wasm_bindgen(js_name = publicKeyBytes))]
     pub fn public_key_bytes(&self) -> Box<[u8]> {
         Box::from(self.0.public.to_bytes())
+    }
+
+    /// Returns the private half of the key pair.
+    #[cfg_attr(target_arch = "wasm32", wasm_bindgen(js_name = privateKeyBytes))]
+    pub fn private_key_bytes(&self) -> Box<[u8]> {
+        Box::from(self.0.secret.to_bytes())
     }
 
     /// Sign data using this key pair.
@@ -81,7 +89,7 @@ impl KeyPair {
 
 #[cfg(test)]
 mod tests {
-    use ed25519_dalek::{KEYPAIR_LENGTH, PUBLIC_KEY_LENGTH};
+    use ed25519_dalek::{PUBLIC_KEY_LENGTH, SECRET_KEY_LENGTH};
 
     use super::KeyPair;
 
@@ -89,17 +97,17 @@ mod tests {
     fn makes_keypair() {
         let key_pair = KeyPair::new();
         assert_eq!(key_pair.public_key().len(), PUBLIC_KEY_LENGTH * 2);
+        assert_eq!(key_pair.private_key().len(), PUBLIC_KEY_LENGTH * 2);
         assert_eq!(key_pair.public_key_bytes().len(), PUBLIC_KEY_LENGTH);
-        assert_eq!(key_pair.to_hex().len(), KEYPAIR_LENGTH * 2);
-        assert_eq!(key_pair.to_bytes().len(), KEYPAIR_LENGTH);
+        assert_eq!(key_pair.private_key_bytes().len(), SECRET_KEY_LENGTH);
     }
 
     #[test]
-    fn key_pair_from_hex_encoding() {
+    fn key_pair_from_private_key() {
         let key_pair = KeyPair::new();
-        let key_pair2 = KeyPair::from_hex(key_pair.to_hex());
+        let key_pair2 = KeyPair::from_private_key(key_pair.private_key());
         assert_eq!(key_pair.public_key_bytes(), key_pair2.public_key_bytes());
-        assert_eq!(key_pair.to_bytes(), key_pair2.to_bytes());
+        assert_eq!(key_pair.private_key_bytes(), key_pair2.private_key_bytes());
     }
 
     #[test]
