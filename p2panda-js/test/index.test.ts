30543cb1995b7211aebadadaf7e355263d38908b
sandreae <git@contact.samandreae.com>
2021-04-29 10:51:19 +0100

    Pass valid seq_num to signEncodeEntry in js-tests

--- p2panda-js/test/index.test.ts
+++ p2panda-js/test/index.test.ts
@@ -31,6 +31,7 @@ describe('Entries', () => {
     const TEST_SCHEMA =
       '0040cf94f6d605657e90c543b0c919070cdaaf7209c5e1ea58acb8f3568fa2114268dc9ac3bafe12af277d286fce7dc59b7c0c348973c4e9dacbe79485e56ac2a702';
     const LOG_ID = 5;
+    const SEQ_NO = 1;
 
     const {
       KeyPair,
@@ -55,7 +56,7 @@ describe('Entries', () => {
       messageEncoded,
       null,
       null,
-      null,
+      BigInt(SEQ_NO),
       BigInt(LOG_ID),
     );
 
