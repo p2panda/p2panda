72c1c6cc1ea6ea0698d4b5501092cbec0d045543
cafca <cafca@001.land>
2021-05-21 01:58:48 +0200

    Add test

---
+++ p2panda-js/test/session.test.ts
@@ -0,0 +1,16 @@
+import { expect } from 'chai';
+
+import { Session } from '../lib';
+
+// const PRIVATE_KEY =
+//   '4c21b14046f284f87f1ea4be4b973664221ad483079a68ed35a6812553b41176';
+
+describe('Session', () => {
+  it('can query entries', async () => {
+    const session = new Session('http://localhost:2020');
+    const entries = await session.queryEntries(
+      '00401d76566758a5b6bfc561f1c936d8fc86b5b42ea22ab1dabf40d249d27dd906401fde147e53f44c103dd02a254916be113e51de1077a946a3a0c1272b9b348437',
+    );
+    expect(entries.length).to.equal(2);
+  });
+});
