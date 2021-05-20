b9a2c0125fa1127a76bed7469789def926696324
Andreas Dzialocha <x12@adz.garden>
2021-05-20 13:54:44 +0200

    Make linter happy

--- p2panda-rs/src/entry/entry.rs
+++ p2panda-rs/src/entry/entry.rs
@@ -27,12 +27,12 @@ use crate::Validate;
 /// use p2panda_rs::entry::{Entry, LogId, SeqNum};
 /// use p2panda_rs::message::{Message, MessageFields, MessageValue};
 /// use p2panda_rs::hash::Hash;
-/// # let SCHEMA_HASH_STR = "004069db5208a271c53de8a1b6220e6a4d7fcccd89e6c0c7e75c833e34dc68d932624f2ccf27513f42fb7d0e4390a99b225bad41ba14a6297537246dbe4e6ce150e8";
+/// # let schema_hash_str = "004069db5208a271c53de8a1b6220e6a4d7fcccd89e6c0c7e75c833e34dc68d932624f2ccf27513f42fb7d0e4390a99b225bad41ba14a6297537246dbe4e6ce150e8";
 ///
 /// // == FIRST ENTRY IN NEW LOG ==
 ///
 /// // Create schema hash
-/// let schema_hash = Hash::new(SCHEMA_HASH_STR)?;
+/// let schema_hash = Hash::new(schema_hash_str)?;
 ///
 /// // Create a MessageFields instance and add a text field string with the key "title"
 /// let mut fields = MessageFields::new();
