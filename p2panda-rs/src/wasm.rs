230a7473f592407ca5179ca5911da341f49f32cc
Andreas Dzialocha <x12@adz.garden>
2021-05-23 13:51:53 +0200

    Update documentation

--- p2panda-rs/src/wasm.rs
+++ p2panda-rs/src/wasm.rs
@@ -20,12 +20,15 @@ use crate::message::{
 };
 
 // Converts any Rust Error type into js_sys:Error while keeping its error message. This helps
-// propagating errors similar like we do in Rust but in WebAssembly contexts.
+// propagating errors similar like we do in Rust but in WebAssembly contexts. It is possible to
+// optionally use a custom error message when required.
 macro_rules! jserr {
+    // Convert error to js_sys::Error with original error message
     ($l:expr) => {
         $l.map_err::<JsValue, _>(|err| js_sys::Error::new(&format!("{}", err)).into())?;
     };
 
+    // Convert error to js_sys::Error with custom error message
     ($l:expr, $err:expr) => {
         $l.map_err::<JsValue, _>(|_| js_sys::Error::new(&format!("{:?}", $err)).into())?;
     };
