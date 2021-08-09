0bfb95f713fa5ff0a29086ca577b121f17e84273
sandreae <git@contact.samandreae.com>
2021-08-09 14:00:31 +0100

    Pass fixed public_key string into nextEntryArgs test

--- p2panda-js/test/session.test.ts
+++ p2panda-js/test/session.test.ts
@@ -5,12 +5,17 @@ import { Session } from '../lib';
 // const PRIVATE_KEY =
 //   '4c21b14046f284f87f1ea4be4b973664221ad483079a68ed35a6812553b41176';
 
+const PUBLIC_KEY =
+  '2deee1fac104aaac753377bbc2ff70fa5d5154abdac4b4577392db88de6a9a49';
+
+const SCHEMA =
+  '00401d76566758a5b6bfc561f1c936d8fc86b5b42ea22ab1dabf40d249d27dd906401fde147e53f44c103dd02a254916be113e51de1077a946a3a0c1272b9b348437';
+
 describe('Session', () => {
   it('can query entries', async () => {
     const session = new Session('http://localhost:2020');
-    const entries = await session.queryEntries(
-      '00401d76566758a5b6bfc561f1c936d8fc86b5b42ea22ab1dabf40d249d27dd906401fde147e53f44c103dd02a254916be113e51de1077a946a3a0c1272b9b348437',
-    );
+    const entries = await session.queryEntries(SCHEMA);
+    console.log(entries);
     expect(entries.length).to.equal(2);
   });
 
@@ -27,13 +32,11 @@ describe('Session', () => {
 
   it('gets next entry args', async () => {
     const session = new Session('http://localhost:2020');
-    const { KeyPair } = await session.loadWasm();
-    const keyPair = new KeyPair();
-    const nextEntryArgs = await session.getNextEntryArgs(
-      keyPair.publicKey(),
-      '00401d76566758a5b6bfc561f1c936d8fc86b5b42ea22ab1dabf40d249d27dd906401fde147e53f44c103dd02a254916be113e51de1077a946a3a0c1272b9b348437',
-    );
+    const nextEntryArgs = await session.getNextEntryArgs(PUBLIC_KEY, SCHEMA);
     expect(nextEntryArgs.entryHashSkiplink).to.equal('SKIPLINK_HASH');
+    expect(nextEntryArgs.entryHashBacklink).to.equal('BACKLINK_HASH');
+    expect(nextEntryArgs.seqNum).to.equal(3);
+    expect(nextEntryArgs.logId).to.equal(1);
   });
 
   it('can publish entries');
