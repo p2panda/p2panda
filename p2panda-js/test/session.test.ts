3a3efee7f5fdc05a6dd49e4d03d83a632cd5cd4c
cafca <cafca@001.land>
2021-08-15 19:14:33 +0200

    Add more tests

--- p2panda-js/test/session.test.ts
+++ p2panda-js/test/session.test.ts
@@ -1,4 +1,8 @@
-import { expect } from 'chai';
+import chai, { assert, expect } from 'chai';
+import sinon from 'sinon';
+import chaiAsPromised from 'chai-as-promised';
+
+chai.use(chaiAsPromised);
 
 import { Session } from '../lib';
 
@@ -10,6 +14,12 @@ const PUBLIC_KEY =
 
 const SCHEMA =
   '00401d76566758a5b6bfc561f1c936d8fc86b5b42ea22ab1dabf40d249d27dd906401fde147e53f44c103dd02a254916be113e51de1077a946a3a0c1272b9b348437';
+
+const ENTRY_ENCODED =
+  '00ba07a8da75dd2f922d62eae7e7ac7c081e06bf0c192b2d8ea1b2ab5e9c59013e0101e700400f2338e42f03bfaab4b721949ad355d964ace34aad4d461ba2b1b42706ada938fe126e189c4b6332f891ee038cf0e9e6723d4911632497683a56dd75c8f6d89f8b411f3bd3f5c8c8114693d7c9de5672f4146ec4acb9bc357015e60bc7425874dd4158bdbdf482bfd5779eb0bc2bf4b4d068a4f7b23c8a03d75c7c61a4fba609';
+
+const MESSAGE_ENCODED =
+  'a466616374696f6e6663726561746566736368656d6178843030343063663934663664363035363537653930633534336230633931393037306364616166373230396335653165613538616362386633353638666132313134323638646339616333626166653132616632373764323836666365376463353962376330633334383937336334653964616362653739343835653536616332613730326776657273696f6e01666669656c6473a26464617465a164546578747818323032312d30352d31335431373a32303a31352e3133375a676d657373616765a164546578746648656c6c6f21';
 
 describe('Session', () => {
   it('can query entries', async () => {
@@ -39,10 +49,6 @@ describe('Session', () => {
   });
 
   it('can publish entries', async () => {
-    const ENTRY_ENCODED =
-      '00ba07a8da75dd2f922d62eae7e7ac7c081e06bf0c192b2d8ea1b2ab5e9c59013e0101e700400f2338e42f03bfaab4b721949ad355d964ace34aad4d461ba2b1b42706ada938fe126e189c4b6332f891ee038cf0e9e6723d4911632497683a56dd75c8f6d89f8b411f3bd3f5c8c8114693d7c9de5672f4146ec4acb9bc357015e60bc7425874dd4158bdbdf482bfd5779eb0bc2bf4b4d068a4f7b23c8a03d75c7c61a4fba609';
-    const MESSAGE_ENCODED =
-      'a466616374696f6e6663726561746566736368656d6178843030343063663934663664363035363537653930633534336230633931393037306364616166373230396335653165613538616362386633353638666132313134323638646339616333626166653132616632373764323836666365376463353962376330633334383937336334653964616362653739343835653536616332613730326776657273696f6e01666669656c6473a26464617465a164546578747818323032312d30352d31335431373a32303a31352e3133375a676d657373616765a164546578746648656c6c6f21';
     const session = new Session('http://localhost:2020');
     const nextEntryArgs = await session.publishEntry(
       ENTRY_ENCODED,
@@ -51,9 +57,32 @@ describe('Session', () => {
     expect(nextEntryArgs.entryHashSkiplink).to.equal('SKIPLINK_HASH');
   });
 
-  it('caches next entry args');
+  it('caches next entry args', async () => {
+    const session = new Session('http://localhost:2020');
+    // add a spy to check whether the value is really retrieved from the cache
+    // and not requested
+    session.client.request = sinon.replace(
+      session.client,
+      'request',
+      sinon.fake(),
+    );
 
-  it('throws when publishing without a key pair');
+    const nextEntryArgs = { test: true };
+    session.setNextEntryArgs(PUBLIC_KEY, SCHEMA, nextEntryArgs);
 
-  it('throws without a configured endpoint');
+    const cacheResponse = await session.getNextEntryArgs(PUBLIC_KEY, SCHEMA);
+    expect(cacheResponse.test).to.equal(nextEntryArgs.test);
+    expect(session.client.request.notCalled).to.be.true;
+  });
+
+  it('throws when missing parameters', async () => {
+    const session = new Session('http://localhost:2020');
+    assert.isRejected(session.publishEntry(null, MESSAGE_ENCODED));
+    assert.isRejected(session.publishEntry(ENTRY_ENCODED, null));
+  });
+
+  it('throws without a configured endpoint', () => {
+    const session = new Session();
+    assert.isRejected(session.publishEntry(ENTRY_ENCODED, MESSAGE_ENCODED));
+  });
 });
