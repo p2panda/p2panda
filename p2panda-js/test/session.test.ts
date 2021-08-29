f8c0efc683476a192dbe15a65df968bebfe2b1be
cafca <cafca@001.land>
2021-08-30 00:23:58 +0200

    Structure test module

--- p2panda-js/test/session.test.ts
+++ p2panda-js/test/session.test.ts
@@ -46,64 +46,70 @@ describe('Session', () => {
     );
   });
 
-  it('can query entries', async () => {
-    const session = new Session('http://localhost:2020');
-    const entries = await session.queryEntries(SCHEMA);
-    expect(entries.length).to.equal(1);
+  describe('queryEntries', () => {
+    it('can query entries', async () => {
+      const session = new Session('http://localhost:2020');
+      const entries = await session.queryEntries(SCHEMA);
+      expect(entries.length).to.equal(1);
+    });
+
+    it('throws when querying without a schema', async () => {
+      const session = new Session('http://localhost:2020');
+      assert.isRejected(session.queryEntries(), 'Schema must be provided');
+    });
   });
 
-  it('throws when querying without a schema', async () => {
-    const session = new Session('http://localhost:2020');
-    assert.isRejected(session.queryEntries(), 'Schema must be provided');
+  describe('publishEntry', () => {
+    it('can publish entries', async () => {
+      const session = new Session('http://localhost:2020');
+      const nextEntryArgs = await session.publishEntry(
+        ENTRY_ENCODED,
+        MESSAGE_ENCODED,
+      );
+      expect(nextEntryArgs.entryHashBacklink).to.equal(
+        BACKLINK_HASH,
+        JSON.stringify(nextEntryArgs, null, 2),
+      );
+    });
+
+    it('throws when publishing without all required parameters', async () => {
+      const session = new Session('http://localhost:2020');
+      assert.isRejected(session.publishEntry(null, MESSAGE_ENCODED));
+      assert.isRejected(session.publishEntry(ENTRY_ENCODED, null));
+    });
   });
 
-  it('gets next entry args', async () => {
-    const session = new Session('http://localhost:2020');
-    const nextEntryArgs = await session.getNextEntryArgs(PUBLIC_KEY, SCHEMA);
-    expect(nextEntryArgs.entryHashSkiplink).to.equal(null);
-    expect(nextEntryArgs.entryHashBacklink).to.equal(BACKLINK_HASH);
-    expect(nextEntryArgs.seqNum).to.equal(2);
-    expect(nextEntryArgs.logId).to.equal(1);
-  });
+  describe('get/setNextEntryArgs', () => {
+    it('returns next entry args from node', async () => {
+      const session = new Session('http://localhost:2020');
+      const nextEntryArgs = await session.getNextEntryArgs(PUBLIC_KEY, SCHEMA);
+      expect(nextEntryArgs.entryHashSkiplink).to.equal(null);
+      expect(nextEntryArgs.entryHashBacklink).to.equal(BACKLINK_HASH);
+      expect(nextEntryArgs.seqNum).to.equal(2);
+      expect(nextEntryArgs.logId).to.equal(1);
+    });
 
-  it('can publish entries', async () => {
-    const session = new Session('http://localhost:2020');
-    const nextEntryArgs = await session.publishEntry(
-      ENTRY_ENCODED,
-      MESSAGE_ENCODED,
-    );
-    expect(nextEntryArgs.entryHashBacklink).to.equal(
-      BACKLINK_HASH,
-      JSON.stringify(nextEntryArgs, null, 2),
-    );
-  });
+    it('returns next entry args from cache', async () => {
+      const session = new Session('http://localhost:2020');
+      // add a spy to check whether the value is really retrieved from the cache
+      // and not requested
+      session.client.request = sinon.replace(
+        session.client,
+        'request',
+        sinon.fake(),
+      );
 
-  it('throws when publishing without all required parameters', async () => {
-    const session = new Session('http://localhost:2020');
-    assert.isRejected(session.publishEntry(null, MESSAGE_ENCODED));
-    assert.isRejected(session.publishEntry(ENTRY_ENCODED, null));
-  });
+      const nextEntryArgs = {
+        entryHashBacklink: BACKLINK_HASH,
+        entryHashSkiplink: undefined,
+        logId: 1,
+        lastSeqNum: 0,
+      };
+      session.setNextEntryArgs(PUBLIC_KEY, SCHEMA, nextEntryArgs);
 
-  it('caches next entry args', async () => {
-    const session = new Session('http://localhost:2020');
-    // add a spy to check whether the value is really retrieved from the cache
-    // and not requested
-    session.client.request = sinon.replace(
-      session.client,
-      'request',
-      sinon.fake(),
-    );
-
-    const nextEntryArgs = {
-      entryHashBacklink: BACKLINK_HASH,
-      entryHashSkiplink: undefined,
-      logId: 1,
-      lastSeqNum: 0,
-    };
-    session.setNextEntryArgs(PUBLIC_KEY, SCHEMA, nextEntryArgs);
-
-    const cacheResponse = await session.getNextEntryArgs(PUBLIC_KEY, SCHEMA);
-    expect(cacheResponse.logId).to.equal(nextEntryArgs.logId);
-    expect(session.client.request.notCalled).to.be.true;
+      const cacheResponse = await session.getNextEntryArgs(PUBLIC_KEY, SCHEMA);
+      expect(cacheResponse.logId).to.equal(nextEntryArgs.logId);
+      expect(session.client.request.notCalled).to.be.true;
+    });
   });
 });
