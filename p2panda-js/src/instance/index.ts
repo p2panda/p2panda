c844c080cf899db77928e1d60d4512ec3f6f7a37
Vincent Ahrend <mail@vincentahrend.com>
2021-09-17 18:42:37 +0200

    Add SPDX headers

--- p2panda-js/src/instance/index.ts
+++ p2panda-js/src/instance/index.ts
@@ -1,2 +1,4 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 export { createInstance, queryInstances } from './instance';
 export { materializeEntries } from './materialiser';
--- p2panda-js/src/session/index.ts
+++ p2panda-js/src/session/index.ts
@@ -1,1 +1,3 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 export * from './session';
