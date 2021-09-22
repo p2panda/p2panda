a9777e04c81b44a68f24679761cf281027f72755
Andreas Dzialocha <x12@adz.garden>
2021-09-22 14:44:38 +0200

    Add some comments

--- p2panda-js/jest.config.js
+++ p2panda-js/jest.config.js
@@ -3,10 +3,16 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleNameMapper: {
+    // Use the same path alias for our codebase as we do in Webpack
     '^~(.*)$': '<rootDir>/src$1',
+    // Map `wasm-node` imports to the build of `wasm-pack`
     'wasm-node': '<rootDir>/wasm-node',
   },
   globals: {
+    // Set `BUILD_TARGET_WEB` to false to import the WebAssembly build for
+    // NodeJS targets during testing. This is usually set via the Webpack
+    // DefinePlugin (see webpack configuration), but since Webpack is not used
+    // during Jest testing we have to set it here as well.
     BUILD_TARGET_WEB: false,
   },
 };
