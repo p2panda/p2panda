a341432c99b5d0c50cab19acea2577d5ec47a9e2
sandreae <git@contact.samandreae.com>
2021-10-18 12:31:38 +0100

    Format author log data properly for tests setup

--- p2panda-tests/src/lib.rs
+++ p2panda-tests/src/lib.rs
@@ -5,22 +5,31 @@ mod templates;
 mod tests;
 pub mod utils;
 
-use serde_json;
 use std::collections::HashMap;
-use std::convert::TryFrom;
+use std::convert::{TryFrom, TryInto};
+
+use bamboo_rs_core::entry::is_lipmaa_required;
+use rstest_reuse;
+use serde::Serialize;
+use serde_json;
 
 use p2panda_rs::entry::{decode_entry, sign_and_encode, Entry, EntrySigned, LogId, SeqNum};
 use p2panda_rs::hash::Hash;
 use p2panda_rs::identity::{Author, KeyPair};
 use p2panda_rs::message::{Message, MessageEncoded, MessageFields, MessageValue};
 
-use bamboo_rs_core::entry::is_lipmaa_required;
-use rstest_reuse;
-
 // const META_SCHEMA: &str  = "004069db5208a271c53de8a1b6220e6a4d7fcccd89e6c0c7e75c833e34dc68d932624f2ccf27513f42fb7d0e4390a99b225bad41ba14a6297537246dbe4e6ce150e8";
 
 pub type TestPandaDB = HashMap<Author, Panda>;
 pub type Logs = HashMap<usize, Vec<(EntrySigned, MessageEncoded)>>;
+
+#[derive(Serialize)]
+pub struct NextEntryArgs {
+    entryHashBacklink: Option<Hash>,
+    entryHashSkiplink: Option<Hash>,
+    seqNum: SeqNum,
+    logId: LogId,
+}
 
 #[derive(Debug)]
 pub struct Panda {
@@ -43,7 +52,156 @@ impl Panda {
             logs: HashMap::new(),
         }
     }
+    
+    pub fn private_key(&self) -> String {
+        hex::encode(self.key_pair.private_key())
+    }
 
+    pub fn public_key(&self) -> String {
+        hex::encode(self.key_pair.public_key())
+    }
+
+    pub fn some_hash(str: &str) -> Option<Hash> {
+        let hash = Hash::new(str);
+        Some(hash.unwrap())
+    }
+
+    pub fn build_message_fields(fields: Vec<(&str, &str)>) -> MessageFields {
+        let mut message_fields = MessageFields::new();
+        for (key, value) in fields.iter() {
+            message_fields
+                .add(key, MessageValue::Text(value.to_string()))
+                .unwrap();
+        }
+        message_fields
+    }
+
+    pub fn name(&self) -> String {
+        self.name.to_owned()
+    }
+
+    fn get_schema_id(&self, schema: Hash) -> usize {
+        self.schema.get(schema.as_str()).unwrap().to_owned()
+    }
+
+    /// Determine the skiplink for the next entry
+    pub fn next_entry_args(&self, log_id: usize) -> NextEntryArgs {
+        let schema_entries = self.logs.get(&log_id).unwrap();
+
+        if schema_entries.len() == 0 {
+            NextEntryArgs {
+                entryHashBacklink: None,
+                entryHashSkiplink: None,
+                seqNum: Panda::seq_num(1),
+                logId: LogId::new(log_id.try_into().unwrap()),
+            }
+        } else {
+            // Get last entry in log
+            let (entry_encoded, _) = schema_entries.get(schema_entries.len() - 1).unwrap();
+            let decoded_last_entry = decode_entry(&entry_encoded, None).unwrap();
+
+            // Get the hash (which is the backlink we need)
+            let backlink = Some(entry_encoded.hash());
+
+            // Get the next sequence number
+            let next_seq_num = decoded_last_entry.seq_num().to_owned().next().unwrap();
+
+            // And then the skiplink
+            let skiplink_seq_num = next_seq_num.skiplink_seq_num().unwrap().as_i64();
+
+            // And finally the log id
+            let log_id = decoded_last_entry.log_id();
+
+            // Check if skiplink is required and return hash if so
+            let skiplink = if is_lipmaa_required(next_seq_num.as_i64() as u64) {
+                let (skiplink_entry, _) = schema_entries.get(skiplink_seq_num as usize).unwrap();
+                Some(skiplink_entry.hash())
+            } else {
+                None
+            };
+
+            NextEntryArgs {
+                entryHashBacklink: backlink,
+                entryHashSkiplink: skiplink,
+                seqNum: next_seq_num,
+                logId: log_id.to_owned(),
+            }
+        }
+    }
+
+    /// Publish an entry to a schema log for this Panda
+    pub fn publish_entry(&mut self, message: Message) {
+        let schema_str = message.schema().as_str();
+
+        let log_id = match self.schema.get(schema_str) {
+            Some(id) => id.to_owned(),
+            None => {
+                let id = self.logs.len() + 1;
+                self.schema.insert(schema_str.to_string(), id);
+                self.logs.insert(id, Vec::new());
+                id
+            }
+        };
+
+        // Calculate next entry args
+        let next_entry_args = self.next_entry_args(log_id);
+
+        // Construct entry from message and entry args then sign and encode it
+        let entry = Panda::entry(
+            &message,
+            &next_entry_args.seqNum,
+            next_entry_args.entryHashBacklink,
+            next_entry_args.entryHashSkiplink,
+        );
+        let entry_encoded = sign_and_encode(&entry, &self.key_pair).unwrap();
+
+        // Encode message
+        let message_encoded = MessageEncoded::try_from(&message).unwrap();
+
+        // Push new entry to schema log
+        let schema_entries = self.logs.get_mut(&log_id).unwrap();
+        schema_entries.push((entry_encoded, message_encoded));
+    }
+
+    pub fn get_entry(&self, schema: &str, seq_num: usize) -> Entry {
+        let log_id = self.schema.get(schema).unwrap();
+        let entry = &self.logs.get(&log_id).unwrap()[seq_num - 1];
+        decode_entry(&entry.0, Some(&entry.1)).unwrap()
+    }
+
+    pub fn get_encoded_entry_and_message(
+        &self,
+        schema: &str,
+        seq_num: usize,
+    ) -> (EntrySigned, MessageEncoded) {
+        let log_id = self.schema.get(schema).unwrap();
+        self.logs.get(log_id).unwrap()[seq_num - 1].clone()
+    }
+
+    pub fn decode(&self) -> HashMap<usize, Vec<Entry>> {
+        let mut decoded_logs: HashMap<usize, Vec<Entry>> = HashMap::new();
+        for (hash, entries) in self.logs.iter() {
+            decoded_logs.insert(hash.to_owned(), Vec::new());
+            let log_entries = decoded_logs.get_mut(hash).unwrap();
+            for (entry_encoded, message_encoded) in entries.iter() {
+                let entry = decode_entry(entry_encoded, Some(message_encoded)).unwrap();
+                log_entries.push(entry);
+            }
+        }
+        decoded_logs
+    }
+
+    pub fn to_json(&self) -> String {
+        serde_json::to_string_pretty(&self.logs).unwrap()
+    }
+
+    pub fn to_json_decoded(&self) -> String {
+        serde_json::to_string_pretty(&self.decode()).unwrap()
+    }
+}
+
+impl Panda {
+    
     pub fn entry(
         message: &Message,
         seq_num: &SeqNum,
@@ -99,129 +257,5 @@ impl Panda {
 
     pub fn keypair() -> KeyPair {
         KeyPair::new()
-    }
-
-    pub fn some_hash(str: &str) -> Option<Hash> {
-        let hash = Hash::new(str);
-        Some(hash.unwrap())
-    }
-
-    pub fn build_message_fields(fields: Vec<(&str, &str)>) -> MessageFields {
-        let mut message_fields = MessageFields::new();
-        for (key, value) in fields.iter() {
-            message_fields
-                .add(key, MessageValue::Text(value.to_string()))
-                .unwrap();
-        }
-        message_fields
-    }
-    
-    pub fn name(&self) -> String {
-        self.name.to_owned()
-    }
-    
-    fn get_schema_id(&self, schema: Hash) -> usize {
-        self.schema.get(schema.as_str()).unwrap().to_owned()
-    }
-
-    /// Determine the skiplink for the next entry
-    fn next_entry_args(&mut self, log_id: usize) -> (SeqNum, Option<Hash>, Option<Hash>) {
-
-        let schema_entries = self.logs.get(&log_id).unwrap();
-
-        if schema_entries.len() == 0 {
-            (Panda::seq_num(1), None, None)
-        } else {
-            // Get last entry in log
-            let (entry_encoded, message_encoded) =
-                schema_entries.get(schema_entries.len() - 1).unwrap();
-            let decoded_last_entry = decode_entry(&entry_encoded, None).unwrap();
-
-            // Get the hash (which is the backlink we need)
-            let backlink = Some(entry_encoded.hash());
-
-            // Get the next sequence number
-            let next_seq_num = decoded_last_entry.seq_num().to_owned().next().unwrap();
-
-            // And then the skiplink
-            let skiplink_seq_num = next_seq_num.skiplink_seq_num().unwrap().as_i64();
-
-            // Check if skiplink is required and return hash if so
-            let skiplink = if is_lipmaa_required(next_seq_num.as_i64() as u64) {
-                let (skiplink_entry, _) = schema_entries.get(skiplink_seq_num as usize).unwrap();
-                Some(skiplink_entry.hash())
-            } else {
-                None
-            };
-
-            // Return next entry args
-            (next_seq_num, backlink, skiplink)
-        }
-    }
-
-    /// Publish an entry to a schema log for this Panda
-    pub fn publish_entry(&mut self, message: Message) {
-        
-        let schema_str = message.schema().as_str();
-
-        let log_id = match self.schema.get(schema_str) {
-            Some(id) => id.to_owned(),
-            None => {
-                let id = self.logs.len() + 1;
-                self.schema.insert(schema_str.to_string(), id);
-                self.logs.insert(id, Vec::new());
-                id
-            }
-        };
-        
-        // Calculate next entry args
-        let (seq_num, backlink, skiplink) = self.next_entry_args(log_id);
-
-        // Construct entry from message and entry args then sign and encode it
-        let entry = Panda::entry(&message, &seq_num, backlink, skiplink);
-        let entry_encoded = sign_and_encode(&entry, &self.key_pair).unwrap();
-
-        // Encode message
-        let message_encoded = MessageEncoded::try_from(&message).unwrap();
-
-        // Push new entry to schema log
-        let schema_entries = self.logs.get_mut(&log_id).unwrap();
-        schema_entries.push((entry_encoded, message_encoded));
-    }
-
-    pub fn get_entry(&self, schema: &str, seq_num: usize) -> Entry {
-        let log_id = self.schema.get(schema).unwrap();
-        let entry = &self.logs.get(&log_id).unwrap()[seq_num - 1];
-        decode_entry(&entry.0, Some(&entry.1)).unwrap()
-    }
-
-    pub fn get_encoded_entry_and_message(
-        &self,
-        schema: &str,
-        seq_num: usize,
-    ) -> (EntrySigned, MessageEncoded) {
-        let log_id = self.schema.get(schema).unwrap();
-        self.logs.get(log_id).unwrap()[seq_num - 1].clone()
-    }
-
-    pub fn decode(&self) -> HashMap<usize, Vec<Entry>> {
-        let mut decoded_logs: HashMap<usize, Vec<Entry>> = HashMap::new();
-        for (hash, entries) in self.logs.iter() {
-            decoded_logs.insert(hash.to_owned(), Vec::new());
-            let log_entries = decoded_logs.get_mut(hash).unwrap();
-            for (entry_encoded, message_encoded) in entries.iter() {
-                let entry = decode_entry(entry_encoded, Some(message_encoded)).unwrap();
-                log_entries.push(entry);
-            }
-        }
-        decoded_logs
-    }
-
-    pub fn to_json(&self) -> String {
-        serde_json::to_string_pretty(&self.logs).unwrap()
-    }
-
-    pub fn to_json_decoded(&self) -> String {
-        serde_json::to_string_pretty(&self.decode()).unwrap()
     }
 }
--- p2panda-tests/src/main.rs
+++ p2panda-tests/src/main.rs
@@ -1,39 +1,80 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 use std::collections::HashMap;
 
-use p2panda_rs::entry::{decode_entry, sign_and_encode, Entry, EntrySigned, LogId, SeqNum};
-use p2panda_rs::message::MessageEncoded;
-use p2panda_tests::Panda;
+use p2panda_rs::entry::{decode_entry, LogId, SeqNum};
+use p2panda_rs::identity::Author;
+use p2panda_rs::hash::Hash;
+use p2panda_rs::message::Message;
+use p2panda_tests::{Panda, NextEntryArgs};
 use p2panda_tests::utils::MESSAGE_SCHEMA;
 
 use serde::Serialize;
 use serde_json;
 
 #[derive(Serialize)]
-pub struct EntryData{
-    entryBytes: EntrySigned,
-    payloadBytes: MessageEncoded,
-    decoded: Entry
+pub struct EncodedEntryData {
+    author: Author,
+    entryBytes: String,
+    entryHash: Hash,
+    payloadBytes: String,
+    payloadHash: Hash,
+    logId: LogId,
+    seqNum: SeqNum
 }
 
-pub fn get_test_data(authors: Vec<Panda>) -> HashMap<String, HashMap<usize, Vec<EntryData>>> {
-    let mut decoded_logs: HashMap<String, HashMap<usize, Vec<EntryData>>>  = HashMap::new();
+#[derive(Serialize)]
+pub struct LogData{
+    encodedEntries: Vec<EncodedEntryData>,
+    decodedMessages: Vec<Message>,
+    nextEntryArgs: NextEntryArgs
+}
+
+#[derive(Serialize)]
+pub struct AuthorData {
+    publicKey: String,
+    privateKey: String,
+    logs: Vec<LogData>
+}
+
+pub fn get_test_data(authors: Vec<Panda>) -> HashMap<String, AuthorData> {
+    let mut decoded_logs: HashMap<String, AuthorData>  = HashMap::new();
     
     for author in authors {
-        decoded_logs.insert(author.name(), HashMap::new());
-        let author_logs = decoded_logs.get_mut(&author.name()).unwrap();
+        let mut author_logs = Vec::new();
         for (log_id, entries) in author.logs.iter() {
-            author_logs.insert(log_id.to_owned(), Vec::new());
+
+            let entry_args = author.next_entry_args(log_id.to_owned());
+            
+            let mut log_data = LogData {
+                encodedEntries: Vec::new(),
+                decodedMessages: Vec::new(),
+                nextEntryArgs: entry_args,
+                
+            };
+            
             for (entry_encoded, message_encoded) in entries.iter() {
                 let entry = decode_entry(entry_encoded, Some(message_encoded)).unwrap();
-                let entry_data = EntryData {
-                    entryBytes: entry_encoded.to_owned(),
-                    payloadBytes: message_encoded.to_owned(),
-                    decoded: entry
+                let message_decoded = entry.message().unwrap();
+                // EncodedEntryData
+                let entry_data = EncodedEntryData {
+                    author: entry_encoded.author(),
+                    entryBytes: entry_encoded.as_str().into(),
+                    entryHash: entry_encoded.hash(),
+                    payloadBytes: message_encoded.as_str().into(),
+                    payloadHash: message_encoded.hash(),
+                    logId: entry.log_id().to_owned(),
+                    seqNum: entry.seq_num().to_owned(),
                 };
-                author_logs.get_mut(log_id).unwrap().push(entry_data);
+                                
+                log_data.encodedEntries.push(entry_data);
+                log_data.decodedMessages.push(message_decoded.to_owned());
             }
-        }    
+            author_logs.push(log_data);
+        }
+        
+
+        let author_data = AuthorData { publicKey: author.public_key(), privateKey: author.private_key(), logs: author_logs };
+        decoded_logs.insert(author.name(), author_data);
     }
     decoded_logs
 }
@@ -42,10 +83,10 @@ fn main() {
     let mut panda = Panda::new("panda".to_string(), Panda::keypair());
     
     panda.publish_entry(Panda::create_message(MESSAGE_SCHEMA, vec![("message", "hello!")]));
-    panda.publish_entry(Panda::create_message(MESSAGE_SCHEMA, vec![("message", "poop!")]));
+    // panda.publish_entry(Panda::create_message(MESSAGE_SCHEMA, vec![("message", "poop!")]));
     
-    let (entry_encoded_1, _) = panda.get_encoded_entry_and_message(MESSAGE_SCHEMA, 1);
-    panda.publish_entry(Panda::update_message(MESSAGE_SCHEMA, entry_encoded_1.hash(), vec![("message", "Smelly!")]));
+    // let (entry_encoded_1, _) = panda.get_encoded_entry_and_message(MESSAGE_SCHEMA, 1);
+    // panda.publish_entry(Panda::update_message(MESSAGE_SCHEMA, entry_encoded_1.hash(), vec![("message", "Smelly!")]));
     
     println!("{}", serde_json::to_string_pretty(&get_test_data(vec![panda])).unwrap());
 }
