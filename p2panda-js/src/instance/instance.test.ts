4e9f2816343e645b63d0f000cbf3879b60e25dc6
sandreae <git@contact.samandreae.com>
2021-10-19 19:25:20 +0100

    New test data and elegant solution

--- p2panda-js/src/instance/instance.test.ts
+++ p2panda-js/src/instance/instance.test.ts
@@ -1,37 +1,49 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+import wasm from '~/wasm';
 import { recoverKeyPair } from '~/identity';
 import { Session } from '~/session';
 import { createInstance } from '.';
+import { Fields } from '~/types';
 
 import TEST_DATA from '~/../test/test-data.json';
-import { marshallResponseFields } from '~/utils';
-import { FieldsTagged } from '~/types';
 
-// const ENTRIES = TEST_DATA.entries;
-// const PUBLIC_KEY = TEST_DATA.meta.keyPair.publicKey;
-// const SCHEMA = TEST_DATA.meta.schema;
-// const ENTRY_ARGS = TEST_DATA.nextEntryArgs;
+const PANDA_LOG = TEST_DATA.panda.logs[0];
+const SCHEMA = PANDA_LOG.decodedMessages[0].schema;
 
 const MOCK_SERVER_URL = 'http://localhost:2020';
 
 describe('instance', () => {
   describe('createInstance', () => {
     it('creates an instance', async () => {
-      const keyPair = await recoverKeyPair(TEST_DATA.meta.keyPair.privateKey);
+      const { decodeEntry } = await wasm;
+
+      const keyPair = await recoverKeyPair(TEST_DATA.panda.privateKey);
       const session = new Session(MOCK_SERVER_URL);
       session.setKeyPair(keyPair);
 
-      const fields = marshallResponseFields(
-        TEST_DATA.decodedEntries[0].message.fields as FieldsTagged,
-      );
+      const fields: Fields = {
+        message: 'A shiny new message!',
+      };
+
       const entryEncoded = await createInstance(fields, {
         keyPair,
-        schema: TEST_DATA.meta.schema,
+        schema: SCHEMA,
         session,
       });
 
-      expect(entryEncoded).toEqual(TEST_DATA.entries[0].entryBytes);
+      const entry = decodeEntry(entryEncoded);
+
+      // Although we don't have a fixture value for the encoded entry (because it doesn't exist
+      // in our test log), we can still compare the decoded values with what we would expect to see.
+      expect(entry.logId).toEqual(PANDA_LOG.nextEntryArgs.logId);
+      expect(entry.seqNum).toEqual(PANDA_LOG.nextEntryArgs.seqNum);
+      expect(entry.entryHashBacklink).toEqual(
+        PANDA_LOG.nextEntryArgs.entryHashBacklink,
+      );
+      expect(entry.entryHashSkiplink).toEqual(
+        PANDA_LOG.nextEntryArgs.entryHashSkiplink,
+      );
     });
   });
 });
--- p2panda-js/src/test-data.test.ts
+++ p2panda-js/src/test-data.test.ts
@@ -3,28 +3,31 @@
 import wasm from '~/wasm';
 import { recoverKeyPair } from './identity';
 
-import TEST_DATA from '../test/test-data.json';
+import TEST_DATA from '~/../test/test-data.json';
+
+const PANDA_LOG = TEST_DATA.panda.logs[0];
+const SCHEMA = PANDA_LOG.decodedMessages[0].schema;
 
 describe('test data', () => {
   it('contains correct keyPair values', async () => {
-    const keyPair = await recoverKeyPair(TEST_DATA.meta.keyPair.privateKey);
+    const keyPair = await recoverKeyPair(TEST_DATA.panda.privateKey);
 
-    expect(keyPair.publicKey()).toEqual(TEST_DATA.meta.keyPair.publicKey);
+    expect(keyPair.publicKey()).toEqual(TEST_DATA.panda.publicKey);
   });
 
   it('contains correct values for signing and encoding first entry in log', async () => {
     const { signEncodeEntry } = await wasm;
-    const keyPair = await recoverKeyPair(TEST_DATA.meta.keyPair.privateKey);
+    const keyPair = await recoverKeyPair(TEST_DATA.panda.privateKey);
 
     const { entryEncoded } = signEncodeEntry(
       keyPair,
-      TEST_DATA.entries[0].payloadBytes,
+      PANDA_LOG.encodedEntries[0].payloadBytes,
       undefined,
       undefined,
       1,
       1,
     );
-    expect(entryEncoded).toEqual(TEST_DATA.entries[0].entryBytes);
+    expect(entryEncoded).toEqual(PANDA_LOG.encodedEntries[0].entryBytes);
   });
 
   it('contains correct message fields', async () => {
@@ -32,19 +35,14 @@ describe('test data', () => {
     const { encodeCreateMessage } = await wasm;
 
     const messageFields = new MessageFields();
-    const fields = TEST_DATA.decodedEntries[0].message.fields;
+    const fields = PANDA_LOG.decodedMessages[0].fields;
     messageFields.add(
-      'description',
-      fields['description']['type'],
-      fields['description']['value'],
+      'message',
+      fields['message']['type'],
+      fields['message']['value'],
     );
-    messageFields.add('name', fields['name']['type'], fields['name']['value']);
+    const encodedMessage = encodeCreateMessage(SCHEMA, messageFields);
 
-    const encodedMessage = encodeCreateMessage(
-      TEST_DATA.meta.schema,
-      messageFields,
-    );
-
-    expect(encodedMessage).toEqual(TEST_DATA.entries[0].payloadBytes);
+    expect(encodedMessage).toEqual(PANDA_LOG.encodedEntries[0].payloadBytes);
   });
 });
