57bd6f13ab71395b98bbf1e0064c915119ab24c1
sandreae <git@contact.samandreae.com>
2021-11-09 12:46:25 +0000

    Exploratory permissions filter methods

--- p2panda-rs/src/materializer/utils.rs
+++ p2panda-rs/src/materializer/utils.rs
@@ -1,11 +1,14 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 use crate::entry::{decode_entry, EntrySigned};
+use crate::identity::Author;
 use crate::materializer::{Edge, MaterializationError};
 use crate::message::MessageEncoded;
 
+pub type InstanceAuthor = Author;
+
 /// Method for marshalling an array of (EntrySigned, MessageEncoded) into an array of Edges which can then
-/// be turned into a DAG. It is here to detach the DAG from any concepts of Entries and Messages. The DAG just sees 
+/// be turned into a DAG. It is here to detach the DAG from any concepts of Entries and Messages. The DAG just sees
 /// Strings. This step will look different once have some more instance related fields in our Message struct.
 pub fn marshall_entries(
     entries: Vec<(EntrySigned, MessageEncoded)>,
@@ -19,9 +22,9 @@ pub fn marshall_entries(
 
         if entry.message().is_none() {
             // The message has been deleted.
-            continue
+            continue;
         }
-        
+
         // If we have a `link` field in the Message then we can use it here.
         // `id` should not be optional (even CREATE messages should have it set) then
         // we wouldn't need the EntrySigned here at all.
@@ -35,4 +38,35 @@ pub fn marshall_entries(
         edges.push((link, id));
     }
     Ok(edges)
+}
+
+/// Filter entries against instance author for a single writer setting. This is needed for materializing System Logs.
+pub fn single_writer(
+    entries: Vec<(EntrySigned, MessageEncoded, InstanceAuthor)>,
+) -> Vec<(EntrySigned, MessageEncoded, InstanceAuthor)> {
+    entries
+        .iter()
+        .cloned()
+        .filter(|(entry_encoded, _, instance_author)| {
+            entry_encoded.author().as_str() == instance_author.as_str()
+        })
+        .collect()
+}
+
+/// Filter entries against permissions for multi writer setting. This is needed for materializing User Logs which allow
+/// update messages from multiple writers via the use of permissions.
+pub fn multi_writer(
+    entries: Vec<(EntrySigned, MessageEncoded, InstanceAuthor)>,
+    permitted_authors: Vec<Author>,
+) -> Vec<(EntrySigned, MessageEncoded, InstanceAuthor)> {
+    entries
+        .iter()
+        .cloned()
+        .filter(|(entry_encoded, _, instance_author)| {
+            entry_encoded.author().as_str() == instance_author.as_str()
+                || permitted_authors.iter().any(|permitted_author| {
+                    permitted_author.as_str() == entry_encoded.author().as_str()
+                })
+        })
+        .collect()
 }
