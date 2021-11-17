b7e050b61bffecec7154f8ffcde23396ab9202c9
sandreae <git@contact.samandreae.com>
2021-11-17 14:09:37 +0000

    Module description

--- p2panda-rs/src/materialiser/mod.rs
+++ p2panda-rs/src/materialiser/mod.rs
@@ -1,5 +1,8 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+//! Materialise data `Instance`s from p2panda `Message`s. Create a causal graph of p2panda `Message`s (aka `Operation`s), 
+//! reconcile any conflicts, order operations deterministically, and reduce operations into data `Instance`s.
+
 mod dag;
 mod utils;
 mod error;
