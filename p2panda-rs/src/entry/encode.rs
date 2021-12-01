3deffcb914902d33b25912a4db03b96ef5f25e4a
Vincent Ahrend <mail@vincentahrend.com>
2021-12-01 10:08:01 +0100

    Update hashes in docstrings and comments

--- p2panda-rs/src/entry/encode.rs
+++ p2panda-rs/src/entry/encode.rs
@@ -28,7 +28,7 @@ use crate::message::MessageEncoded;
 /// let key_pair = KeyPair::new();
 ///
 /// // Create message
-/// let schema_hash = Hash::new("004069db5208a271c53de8a1b6220e6a4d7fcccd89e6c0c7e75c833e34dc68d932624f2ccf27513f42fb7d0e4390a99b225bad41ba14a6297537246dbe4e6ce150e8")?;
+/// let schema_hash = Hash::new("0020c65567ae37efea293e34a9c7d13f8f2bf23dbdc3b5c7b9ab46293111c48fc78b")?;
 /// let mut fields = MessageFields::new();
 /// fields.add("title", MessageValue::Text("Hello, Panda!".to_owned()))?;
 /// let message = Message::new_create(schema_hash, fields)?;
--- p2panda-rs/src/entry/entry.rs
+++ p2panda-rs/src/entry/entry.rs
@@ -29,7 +29,7 @@ use crate::Validate;
 /// use p2panda_rs::entry::{Entry, LogId, SeqNum};
 /// use p2panda_rs::message::{Message, MessageFields, MessageValue};
 /// use p2panda_rs::hash::Hash;
-/// # let schema_hash_str = "004069db5208a271c53de8a1b6220e6a4d7fcccd89e6c0c7e75c833e34dc68d932624f2ccf27513f42fb7d0e4390a99b225bad41ba14a6297537246dbe4e6ce150e8";
+/// # let schema_hash_str = "0020c65567ae37efea293e34a9c7d13f8f2bf23dbdc3b5c7b9ab46293111c48fc78b";
 ///
 /// // == FIRST ENTRY IN NEW LOG ==
 ///
@@ -63,9 +63,9 @@ use crate::Validate;
 /// use p2panda_rs::hash::Hash;
 ///
 /// // == ENTRY IN EXISTING LOG ==
-/// # let backlink_hash_string = "004069db5208a271c53de8a1b6220e6a4d7fcccd89e6c0c7e75c833e34dc68d932624f2ccf27513f42fb7d0e4390a99b225bad41ba14a6297537246dbe4e6ce150e8";
-/// # let skiplink_hash_string = "004069db5208a271c53de8a1b6220e6a4d7fcccd89e6c0c7e75c833e34dc68d932624f2ccf27513f42fb7d0e4390a99b225bad41ba14a6297537246dbe4e6ce150e8";
-/// # let schema_hash_string = "004069db5208a271c53de8a1b6220e6a4d7fcccd89e6c0c7e75c833e34dc68d932624f2ccf27513f42fb7d0e4390a99b225bad41ba14a6297537246dbe4e6ce150e8";
+/// # let backlink_hash_string = "0020b177ec1bf26dfb3b7010d473e6d44713b29b765b99c6e60ecbfae742de496543";
+/// # let skiplink_hash_string = "0020b177ec1bf26dfb3b7010d473e6d44713b29b765b99c6e60ecbfae742de496543";
+/// # let schema_hash_string = "0020c65567ae37efea293e34a9c7d13f8f2bf23dbdc3b5c7b9ab46293111c48fc78b";
 ///
 /// // Create schema hash
 /// let schema_hash = Hash::new(schema_hash_string)?;
