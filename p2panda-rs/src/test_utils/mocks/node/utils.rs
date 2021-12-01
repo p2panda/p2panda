363433afd8a58c36a7a711aee2ab71b56ee1b202
cafca <cafca@001.land>
2021-12-01 09:10:45 +0100

    Updated constants
    Not sure whether there is a specific method yet to generate these?

--- p2panda-rs/src/test_utils/mocks/node/utils.rs
+++ p2panda-rs/src/test_utils/mocks/node/utils.rs
@@ -3,16 +3,20 @@
 //! utilities and hard coded system schema values
 
 /// hard coded meta schema system schema hash
-pub const META_SCHEMA_HASH: &str = "004069db5208a271c53de8a1b6220e6a4d7fcccd89e6c0c7e75c833e34dc68d932624f2ccf27513f42fb7d0e4390a99b225bad41ba14a6297537246dbe4e6ce16079";
+pub const META_SCHEMA_HASH: &str =
+    "0020c65567ae37efea293e34a9c7d13f8f2bf23dbdc3b5c7b9ab46293111c48fc78b";
 
 /// hard coded key package system schema hash
-pub const KEY_PACKAGE_SCHEMA_HASH: &str = "004069db5208a271c53de8a1b6220e6a4d7fcccd89e6c0c7e75c833e34dc68d932624f2ccf27513f42fb7d0e4390a99b225bad41ba14a6297537246dbe4e6cb59430";
+pub const KEY_PACKAGE_SCHEMA_HASH: &str =
+    "0020c65567ae37efea293e34a9c7d13f8f2bf23dbdc3b5c7b9ab46293111c48fc78b";
 
 /// hard coded group system schema hash
-pub const GROUP_SCHEMA_HASH: &str = "004069db5208a271c53de8a1b6220e6a4d7fcccd89e6c0c7e75c833e34dc68d932624f2ccf27513f42fb7d0e4390a99b225bad41ba14a6297537246dbe4e6ce150e8";
+pub const GROUP_SCHEMA_HASH: &str =
+    "0020c65567ae37efea293e34a9c7d13f8f2bf23dbdc3b5c7b9ab46293111c48fc78b";
 
 /// hard coded permission system schema hash
-pub const PERMISSIONS_SCHEMA_HASH: &str = "004069db5208a271c53de8a1b6220e6a4d7fcccd89e6c0c7e75c833e34dc68d932624f2ccf27513f42fb7d0e4390a99b225bad41ba14a6297537246dbe4e6ce322e8";
+pub const PERMISSIONS_SCHEMA_HASH: &str =
+    "0020c65567ae37efea293e34a9c7d13f8f2bf23dbdc3b5c7b9ab46293111c48fc78b";
 
 /// A custom `Result` type to be able to dynamically propagate `Error` types.
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
