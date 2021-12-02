3994794e477a0c0bd44f565ece6bea815521478a
Andreas Dzialocha <x12@adz.garden>
2021-12-02 13:20:55 +0100

    Change formatting in documentation

--- p2panda-rs/src/secret_group/mod.rs
+++ p2panda-rs/src/secret_group/mod.rs
@@ -3,11 +3,11 @@
 //! Create and moderate large secret groups to maintain key material for secure messaging.
 //!
 //! p2panda uses the [MLS] (Messaging Layer Security) protocol for group key negotiation to
-//! establish secrets in a large group of users for "Sender Ratchet Secrets" or "Long Term
-//! Secrets". Both settings give confidentiality, authenticity and post-compromise security, while
-//! the sender ratchet scheme also gives forward secrecy.
+//! establish secrets in a group of users for _Sender Ratchet Secrets_ or _Long Term Secrets_. Both
+//! settings give confidentiality, authenticity and post-compromise security, while the sender
+//! ratchet scheme also gives forward secrecy.
 //!
-//! A group of users sharing that secret state is called a "secret group" in p2panda. Sender
+//! A group of users sharing that secret state is called a _secret group_ in p2panda. Sender
 //! ratchet encryption is interesting for applications with high security standards where every
 //! message is individually protected with an epheremal key, whereas long-term secret encryption is
 //! useful for building application where keys material is reused for multiple messages over longer
