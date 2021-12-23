b1e09508a040c3b57ef464983ff7ab9bda466d7c
Sam Andreae <contact@samandreae.com>
2021-12-23 13:36:03 +0000

    Refactor and add comments to json_data.rs

--- p2panda-rs/src/test_utils/test_data/json_data.rs
+++ p2panda-rs/src/test_utils/test_data/json_data.rs
@@ -57,41 +57,50 @@ pub struct AuthorData {
 /// Convert log data from a vector of authors into structs which can be json formatted in the way
 /// we expect for our test data.
 pub fn generate_test_data(node: &mut Node, clients: Vec<Client>) -> HashMap<String, AuthorData> {
-    let mut decoded_logs: HashMap<String, AuthorData> = HashMap::new();
+    // Init test data map
+    let mut test_data: HashMap<String, AuthorData> = HashMap::new();
 
+    // Iterate over authors
     for (author_hash, author_logs) in node.db() {
         let author = Author::new(&author_hash).unwrap();
         let mut author_logs_data = Vec::new();
 
+        // Iterate over the authors logs
         for (_log_id, log) in author_logs.iter() {
+            // Init empty log data
             let mut log_data = LogData {
                 encoded_entries: Vec::new(),
                 decoded_operations: Vec::new(),
                 next_entry_args: Vec::new(),
             };
+
+            // Set the document id
             let document_id = log.entries()[0].hash();
+
+            // Iterate over entries in this document log
             for log_entry in log.entries().iter() {
-                let operation_encoded = OperationEncoded::try_from(&log_entry.operation).unwrap();
+                // Encode the operation
+                let operation_encoded = OperationEncoded::try_from(&log_entry.operation()).unwrap();
+
+                // Decode the entry adding it's operation back in
                 let entry =
                     decode_entry(&log_entry.entry_encoded, Some(&operation_encoded)).unwrap();
-                let next_entry_args = node
-                    .next_entry_args(&author, Some(&document_id), Some(entry.seq_num()))
-                    .unwrap();
-                let operation_decoded = entry.operation().unwrap();
+
+                // Compose encoded entry data
                 let entry_data = EncodedEntryData {
                     author: author.clone(),
-                    entry_bytes: log_entry.entry_encoded.as_str().into(),
-                    entry_hash: log_entry.entry_encoded.hash(),
+                    entry_bytes: log_entry.entry_encoded().as_str().into(),
+                    entry_hash: log_entry.entry_encoded().hash(),
                     payload_bytes: operation_encoded.as_str().into(),
                     payload_hash: operation_encoded.hash(),
                     log_id: entry.log_id().to_owned(),
                     seq_num: entry.seq_num().to_owned(),
                 };
 
-                log_data.encoded_entries.push(entry_data);
-                log_data
-                    .decoded_operations
-                    .push(operation_decoded.to_owned());
+                // Get next entry args for this document log
+                let next_entry_args = node
+                    .next_entry_args(&author, Some(&document_id), Some(entry.seq_num()))
+                    .unwrap();
 
                 let json_entry_args = NextEntryArgs {
                     entry_hash_backlink: next_entry_args.backlink,
@@ -100,9 +109,13 @@ pub fn generate_test_data(node: &mut Node, clients: Vec<Client>) -> HashMap<Stri
                     log_id: next_entry_args.log_id,
                 };
 
+                // Push all data for this entry to the log
+                log_data.encoded_entries.push(entry_data);
+                log_data.decoded_operations.push(log_entry.operation());
                 log_data.next_entry_args.push(json_entry_args);
             }
 
+            // Get the final next entry args for this log
             let final_next_entry_args = node
                 .next_entry_args(&author, Some(&Hash::new(&log.document()).unwrap()), None)
                 .unwrap();
@@ -114,21 +127,28 @@ pub fn generate_test_data(node: &mut Node, clients: Vec<Client>) -> HashMap<Stri
                 log_id: final_next_entry_args.log_id,
             };
 
+            // Push next entry args to the log data
             log_data.next_entry_args.push(json_entry_args);
+
+            // Push the log data to the author log array
             author_logs_data.push(log_data);
         }
 
+        // Get the client name
         let client = clients
             .iter()
             .find(|client| client.public_key() == author.as_str())
             .unwrap();
 
+        // Compose the author data
         let author_data = AuthorData {
             public_key: client.public_key(),
             private_key: client.private_key(),
             logs: author_logs_data,
         };
-        decoded_logs.insert(client.name(), author_data);
+
+        // Push all data for this author to the test data
+        test_data.insert(client.name(), author_data);
     }
-    decoded_logs
+    test_data
 }
