2258fdbce98a2933f32854df7698c2a70157ff14
Andreas Dzialocha <x12@adz.garden>
2021-12-25 23:26:11 +0100

    Shorter first introduction in materialiser module

--- p2panda-rs/src/materialiser/mod.rs
+++ p2panda-rs/src/materialiser/mod.rs
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-//! Materialise data instances from p2panda operations. Create a causal graph of p2panda operations
-//! reconcile branches, solve version conflicts automatically, order operations deterministically
-//! and reduce them into data instances.
+//! Materialise data instances from p2panda operations.
+//!
+//! Create a causal graph of p2panda operations reconcile branches, solve version conflicts
+//! automatically, order operations deterministically and reduce them into data instances.
 mod dag;
 mod error;
 mod filter;
