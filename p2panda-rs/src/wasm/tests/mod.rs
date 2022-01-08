0b22e2fe71e5a4d34a8bacb90d0e481edc653a3b
Andreas Dzialocha <x12@adz.garden>
2022-01-08 17:07:13 +0100

    Remove unnecessary visibility of test exports

--- p2panda-rs/src/wasm/tests/mod.rs
+++ p2panda-rs/src/wasm/tests/mod.rs
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 //! Tests for `wasm` module in `p2panda_rs`.
-pub mod entry;
-pub mod key_pair;
-pub mod operation;
+mod entry;
+mod key_pair;
+mod operation;
