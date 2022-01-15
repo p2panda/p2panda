3adbec340a4a36a3381731ce6cce10f0729295ce
Sam Andreae <contact@samandreae.com>
2022-01-15 11:16:57 +0000

    Refactor walk method

--- p2panda-rs/src/materialiser/graph.rs
+++ p2panda-rs/src/materialiser/graph.rs
@@ -1,22 +1,22 @@
 use std::collections::HashMap;
 
 #[derive(Debug)]
-pub struct Graph<T> {
+pub struct Graph<T: PartialEq> {
     nodes: HashMap<String, Node<T>>,
     visited: Vec<String>,
     sorted: Vec<String>,
     queue: Vec<String>,
 }
 
-#[derive(Debug)]
-struct Node<T> {
+#[derive(Debug, PartialEq)]
+struct Node<T: PartialEq> {
     id: String,
     elem: T,
     previous: Vec<String>,
     next: Vec<String>,
 }
 
-impl<T> Node<T> {
+impl<T: PartialEq> Node<T> {
     pub fn is_root(&self) -> bool {
         self.previous.is_empty()
     }
@@ -38,7 +38,7 @@ impl<T> Node<T> {
     }
 }
 
-impl<T> Graph<T> {
+impl<'a, T: PartialEq> Graph<T> {
     pub fn new() -> Self {
         Graph {
             nodes: HashMap::new(),
@@ -71,7 +71,7 @@ impl<T> Graph<T> {
         }
     }
 
-    fn get_node_by_id(&self, id: &str) -> Option<&Node<T>> {
+    fn get_node_by_id(&'a self, id: &str) -> Option<&'a Node<T>> {
         self.nodes.get(id)
     }
 
@@ -103,7 +103,7 @@ impl<T> Graph<T> {
         self.nodes.get(id).map(|node| &node.elem)
     }
 
-    fn next<'a>(&self, visited: &[String], node: &'a Node<T>) -> Option<Vec<String>> {
+    fn next(&self, visited: &[String], node: &'a Node<T>) -> Option<Vec<String>> {
         let mut next_node_ids = Vec::new();
         for link in &node.next() {
             if !visited.contains(link) {
@@ -113,32 +113,46 @@ impl<T> Graph<T> {
         if next_node_ids.is_empty() {
             return None;
         };
+        next_node_ids.reverse();
         Some(next_node_ids)
     }
 
-    pub fn walk(&mut self) -> Vec<String> {
+    pub fn walk(&'a mut self) -> Vec<String> {
         let root_node = self.nodes.values().find(|node| node.is_root()).unwrap();
         let mut queue = vec![root_node];
         let mut visited = Vec::new();
+
+        let push_to_queue = |queue: &mut Vec<&'a Node<T>>, node_id: &str| {
+            let node = self.get_node_by_id(node_id).unwrap();
+            if !queue.contains(&node) {
+                println!("{}: push to queue", node_id);
+                queue.push(node)
+            };
+        };
+
+        let push_to_visited = |visited: &mut Vec<String>, node_id| visited.push(node_id);
+
         while let Some(mut current_node) = queue.pop() {
-            visited.push(current_node.id());
+            push_to_visited(&mut visited, current_node.id());
             while let Some(mut next_node_ids) = self.next(&visited, current_node) {
                 let next_node_id = next_node_ids.pop().unwrap();
                 while let Some(node_to_be_queued) = next_node_ids.pop() {
-                    queue.push(self.get_node_by_id(&node_to_be_queued).unwrap())
+                    push_to_queue(&mut queue, &node_to_be_queued)
                 }
                 if let Some(next_node) = self.get_node_by_id(&next_node_id) {
                     if next_node.is_merge() {
-                        println!("{}: is merge", next_node.id());
                         if self.dependencies_visited(&visited, next_node) {
-                            println!("{}: has all dependencies met", next_node.id());
-                            queue.push(next_node);
+                            println!("{}: is merge and has all dependencies met", next_node.id());
+                            push_to_queue(&mut queue, &next_node.id());
                             break;
                         }
-                        println!("{}: does not have dependencies met", next_node.id());
+                        println!(
+                            "{}: is merge and does not have dependencies met",
+                            next_node.id()
+                        );
                         break;
                     }
-                    visited.push(next_node.id());
+                    push_to_visited(&mut visited, next_node.id());
                     current_node = next_node;
                 }
             }
@@ -162,19 +176,28 @@ mod test {
         graph.add_node("f", 3);
         graph.add_node("g", 3);
         graph.add_node("h", 3);
+        graph.add_node("i", 3);
+        graph.add_node("j", 3);
+        graph.add_node("k", 3);
 
         graph.add_link("a", "b");
         graph.add_link("b", "c");
         graph.add_link("c", "d");
-        graph.add_link("a", "e");
+        graph.add_link("d", "e");
         graph.add_link("e", "f");
-        graph.add_link("f", "d");
-        graph.add_link("d", "h");
-        graph.add_link("h", "g");
 
-        let node_1 = graph.get("a");
-        assert_eq!(1, *node_1.unwrap());
+        graph.add_link("a", "g");
+        graph.add_link("g", "h");
+        graph.add_link("h", "d");
 
-        println!("{:#?}", graph.walk());
+        graph.add_link("c", "i");
+        graph.add_link("i", "j");
+        graph.add_link("j", "k");
+        graph.add_link("k", "f");
+
+        assert_eq!(
+            graph.walk(),
+            ["a", "b", "c", "i", "j", "k", "g", "h", "d", "e", "f",]
+        )
     }
 }
