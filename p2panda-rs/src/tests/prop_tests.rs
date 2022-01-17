f88745d08addd3a858b2abd4440ea96c849cb314
Sam Andreae <contact@samandreae.com>
2022-01-17 19:33:45 +0000

    Include round trip encode decode operation

--- p2panda-rs/src/tests/prop_tests.rs
+++ p2panda-rs/src/tests/prop_tests.rs
@@ -1,6 +1,8 @@
+use std::convert::TryFrom;
+
 // Bring the macros and other important things into scope.
 use crate::hash::Hash;
-use crate::operation::{Operation, OperationFields};
+use crate::operation::{Operation, OperationEncoded, OperationFields};
 use proptest::prelude::*;
 
 proptest! {
@@ -12,8 +14,11 @@ proptest! {
             prop_assert!(operation.is_err());
         } else {
             prop_assert!(operation.is_ok());
-            // println!("{:#?}", operation.unwrap());
-        }
 
+            let operation_encoded = OperationEncoded::try_from(operation.as_ref().unwrap()).unwrap();
+            let decoded_operation = Operation::from(&operation_encoded);
+
+            prop_assert_eq!(decoded_operation, operation.unwrap());
+        }
     }
 }
