e87498921ba64ce70e100361fa0df32b92df9f6d
cafca <cafca@001.land>
2022-01-31 17:32:49 +0100

    Refactor CDDL, use Schema struct, add tests

--- p2panda-rs/src/schema/system.rs
+++ p2panda-rs/src/schema/system.rs
@@ -4,6 +4,8 @@ use crate::hash::Hash;
 use crate::schema::fields::Type;
 use crate::schema::SchemaBuilder;
 
+use super::Schema;
+
 /// Make CDDL definition for operations from a given schema
 fn make_schema_cddl(
     schema_name: &str,
@@ -24,42 +26,53 @@ fn make_schema_cddl(
         hash = tstr .regexp "[0-9a-f]{{68}}"
 
         operation-body-{name}-{hash} = (
-            action: "create", fields: operation-fields-{name}-{hash}"#,
+            action: "create",
+            fields: operation-fields-{name}-{hash}
+        )
+        "#,
         hash = hash,
         name = schema_name
     );
 
     if can_update {
         schema.push_str(&format!(
-            r#" //
-        action: "update", fields: operation-fields-{name}-{hash}, previousOperations: [1* hash]"#,
+            r#"
+        operation-body-{name}-{hash} //= (
+            action: "update",
+            fields: operation-fields-{name}-{hash},
+            previousOperations: [1* hash]
+        )
+        "#,
             hash = hash,
             name = schema_name
         ));
     }
 
     if can_delete {
-        schema.push_str(
-            r#" //
-        action: "delete", previousOperations: [1* hash]"#,
-        );
+        schema.push_str(&format!(
+            r#"
+        operation-body-{name}-{hash} //= (
+            action: "delete",
+            previousOperations: [1* hash]
+        )
+        "#,
+            hash = hash,
+            name = schema_name
+        ));
     }
 
-    // Close operation-body definition
-    schema.push_str("\n\t)\n");
-
     // Insert definition for operation fields from schema builder
-    schema.push_str(&format!("{}", &fields_schema_builder));
+    schema.push_str(&format!("\n\t{}", &fields_schema_builder));
 
     schema
 }
 
 /// Create CDDL definition for bookmarks schema
-fn get_bookmarks_schema() -> (String, String) {
+fn get_bookmarks_schema() -> Schema {
     let schema_name = "bookmarks";
-    let schema_hash: String = Hash::new_from_bytes(vec![1, 2, 3]).unwrap().as_str().into();
+    let schema_hash = Hash::new_from_bytes(vec![1, 2, 3]).unwrap();
 
-    let fields_label = format!("operation-fields-{}-{}", schema_name, schema_hash);
+    let fields_label = format!("operation-fields-{}-{}", schema_name, schema_hash.as_str());
     let mut fields_schema = SchemaBuilder::new(fields_label);
     fields_schema
         .add_operation_field("title".into(), Type::Tstr)
@@ -71,22 +84,29 @@ fn get_bookmarks_schema() -> (String, String) {
         .add_operation_field("created".into(), Type::Tstr)
         .unwrap();
 
-    let cddl = make_schema_cddl(schema_name, &schema_hash, &fields_schema, true, true);
-
-    (schema_hash, cddl)
+    let cddl = make_schema_cddl(
+        schema_name,
+        schema_hash.as_str(),
+        &fields_schema,
+        true,
+        false,
+    );
+    Schema::new(&schema_hash, &cddl).unwrap()
 }
 
 /// Returns global CDDL definition that validates all registered schemas' operations
 pub fn get_system_cddl() -> String {
     // (Hash, CDDL) pairs of all known schemas
-    let system_schemas: [(String, String); 1] = [get_bookmarks_schema()];
+    let system_schemas: [Schema; 1] = [get_bookmarks_schema()];
 
     // Concatenated into one string
     let system_schema_cddl = system_schemas
         .iter()
-        .map(|(_, schema)| format!("{}\n\n", schema))
+        .map(|schema| format!("{}\n\n", schema))
         .reduce(|cur, acc| format!("{}\n\n{}", acc, cur))
         .unwrap();
+
+    // println!("{}", system_schema_cddl);
 
     system_schema_cddl
 }
@@ -97,10 +117,11 @@ mod tests {
     use crate::{
         hash::Hash,
         operation::{Operation, OperationEncoded, OperationFields, OperationValue},
+        schema::{ValidateOperation, Schema},
     };
 
     #[test]
-    fn test_validate_with_system_cddl() {
+    fn test_validate_correct_system_cddl() {
         use std::convert::TryFrom;
 
         let schema_hash = Hash::new_from_bytes(vec![1, 2, 3]).unwrap();
@@ -116,11 +137,39 @@ mod tests {
         fields
             .add("created", OperationValue::Text("2022-01-31".into()))
             .unwrap();
+        let prev_ops: Vec<Hash> = vec![Hash::new_from_bytes(vec![1, 2, 3]).unwrap()];
+        let operation = Operation::new_update(schema_hash.clone(), prev_ops, fields).unwrap();
 
-        let operation = Operation::new_create(schema_hash, fields).unwrap();
+        let cddl = get_system_cddl();
+        // This instance of `Schema` is not representing a specific schema but the entirety of
+        // registered schemas.
+        let system_cddl = Schema::new(&schema_hash, &cddl).unwrap();
+
+        let operation_encoded = OperationEncoded::try_from(&operation).unwrap();
+        assert!(system_cddl.validate_operation(operation_encoded.to_bytes()).is_ok());
+    }
+
+    #[test]
+    fn test_validate_failing_system_cddl() {
+        use std::convert::TryFrom;
+
+        let schema_hash = Hash::new_from_bytes(vec![1, 2, 3]).unwrap();
+
+        // Create test operation
+        let mut fields = OperationFields::new();
+        fields
+            .add("title", OperationValue::Text("p2panda".into()))
+            .unwrap();
+        fields
+            .add("url", OperationValue::Text("https://p2panda.org".into()))
+            .unwrap();
+
+        let operation = Operation::new_create(schema_hash.clone(), fields).unwrap();
 
         let cddl = get_system_cddl();
         let operation_encoded = OperationEncoded::try_from(&operation).unwrap();
-        cddl::validate_cbor_from_slice(&cddl, &operation_encoded.to_bytes()).unwrap();
+
+        let system_cddl = Schema::new(&schema_hash, &cddl).unwrap();
+        assert!(system_cddl.validate_operation(operation_encoded.to_bytes()).is_err());
     }
 }
