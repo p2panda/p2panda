ac2ad03ab1abaccb465ef324b9cc3a477d627d4e
Sam Andreae <contact@samandreae.com>
2022-02-03 10:45:48 +0000

    Make clippy happy

--- p2panda-rs/src/graph/mod.rs
+++ p2panda-rs/src/graph/mod.rs
@@ -5,6 +5,7 @@
 //! Create a causal graph of p2panda operations reconcile branches, solve version conflicts
 //! automatically, order operations deterministically and reduce them into data instances.
 mod error;
+#[allow(clippy::module_inception)]
 mod graph;
 
 pub use error::GraphError;
