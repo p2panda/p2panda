a2733703d4a19b704eaa1af5b94b52892b022163
Andreas Dzialocha <x12@adz.garden>
2022-02-10 19:04:32 +0100

    Add a comment

--- p2panda-rs/src/schema/mod.rs
+++ p2panda-rs/src/schema/mod.rs
@@ -28,7 +28,14 @@ pub fn validate_schema(cddl_schema: &str, bytes: Vec<u8>) -> Result<(), SchemaEr
         Err(cbor::Error::Validation(err)) => {
             let err_str = err
                 .iter()
-                .map(|fe| format!("{}", fe).replace("\"", "'"))
+                .map(|fe| {
+                    format!("{}", fe)
+                        // Quotes escaped in error messages from `cddl` crate are actually not unescaped by
+                        // format macro.
+                        //
+                        // See: https://github.com/anweiss/cddl/blob/main/src/validator/cbor.rs#L100
+                        .replace("\"", "'")
+                })
                 .collect::<Vec<String>>();
 
             Err(error::SchemaError::InvalidSchema(err_str))
