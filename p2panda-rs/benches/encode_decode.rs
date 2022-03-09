5d536af6b862ebecf993a3e8731db1be55e0ee82
Vincent Ahrend <mail@vincentahrend.com>
2022-03-09 12:36:49 +0100

    Add docstring and cleanup

--- p2panda-rs/benches/encode_decode.rs
+++ p2panda-rs/benches/encode_decode.rs
@@ -1,5 +1,10 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+//! Benchmark the performance of encoding and decoding entries and operations.
+//!
+//! An [`Entry`] and accompanying [`Operation`] are encoded and decoded for varying payload sizes
+//! and throughput is measured.
+
 use std::convert::TryFrom;
 
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
@@ -21,6 +26,7 @@ fn run_encode(payload: &str, key_pair: &KeyPair) -> (EntrySigned, OperationEncod
         .add("payload", OperationValue::Text(payload.to_owned()))
         .unwrap();
 
+    // This is a random schema id that doesn't correspond to an actually published schema.
     let hash =
         SchemaId::new("0020d3ce4e85222017ffcb4e5ee032716e2e391478379a29e25bc35d74dd614e4132")
             .unwrap();
@@ -40,13 +46,13 @@ fn run_encode(payload: &str, key_pair: &KeyPair) -> (EntrySigned, OperationEncod
     (entry_encoded, operation_encoded)
 }
 
-/// Decode an [`Entry`] and [`Operation`] from their encoded forms
+/// Decode an [`Entry`] and [`Operation`] from their encoded forms.
 fn run_decode(entry_encoded: &EntrySigned, operation_encoded: &OperationEncoded) {
     decode_entry(entry_encoded, Some(operation_encoded)).unwrap();
     Operation::try_from(operation_encoded).unwrap();
 }
 
-/// Construct a random string given a size
+/// Construct a random string given a size.
 fn random_string(size: usize) -> String {
     let mut rng = thread_rng();
     (0..size)
@@ -54,35 +60,43 @@ fn random_string(size: usize) -> String {
         .collect()
 }
 
+fn get_benchmark_name(function: &str, size: &usize) -> BenchmarkId {
+    static KB: usize = 1024;
+    let benchmark_parameter = match size > &KB {
+        false => format!("{} B", size),
+        true => format!("{} KiB", size / KB)
+    };
+    BenchmarkId::new(function, benchmark_parameter)
+}
+
 fn criterion_benchmark(c: &mut Criterion) {
     static KB: usize = 1024;
     let key_pair = KeyPair::new();
 
     // Test encoding performance for a range of payload sizes
-    let mut encode_group = c.benchmark_group("encode_payload");
-    for size in [16, KB, 16 * KB, 128 * KB, 1024 * KB].iter() {
+    let mut encode_decode = c.benchmark_group("entry and operation");
+    for size in [16, KB, 16 * KB, 64 * KB, 256 * KB, 1024 * KB].iter() {
         let payload = random_string(*size);
 
-        encode_group.throughput(Throughput::Bytes(*size as u64));
-        encode_group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &_size| {
+        encode_decode.throughput(Throughput::Bytes(*size as u64));
+        encode_decode.bench_with_input(get_benchmark_name("encode", size), size, |b, &_size| {
             b.iter(|| run_encode(&payload, &key_pair))
         });
     }
-    encode_group.finish();
 
     // Test decoding performance for a range of payload sizes
-    let mut decode_group = c.benchmark_group("decode_payload");
-    for size in [16, KB, 16 * KB, 128 * KB, 1024 * KB].iter() {
+    for size in [16, KB, 16 * KB, 64 * KB, 256 * KB, 1024 * KB].iter() {
         let payload = random_string(*size);
         let (entry_encoded, operation_encoded) = run_encode(&payload, &key_pair);
 
-        decode_group.throughput(Throughput::Bytes(*size as u64));
-        decode_group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &_size| {
+        encode_decode.throughput(Throughput::Bytes(*size as u64));
+        encode_decode.bench_with_input(get_benchmark_name("decode", size), size, |b, &_size| {
             b.iter(|| run_decode(&entry_encoded, &operation_encoded))
         });
     }
-    decode_group.finish();
+    encode_decode.finish();
 }
+
 
 criterion_group!(benches, criterion_benchmark);
 criterion_main!(benches);
