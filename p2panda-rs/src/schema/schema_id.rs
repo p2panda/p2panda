ac27d9a12917b5874508f86b150dd1407113fefb
Vincent Ahrend <mail@vincentahrend.com>
2022-03-16 14:29:00 +0100

    Add missing test

--- p2panda-rs/src/schema/schema_id.rs
+++ p2panda-rs/src/schema/schema_id.rs
@@ -177,6 +177,7 @@ mod test {
     #[test]
     fn invalid_deserialization() {
         assert!(serde_json::from_str::<SchemaId>("[\"This is not a hash\"]").is_err());
+        assert!(serde_json::from_str::<SchemaId>("5").is_err());
         assert!(serde_json::from_str::<SchemaId>(
             "0020c65567ae37efea293e34a9c7d13f8f2bf23dbdc3b5c7b9ab46293111c48fc78b"
         )
