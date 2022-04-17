a306024ce791bc588fdccea6e56dbbbceb640769
Sam Andreae <contact@samandreae.com>
2022-04-17 11:45:11 +0100

    Remove deprecated structs

--- p2panda-rs/src/storage_provider/mod.rs
+++ p2panda-rs/src/storage_provider/mod.rs
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-//! Storage provider models and traits needed for implementing custom p2panda storage solutions.
+//! Storage provider traits needed for implementing custom p2panda storage solutions.
 pub mod errors;
-// pub mod models;
 pub mod traits;
 
 pub use errors::ValidationError;
