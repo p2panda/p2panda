5ec5aac6827d3270273e2ef1099c9634b874ec53
Sam Andreae <contact@samandreae.com>
2022-05-18 12:02:27 +0900

    Move operation trait definitions over from aquadoggo

--- p2panda-rs/src/storage_provider/errors.rs
+++ p2panda-rs/src/storage_provider/errors.rs
@@ -114,3 +114,19 @@ pub enum PublishEntryError {
     #[error("Invalid Entry and Operation pair with id {0}")]
     InvalidEntryWithOperation(Hash),
 }
+
+/// `OperationStore` errors.
+#[derive(thiserror::Error, Debug)]
+pub enum OperationStorageError {
+    /// Catch all error which implementers can use for passing their own errors up the chain.
+    #[error("Error occured in OperationStore: {0}")]
+    Custom(String),
+
+    /// A fatal error occured when performing a storage query.
+    #[error("A fatal error occured in OperationStore: {0}")]
+    FatalStorageError(String),
+
+    /// Error which originates in `insert_operation()` when the insertion fails.
+    #[error("Error occured when inserting an operation with id {0:?} into storage")]
+    InsertionError(OperationId),
+}
