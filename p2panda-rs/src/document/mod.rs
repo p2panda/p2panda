528b7d5822b5b02d19d3830e5a76691c7479d3e3
Andreas Dzialocha <x12@adz.garden>
2022-07-21 16:06:34 +0200

    Fix doc test

--- p2panda-rs/src/document/mod.rs
+++ p2panda-rs/src/document/mod.rs
@@ -43,7 +43,7 @@ use p2panda_rs::document::{DocumentBuilder, DocumentViewValue, DocumentViewField
 # let mut node = Node::new();
 #
 # let mut node = Node::new();
-# 
+#
 # let (polar_entry_1_hash, _) = send_to_node(
 #     &mut node,
 #     &polar,
@@ -127,7 +127,7 @@ let document = DocumentBuilder::new(operations.clone()).build();
 assert!(document.is_ok());
 
 let document = document.unwrap();
-assert_eq!(format!("{}", document), "<Document 70e829>");
+assert_eq!(document.short_repr(), "<Document 70e829>");
 
 // This process already builds, sorts and reduces the document. We can now
 // access the derived view to check it's values.
