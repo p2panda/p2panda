960c184df895d51509fb4477faa93c71c942dd44
Andreas Dzialocha <x12@adz.garden>
2022-08-11 20:36:18 +0200

    Add script to compile WebAssembly

---
+++ p2panda-js/scripts/compile-wasm.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+
+# Exit when any command fails
+set -e
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# User Arguments
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Run in development or production mode
+MODE="${1:-development}"
+
+# Path to Rust project with Cargo.toml file
+RUST_DIR="${2:-../p2panda-rs}"
+
+# Path to temporary folder where compiled files are stored
+TMP_DIR="${3:-./wasm}"
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Names of the compilation targets
+NODE_PROJECT=node
+WEB_PROJECT=web
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+echo "Compile '$RUST_DIR' and optimize WebAssembly in '$MODE' mode"
+
+# Check if Rust project exists
+if [[ ! -f $RUST_DIR/Cargo.toml ]]
+then
+    echo "△ Could not find Rust project in '$RUST_DIR'"
+    exit 1
+fi
+
+# Check if tools are missing
+ensure_installed () {
+  if ! command -v $1 &> /dev/null
+  then
+      echo "△ '$1' needs to be installed first"
+      exit 1
+  fi
+}
+
+ensure_installed cargo
+ensure_installed wasm-bindgen
+
+# Finds and returns a .wasm file in a folder
+find_wasm_file () {
+  ls $1/*.wasm
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+echo "◆ Compiling WebAssembly"
+
+if [[ $MODE == "development" ]]
+then
+  cargo --quiet build \
+    --target=wasm32-unknown-unknown \
+    --manifest-path $RUST_DIR/Cargo.toml
+else
+  cargo --quiet build \
+    --target=wasm32-unknown-unknown \
+    --release \
+    --manifest-path $RUST_DIR/Cargo.toml
+fi
+
+RELEASE_WASM=$(find_wasm_file "$RUST_DIR/target/wasm32-unknown-unknown/release")
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+echo "◇ Adjusting WebAssembly for 'node' target"
+wasm-bindgen \
+  --out-dir=$RUST_DIR/$NODE_PROJECT \
+  --out-name=index \
+  --target=nodejs \
+  $RELEASE_WASM
+
+echo "◇ Adjusting WebAssembly for 'web' target"
+wasm-bindgen \
+  --out-dir=$RUST_DIR/$WEB_PROJECT \
+  --out-name=index \
+  --target=web \
+  --omit-default-module-path \
+  $RELEASE_WASM
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+if [[ $MODE == "production" ]]
+then
+  ensure_installed wasm-opt
+
+  echo "◌ Optimize 'node' target output for speed"
+
+  INPUT_WASM=$(find_wasm_file "$RUST_DIR/$NODE_PROJECT/$RUST_PROJECT")
+  OUTPUT_WASM=$TMP_DIR/optimized-node.wasm
+  wasm-opt -O -o $OUTPUT_WASM $INPUT_WASM
+  mv $OUTPUT_WASM $INPUT_WASM
+
+  echo "◌ Optimize 'web' target for size"
+
+  INPUT_WASM=$(find_wasm_file "$RUST_DIR/$WEB_PROJECT/$RUST_PROJECT")
+  OUTPUT_WASM=$TMP_DIR/optimized-web.wasm
+  input_filesize=$(wc -c < $INPUT_WASM)
+  wasm-opt -Os -o $OUTPUT_WASM $INPUT_WASM
+  output_filesize=$(wc -c < $OUTPUT_WASM)
+  mv $OUTPUT_WASM $INPUT_WASM
+  echo "▷ file size before: $input_filesize / after: $output_filesize bytes"
+else
+  echo "◌ Skip optimizations in 'development' mode"
+fi
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Move compiled files over
+rm -rf $TMP_DIR
+mkdir -p $TMP_DIR
+mv $RUST_DIR/$NODE_PROJECT $TMP_DIR
+mv $RUST_DIR/$WEB_PROJECT $TMP_DIR
