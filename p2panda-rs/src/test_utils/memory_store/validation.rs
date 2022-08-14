d5741289d2f48b1fc7c1ca0d19cacdfb3522e839
Sam Andreae <contact@samandreae.com>
2022-08-14 17:41:39 +0100

    Validation tests

--- p2panda-rs/src/test_utils/memory_store/validation.rs
+++ p2panda-rs/src/test_utils/memory_store/validation.rs
@@ -229,196 +229,212 @@ pub async fn get_checked_document_id_for_view_id<S: StorageProvider>(
     Ok(document_id.to_owned())
 }
 
-//
-// #[cfg(test)]
-// mod tests {
-//     use std::convert::TryFrom;
-//
-//     use crate::document::DocumentId;
-//     use crate::entry::{LogId, SeqNum};
-//     use crate::identity::{Author, KeyPair};
-//     use crate::storage_provider::traits::EntryWithOperation;
-//     use crate::test_utils::constants::PRIVATE_KEY;
-//     use crate::test_utils::fixtures::{key_pair, random_document_id};
-//     use rstest::rstest;
-//
-//     use super::{
-//         ensure_document_not_deleted, get_expected_skiplink, increment_log_id, increment_seq_num,
-//         is_next_seq_num, verify_log_id,
-//     };
-//
-//     #[rstest]
-//     #[case(LogId::new(0), LogId::new(1))]
-//     #[should_panic(expected = "Max log id reached")]
-//     #[case(LogId::new(u64::MAX), LogId::new(1))]
-//     fn increments_log_id(#[case] log_id: LogId, #[case] expected_next_log_id: LogId) {
-//         let mut log_id = log_id;
-//         let next_log_id = increment_log_id(&mut log_id).unwrap();
-//         assert_eq!(next_log_id, expected_next_log_id)
-//     }
-//
-//     #[rstest]
-//     #[case( SeqNum::new(1).unwrap(), SeqNum::new(2).unwrap())]
-//     #[should_panic(expected = "Max sequence number reached")]
-//     #[case(SeqNum::new(u64::MAX).unwrap(), SeqNum::new(1).unwrap())]
-//     fn increments_seq_num(#[case] seq_num: SeqNum, #[case] expected_next_seq_num: SeqNum) {
-//         let mut seq_num = seq_num;
-//         let next_seq_num = increment_seq_num(&mut seq_num).unwrap();
-//         assert_eq!(next_seq_num, expected_next_seq_num)
-//     }
-//
-//     #[rstest]
-//     #[case::valid_seq_num(Some(SeqNum::new(2).unwrap()), SeqNum::new(3).unwrap())]
-//     #[should_panic(
-//         expected = "Entry's claimed seq num of 2 does not match expected seq num of 3 for given author and log"
-//     )]
-//     #[case::seq_num_already_used(Some(SeqNum::new(2).unwrap()),SeqNum::new(2).unwrap())]
-//     #[should_panic(
-//         expected = "Entry's claimed seq num of 4 does not match expected seq num of 3 for given author and log"
-//     )]
-//     #[case::seq_num_too_high(Some(SeqNum::new(2).unwrap()),SeqNum::new(4).unwrap())]
-//     #[should_panic(expected = "Max sequence number reached")]
-//     #[case::seq_num_too_high(Some(SeqNum::new(u64::MAX).unwrap()),SeqNum::new(4).unwrap())]
-//     #[should_panic(
-//         expected = "Entry's claimed seq num of 3 does not match expected seq num of 1 for given author and log"
-//     )]
-//     #[case::no_seq_num(None, SeqNum::new(3).unwrap())]
-//     fn verifies_seq_num(#[case] latest_seq_num: Option<SeqNum>, #[case] claimed_seq_num: SeqNum) {
-//         is_next_seq_num(latest_seq_num.as_ref(), &claimed_seq_num).unwrap();
-//     }
-//
-//     #[rstest]
-//     #[case::existing_document(KeyPair::from_private_key_str(PRIVATE_KEY).unwrap(), LogId::default(), None)]
-//     #[case::new_document(KeyPair::from_private_key_str(PRIVATE_KEY).unwrap(), LogId::new(2), Some(random_document_id()))]
-//     #[case::existing_document_new_author(KeyPair::new(), LogId::new(0), None)]
-//     #[should_panic(
-//         expected = "Entry's claimed log id of 1 does not match existing log id of 0 for given author and document"
-//     )]
-//     #[case::already_occupied_log_id_for_existing_document(KeyPair::from_private_key_str(PRIVATE_KEY).unwrap(), LogId::new(1), None)]
-//     #[should_panic(
-//         expected = "Entry's claimed log id of 2 does not match existing log id of 0 for given author and document"
-//     )]
-//     #[case::new_log_id_for_existing_document(KeyPair::from_private_key_str(PRIVATE_KEY).unwrap(), LogId::new(2), None)]
-//     #[should_panic(
-//         expected = "Entry's claimed log id of 1 does not match expected next log id of 0 for given author"
-//     )]
-//     #[case::new_author_not_next_log_id(KeyPair::new(), LogId::new(1), None)]
-//     #[should_panic(
-//         expected = "Entry's claimed log id of 0 does not match expected next log id of 2 for given author"
-//     )]
-//     #[case::new_document_occupied_log_id(KeyPair::from_private_key_str(PRIVATE_KEY).unwrap(), LogId::new(0), Some(random_document_id()))]
-//     #[should_panic(
-//         expected = "Entry's claimed log id of 3 does not match expected next log id of 2 for given author"
-//     )]
-//     #[case::new_document_not_next_log_id(KeyPair::from_private_key_str(PRIVATE_KEY).unwrap(), LogId::new(3), Some(random_document_id()))]
-//     fn verifies_log_id(
-//         #[case] key_pair: KeyPair,
-//         #[case] claimed_log_id: LogId,
-//         #[case] document_id: Option<DocumentId>,
-//         #[from(test_db)]
-//         #[with(2, 2, 1)]
-//         runner: TestDatabaseRunner,
-//     ) {
-//         runner.with_db_teardown(move |db: TestDatabase| async move {
-//             // Unwrap the passed document id or select the first valid one from the database.
-//             let document_id =
-//                 document_id.unwrap_or_else(|| db.test_data.documents.first().unwrap().to_owned());
-//
-//             let author = Author::try_from(key_pair.public_key().to_owned()).unwrap();
-//
-//             verify_log_id(&db.store, &author, &claimed_log_id, &document_id)
-//                 .await
-//                 .unwrap();
-//         })
-//     }
-//
-//     #[rstest]
-//     #[case::expected_skiplink_is_in_store_and_is_same_as_backlink(KeyPair::from_private_key_str(PRIVATE_KEY).unwrap(), LogId::default(), SeqNum::new(4).unwrap())]
-//     #[should_panic(
-//         expected = "Expected skiplink target not found in store: <Author 53fc96>, log id 0, seq num 19"
-//     )]
-//     #[case::skiplink_not_in_store(KeyPair::from_private_key_str(PRIVATE_KEY).unwrap(), LogId::default(), SeqNum::new(20).unwrap())]
-//     #[should_panic(expected = "Expected skiplink target not found in store")]
-//     #[case::author_does_not_exist(KeyPair::new(), LogId::default(), SeqNum::new(5).unwrap())]
-//     #[should_panic(expected = "<Author 53fc96>, log id 4, seq num 6")]
-//     #[case::log_id_is_wrong(KeyPair::from_private_key_str(PRIVATE_KEY).unwrap(), LogId::new(4), SeqNum::new(7).unwrap())]
-//     #[should_panic(expected = "Entry with seq num 1 can not have skiplink")]
-//     #[case::seq_num_is_one(KeyPair::from_private_key_str(PRIVATE_KEY).unwrap(), LogId::new(0), SeqNum::new(1).unwrap())]
-//     fn get_expected_skiplink_errors(
-//         #[case] key_pair: KeyPair,
-//         #[case] log_id: LogId,
-//         #[case] seq_num: SeqNum,
-//         #[from(test_db)]
-//         #[with(7, 1, 1)]
-//         runner: TestDatabaseRunner,
-//     ) {
-//         runner.with_db_teardown(move |db: TestDatabase| async move {
-//             let author = Author::try_from(key_pair.public_key().to_owned()).unwrap();
-//
-//             get_expected_skiplink(&db.store, &author, &log_id, &seq_num)
-//                 .await
-//                 .unwrap();
-//         })
-//     }
-//
-//     #[rstest]
-//     #[should_panic(expected = "Entry with seq num 1 can not have skiplink")]
-//     #[case(SeqNum::new(1).unwrap(), SeqNum::new(1).unwrap())]
-//     #[case(SeqNum::new(2).unwrap(), SeqNum::new(1).unwrap())]
-//     #[case(SeqNum::new(3).unwrap(), SeqNum::new(2).unwrap())]
-//     #[case(SeqNum::new(4).unwrap(), SeqNum::new(1).unwrap())]
-//     #[case(SeqNum::new(5).unwrap(), SeqNum::new(4).unwrap())]
-//     #[case(SeqNum::new(6).unwrap(), SeqNum::new(5).unwrap())]
-//     #[case(SeqNum::new(7).unwrap(), SeqNum::new(6).unwrap())]
-//     #[case(SeqNum::new(8).unwrap(), SeqNum::new(4).unwrap())]
-//     #[case(SeqNum::new(9).unwrap(), SeqNum::new(8).unwrap())]
-//     #[case(SeqNum::new(10).unwrap(), SeqNum::new(9).unwrap())]
-//     fn gets_expected_skiplink(
-//         key_pair: KeyPair,
-//         #[case] seq_num: SeqNum,
-//         #[case] expected_seq_num: SeqNum,
-//         #[from(test_db)]
-//         #[with(10, 1, 1)]
-//         runner: TestDatabaseRunner,
-//     ) {
-//         runner.with_db_teardown(move |db: TestDatabase| async move {
-//             let author = Author::try_from(key_pair.public_key().to_owned()).unwrap();
-//
-//             let skiplink_entry =
-//                 get_expected_skiplink(&db.store, &author, &LogId::default(), &seq_num)
-//                     .await
-//                     .unwrap();
-//
-//             assert_eq!(skiplink_entry.seq_num(), expected_seq_num)
-//         })
-//     }
-//
-//     #[rstest]
-//     #[should_panic(expected = "Document is deleted")]
-//     fn identifies_deleted_document(
-//         #[from(test_db)]
-//         #[with(3, 1, 1, true)]
-//         runner: TestDatabaseRunner,
-//     ) {
-//         runner.with_db_teardown(move |db: TestDatabase| async move {
-//             let document_id = db.test_data.documents.first().unwrap();
-//             ensure_document_not_deleted(&db.store, document_id)
-//                 .await
-//                 .unwrap();
-//         })
-//     }
-//
-//     #[rstest]
-//     fn identifies_not_deleted_document(
-//         #[from(test_db)]
-//         #[with(3, 1, 1, false)]
-//         runner: TestDatabaseRunner,
-//     ) {
-//         runner.with_db_teardown(move |db: TestDatabase| async move {
-//             let document_id = db.test_data.documents.first().unwrap();
-//             assert!(ensure_document_not_deleted(&db.store, document_id)
-//                 .await
-//                 .is_ok());
-//         })
-//     }
-// }
+#[cfg(test)]
+mod tests {
+    use rstest::rstest;
+
+    use crate::document::DocumentId;
+    use crate::entry::traits::AsEntry;
+    use crate::entry::{LogId, SeqNum};
+    use crate::identity::{Author, KeyPair};
+    use crate::test_utils::constants::PRIVATE_KEY;
+    use crate::test_utils::fixtures::{key_pair, random_document_id};
+    use crate::test_utils::memory_store::helpers::{
+        populate_test_db, test_db_config, PopulateDatabaseConfig, TestStore,
+    };
+    use crate::test_utils::memory_store::MemoryStore;
+
+    use super::{
+        ensure_document_not_deleted, get_expected_skiplink, increment_log_id, increment_seq_num,
+        is_next_seq_num, verify_log_id,
+    };
+
+    #[rstest]
+    #[case(LogId::new(0), LogId::new(1))]
+    #[should_panic(expected = "Max log id reached")]
+    #[case(LogId::new(u64::MAX), LogId::new(1))]
+    fn increments_log_id(#[case] log_id: LogId, #[case] expected_next_log_id: LogId) {
+        let mut log_id = log_id;
+        let next_log_id = increment_log_id(&mut log_id).unwrap();
+        assert_eq!(next_log_id, expected_next_log_id)
+    }
+
+    #[rstest]
+    #[case( SeqNum::new(1).unwrap(), SeqNum::new(2).unwrap())]
+    #[should_panic(expected = "Max sequence number reached")]
+    #[case(SeqNum::new(u64::MAX).unwrap(), SeqNum::new(1).unwrap())]
+    fn increments_seq_num(#[case] seq_num: SeqNum, #[case] expected_next_seq_num: SeqNum) {
+        let mut seq_num = seq_num;
+        let next_seq_num = increment_seq_num(&mut seq_num).unwrap();
+        assert_eq!(next_seq_num, expected_next_seq_num)
+    }
+
+    #[rstest]
+    #[case::valid_seq_num(Some(SeqNum::new(2).unwrap()), SeqNum::new(3).unwrap())]
+    #[should_panic(
+        expected = "Entry's claimed seq num of 2 does not match expected seq num of 3 for given author and log"
+    )]
+    #[case::seq_num_already_used(Some(SeqNum::new(2).unwrap()),SeqNum::new(2).unwrap())]
+    #[should_panic(
+        expected = "Entry's claimed seq num of 4 does not match expected seq num of 3 for given author and log"
+    )]
+    #[case::seq_num_too_high(Some(SeqNum::new(2).unwrap()),SeqNum::new(4).unwrap())]
+    #[should_panic(expected = "Max sequence number reached")]
+    #[case::seq_num_too_high(Some(SeqNum::new(u64::MAX).unwrap()),SeqNum::new(4).unwrap())]
+    #[should_panic(
+        expected = "Entry's claimed seq num of 3 does not match expected seq num of 1 for given author and log"
+    )]
+    #[case::no_seq_num(None, SeqNum::new(3).unwrap())]
+    fn verifies_seq_num(#[case] latest_seq_num: Option<SeqNum>, #[case] claimed_seq_num: SeqNum) {
+        is_next_seq_num(latest_seq_num.as_ref(), &claimed_seq_num).unwrap();
+    }
+
+    #[rstest]
+    #[case::existing_document(KeyPair::from_private_key_str(PRIVATE_KEY).unwrap(), LogId::default(), None)]
+    #[case::new_document(KeyPair::from_private_key_str(PRIVATE_KEY).unwrap(), LogId::new(2), Some(random_document_id()))]
+    #[case::existing_document_new_author(KeyPair::new(), LogId::new(0), None)]
+    #[should_panic(
+        expected = "Entry's claimed log id of 1 does not match existing log id of 0 for given author and document"
+    )]
+    #[case::already_occupied_log_id_for_existing_document(KeyPair::from_private_key_str(PRIVATE_KEY).unwrap(), LogId::new(1), None)]
+    #[should_panic(
+        expected = "Entry's claimed log id of 2 does not match existing log id of 0 for given author and document"
+    )]
+    #[case::new_log_id_for_existing_document(KeyPair::from_private_key_str(PRIVATE_KEY).unwrap(), LogId::new(2), None)]
+    #[should_panic(
+        expected = "Entry's claimed log id of 1 does not match expected next log id of 0 for given author"
+    )]
+    #[case::new_author_not_next_log_id(KeyPair::new(), LogId::new(1), None)]
+    #[should_panic(
+        expected = "Entry's claimed log id of 0 does not match expected next log id of 2 for given author"
+    )]
+    #[case::new_document_occupied_log_id(KeyPair::from_private_key_str(PRIVATE_KEY).unwrap(), LogId::new(0), Some(random_document_id()))]
+    #[should_panic(
+        expected = "Entry's claimed log id of 3 does not match expected next log id of 2 for given author"
+    )]
+    #[case::new_document_not_next_log_id(KeyPair::from_private_key_str(PRIVATE_KEY).unwrap(), LogId::new(3), Some(random_document_id()))]
+    #[tokio::test]
+    async fn verifies_log_id(
+        #[case] key_pair: KeyPair,
+        #[case] claimed_log_id: LogId,
+        #[case] document_id: Option<DocumentId>,
+        #[from(test_db_config)]
+        #[with(2, 2, 1)]
+        config: PopulateDatabaseConfig,
+    ) {
+        let store = MemoryStore::default();
+        let mut db = TestStore::new(&store);
+        populate_test_db(&mut db, &config).await;
+
+        // Unwrap the passed document id or select the first valid one from the database.
+        let document_id =
+            document_id.unwrap_or_else(|| db.test_data.documents.first().unwrap().to_owned());
+
+        let author = Author::from(key_pair.public_key());
+
+        verify_log_id(&db.store, &author, &claimed_log_id, &document_id)
+            .await
+            .unwrap();
+    }
+
+    #[rstest]
+    #[case::expected_skiplink_is_in_store_and_is_same_as_backlink(KeyPair::from_private_key_str(PRIVATE_KEY).unwrap(), LogId::default(), SeqNum::new(4).unwrap())]
+    #[should_panic(
+        expected = "Expected skiplink target not found in store: <Author 53fc96>, log id 0, seq num 19"
+    )]
+    #[case::skiplink_not_in_store(KeyPair::from_private_key_str(PRIVATE_KEY).unwrap(), LogId::default(), SeqNum::new(20).unwrap())]
+    #[should_panic(expected = "Expected skiplink target not found in store")]
+    #[case::author_does_not_exist(KeyPair::new(), LogId::default(), SeqNum::new(5).unwrap())]
+    #[should_panic(expected = "<Author 53fc96>, log id 4, seq num 6")]
+    #[case::log_id_is_wrong(KeyPair::from_private_key_str(PRIVATE_KEY).unwrap(), LogId::new(4), SeqNum::new(7).unwrap())]
+    #[should_panic(expected = "Entry with seq num 1 can not have skiplink")]
+    #[case::seq_num_is_one(KeyPair::from_private_key_str(PRIVATE_KEY).unwrap(), LogId::new(0), SeqNum::new(1).unwrap())]
+    #[tokio::test]
+    async fn get_expected_skiplink_errors(
+        #[case] key_pair: KeyPair,
+        #[case] log_id: LogId,
+        #[case] seq_num: SeqNum,
+        #[from(test_db_config)]
+        #[with(7, 1, 1)]
+        config: PopulateDatabaseConfig,
+    ) {
+        let store = MemoryStore::default();
+        let mut db = TestStore::new(&store);
+        populate_test_db(&mut db, &config).await;
+
+        let author = Author::from(key_pair.public_key());
+
+        get_expected_skiplink(&db.store, &author, &log_id, &seq_num)
+            .await
+            .unwrap();
+    }
+
+    #[rstest]
+    #[should_panic(expected = "Entry with seq num 1 can not have skiplink")]
+    #[case(SeqNum::new(1).unwrap(), SeqNum::new(1).unwrap())]
+    #[case(SeqNum::new(2).unwrap(), SeqNum::new(1).unwrap())]
+    #[case(SeqNum::new(3).unwrap(), SeqNum::new(2).unwrap())]
+    #[case(SeqNum::new(4).unwrap(), SeqNum::new(1).unwrap())]
+    #[case(SeqNum::new(5).unwrap(), SeqNum::new(4).unwrap())]
+    #[case(SeqNum::new(6).unwrap(), SeqNum::new(5).unwrap())]
+    #[case(SeqNum::new(7).unwrap(), SeqNum::new(6).unwrap())]
+    #[case(SeqNum::new(8).unwrap(), SeqNum::new(4).unwrap())]
+    #[case(SeqNum::new(9).unwrap(), SeqNum::new(8).unwrap())]
+    #[case(SeqNum::new(10).unwrap(), SeqNum::new(9).unwrap())]
+    #[tokio::test]
+    async fn gets_expected_skiplink(
+        key_pair: KeyPair,
+        #[case] seq_num: SeqNum,
+        #[case] expected_seq_num: SeqNum,
+        #[from(test_db_config)]
+        #[with(10, 1, 1)]
+        config: PopulateDatabaseConfig,
+    ) {
+        let store = MemoryStore::default();
+        let mut db = TestStore::new(&store);
+        populate_test_db(&mut db, &config).await;
+
+        let author = Author::from(key_pair.public_key());
+
+        let skiplink_entry = get_expected_skiplink(&db.store, &author, &LogId::default(), &seq_num)
+            .await
+            .unwrap();
+
+        assert_eq!(skiplink_entry.seq_num(), &expected_seq_num)
+    }
+
+    #[rstest]
+    #[should_panic(expected = "Document is deleted")]
+    #[tokio::test]
+    async fn identifies_deleted_document(
+        #[from(test_db_config)]
+        #[with(3, 1, 1, true)]
+        config: PopulateDatabaseConfig,
+    ) {
+        let store = MemoryStore::default();
+        let mut db = TestStore::new(&store);
+        populate_test_db(&mut db, &config).await;
+
+        let document_id = db.test_data.documents.first().unwrap();
+        ensure_document_not_deleted(&db.store, document_id)
+            .await
+            .unwrap();
+    }
+
+    #[rstest]
+    #[tokio::test]
+    async fn identifies_not_deleted_document(
+        #[from(test_db_config)]
+        #[with(3, 1, 1, false)]
+        config: PopulateDatabaseConfig,
+    ) {
+        let store = MemoryStore::default();
+        let mut db = TestStore::new(&store);
+        populate_test_db(&mut db, &config).await;
+
+        let document_id = db.test_data.documents.first().unwrap();
+        assert!(ensure_document_not_deleted(&db.store, document_id)
+            .await
+            .is_ok());
+    }
+}
