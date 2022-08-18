63c096c8792f9dd41098255f2ae1ab39fade7275
Andreas Dzialocha <x12@adz.garden>
2022-08-18 11:55:51 +0200

    Add all lib-level exports

--- p2panda-js/src/p2panda.ts
+++ p2panda-js/src/p2panda.ts
@@ -1,3 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 export { KeyPair, verifySignature } from './identity';
+export { OperationFields, encodeOperation, decodeOperation } from './operation';
+export { generateHash } from './hash';
+export { hexToBytes, bytesToHex } from './utils';
+export { signAndEncodeEntry, decodeEntry } from './entry';
