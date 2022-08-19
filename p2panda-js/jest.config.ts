b60f3db8a92bdecf7e1cee1910c2086279781273
Andreas Dzialocha <x12@adz.garden>
2022-08-19 13:02:44 +0200

    Change outdated comment in jest.config.ts

--- p2panda-js/jest.config.ts
+++ p2panda-js/jest.config.ts
@@ -9,8 +9,8 @@ export default {
   coveragePathIgnorePatterns: ['<rootDir>/wasm'],
   globals: {
     // Set `BUILD_TARGET_WEB` to false to import the WebAssembly build for
-    // NodeJS targets during testing. This is usually set via the Webpack
-    // DefinePlugin (see webpack configuration), but since Webpack is not used
+    // NodeJS targets during testing. This is usually set via the Rollup
+    // "define" plugin (see rollup configuration), but since Rollup is not used
     // during Jest testing we have to set it here as well.
     BUILD_TARGET_WEB: false,
   },
