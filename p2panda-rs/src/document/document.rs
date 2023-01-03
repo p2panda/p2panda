89dbc5e95fa6b2cef66639d6781839722dc41d27
Sam Andreae <contact@samandreae.com>
2023-01-03 19:06:52 +0000

    Finish Document docs with examples

--- p2panda-rs/src/document/document.rs
+++ p2panda-rs/src/document/document.rs
@@ -31,41 +31,135 @@ pub struct DocumentMeta {
     operations: Vec<(OperationId, Operation, PublicKey)>,
 }
 
-/// A replicatable data type designed to handle concurrent updates in a way where all replicas
-/// eventually resolve to the same deterministic value.
+/// High-level datatype representing data published to the p2panda network as key-value pairs.
 ///
-/// `Document`s contain a fixed set of sorted operations along with their resolved document view
-/// and metadata. Documents are constructed by passing an unsorted collection of operations to
-/// the `DocumentBuilder`.
-
-/// High-level datatype representing data published to the p2panda network as key-value pairs. 
-/// 
-/// Documents are multi-writer and have automatic conflict resolution strategies which produce deterministic 
-/// state for any two replicas. The underlying structure which make this possible is a directed acyclic graph 
-/// of `Operation`'s. To arrive at the current state of a document the graph is topologically sorted,
-/// with any branches being ordered according to the conflicting operations `OperationId`. When each
-/// operation's change is applied in order then we have a LWW (last write wins) resolution strategy.
-/// 
+/// Documents are multi-writer and have automatic conflict resolution strategies which produce deterministic
+/// state for any two replicas. The underlying structure which make this possible is a directed acyclic graph
+/// of [`Operation`]'s. To arrive at the current state of a document the graph is topologically sorted,
+/// with any branches being ordered according to the conflicting operations [`OperationId`]. Each operation's
+/// mutation is applied in order which results in a LWW (last write wins) resolution strategy.
+///
 /// All documents have an accomapanying `Schema` which describes the shape of the data they will contain. Every
 /// operation should have been validated aginst this schema before being included in the graph.
-/// 
-/// Documents are constructed through the `DocumentBuilder` struct like so:
-/// 
+///
+/// Documents are constructed through the [`DocumentBuilder`] or by conversion from vectors of a type implementing
+/// the [`AsOperation`], [`WithOperationId`] and [`WithPublicKey`].
+///
 /// ## Example
-/// 
-/// @TODO
-/// 
+///
+/// ```
+/// # extern crate p2panda_rs;
+/// #
+/// # fn main() -> Result<(), Box<dyn std::error::Error>> {
+/// # #[cfg(feature = "test-utils")]
+/// # {
+/// # use p2panda_rs::operation::{OperationBuilder, OperationAction, OperationValue};
+/// # use p2panda_rs::document::DocumentBuilder;
+/// # use p2panda_rs::identity::KeyPair;
+/// # use p2panda_rs::schema::SchemaId;
+/// # use p2panda_rs::test_utils::fixtures::{random_operation_id, random_document_view_id};
+/// #
+/// # let schema_id = SchemaId::Application("cafe".to_string(), random_document_view_id());
+/// # let public_key = KeyPair::new().public_key().to_owned();
+/// # let operation_id = random_operation_id();
+/// #
+/// // Construct a CREATE operation.
+/// let operation_value = OperationValue::String("Panda Cafe".to_string());
+/// let operation = OperationBuilder::new(&schema_id)
+///     .action(OperationAction::Create)
+///     .fields(&[("name", operation_value.clone())])
+///     .build()
+///     .unwrap();
+///
+/// // Build a document from a single operation, we include it's id and the public key of the 
+/// // author who published it.
+/// let document = DocumentBuilder::new(vec![(operation_id, operation, public_key)]).build()?;
+/// // The document view value contains the value we expect for the "name" field as well 
+/// // as the id of the operation which last updated this field.
+/// assert_eq!(
+///     document.view().unwrap().get("name"),
+///     Some(&DocumentViewValue::new(&operation_id, &operation_alue))
+/// );
+/// # }
+/// # Ok(())
+/// # }
+/// ```
+///
+/// As can be seen in this example a tuple of ([`OperationId`], [`Operation`], [`PublicKey`]) is required for each operation
+/// included in the graph. The [`OperationId`] and [`PublicKey`] are both derived from the signed [`Entry`] the operation was
+/// published on.
+///
 /// A useful characteristic of documents is the ability to view state from any point in the past (as long as the
 /// operations have been retained, see below for details on this). Every state a document has passed through can
-/// be identified by a `DocumentViewId` which is the id's of the current graph tip operations. This id can be 
-/// used to request a view onto any state from the documents past.
+/// be identified by a [`DocumentViewId`] which is the id's of the current graph tip operations. This id can be
+/// used to request a view onto any state from the documents past.]
+///
+/// ## Example
+///
+/// ```
+/// # extern crate p2panda_rs;
+/// #
+/// # fn main() -> Result<(), Box<dyn std::error::Error>> {
+/// # #[cfg(feature = "test-utils")]
+/// # {
+/// # use p2panda_rs::operation::{OperationBuilder, OperationAction, OperationValue};
+/// # use p2panda_rs::document::{DocumentBuilder, DocumentViewId, DocumentViewValue};
+/// # use p2panda_rs::identity::KeyPair;
+/// # use p2panda_rs::schema::SchemaId;
+/// # use p2panda_rs::test_utils::fixtures::{random_operation_id, random_document_view_id};
+/// #
+/// # let schema_id = SchemaId::Application("cafe".to_string(), random_document_view_id());
+/// # let public_key = KeyPair::new().public_key().to_owned();
+/// # let operation_id_1 = random_operation_id();
+/// # let operation_id_2 = random_operation_id();
+/// #
+/// // Construct a CREATE operation.
+/// let operation_1_value = OperationValue::String("Panda Cafe".to_string());
+/// let operation_1 = OperationBuilder::new(&schema_id)
+///     .action(OperationAction::Create)
+///     .fields(&[("name", operation_1_value.clone())])
+///     .build()
+///     .unwrap();
+///
+/// // Construct an UPDATE operation.
+/// let operation_2_value = OperationValue::String("Polar Bear Cafe".to_string());
+/// let operation_2 = OperationBuilder::new(&schema_id)
+///     .action(OperationAction::Update)
+///     .fields(&[("name", operation_2_value.clone())])
+///     .previous(&DocumentViewId::new(&[operation_id_1.clone()]))
+///     .build()
+///     .unwrap();
+///
+/// // Construct a document builder from these two operations.
+/// let document_builder = DocumentBuilder::new(vec![
+///     (operation_id_1.clone(), operation_1, public_key),
+///     (operation_id_2.clone(), operation_2, public_key),
+/// ]);
+///
+/// // Build the document to it's latest view.
+/// let document = document_builder.build().unwrap();
 /// 
-/// Notes on CRDTs and scope:
+/// // The document view value contains the value we expect for the "name" field as well 
+/// // as the id of the operation which last updated this field.
+/// assert_eq!(
+///     document.view().unwrap().get("name"),
+///     Some(&DocumentViewValue::new(&operation_id_2, &operation_2_value))
+/// );
+///
+/// // Derive a document view id for the initial document state.
+/// let document_view_id_1 = DocumentViewId::new(&[operation_id_1.clone()]);
+/// // Build the document again but to this earlier state.
+/// let document = document_builder.build_to_view_id(Some(document_view_id_1)).unwrap();
 /// 
-/// Documents offer a relatively limited and opinionated approach to conflict resolution, we don't intend to 
-/// offer the advanced features many modern CRDT's provide. However with good data design and exploiting 
-/// per-field updates, there are many suitable use cases. If other conflict resolution strategies are needed, 
-/// there is much scope for integrating battle tested solutions in the future. 
+/// assert_eq!(
+///     document.view().unwrap().get("name"),
+///     Some(&DocumentViewValue::new(&operation_id_1, &operation_1_value))
+/// )
+/// # }
+/// # Ok(())
+/// # }
+///
+/// ```
 #[derive(Debug, Clone)]
 pub struct Document {
     id: DocumentId,
@@ -798,5 +892,53 @@ mod tests {
                 .value(),
             &OperationValue::String("Penguin Cafe!!!".to_string())
         );
+    }
+
+    #[test]
+    fn doc_test() {
+        use crate::identity::KeyPair;
+        use crate::operation::{OperationAction, OperationBuilder, OperationValue};
+        use crate::document::{DocumentBuilder, DocumentViewId, DocumentViewValue};
+        use crate::schema::SchemaId;
+
+        let schema_id = SchemaId::Application("cafe".to_string(), random_document_view_id());
+        let public_key = KeyPair::new().public_key().to_owned();
+        let operation_id_1 = random_operation_id();
+        let operation_id_2 = random_operation_id();
+
+        let operation_1_value = OperationValue::String("Panda Cafe".to_string());
+        let operation_1 = OperationBuilder::new(&schema_id)
+            .action(OperationAction::Create)
+            .fields(&[("name", operation_1_value.clone())])
+            .build()
+            .unwrap();
+
+        let document_view_id_1 = DocumentViewId::new(&[operation_id_1.clone()]);
+
+        let operation_2_value = OperationValue::String("Polar Bear Cafe".to_string());
+        let operation_2 = OperationBuilder::new(&schema_id)
+            .action(OperationAction::Update)
+            .fields(&[("name", operation_2_value.clone())])
+            .previous(&DocumentViewId::new(&[operation_id_1.clone()]))
+            .build()
+            .unwrap();
+
+        let document_builder = DocumentBuilder::new(vec![
+            (operation_id_1.clone(), operation_1, public_key),
+            (operation_id_2.clone(), operation_2, public_key),
+        ]);
+
+        let document = document_builder.build().unwrap();
+        assert_eq!(
+            document.view().unwrap().get("name"),
+            Some(&DocumentViewValue::new(&operation_id_2, &operation_2_value))
+        );
+
+        let document = document_builder.build_to_view_id(Some(document_view_id_1)).unwrap();
+        assert_eq!(
+            document.view().unwrap().get("name"),
+            Some(&DocumentViewValue::new(&operation_id_1, &operation_1_value))
+        )
+
     }
 }
