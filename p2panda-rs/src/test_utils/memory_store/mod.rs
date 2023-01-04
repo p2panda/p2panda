1fa1e8e1e8c2440945bf840d50f1cb2a2d983a30
Sam Andreae <contact@samandreae.com>
2023-01-04 14:43:57 +0000

    Improve module docs

--- p2panda-rs/src/test_utils/memory_store/mod.rs
+++ p2panda-rs/src/test_utils/memory_store/mod.rs
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-//! `MemoryStore` implementation of all storage provider traits.
-//!
-//! Used in the mock node and for testing.
+//! Structs and methods for interacting with the `StorageProvider` traits.
+//! 
+//! - `MemoryStore` implementation of all storage provider traits
+//! - `domain` and `validation` methods for publishing and validating entries and operations
+//! - helpers for populating a store with test data
 pub mod domain;
 mod provider;
 mod stores;
