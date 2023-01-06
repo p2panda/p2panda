4dd5b4e794524b8d0804ce59c8508bb4829ca892
Sam Andreae <contact@samandreae.com>
2023-01-06 21:17:53 +0000

    Docs for storage provider module

--- p2panda-rs/src/storage_provider/traits/mod.rs
+++ p2panda-rs/src/storage_provider/traits/mod.rs
@@ -1,6 +1,21 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-//! Traits used when implementing a custom storage provider for a p2panda client or node.
+//! Traits describing the minimum required API which should be implemented by any 
+//! persistent storage layer handling data for a p2panda peer. 
+//! 
+//! The primary data types which require persisting are `Entry` and `Operation`. These
+//! are the immutable objects which peers publish and replicate. Their storage methods  
+//! are described in [`EntryStore`] and [`OperationStore`]. Entries are associated with 
+//! logs, which can be queried via the [`LogStore`]. 
+//! 
+//! [`DocumentStore`] offers a basic API for querying documents with default trait
+//! implementations built on top of the previously described stores. Documents are
+//! entirely derived from their respective operations, a documents' current state, as 
+//! well as any historic views which need to be retained, can be seen as a caching 
+//! layer on top of the persisted, immutable operations. The default implementations
+//! offered here build documents new on each query and are only intended as a
+//! convenience. Efficient storage and querying of documents should be considered 
+//! by implementers of these traits.
 mod document_store;
 mod entry_store;
 mod log_store;
