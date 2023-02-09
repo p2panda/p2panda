19aade4a90822f3b1b0fdbaf70e5582504094aa7
Sam Andreae <contact@samandreae.com>
2023-02-09 09:44:34 +0000

    Don't mention non-existent default document store implementation

--- p2panda-rs/src/storage_provider/traits/mod.rs
+++ p2panda-rs/src/storage_provider/traits/mod.rs
@@ -10,14 +10,11 @@
 //! are described in [`EntryStore`] and [`OperationStore`]. Entries are associated with
 //! logs, which can be queried via the [`LogStore`].
 //!
-//! [`DocumentStore`] offers a basic API for querying documents with default trait
-//! implementations built on top of the previously described stores. Documents are
+//! [`DocumentStore`] outlines a basic API for querying documents. Documents are
 //! entirely derived from their respective operations, a documents' current state, as
 //! well as any historic views which need to be retained, can be seen as a caching
-//! layer on top of the persisted, immutable operations. The default implementations
-//! offered here build documents new on each query and are only offered as an easy
-//! out-of-the-box option. Efficient storage and querying of documents should be
-//! considered by implementers of these traits.
+//! layer on top of the persisted, immutable operations. Efficient storage and
+//! querying of documents should be considered by implementers of these traits.
 mod document_store;
 mod entry_store;
 mod log_store;
