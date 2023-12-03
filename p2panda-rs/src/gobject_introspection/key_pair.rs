f6ad1d2010859b719c1c61d5da783f6251fc9e24
Jan Fooken <git@janvhs.com>
2023-12-03 17:44:24 +0100

    Gir C API: implement most functions of key_pair.rs

--- p2panda-rs/src/gobject_introspection/key_pair.rs
+++ p2panda-rs/src/gobject_introspection/key_pair.rs
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-use std::ffi::CStr;
+use std::{
+    ffi::{CStr, CString},
+    ptr::NonNull,
+};
 
 use libc::c_char;
 
@@ -9,15 +12,47 @@ use crate::{identity::KeyPair as KeyPairNonC, test_utils::fixtures::private_key}
 #[repr(C)]
 pub struct KeyPair(KeyPairNonC);
 
-impl KeyPair {
-    pub extern "C" fn from_private_key(private_key: *const c_char) -> KeyPair {
-        let private_key = unsafe {
-            let c_repr = CStr::from_ptr(private_key);
-            c_repr.to_str().expect("convert the private key from C")
-        };
+#[no_mangle]
+pub extern "C" fn key_pair_from_private_key(private_key: *const c_char) -> KeyPair {
+    let private_key = unsafe {
+        assert!(!private_key.is_null());
 
-        let key_pair_inner = KeyPairNonC::from_private_key_str(private_key).expect("get a key pair");
+        let c_repr = CStr::from_ptr(private_key);
+        c_repr.to_str().expect("convert the private key from C")
+    };
 
-        KeyPair(key_pair_inner)
-    }
+    let key_pair_inner = KeyPairNonC::from_private_key_str(private_key).expect("get a key pair");
+
+    KeyPair(key_pair_inner)
+}
+
+#[no_mangle]
+pub extern "C" fn key_pair_public_key(instance: &KeyPair) -> *const c_char {
+    let key = instance.0.public_key().to_bytes();
+    CString::new(key).unwrap().into_raw()
+}
+
+#[no_mangle]
+pub extern "C" fn key_pair_private_key(instance: &KeyPair) -> *const c_char {
+    let key = instance.0.private_key().to_bytes();
+    CString::new(key).unwrap().into_raw()
+}
+
+#[no_mangle]
+pub extern "C" fn sign(instance: &KeyPair, value: *mut c_char) -> *const c_char {
+    let value = unsafe {
+        assert!(!value.is_null());
+
+        CStr::from_ptr(value)
+    };
+
+    let value = value.to_str().unwrap();
+
+    let signature = instance.0.sign(value.as_bytes());
+    CString::new(signature.to_bytes()).unwrap().into_raw()
+}
+
+#[no_mangle]
+pub extern "C" fn verify_signature(public_key: *const c_char, bytes: *const c_char, signature: *const c_char) {
+    todo!()
 }
