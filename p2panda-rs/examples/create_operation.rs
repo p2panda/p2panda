8b3128c0f8dc8b8ae9111ecddbf7e260243d979b
Sam Andreae <contact@samandreae.com>
2023-12-05 21:25:57 +0000

    Update example

--- p2panda-rs/examples/create_operation.rs
+++ p2panda-rs/examples/create_operation.rs
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-use p2panda_rs::entry::encode::encode_entry;
-use p2panda_rs::entry::EntryBuilder;
-use p2panda_rs::identity::KeyPair;
-use p2panda_rs::operation::encode::encode_operation;
-use p2panda_rs::operation::OperationBuilder;
+use p2panda_rs::identity_v2::KeyPair;
+use p2panda_rs::operation_v2::body::encode::encode_body;
+use p2panda_rs::operation_v2::header::encode::encode_header;
+use p2panda_rs::operation_v2::OperationBuilder;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Id of the schema which describes the data we want to publish. This should
@@ -15,21 +14,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Generate new Ed25519 key pair.
     let key_pair = KeyPair::new();
 
-    // Add field data to "create" operation.
+    // Build and sign a CREATE operation.
     let operation = OperationBuilder::new(&SCHEMA_ID.parse()?)
         .fields(&[("username", "panda".into())])
-        .build()?;
+        .sign(&key_pair)?;
 
-    // Encode operation into bytes.
-    let encoded_operation = encode_operation(&operation)?;
+    // Encode operation header and body.
+    let encoded_header = encode_header(operation.header())?;
+    let encoded_body = encode_body(operation.body())?;
 
-    // Create Bamboo entry (append-only log data type) with operation as payload.
-    let entry = EntryBuilder::new().sign(&encoded_operation, &key_pair)?;
-
-    // Encode entry into bytes.
-    let encoded_entry = encode_entry(&entry)?;
-
-    println!("{} {}", encoded_entry, encoded_operation);
+    println!("{} {}", encoded_header, encoded_body);
 
     Ok(())
 }
