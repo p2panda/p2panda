baf3a7bb29d5648caec0030df96b97845e9bd1fa
Sam Andreae <contact@samandreae.com>
2023-12-19 22:51:41 +0000

    Construct Body when validating PlainOperations

--- p2panda-rs/src/operation_v2/validate.rs
+++ p2panda-rs/src/operation_v2/validate.rs
@@ -1,20 +1,22 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 //! Collection of low-level validation methods for operations.
+use crate::operation_v2::body::plain::PlainOperation;
 use crate::operation_v2::body::traits::Schematic;
+use crate::operation_v2::body::Body;
 use crate::operation_v2::error::ValidateOperationError;
-use crate::operation_v2::header::traits::Actionable;
-use crate::operation_v2::{Operation, OperationAction};
+use crate::operation_v2::OperationAction;
 use crate::schema::validate::{validate_all_fields, validate_only_given_fields};
 use crate::schema::Schema;
 use crate::Human;
 
 /// Checks the fields and format of an operation against a schema.
-pub fn validate_operation(
-    operation: &Operation,
+pub fn validate_plain_operation(
+    action: &OperationAction,
+    plain_operation: &PlainOperation,
     schema: &Schema,
-) -> Result<(), ValidateOperationError> {
-    let claimed_schema_id = operation.schema_id();
+) -> Result<Body, ValidateOperationError> {
+    let claimed_schema_id = plain_operation.schema_id();
 
     // Make sure the schema id and given schema matches
     if claimed_schema_id != schema.id() {
@@ -24,13 +26,13 @@ pub fn validate_operation(
         ));
     }
 
-    let _ = match (operation.action(), operation.plain_fields()) {
-        (OperationAction::Create, Some(fields)) => validate_all_fields(&fields, schema),
-        (OperationAction::Update, Some(fields)) => validate_only_given_fields(&fields, schema),
-        (OperationAction::Delete, None) => return Ok(()),
+    let fields = match (action, plain_operation.plain_fields()) {
+        (OperationAction::Create, Some(fields)) => validate_all_fields(&fields, schema).ok(),
+        (OperationAction::Update, Some(fields)) => validate_only_given_fields(&fields, schema).ok(),
+        (OperationAction::Delete, None) => None,
         // All other cases should not occur if correct validation of operation has been performed.
         (_, _) => unreachable!(),
-    }?;
+    };
 
-    Ok(())
+    Ok(Body(schema.id().clone(), fields))
 }
