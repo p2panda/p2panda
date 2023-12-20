f842ea4b7e838151120e250f48e07440514b8f4c
Sam Andreae <contact@samandreae.com>
2023-12-20 06:33:57 +0000

    Update const hashes in test utils

--- p2panda-rs/src/test_utils/constants.rs
+++ p2panda-rs/src/test_utils/constants.rs
@@ -10,11 +10,11 @@ use crate::test_utils::fixtures::{document_id, document_view_id, schema_fields, 
 
 /// Hash value, used when a hash is needed for testing. It's the default hash in fixtures
 /// when a custom value isn't specified.
-pub const HASH: &str = "0020b177ec1bf26dfb3b7010d473e6d44713b29b765b99c6e60ecbfae742de496543";
+pub const HASH: &str = "b177ec1bf26dfb3b7010d473e6d44713b29b765b99c6e60ecbfae742de496543";
 
 /// Schema id, used as the default in all fixtures.
 pub const SCHEMA_ID: &str =
-    "venue_0020c65567ae37efea293e34a9c7d13f8f2bf23dbdc3b5c7b9ab46293111c48fc78b";
+    "venue_c65567ae37efea293e34a9c7d13f8f2bf23dbdc3b5c7b9ab46293111c48fc78b";
 
 /// Private key string, used as default for creating private keys in fixtures.
 pub const PRIVATE_KEY: &str = "eb852fefa703901e42f17cdc2aa507947f392a72101b2c1a6d30023af14f75e2";
@@ -26,19 +26,19 @@ pub const SKIPLINK_SEQ_NUMS: [u64; 5] = [4, 8, 12, 13, 17];
 pub fn test_fields() -> Vec<(&'static str, OperationValue)> {
     // Define a bunch of hard-coded test values to preserve constant behaviour of this function.
     let document_id_1 =
-        document_id("0020b177ec1bf26dfb3b7010d473e6d44713b29b765b99c6e60ecbfae742de496543");
+        document_id("b177ec1bf26dfb3b7010d473e6d44713b29b765b99c6e60ecbfae742de496543");
     let document_id_2 =
-        document_id("00209a2149589672fa1ac2348e48b4c56fc208a0eff44938464dd2091850f444a323");
+        document_id("9a2149589672fa1ac2348e48b4c56fc208a0eff44938464dd2091850f444a323");
     let document_view_id_1 = document_view_id(vec![
-        "0020f7a17e14b9a5e87435decdbc28d562662fbf37da39b94e8469d8e1873336e80e",
-        "0020475488c0e2bbb9f5a81929e2fe11de81c1f83c8045de41da43899d25ad0d4afa",
+        "f7a17e14b9a5e87435decdbc28d562662fbf37da39b94e8469d8e1873336e80e",
+        "475488c0e2bbb9f5a81929e2fe11de81c1f83c8045de41da43899d25ad0d4afa",
     ]);
     let document_view_id_2 = document_view_id(vec![
-        "00204f0dd3a1b8205b6d4ce3fd4c158bb91c9e131bd842e727164ea220b5b6d09346",
+        "4f0dd3a1b8205b6d4ce3fd4c158bb91c9e131bd842e727164ea220b5b6d09346",
     ]);
     let document_view_id_3 = document_view_id(vec![
-        "0020995d53f460293c5686c42037b72787ed28668ad8b6d18e9d5f02c5d3301161f0",
-        "002019ed3e9b39cd17f1dbc0f6e31a6e7b9c9ab7e349332e710c946a441b7d308eb5",
+        "995d53f460293c5686c42037b72787ed28668ad8b6d18e9d5f02c5d3301161f0",
+        "19ed3e9b39cd17f1dbc0f6e31a6e7b9c9ab7e349332e710c946a441b7d308eb5",
     ]);
 
     [
