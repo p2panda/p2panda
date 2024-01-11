8d5c59b4b0edaf0a43107deb3c9d2edfde9cbc0d
Sam Andreae <contact@samandreae.com>
2024-01-11 19:34:32 +0000

    Remove unused file

--- p2panda-rs/src/operation/validation.rs
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
-use crate::operation::header::{Header, HeaderExtension};
-use crate::operation::traits::Actionable;
-use crate::operation::OperationAction;
-use crate::Validate;
