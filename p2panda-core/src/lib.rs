4024423cf1b034bd16411067ba480375e0ae74c0
adz <x12@adz.garden>
2024-07-02 13:51:56 +0200

    Introduce identity module with ed25519 DSA methods

--- p2panda-core/src/lib.rs
+++ p2panda-core/src/lib.rs
@@ -1,4 +1,8 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 pub mod hash;
+pub mod identity;
 mod serde;
+
+pub use hash::{Hash, HashError};
+pub use identity::{IdentityError, PrivateKey, PublicKey, Signature};
