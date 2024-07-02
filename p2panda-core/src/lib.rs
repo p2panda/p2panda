b722ee3aa9ab975b107c38b03332915d58885ac8
adz <x12@adz.garden>
2024-07-02 13:35:15 +0200

    Introduce Hash struct holding a BLAKE3 hash

--- p2panda-core/src/lib.rs
+++ p2panda-core/src/lib.rs
@@ -1,14 +1,4 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+// SPDX-License-Identifier: AGPL-3.0-or-later
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod hash;
+mod serde;
