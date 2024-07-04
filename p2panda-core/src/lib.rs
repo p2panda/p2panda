3623425de5cd306a392976d157ea88ff44897566
Sam Andreae <contact@samandreae.com>
2024-07-04 16:08:27 +0100

    Fix: export Header from lib

--- p2panda-core/src/lib.rs
+++ p2panda-core/src/lib.rs
@@ -8,5 +8,5 @@ mod serde;
 pub use hash::{Hash, HashError};
 pub use identity::{IdentityError, PrivateKey, PublicKey, Signature};
 pub use operation::{
-    validate_header, validate_operation, Body, Operation, OperationError, UnsignedHeader,
+    validate_header, validate_operation, Body, Operation, OperationError, UnsignedHeader, Header
 };
