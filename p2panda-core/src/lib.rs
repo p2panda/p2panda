35d0e28e34782877f4f7cfe5c237c50283d9ef66
Sam Andreae <contact@samandreae.com>
2024-07-04 16:11:41 +0100

    fmt

--- p2panda-core/src/lib.rs
+++ p2panda-core/src/lib.rs
@@ -8,5 +8,5 @@ mod serde;
 pub use hash::{Hash, HashError};
 pub use identity::{IdentityError, PrivateKey, PublicKey, Signature};
 pub use operation::{
-    validate_header, validate_operation, Body, Operation, OperationError, UnsignedHeader, Header
+    validate_header, validate_operation, Body, Header, Operation, OperationError, UnsignedHeader,
 };
