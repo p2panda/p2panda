c0fb1935a29a1ea3f6c89dde5b823df49b170797
Sam Andreae <contact@samandreae.com>
2024-07-08 22:52:31 +0100

    Rename extension -> extensions

--- p2panda-core/src/lib.rs
+++ p2panda-core/src/lib.rs
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-pub mod extension;
+pub mod extensions;
 pub mod hash;
 pub mod identity;
 pub mod operation;
 mod serde;
 
-pub use extension::Extension;
+pub use extensions::Extensions;
 pub use hash::{Hash, HashError};
 pub use identity::{IdentityError, PrivateKey, PublicKey, Signature};
 pub use operation::{
