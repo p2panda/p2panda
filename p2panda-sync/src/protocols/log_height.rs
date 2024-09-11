6ad5d821b382fd05d57dd3715843b7e66e0625f3
Sam Andreae <contact@samandreae.com>
2024-09-12 00:08:31 +0100

    WIP: Update LogHeightProtocol tests

--- p2panda-sync/src/protocols/log_height.rs
+++ p2panda-sync/src/protocols/log_height.rs
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 use std::collections::HashMap;
+use std::fmt::Debug;
 use std::sync::{Arc, RwLock, RwLockReadGuard, RwLockWriteGuard};
 
 use async_trait::async_trait;
@@ -22,7 +23,7 @@ pub type LogHeights = Vec<(PublicKey, SeqNum)>;
 #[allow(clippy::large_enum_variant)]
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub enum Message<E = DefaultExtensions> {
-    Have(LogHeights),
+    Have(LogId, LogHeights),
     Operation(Header<E>, Option<Body>),
     SyncDone,
 }
@@ -42,20 +43,28 @@ where
 static LOG_HEIGHT_PROTOCOL_NAME: &str = "p2panda/log_height";
 
 #[derive(Clone, Debug, Default)]
-pub struct LogHeightSyncProtocol {
+pub struct LogHeightSyncProtocol<E>
+where
+    E: Clone + Default,
+    MemoryStore<LogId, E>: Default,
+{
     pub log_ids: HashMap<TopicId, LogId>,
-    pub store: Arc<RwLock<MemoryStore<LogId, DefaultExtensions>>>,
+    pub store: Arc<RwLock<MemoryStore<LogId, E>>>,
 }
 
-impl LogHeightSyncProtocol {
+impl<E> LogHeightSyncProtocol<E>
+where
+    E: Clone + Default,
+    MemoryStore<LogId, E>: Default,
+{
     pub fn log_id(&self, topic: &TopicId) -> Option<&LogId> {
         self.log_ids.get(topic)
     }
-    pub fn read_store(&self) -> RwLockReadGuard<MemoryStore<LogId, DefaultExtensions>> {
+    pub fn read_store(&self) -> RwLockReadGuard<MemoryStore<LogId, E>> {
         self.store.read().expect("error getting read lock on store")
     }
 
-    pub fn write_store(&self) -> RwLockWriteGuard<MemoryStore<LogId, DefaultExtensions>> {
+    pub fn write_store(&self) -> RwLockWriteGuard<MemoryStore<LogId, E>> {
         self.store
             .write()
             .expect("error getting write lock on store")
@@ -63,7 +72,11 @@ impl LogHeightSyncProtocol {
 }
 
 #[async_trait]
-impl SyncProtocol for LogHeightSyncProtocol {
+impl<E> SyncProtocol for LogHeightSyncProtocol<E>
+where
+    E: Clone + Debug + Default + Send + Sync + for<'a> Deserialize<'a> + Serialize,
+    MemoryStore<LogId, E>: Default,
+{
     fn name(&self) -> &'static str {
         LOG_HEIGHT_PROTOCOL_NAME
     }
@@ -89,14 +102,81 @@ impl SyncProtocol for LogHeightSyncProtocol {
             .get_log_heights(log_id.to_string())
             .expect("memory store error");
 
-        sink.send(Message::Have(local_log_heights.clone())).await?;
+        sink.send(Message::<E>::Have(
+            log_id.to_string(),
+            local_log_heights.clone(),
+        ))
+        .await?;
+        // As we initiated this sync session we are done after sending the Have message.
+        sink.send(Message::SyncDone).await?;
+        sync_done_sent = true;
 
         while let Some(result) = stream.next().await {
             let message = result?;
             debug!("message received: {:?}", message);
 
-            let replies = match &message {
-                Message::Have(log_heights) => {
+            match &message {
+                Message::Have(_, _) => {
+                    return Err(SyncError::Protocol(
+                        "unexpected Have message received".to_string(),
+                    ))
+                }
+                Message::Operation(header, body) => {
+                    let operation = Operation {
+                        hash: header.hash(),
+                        header: header.clone(),
+                        body: body.clone(),
+                    };
+                    let inserted = self
+                        .write_store()
+                        .insert_operation(operation.clone(), log_id.to_string())
+                        .map_err(|e| SyncError::Protocol(e.to_string()))?;
+
+                    if inserted {
+                        let mut bytes = Vec::new();
+                        ciborium::into_writer(&(operation.header, operation.body), &mut bytes)
+                            .map_err(|e| SyncError::Protocol(e.to_string()))?;
+                        app_tx.send(AppMessage::Bytes(bytes)).await?;
+                    }
+                }
+                Message::SyncDone => {
+                    sync_done_received = true;
+                }
+            };
+
+            if sync_done_received && sync_done_sent {
+                break;
+            }
+        }
+
+        // @NOTE: It's important to call this method before the streams are dropped, it makes
+        // sure all bytes are flushed from the sink before closing so that no messages are
+        // lost.
+        sink.flush().await?;
+        sink.close().await?;
+        debug!("sync session finished");
+
+        Ok(())
+    }
+
+    async fn accept(
+        self: Arc<Self>,
+        tx: Box<dyn AsyncWrite + Send + Unpin>,
+        rx: Box<dyn AsyncRead + Send + Unpin>,
+        _app_tx: Box<dyn Sink<AppMessage, Error = SyncError> + Send + Unpin>,
+    ) -> Result<(), SyncError> {
+        let mut sync_done_sent = false;
+        let mut sync_done_received = false;
+
+        let mut sink = into_sink(tx);
+        let mut stream = into_stream(rx);
+
+        while let Some(result) = stream.next().await {
+            let message: Message<E> = result?;
+            debug!("message received: {:?}", message);
+
+            let mut replies = match &message {
+                Message::Have(log_id, log_heights) => {
                     let mut messages = vec![];
 
                     let local_log_heights = self
@@ -141,36 +221,22 @@ impl SyncProtocol for LogHeightSyncProtocol {
                         }
                     }
 
-                    // As we have processed the remotes `Have` message then we are "done" from
-                    // this end.
-                    messages.push(Message::SyncDone);
-                    sync_done_sent = true;
                     messages
                 }
-                Message::Operation(header, body) => {
-                    let operation = Operation {
-                        hash: header.hash(),
-                        header: header.clone(),
-                        body: body.clone(),
-                    };
-                    let inserted = self
-                        .write_store()
-                        .insert_operation(operation.clone(), log_id.to_string())
-                        .map_err(|e| SyncError::Protocol(e.to_string()))?;
-
-                    if inserted {
-                        let mut bytes = Vec::new();
-                        ciborium::into_writer(&(operation.header, operation.body), &mut bytes)
-                            .map_err(|e| SyncError::Protocol(e.to_string()))?;
-                        app_tx.send(AppMessage::Bytes(bytes)).await?;
-                    }
-                    vec![]
+                Message::Operation(_, _) => {
+                    return Err(SyncError::Protocol(
+                        "unexpected operation received".to_string(),
+                    ));
                 }
                 Message::SyncDone => {
                     sync_done_received = true;
                     vec![]
                 }
             };
+            // As we have processed the remotes `Have` message then we are "done" from
+            // this end.
+            replies.push(Message::SyncDone);
+            sync_done_sent = true;
 
             // @TODO: we'd rather process all messages at once using `send_all`. For this
             // we need to turn `replies` into a stream.
@@ -183,19 +249,14 @@ impl SyncProtocol for LogHeightSyncProtocol {
             }
         }
 
+        // @NOTE: It's important to call this method before the streams are dropped, it makes
+        // sure all bytes are flushed from the sink before closing so that no messages are
+        // lost.
+        sink.flush().await?;
         sink.close().await?;
         debug!("sync session finished");
 
         Ok(())
-    }
-
-    async fn accept(
-        self: Arc<Self>,
-        tx: Box<dyn AsyncWrite + Send + Unpin>,
-        rx: Box<dyn AsyncRead + Send + Unpin>,
-        mut app_tx: Box<dyn Sink<AppMessage, Error = SyncError> + Send + Unpin>,
-    ) -> Result<(), SyncError> {
-        todo!()
     }
 }
 
@@ -292,7 +353,7 @@ mod tests {
 
         // Write some message into peer_b's send buffer
         let message1: Message<DefaultExtensions> =
-            Message::Have(vec![(private_key.public_key(), 0)]);
+            Message::Have(LOG_ID.to_string(), vec![(private_key.public_key(), 0)]);
         let message2: Message<DefaultExtensions> = Message::SyncDone;
         let message_bytes = vec![message1.to_bytes(), message2.to_bytes()].concat();
         peer_b.write_all(&message_bytes[..]).await.unwrap();
@@ -321,10 +382,6 @@ mod tests {
         let mut buf = Vec::new();
         peer_b.read_to_end(&mut buf).await.unwrap();
 
-        // It should contain the following two sync messages (these are the ones peer_b is
-        // missing)
-        let received_message0 =
-            Message::<DefaultExtensions>::Have(vec![(private_key.public_key(), 2)]);
         let received_message1 =
             Message::Operation(operation1.header.clone(), operation1.body.clone());
         let received_message2 =
@@ -333,7 +390,6 @@ mod tests {
         assert_eq!(
             buf,
             [
-                received_message0.to_bytes(),
                 received_message1.to_bytes(),
                 received_message2.to_bytes(),
                 receive_message3.to_bytes()
@@ -348,38 +404,7 @@ mod tests {
         const LOG_ID: &str = "messages";
 
         // Create a store for peer a and populate it with operations
-        let mut store1 = MemoryStore::default();
-
-        let private_key1 = PrivateKey::new();
-        let body = Body::new("Hello, Sloth!".as_bytes());
-        let operation0 = generate_operation(&private_key1, body.clone(), 0, 0, None, None);
-        let operation1 = generate_operation(
-            &private_key1,
-            body.clone(),
-            1,
-            100,
-            Some(operation0.hash),
-            None,
-        );
-        let operation2 = generate_operation(
-            &private_key1,
-            body.clone(),
-            2,
-            200,
-            Some(operation1.hash),
-            None,
-        );
-
-        // Insert these operations to the store using `TOPIC_ID` as the log id
-        store1
-            .insert_operation(operation0.clone(), LOG_ID.to_string())
-            .unwrap();
-        store1
-            .insert_operation(operation1.clone(), LOG_ID.to_string())
-            .unwrap();
-        store1
-            .insert_operation(operation2.clone(), LOG_ID.to_string())
-            .unwrap();
+        let store1 = MemoryStore::default();
 
         // Construct a log height protocol and engine for peer a
         let peer_a_protocol = Arc::new(LogHeightSyncProtocol {
@@ -388,7 +413,39 @@ mod tests {
         });
 
         // Create an empty store for peer a and construct their sync protocol and engine
-        let store2 = MemoryStore::default();
+        let mut store2 = MemoryStore::default();
+
+        let private_key = PrivateKey::new();
+        let body = Body::new("Hello, Sloth!".as_bytes());
+        let operation0 = generate_operation(&private_key, body.clone(), 0, 0, None, None);
+        let operation1 = generate_operation(
+            &private_key,
+            body.clone(),
+            1,
+            100,
+            Some(operation0.hash),
+            None,
+        );
+        let operation2 = generate_operation(
+            &private_key,
+            body.clone(),
+            2,
+            200,
+            Some(operation1.hash),
+            None,
+        );
+
+        // Insert these operations to the store using `TOPIC_ID` as the log id
+        store2
+            .insert_operation(operation0.clone(), LOG_ID.to_string())
+            .unwrap();
+        store2
+            .insert_operation(operation1.clone(), LOG_ID.to_string())
+            .unwrap();
+        store2
+            .insert_operation(operation2.clone(), LOG_ID.to_string())
+            .unwrap();
+
         // Construct a log height protocol and engine for peer a
         let peer_b_protocol = Arc::new(LogHeightSyncProtocol {
             store: Arc::new(RwLock::new(store2)),
