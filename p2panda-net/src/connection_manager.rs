cf4d219d1cbf28ced9f366fd509aeb760e554aad
glyph <glyph@mycelial.technology>
2024-09-17 09:00:45 +0100

    Introduce additional connection handling methods

--- p2panda-net/src/connection_manager.rs
+++ p2panda-net/src/connection_manager.rs
@@ -8,7 +8,7 @@
 // - Handle inbound peer connections
 // - Invoke sync sessions
 // - Record successful sync sessions
-//   - I think...
+//   - I think...this may be a concern of the sync actor
 // - Disconnect cleanly
 //
 // Second-pass features:
@@ -25,7 +25,7 @@ use futures_lite::future::Boxed as BoxedFuture;
 use iroh_gossip::proto::TopicId;
 use iroh_net::endpoint::{self, Connecting, Connection};
 use iroh_net::{Endpoint, NodeId};
-use tracing::debug_span;
+use tracing::{debug, debug_span, warn};
 
 use crate::protocols::ProtocolHandler;
 
@@ -39,6 +39,7 @@ pub const SYNC_CONNECTION_ALPN: &[u8] = b"/p2panda-net-sync/0";
 struct ConnectionManager {
     active_connections: HashSet<NodeId>,
     address_book: HashMap<NodeId, TopicId>,
+    completed_sync_sessions: HashSet<NodeId>,
     endpoint: Endpoint,
 }
 
@@ -47,23 +48,39 @@ impl ConnectionManager {
         Self {
             active_connections: HashSet::new(),
             address_book: HashMap::new(),
+            completed_sync_sessions: HashSet::new(),
             endpoint,
         }
     }
 
-    pub async fn connect(&mut self, peer: NodeId) -> Result<Connection> {
-        let connection = self
-            .endpoint
-            .connect_by_node_id(peer, SYNC_CONNECTION_ALPN)
-            .await?;
+    /// Attempt to connect with the given peer.
+    ///
+    /// A `None` value will be returned if a connection has already been established and is
+    /// currently active.
+    pub async fn connect(&mut self, peer: NodeId) -> Result<Option<Connection>> {
+        if self.currently_connected(&peer) {
+            Ok(None)
+        } else {
+            let connection = self
+                .endpoint
+                .connect_by_node_id(peer, SYNC_CONNECTION_ALPN)
+                .await?;
 
-        self.active_connections.insert(peer);
+            self.active_connections.insert(peer);
 
-        Ok(connection)
+            if let Err(err) = self.listen_for_disconnection(connection.clone()).await {
+                warn!("failed to spawn disconnection listener: {err}")
+            }
+
+            Ok(Some(connection))
+        }
     }
 
+    /// Close the given connection and remove the associated peer from the set of active
+    /// connections.
     pub fn disconnect(&mut self, connection: Connection) -> Result<()> {
         // @NOTE: Only call this after any related `SendStream` has been flushed with `finish()`.
+        // Calling `flush()` is the responsibility of the sync engine / actor.
         connection.close(0u8.into(), b"close from disconnect");
 
         // @TODO: Handle the case where the connection is not explicitly closed, but has closed due
@@ -76,17 +93,67 @@ impl ConnectionManager {
         Ok(())
     }
 
-    pub async fn handle_connection(&self, connection: Connection) -> Result<()> {
-        let peer = endpoint::get_remote_node_id(&connection)?;
-        let remote_addr = connection.remote_address();
-        let connection_id = connection.stable_id() as u64;
-        let _span = debug_span!("connection", connection_id, %remote_addr);
+    /// Listen for closure of the connection; this may occur due to an error or because of an
+    /// action taken by the remote peer.
+    async fn listen_for_disconnection(&self, connection: Connection) -> Result<()> {
+        tokio::task::spawn(async move {
+            let reason = connection.closed().await;
+            debug!("sync connection closed: {reason}");
 
-        // @TODO: Send the connection to the sync actor.
-        //
-        // self.engine_actor_tx
-        //    .send(ToEngineActor::SyncAccept { peer, connection })
-        //    .await?;
+            // @TODO: Send `Disconnect` event on local message bus.
+        })
+        .await?;
+
+        Ok(())
+    }
+
+    /// Log sync as completed for the given peer.
+    fn complete_sync(&mut self, peer: NodeId) {
+        // Ignore the returned `bool`.
+        let _ = self.completed_sync_sessions.insert(peer);
+    }
+
+    /// Query the sync state of the given peer.
+    fn sync_completed(&self, peer: &NodeId) -> bool {
+        self.completed_sync_sessions.contains(peer)
+    }
+
+    /// Query the connection state of the given peer.
+    fn currently_connected(&self, peer: &NodeId) -> bool {
+        self.active_connections.contains(peer)
+    }
+
+    /// Handle an inbound connection attempt for the SYNC_CONNECTION ALPN.
+    ///
+    /// The connection will be dropped if one has already been established and is currently active.
+    ///
+    /// The sync actor will be invoked with the connection if a successful sync session has not
+    /// previously been completed with the connecting peer.
+    pub async fn handle_connection(&mut self, connection: Connection) -> Result<()> {
+        let peer = endpoint::get_remote_node_id(&connection)?;
+
+        if self.currently_connected(&peer) {
+            self.disconnect(connection)?
+        } else {
+            let remote_addr = connection.remote_address();
+            let connection_id = connection.stable_id() as u64;
+            let _span = debug_span!("sync connection", connection_id, %remote_addr);
+
+            // @TODO: Consider using a `Connected` event to avoid issues around mutability of
+            // `self`.
+
+            if !self.sync_completed(&peer) {
+                self.active_connections.insert(peer);
+
+                // @TODO: Spawn `closed()` listener.
+
+                // @TODO: Send the connection to the sync actor.
+                //
+                // self.engine_actor_tx
+                //    .send(ToEngineActor::SyncAccept { peer, connection })
+                //    .await?;
+            }
+        }
 
         Ok(())
     }
