b5d21eb132895157d7b10f4b6547addb152f1522
adz <x12@adz.garden>
2024-09-18 20:41:28 +0200

    Ingest method to validate, persist and prune, or retry

--- p2panda-engine/src/operation.rs
+++ p2panda-engine/src/operation.rs
@@ -1,9 +1,14 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 use ciborium::de::Error as CiboriumError;
-use p2panda_core::{Body, Header};
-use serde::de::DeserializeOwned;
+use p2panda_core::{
+    validate_backlink, validate_operation, Body, Extension, Header, Operation, OperationError,
+};
+use p2panda_store::{LogStore, OperationStore, StoreError};
+use serde::{de::DeserializeOwned, Serialize};
 use thiserror::Error;
+
+use crate::{PruneFlag, StreamName};
 
 /// Encoded bytes of an operation header and optional body.
 pub type RawOperation = (Vec<u8>, Option<Vec<u8>>);
@@ -63,4 +68,128 @@ impl From<CiboriumError<std::io::Error>> for DecodeError {
             CiboriumError::RecursionLimitExceeded => DecodeError::RecursionLimitExceeded,
         }
     }
+}
+
+#[derive(Debug)]
+pub enum IngestResult<E> {
+    /// Operation has been successfully validated and persisted.
+    Complete(Operation<E>),
+
+    /// We're missing previous operations before we can try validating the backlink of this
+    /// operation.
+    ///
+    /// The number indicates how many operations we are lacking before we can attempt validation
+    /// again.
+    Retry(Operation<E>, u64),
+}
+
+/// Checks an incoming operation for log integrity and persists it into the store when valid.
+///
+/// This method also automatically prunes the log when a prune flag was set in the header.
+///
+/// If the operation seems valid but we're still lacking information (as it might have arrived
+/// out-of-order) this method does not fail but indicates that we might have to retry again later.
+///
+/// The trait bounds requires the operation header to contain a prune flag and stream name as
+/// specified by the core p2panda specification.
+pub async fn ingest_operation<S, L, E>(
+    store: &mut S,
+    header: Header<E>,
+    body: Option<Body>,
+) -> Result<IngestResult<E>, IngestError>
+where
+    S: OperationStore<StreamName, E> + LogStore<StreamName, E>,
+    E: Clone + Serialize + DeserializeOwned + Extension<StreamName> + Extension<PruneFlag>,
+{
+    let operation = Operation {
+        hash: header.hash(),
+        header,
+        body,
+    };
+
+    if let Err(err) = validate_operation(&operation) {
+        return Err(IngestError::InvalidOperation(err));
+    }
+
+    let already_exists = store.get_operation(operation.hash).await?.is_some();
+    if !already_exists {
+        let stream_name: StreamName = operation
+            .header
+            .extract()
+            .ok_or(IngestError::MissingHeaderExtension("stream_name".into()))?;
+        let prune_flag: PruneFlag = operation
+            .header
+            .extract()
+            .ok_or(IngestError::MissingHeaderExtension("prune_flag".into()))?;
+
+        // If no pruning flag is set, we expect the log to have integrity with the previously given
+        // operation
+        // @TODO: Move this into `p2panda-core`
+        if !prune_flag.is_set() && operation.header.seq_num > 0 {
+            let latest_operation = store
+                .latest_operation(&operation.header.public_key, &stream_name)
+                .await?;
+
+            match latest_operation {
+                Some(latest_operation) => {
+                    if let Err(err) = validate_backlink(&latest_operation.header, &operation.header)
+                    {
+                        match err {
+                            // These errors signify that the sequence number is monotonic
+                            // incrementing and correct, however the backlink does not match
+                            OperationError::BacklinkMismatch
+                            | OperationError::BacklinkMissing
+                            // Log can only contain operations from one author
+                            | OperationError::TooManyAuthors => {
+                                return Err(IngestError::InvalidOperation(err))
+                            }
+                            // We observe a gap in the log and therefore can't validate the
+                            // backlink yet
+                            OperationError::SeqNumNonIncremental(expected, given) => {
+                                return Ok(IngestResult::Retry(operation, expected - given))
+                            }
+                            _ => unreachable!("other error cases have been handled before"),
+                        }
+                    }
+                }
+                // We're missing the whole log so far
+                None => {
+                    return Ok(IngestResult::Retry(
+                        operation.clone(),
+                        operation.header.seq_num,
+                    ))
+                }
+            }
+        }
+
+        store.insert_operation(&operation, &stream_name).await?;
+
+        if prune_flag.is_set() {
+            store
+                .delete_operations(
+                    &operation.header.public_key,
+                    &stream_name,
+                    operation.header.seq_num,
+                )
+                .await?;
+        }
+    }
+
+    Ok(IngestResult::Complete(operation))
+}
+
+#[derive(Debug, Error)]
+pub enum IngestError {
+    /// Operation can not be authenticated, has broken log- or payload integrity or doesn't follow
+    /// the p2panda specification.
+    #[error("operation validation failed: {0}")]
+    InvalidOperation(OperationError),
+
+    /// Header did not contain the extensions required by the p2panda specification.
+    #[error("missing \"{0}\" extension in header")]
+    MissingHeaderExtension(String),
+
+    /// Critical storage failure occurred. This is usually a reason to panic.
+    #[error(transparent)]
+    StoreError(#[from] StoreError),
 }
