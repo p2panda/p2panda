aa11cfcddbfaf87053e919c558963b3012588a96
adz <x12@adz.garden>
2024-09-18 20:42:43 +0200

    Expose stream methods

--- p2panda-engine/src/stream/mod.rs
+++ p2panda-engine/src/stream/mod.rs
@@ -1,4 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-pub mod decode;
-pub mod ingest;
+mod decode;
+mod ingest;
+
+pub use decode::{Decode, DecodeExt};
+pub use ingest::{Ingest, IngestExt};
