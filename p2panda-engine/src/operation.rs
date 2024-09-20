654a531c76cdcd98a0a25fbef46bd231ff0f16af
adz <x12@adz.garden>
2024-09-20 14:03:34 +0200

    WIP: Count retry attempts and throw error when max was reached

--- p2panda-engine/src/operation.rs
+++ p2panda-engine/src/operation.rs
@@ -97,7 +97,7 @@ pub async fn ingest_operation<S, L, E>(
     store: &mut S,
     header: Header<E>,
     body: Option<Body>,
-) -> Result<IngestResult<E>, IngestError>
+) -> Result<IngestResult<E>, IngestError<E>>
 where
     S: OperationStore<L, E> + LogStore<L, E>,
     E: Clone + Serialize + DeserializeOwned + Extension<L> + Extension<PruneFlag>,
@@ -181,7 +181,7 @@ where
 }
 
 #[derive(Debug, Error)]
-pub enum IngestError {
+pub enum IngestError<E> {
     /// Operation can not be authenticated, has broken log- or payload integrity or doesn't follow
     /// the p2panda specification.
     #[error("operation validation failed: {0}")]
@@ -194,6 +194,9 @@ pub enum IngestError {
     /// Critical storage failure occurred. This is usually a reason to panic.
     #[error(transparent)]
     StoreError(#[from] StoreError),
+
+    #[error("too many attempts to ingest out-of-order operation ({2} behind in log)")]
+    MaxAttemptsReached(Header<E>, Option<Body>, u64),
 }
 
 #[cfg(test)]
--- p2panda-engine/src/stream/ingest.rs
+++ p2panda-engine/src/stream/ingest.rs
@@ -7,7 +7,7 @@ use std::pin::Pin;
 use futures_channel::mpsc::{self};
 use futures_util::stream::{Fuse, FusedStream};
 use futures_util::task::{Context, Poll};
-use futures_util::{ready, Sink, Stream, StreamExt};
+use futures_util::{ready, select, Sink, Stream, StreamExt};
 use p2panda_core::{Body, Extension, Header, Operation};
 use p2panda_store::{LogStore, OperationStore};
 use pin_project::pin_project;
@@ -20,13 +20,13 @@ use crate::macros::{delegate_access_inner, delegate_sink};
 use crate::operation::{ingest_operation, IngestError, IngestResult};
 
 pub trait IngestExt<S, L, E>: Stream<Item = (Header<E>, Option<Body>)> {
-    fn ingest(self, store: S, ooo_buffer_size: usize) -> Ingest<Self, S, L, E>
+    fn ingest(self, store: S, ooo_buffer_size: usize, max_attempts: usize) -> Ingest<Self, S, L, E>
     where
         S: OperationStore<L, E> + LogStore<L, E>,
         E: Clone + Serialize + DeserializeOwned + Extension<L> + Extension<PruneFlag>,
         Self: Sized,
     {
-        Ingest::new(self, store, ooo_buffer_size)
+        Ingest::new(self, store, ooo_buffer_size, max_attempts)
     }
 }
 
@@ -44,9 +44,10 @@ where
     #[pin]
     stream: Fuse<St>,
     store: S,
-    ooo_buffer_tx: mpsc::Sender<(Header<E>, Option<Body>)>,
+    ooo_buffer_tx: mpsc::Sender<IngestAttempt<E>>,
     #[pin]
-    ooo_buffer_rx: mpsc::Receiver<(Header<E>, Option<Body>)>,
+    ooo_buffer_rx: mpsc::Receiver<IngestAttempt<E>>,
+    max_attempts: usize,
     _marker: PhantomData<L>,
 }
 
@@ -56,15 +57,22 @@ where
     S: OperationStore<L, E> + LogStore<L, E>,
     E: Clone + Serialize + DeserializeOwned + Extension<L> + Extension<PruneFlag>,
 {
-    pub(super) fn new(stream: St, store: S, ooo_buffer_size: usize) -> Ingest<St, S, L, E> {
-        let (ooo_buffer_tx, ooo_buffer_rx) =
-            mpsc::channel::<(Header<E>, Option<Body>)>(ooo_buffer_size);
+    pub(super) fn new(
+        stream: St,
+        store: S,
+        ooo_buffer_size: usize,
+        max_attempts: usize,
+    ) -> Ingest<St, S, L, E> {
+        let (ooo_buffer_tx, ooo_buffer_rx) = mpsc::channel::<IngestAttempt<E>>(ooo_buffer_size);
+
+        let stream = select! {};
 
         Ingest {
             store,
             stream: stream.fuse(),
             ooo_buffer_tx,
             ooo_buffer_rx,
+            max_attempts,
             _marker: PhantomData,
         }
     }
@@ -78,9 +86,10 @@ where
     S: Clone + OperationStore<L, E> + LogStore<L, E>,
     E: Clone + Serialize + DeserializeOwned + Extension<L> + Extension<PruneFlag>,
 {
-    type Item = Result<Operation<E>, IngestError>;
+    type Item = Result<Operation<E>, IngestError<E>>;
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        let max_attempts = self.max_attempts;
         let mut store = self.store.clone();
         let mut this = self.project();
 
@@ -89,17 +98,17 @@ where
             //    terminated, check the internal out-of-order buffer next. We always prefer pulling
             //    from the external stream first as freshly incoming data should be prioritized.
             let res = match this.stream.as_mut().poll_next(cx) {
-                Poll::Ready(Some((header, body))) => Some((header, body)),
+                Poll::Ready(Some((header, body))) => Some(IngestAttempt(header, body, 1)),
                 Poll::Pending => ready!(this.ooo_buffer_rx.as_mut().poll_next(cx)),
                 Poll::Ready(None) => match this.ooo_buffer_rx.as_mut().poll_next(cx) {
-                    Poll::Ready(Some((header, body))) => Some((header, body)),
+                    Poll::Ready(Some(attempt)) => Some(attempt),
                     // If there's no value coming from the buffer _and_ the external stream is
                     // terminated, we can be sure nothing will come anymore
                     Poll::Pending => None,
                     Poll::Ready(None) => None,
                 },
             };
-            let Some((header, body)) = res else {
+            let Some(IngestAttempt(header, body, counter)) = res else {
                 // Both external stream and buffer stream has ended, so we stop here as well
                 return Poll::Ready(None);
             };
@@ -111,24 +120,36 @@ where
             let ingest_res = ready!(ingest_fut.poll(cx));
 
             // 3. If the operation arrived out-of-order we can push it back into the internal
-            //    buffer and try again later, otherwise forward the result of ingest to the
-            //    consumer.
+            //    buffer and try again later (attempted for a configured number of times),
+            //    otherwise forward the result of ingest to the consumer.
             match ingest_res {
-                // @TODO: Remove the item from buffer if we've tried for too many times
-                Ok(IngestResult::Retry(header, body, _)) => {
+                Ok(IngestResult::Retry(header, body, num_missing)) => {
+                    println!("retry {num_missing} {counter}");
+                    if counter > max_attempts {
+                        return Poll::Ready(Some(Err(IngestError::MaxAttemptsReached(
+                            header,
+                            body,
+                            num_missing,
+                        ))));
+                    }
+
                     // Push operation back into the internal queue, if something goes wrong here
                     // this must be an critical failure
                     let Ok(_) = ready!(this.ooo_buffer_tx.poll_ready(cx)) else {
                         break Poll::Ready(None);
                     };
 
-                    let Ok(_) = this.ooo_buffer_tx.start_send((header, body)) else {
+                    let Ok(_) =
+                        this.ooo_buffer_tx
+                            .start_send(IngestAttempt(header, body, counter + 1))
+                    else {
                         break Poll::Ready(None);
                     };
 
                     continue;
                 }
                 Ok(IngestResult::Complete(operation)) => {
+                    println!("complete!");
                     return Poll::Ready(Some(Ok(operation)));
                 }
                 Err(err) => {
@@ -162,6 +183,9 @@ where
     delegate_sink!(stream, (Header<E>, Option<Body>));
 }
 
+#[derive(Debug)]
+struct IngestAttempt<E>(Header<E>, Option<Body>, usize);
+
 #[cfg(test)]
 mod tests {
     use futures_util::stream::iter;
@@ -190,15 +214,16 @@ mod tests {
                     Err(_) => None,
                 }
             })
-            .ingest(store, 16);
+            .ingest(store, 16, 5);
 
-        let res: Result<Vec<Operation<Extensions>>, IngestError> = stream.try_collect().await;
+        let res: Result<Vec<Operation<Extensions>>, IngestError<Extensions>> =
+            stream.try_collect().await;
         assert!(res.is_ok());
     }
 
     #[tokio::test]
     async fn out_of_order() {
-        let items_num = 10;
+        let items_num = 100;
         let store = MemoryStore::<StreamName, Extensions>::new();
 
         let mut items: Vec<RawOperation> = mock_stream().take(items_num).collect().await;
@@ -212,7 +237,7 @@ mod tests {
                     Err(_) => None,
                 }
             })
-            .ingest(store, 32);
+            .ingest(store, 32, 5);
 
         let res: Vec<Operation<Extensions>> = stream.try_collect().await.expect("not fail");
         assert_eq!(res.len(), items_num);
