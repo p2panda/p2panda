ae7b7a7810afe117e7779a5beaf497c0816f53e3
adz <x12@adz.garden>
2024-10-15 17:36:49 +0200

    Move MemoryStore begind a feature flag

--- p2panda-store/src/lib.rs
+++ p2panda-store/src/lib.rs
@@ -1,7 +1,12 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+#[cfg(feature = "memory")]
 pub mod memory_store;
 pub mod traits;
 
+#[cfg(feature = "memory")]
 pub use memory_store::MemoryStore;
-pub use traits::{LogStore, OperationStore, RawStore, StoreError};
+pub use traits::{
+    LocalLogStore, LocalOperationStore, LocalRawStore, LogStore, OperationStore, RawStore,
+    StoreError,
+};
