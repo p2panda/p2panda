c4e5954a3700752a152e770efd124c6f02589910
adz <x12@adz.garden>
2024-10-18 18:30:21 +0200

    Move stream behind a default feature flag

--- p2panda-engine/src/lib.rs
+++ p2panda-engine/src/lib.rs
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 pub mod extensions;
+#[cfg(feature = "stream")]
 mod macros;
 pub mod operation;
+#[cfg(feature = "stream")]
 mod stream;
 #[cfg(test)]
 mod test_utils;
 
+#[cfg(feature = "stream")]
 pub use stream::*;
