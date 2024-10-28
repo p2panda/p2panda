8b1c5ee05378313ad49a538e42e57eae8640a523
glyph <glyph@mycelial.technology>
2024-10-28 07:35:43 +0000

    Add doc strings for types exposed by the public API

--- p2panda-net/src/lib.rs
+++ p2panda-net/src/lib.rs
@@ -18,5 +18,7 @@ pub use tokio_util::task::AbortOnDropHandle;
 #[cfg(feature = "log-sync")]
 pub use p2panda_sync::log_sync::LogSyncProtocol;
 
+/// A unique 32 byte identifier for a network.
 pub type NetworkId = [u8; 32];
+/// A unique 32 byte identifier for a gossip topic.
 pub type TopicId = [u8; 32];
