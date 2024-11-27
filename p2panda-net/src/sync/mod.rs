9fe52818907b847458600dfeab42b5da1549f72e
adz <x12@adz.garden>
2024-11-27 16:03:13 +0100

    Add more comments, improve error handling and await task result

--- p2panda-net/src/sync/mod.rs
+++ p2panda-net/src/sync/mod.rs
@@ -6,6 +6,7 @@ pub(crate) mod manager;
 
 pub use config::{ResyncConfiguration, SyncConfiguration};
 pub use handler::{SyncConnection, SYNC_CONNECTION_ALPN};
+use tokio::task::JoinHandle;
 
 use std::sync::Arc;
 
@@ -152,35 +153,41 @@ where
 {
     debug!("accept sync session with peer {}", peer);
 
-    // Set up a channel for receiving new application messages.
+    // Set up a channel for receiving messages from the sync session.
     let (tx, mut rx) = mpsc::channel::<FromSync<T>>(128);
     let mut sink = PollSender::new(tx).sink_map_err(|e| SyncError::Critical(e.to_string()));
 
-    // Set up a channel for sending over errors to the task which happened during sync.
+    // Set up a channel for sending over errors to the "glue" task which occurred during sync.
     let (sync_error_tx, mut sync_error_rx) = oneshot::channel::<SyncError>();
 
-    // Spawn a task which picks up any new application messages and sends them on to the engine
-    // for handling.
-    tokio::spawn(async move {
+    // Spawn a "glue" task which represents the layer between the sync session and the engine.
+    //
+    // It picks up any messages from the sync session makes sure that the "Two-Phase Sync Flow" is
+    // followed (I. "Handshake" Phase & II. "Data Sync" Phase) and the engine accordingly informed
+    // about it.
+    //
+    // If the task detects any invalid behaviour of the sync flow, it fails critically, indicating
+    // that the sync protocol implementation does not behave correctly and is not compatible with
+    // the engine.
+    //
+    // Additionally, the task forwards any synced application data straight to the engine.
+    let glue_task_handle: JoinHandle<Result<(), SyncError>> = tokio::spawn(async move {
         let mut topic = None;
 
         loop {
             tokio::select! {
                 biased;
+
                 Some(message) = rx.recv() => {
-                    // 1. Handshake Phase.
-                    // ~~~~~~~~~~~~~~~~~~~
+                    // I. Handshake Phase.
                     //
-                    // At the beginning of every sync session the "accepting" peer needs to learn the
-                    // topic of the "initiating" peer during the handshake phase. This is the first
-                    // message we're expecting:
-                    if let FromSync::HandshakeSuccess(handshake_topic) = &message {
+                    // At the beginning of every sync session the "accepting" peer needs to learn
+                    // the topic of the "initiating" peer during the handshake phase. This is
+                    // _always_ the first message we're expecting:
+                    if let FromSync::HandshakeSuccess(handshake_topic) = message {
                         // It should only be sent once so topic should be `None` now.
                         if topic.is_some() {
-                            // @TODO(glyph): In the future we should either notify the engine actor
-                            // of this failure directly or return the error to the upstream caller.
-                            error!("sync protocol violation: received handshake message twice from peer {}", peer);
-                            break;
+                            return Err(SyncError::Critical("received handshake message twice from sync session in handshake phase".into()));
                         }
 
                         topic = Some(handshake_topic.clone());
@@ -190,28 +197,35 @@ where
                         engine_actor_tx
                             .send(ToEngineActor::SyncHandshakeSuccess {
                                 peer,
-                                topic: handshake_topic.clone(),
+                                topic: handshake_topic,
                             })
                             .await
-                            .expect("engine channel closed");
+                            .map_err(|err| SyncError::Critical(format!("engine_actor_tx failed: {err}")))?;
 
                         continue;
                     }
 
-                    // 2. Data Sync Phase.
-                    // ~~~~~~~~~~~~~~~~~~~
+                    // II. Data Sync Phase.
                     //
+                    // At this stage we're beginning the actual "sync" protocol and expect messages
+                    // containing the data which was received from the "initiating" peer.
+                    //
+                    // Please note that in not all sync implementations the "accepting" peers
+                    // receives data.
+
                     // The topic must be known at this point in order to process further messages.
+                    //
+                    // Any sync protocol implementation should have already failed with an
+                    // "unexpected behaviour" error if the topic wasn't learned. If this didn't
+                    // happen (due to an incorrect implementation) we will critically fail now.
                     let Some(topic) = &topic else {
-                        error!("sync protocol violation: topic not received from peer {}", peer);
-                        return;
+                        return Err(SyncError::Critical("never received handshake message from sync session in handshake phase".into()));
                     };
 
+                    // From this point on we are only expecting "data" messages from the sync
+                    // session.
                     let FromSync::Data(header, payload) = message else {
-                        // @TODO(glyph): In the future we should either notify the engine actor
-                        // of this failure directly or return the error to the upstream caller.
-                        error!("sync protocol violation: expected message bytes from peer {}", peer);
-                        return;
+                        return Err(SyncError::Critical("expected to receive only data messages from sync session in data sync phase".into()));
                     };
 
                     engine_actor_tx
@@ -222,37 +236,50 @@ where
                             topic: topic.clone(),
                         })
                         .await
-                        .expect("engine channel closed");
+                        .map_err(|err| SyncError::Critical(format!("engine_actor_tx failed: {err}")))?;
                 },
-                Ok(_) = &mut sync_error_rx => {
+                Ok(err) = &mut sync_error_rx => {
                     engine_actor_tx
                         .send(ToEngineActor::SyncFailed {
                             peer,
                             topic: topic.clone(),
                         })
                         .await
-                        .expect("engine channel closed");
-                    return;
+                        .map_err(|err| SyncError::Critical(format!("engine_actor_tx failed: {err}")))?;
+
+                    // If we're observing a critical error we terminate the task here and propagate
+                    // that error further up.
+                    //
+                    // For any other error we're trusting the sync protocol implementation to
+                    // properly wind down.
+                    if let SyncError::Critical(err) = err {
+                        return Err(SyncError::Critical(err));
+                    } else {
+                        return Ok(());
+                    }
                 },
                 else => {
+                    // Stream from sync session got terminated.
                     break;
                 }
             }
         }
 
         // If topic was never set then we didn't receive any messages. In that case, the engine
-        // wasn't informed it should buffer messages and so we can return here.
+        // wasn't ever informed, so we can return here silently.
         let Some(topic) = topic else {
-            return;
+            return Ok(());
         };
 
         engine_actor_tx
             .send(ToEngineActor::SyncDone { peer, topic })
             .await
-            .expect("engine channel closed");
+            .map_err(|err| SyncError::Critical(format!("engine_actor_tx failed: {err}")))?;
+
+        Ok(())
     });
 
-    // Run the sync protocol.
+    // Run the "accepting peer" side of the sync protocol.
     let result = sync_protocol
         .accept(
             Box::new(&mut send),
@@ -261,10 +288,25 @@ where
         )
         .await;
 
-    if let Err(sync_err) = result {
+    // The sync protocol failed and we're informing the "glue" task about it, so it can accordingly
+    // wind down and inform the engine about it.
+    if let Err(sync_session_err) = result {
         sync_error_tx
-            .send(sync_err)
-            .expect("error oneshot sender failed");
+            .send(sync_session_err)
+            .map_err(|err| SyncError::Critical(format!("sync_error_tx failed: {err}")))?;
+
+        // We're expecting the task to exit with a result soon, we're awaiting it here ..
+        let glue_task_result = glue_task_handle
+            .await
+            .map_err(|err| SyncError::Critical(format!("glue task handle failed: {err}")))?;
+
+        // .. to inform some brave developer who will read the error logs ..
+        if let Err(SyncError::Critical(err)) = &glue_task_result {
+            error!("critical error in sync protocol: {err}");
+        }
+
+        // .. and to forward it further!
+        glue_task_result?;
     }
 
     Ok(())
@@ -290,24 +332,30 @@ mod sync_protocols {
         Done,
     }
 
-    /// A sync implementation which returns an error.
+    /// A sync implementation which returns a mocked error.
     #[derive(Debug)]
-    pub enum Protocol {
-        // A critical error is triggered inside `accept()` after sync messages have been exchanged.
+    pub enum FailingProtocol {
+        /// A critical error is triggered inside `accept()` after sync messages have been
+        /// exchanged.
         AcceptCritical,
-        // A critical error is triggered inside `initiate()` after the handshake is complete.
+
+        /// A critical error is triggered inside `initiate()` after the handshake is complete.
         InitiateCritical,
-        // An unexpected error is triggered inside `accept()` by sending the topic twice from
-        // `initiate()`.
+
+        /// An unexpected error is triggered inside `accept()` by sending the topic twice from
+        /// `initiate()`.
         AcceptUnexpectedBehaviour,
-        // An unexpected error is triggered inside `initiate()` by sending a topic from `accept()`.
+
+        /// An unexpected error is triggered inside `initiate()` by sending a topic from
+        /// `accept()`.
         InitiateUnexpectedBehaviour,
-        // No errors are explicitly triggered; used for "happy path" test.
+
+        /// No errors are explicitly triggered; used for "happy path" test.
         NoError,
     }
 
     #[async_trait]
-    impl<'a> SyncProtocol<'a, TestTopic> for Protocol {
+    impl<'a> SyncProtocol<'a, TestTopic> for FailingProtocol {
         fn name(&self) -> &'static str {
             static PROTOCOL_NAME: &str = "error_protocol";
             PROTOCOL_NAME
@@ -329,14 +377,14 @@ mod sync_protocols {
 
             sink.send(ProtocolMessage::Topic(topic.clone())).await?;
             // Simulate unexpected behaviour by sending the topic a second time.
-            if let Protocol::AcceptUnexpectedBehaviour = *self {
+            if let FailingProtocol::AcceptUnexpectedBehaviour = *self {
                 sink.send(ProtocolMessage::Topic(topic.clone())).await?;
             }
 
             sink.send(ProtocolMessage::Done).await?;
             app_tx.send(FromSync::HandshakeSuccess(topic)).await?;
 
-            if let Protocol::InitiateCritical = *self {
+            if let FailingProtocol::InitiateCritical = *self {
                 return Err(SyncError::Critical("initiator".to_string()));
             }
 
@@ -372,7 +420,7 @@ mod sync_protocols {
             let mut stream = into_cbor_stream(rx);
 
             // Simulate unexpected behaviour by sending the topic from the acceptor.
-            if let Protocol::InitiateUnexpectedBehaviour = *self {
+            if let FailingProtocol::InitiateUnexpectedBehaviour = *self {
                 let topic = TestTopic::new("unexpected behaviour test");
                 sink.send(ProtocolMessage::Topic(topic)).await?;
             }
@@ -398,7 +446,7 @@ mod sync_protocols {
                 }
             }
 
-            if let Protocol::AcceptCritical = *self {
+            if let FailingProtocol::AcceptCritical = *self {
                 return Err(SyncError::Critical("acceptor".to_string()));
             }
 
@@ -426,7 +474,7 @@ mod tests {
     use crate::engine::ToEngineActor;
     use crate::{sync, TopicId};
 
-    use super::sync_protocols::Protocol;
+    use super::sync_protocols::FailingProtocol;
 
     #[derive(Clone, Debug, PartialEq, Eq, Hash, Deserialize, Serialize)]
     pub struct TestTopic(String, [u8; 32]);
@@ -450,7 +498,7 @@ mod tests {
         let peer_a = NodeId::from_bytes(PrivateKey::new().public_key().as_bytes()).unwrap();
         let peer_b = NodeId::from_bytes(PrivateKey::new().public_key().as_bytes()).unwrap();
         let topic = TestTopic::new("critical error test");
-        let sync_protocol = Arc::new(Protocol::AcceptCritical);
+        let sync_protocol = Arc::new(FailingProtocol::AcceptCritical);
 
         // Duplex streams which simulate both ends of a bi-directional network connection.
         let (peer_a_stream, peer_b_stream) = tokio::io::duplex(64 * 1024);
@@ -529,7 +577,7 @@ mod tests {
         let peer_a = NodeId::from_bytes(PrivateKey::new().public_key().as_bytes()).unwrap();
         let peer_b = NodeId::from_bytes(PrivateKey::new().public_key().as_bytes()).unwrap();
         let topic = TestTopic::new("critical error test");
-        let sync_protocol = Arc::new(Protocol::AcceptUnexpectedBehaviour);
+        let sync_protocol = Arc::new(FailingProtocol::AcceptUnexpectedBehaviour);
 
         // Duplex streams which simulate both ends of a bi-directional network connection.
         let (peer_a_stream, peer_b_stream) = tokio::io::duplex(64 * 1024);
@@ -608,7 +656,7 @@ mod tests {
         let peer_a = NodeId::from_bytes(PrivateKey::new().public_key().as_bytes()).unwrap();
         let peer_b = NodeId::from_bytes(PrivateKey::new().public_key().as_bytes()).unwrap();
         let topic = TestTopic::new("critical error test");
-        let sync_protocol = Arc::new(Protocol::InitiateCritical);
+        let sync_protocol = Arc::new(FailingProtocol::InitiateCritical);
 
         // Duplex streams which simulate both ends of a bi-directional network connection.
         let (peer_a_stream, peer_b_stream) = tokio::io::duplex(64 * 1024);
@@ -696,7 +744,7 @@ mod tests {
         let peer_a = NodeId::from_bytes(PrivateKey::new().public_key().as_bytes()).unwrap();
         let peer_b = NodeId::from_bytes(PrivateKey::new().public_key().as_bytes()).unwrap();
         let topic = TestTopic::new("critical error test");
-        let sync_protocol = Arc::new(Protocol::InitiateUnexpectedBehaviour);
+        let sync_protocol = Arc::new(FailingProtocol::InitiateUnexpectedBehaviour);
 
         // Duplex streams which simulate both ends of a bi-directional network connection.
         let (peer_a_stream, peer_b_stream) = tokio::io::duplex(64 * 1024);
@@ -787,7 +835,7 @@ mod tests {
         let peer_a = NodeId::from_bytes(PrivateKey::new().public_key().as_bytes()).unwrap();
         let peer_b = NodeId::from_bytes(PrivateKey::new().public_key().as_bytes()).unwrap();
         let topic = TestTopic::new("successful sync test");
-        let sync_protocol = Arc::new(Protocol::NoError);
+        let sync_protocol = Arc::new(FailingProtocol::NoError);
 
         // Duplex streams which simulate both ends of a bi-directional network connection.
         let (peer_a_stream, peer_b_stream) = tokio::io::duplex(64 * 1024);
