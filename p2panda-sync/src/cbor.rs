dd985893e164dc5367937b4299e7ad3735097689
adz <x12@adz.garden>
2024-11-27 12:22:04 +0100

    Do not advance buffer on error cases

--- p2panda-sync/src/cbor.rs
+++ p2panda-sync/src/cbor.rs
@@ -13,6 +13,14 @@ use tokio_util::compat::{FuturesAsyncReadCompatExt, FuturesAsyncWriteCompatExt};
 
 use crate::SyncError;
 
+/// Implementation of the tokio codec traits to encode- and decode CBOR data as a stream.
+///
+/// CBOR allows message framing based on initial "headers" for each "data item", which indicate the
+/// type of data and the expected "body" length to be followed. A stream-based decoder can attempt
+/// parsing these headers and then reason about if it has enough information to proceed.
+///
+/// Read more on CBOR in streaming applications here:
+/// https://www.rfc-editor.org/rfc/rfc8949.html#section-5.1
 #[derive(Clone, Debug)]
 pub struct CborCodec<T> {
     _phantom: PhantomData<T>,
@@ -38,10 +46,14 @@ where
 {
     type Error = SyncError;
 
+    /// Encodes an serializable item into CBOR bytes and adds them to the buffer.
     fn encode(&mut self, item: T, dst: &mut BytesMut) -> Result<(), Self::Error> {
         let bytes = encode_cbor(&item).map_err(|err| {
+            // When we've failed encoding our _own_ messages something seriously went wrong.
             SyncError::Critical(format!("CBOR codec failed encoding message, {err}"))
         })?;
+        // Append the encoded CBOR bytes to the buffer instead of replacing it, we might already
+        // have previously encoded items in it.
         dst.extend_from_slice(&bytes);
         Ok(())
     }
@@ -54,17 +66,40 @@ where
     type Item = T;
     type Error = SyncError;
 
+    /// CBOR decoder method taking as an argument the bytes that has been read so far, and when it
+    /// is called, it will be in one of the following situations:
+    ///
+    /// 1. The buffer contains less than a full frame.
+    /// 2. The buffer contains exactly a full frame.
+    /// 3. The buffer contains more than a full frame.
     fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
-        let reader = src.reader();
-        let result: Result<Self::Item, _> = decode_cbor(reader);
+        // Keep a reference of the buffer to not advance the main buffer itself (yet).
+        let mut bytes: &[u8] = src.as_ref();
+        let starting = bytes.len();
+
+        // Attempt decoding the buffer and remember how many bytes we've advanced it doing that.
+        let result: Result<Self::Item, _> = decode_cbor(&mut bytes);
+        let ending = bytes.len();
+
         match result {
-            Ok(item) => Ok(Some(item)),
+            Ok(item) => {
+                // We've successfully could read one full frame from the buffer. We're finally
+                // advancing it for the next decode iteration and yield the resulting data item to
+                // the stream.
+                src.advance(starting - ending);
+                Ok(Some(item))
+            }
+            // Note that the buffer is not further advanced in case of an error.
             Err(ref error) => match error {
                 DecodeError::Io(err) => {
-                    // Sometimes the EOF is signalled as IO error.
                     if err.kind() == std::io::ErrorKind::UnexpectedEof {
+                        // EOF errors indicate that our buffer doesn't contain enough data to
+                        // decode a whole CBOR frame. We're yielding no data item and re-try
+                        // decoding in the next iteration.
                         Ok(None)
                     } else {
+                        // An I/O error during decoding usually indicates something wrong with our
+                        // system (lack of system memory etc.).
                         Err(SyncError::Critical(format!(
                             "CBOR codec failed decoding message due to i/o error, {err}"
                         )))
@@ -76,6 +111,16 @@ where
     }
 }
 
+/// Returns a reader for your data type, receiving CBOR encoded byte-streams of it and handling the
+/// framing of them automatically.
+///
+/// This can be used in various sync protocol implementations where we need to receive data via a
+/// wire protocol between two peers.
+///
+/// This is a convenience method if you want to use CBOR encoding and serde to handle your wire
+/// protocol message encoding and framing without implementing it yourself. If you're interested in
+/// your own approach you can either implement your own `FramedRead` or `Sink` (if you're not
+/// interested in the tokio codec traits).
 pub fn into_cbor_stream<'a, M>(
     rx: Box<&'a mut (dyn AsyncRead + Send + Unpin)>,
 ) -> impl Stream<Item = Result<M, SyncError>> + Send + Unpin + 'a
@@ -85,6 +130,15 @@ where
     FramedRead::new(rx.compat(), CborCodec::<M>::new())
 }
 
+/// Returns a writer for your data type, sending CBOR encoded byte-streams of it.
+///
+/// This can be used in various sync protocol implementations where we need to send data via a wire
+/// protocol between two peers.
+///
+/// This is a convenience method if you want to use CBOR encoding and serde to handle your wire
+/// protocol message encoding and framing without implementing it yourself. If you're interested in
+/// your own approach you can either implement your own `FramedWrite` or `Stream` (if you're not
+/// interested in the tokio codec traits).
 pub fn into_cbor_sink<'a, M>(
     tx: Box<&'a mut (dyn AsyncWrite + Send + Unpin)>,
 ) -> impl Sink<M, Error = SyncError> + Send + Unpin + 'a
