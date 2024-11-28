fe9e31ce9b43a41135d58893df9abb83393a7cab
adz <x12@adz.garden>
2024-11-28 12:06:24 +0100

    Fix imports after rebase

--- p2panda-net/src/sync/mod.rs
+++ p2panda-net/src/sync/mod.rs
@@ -8,5 +8,7 @@ pub(crate) mod manager;
 #[cfg(test)]
 mod tests;
 
+pub use accept::accept_sync;
 pub use config::{ResyncConfiguration, SyncConfiguration};
 pub use handler::{SyncConnection, SYNC_CONNECTION_ALPN};
+pub use initiate::initiate_sync;
