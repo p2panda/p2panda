cb324cfac2a84c798a1ad0d9c30b35cfbaa9fe5f
adz <x12@adz.garden>
2024-12-01 10:57:42 +0100

    Write more about offline-first nature and extensions

--- p2panda-net/src/lib.rs
+++ p2panda-net/src/lib.rs
@@ -1,39 +1,66 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-//! `p2panda-net` is a data-type-agnostic p2p networking layer which offers robust, direct
+//! `p2panda-net` is a data-type-agnostic p2p networking layer offering robust, direct
 //! communication to any device, no matter where they are.
 //!
-//! It offers a stream-based API for your higher application layers: Subscribe to any "topic" your
-//! application is interested in and `p2panda-net` will automatically discover peers who are
-//! interested in the same data.
+//! It provides a stream-based API for higher application layers: Applications subscribe to any
+//! "topic" they are interested in and `p2panda-net` will automatically discover similar peers and
+//! transport raw bytes between them.
 //!
-//! Additionally `p2panda-net` can be extended with custom peer discovery techniques or sync
-//! protocols, allowing your applications to "catch up on past data", so they can eventually
-//! converge on the same state.
+//! Additionally `p2panda-net` can be extended with custom sync protocol implementations for all
+//! data types, allowing applications to "catch up on past data", eventually converging to the same
+//! state.
 //!
 //! ## Features
 //!
 //! Most of the lower-level networking of `p2panda-net` is made possible by the work of
 //! [iroh](https://github.com/n0-computer/iroh/) utilising well-established and known standards,
-//! like QUIC for transport, STUN for holepunching, Tailscale's DERP (Designated Encrypted Relay
-//! for Packets) for relay fallbacks, PlumTree and HyParView for gossipping.
+//! like QUIC for transport, STUN for establishing direct connections between devices, Tailscale's
+//! DERP (Designated Encrypted Relay for Packets) for relay fallbacks, PlumTree and HyParView for
+//! broadcast-based gossip overlays.
 //!
-//! p2panda adds all functionality on top of iroh we believe is crucial for peer-to-peer
-//! application development, without tying ourselves too close to any pre-defined data types and
-//! allowing plenty space for customisation:
+//! p2panda adds crucial functionality on top of iroh for peer-to-peer application development,
+//! without tying developers too close to any pre-defined data types and allowing plenty space for
+//! customisation:
 //!
-//! * Data of any kind can be exchanged efficiently via gossip broadcast ("live mode") or via a
-//! sync protocol between two peers
-//! * Custom queries to express interest in certain data of your application
-//! * Ambient peer discovery, learning about new peers you might not know about
-//! * Ambient topic discovery, learning what peers are interested in
-//! * Sync protocol API, providing the eventual-consistency guarantee that your peers will converge
-//! on the same state over time
-//! * Manages connections, automatically syncs with discovered peers and re-tries on faults
-//! * Extension to handle [sync of large files](https://docs.rs/p2panda-blobs)
+//! 1. Data of any kind can be exchanged efficiently via gossip broadcast ("live mode") or via sync
+//!    protocols between two peers ("catching up on past state")
+//! 2. Custom queries to express interest in certain data of applications
+//! 3. Ambient peer discovery: Learning about new, previously unknown peers in the network
+//! 4. Ambient topic discovery: Learning what peers are interested in, automatically forming
+//!    overlay networks per topic
+//! 5. Sync protocol API, providing an eventual-consistency guarantee that peers will converge on
+//!    the same state over time
+//! 6. Manages connections, automatically syncs with discovered peers and re-tries on faults
+//! 7. Extension for networks to handle efficient [sync of large
+//!    files](https://docs.rs/p2panda-blobs)
 //!
-//! `p2panda-net` is designed to run on top of bi-directional connections on the IP layer (aka "The
-//! Internet"). For use-
+//! ## Offline-First
+//!
+//! This networking crate is designed to run on top of bi-directional, ordered connections on the
+//! IP layer (aka "The Internet"), with robustness to work in environments with instable
+//! connectivity or offline time-periods.
+//!
+//! While this IP-based networking implementation should provide for many "modern" use-cases,
+//! p2panda data-types are designed for more extreme scenarios where connectivity can _never_ be
+//! assumed and data transmission is highly "delay tolerant": For example "broadcast-only"
+//! topologies on top of BLE (Bluetooth Low Energy), LoRa or even Digital Radio Communication
+//! infrastructure.
+//!
+//! ## Extensions
+//!
+//! `p2panda-net` is agnostic to any data type (sending and receiving raw byte streams) and can
+//! seamlessly be extended with external or official p2panda implementations. We provide p2panda's
+//! fork-tolerant and prunable append-only logs in `p2panda-core`, offering single-writer and
+//! multi-writer streams, authentication, deletion, ordering and much more. It can be further
+//! extended with an efficient sync implementation in `p2panda-sync` and validation and fast
+//! stream-based ingest solutions in `p2panda-streams`. Lastly we maintain persistance layer APIs
+//! in `p2panda-store` for in-memory storage or embeddable, SQL-based databases. In the future we
+//! will provide additional implementations for managing access control and group encryption.
+//!
+//! For discovery of peers on the local network, we currently provide an mDNS-based implementation
+//! in `p2panda-discovery`, later additional techniques like "rendesvouz" nodes and random walk
+//! algorithms.
 //!
 //! ## Example
 //!
