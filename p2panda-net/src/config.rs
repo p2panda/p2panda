94e2113bc1d7a53db094ea9a8ea8fac249ec0f65
adz <x12@adz.garden>
2024-12-02 17:38:11 +0100

    Better default network id, more docs in config

--- p2panda-net/src/config.rs
+++ p2panda-net/src/config.rs
@@ -10,7 +10,10 @@ use crate::{NetworkId, NodeAddress, RelayUrl};
 pub const DEFAULT_BIND_PORT: u16 = 2022;
 
 /// Default network id.
-pub const DEFAULT_NETWORK_ID: NetworkId = [0; 32];
+pub const DEFAULT_NETWORK_ID: NetworkId = [
+    247, 69, 248, 242, 132, 120, 159, 230, 98, 100, 214, 200, 78, 40, 79, 94, 174, 8, 12, 27, 84,
+    195, 246, 159, 132, 240, 79, 208, 1, 43, 132, 118,
+];
 
 /// Configuration parameters for the local network node.
 #[derive(Clone, Debug, Serialize, Deserialize)]
@@ -18,17 +21,19 @@ pub struct Config {
     /// Bind port for the IPv4 socket. The IPv6 socket will be bound to `bind_port` + 1.
     pub bind_port: u16,
 
-    /// Direct node addresses for the local node (e.g. "0.0.0.0:2026").
+    /// Node addresses of remote peers which are directly reachable (no STUN or relay required).
+    /// These will be added to the address book.
     pub direct_node_addresses: Vec<NodeAddress>,
 
     /// Identifier of the network to be joined.
     pub network_id: NetworkId,
 
-    /// Path to the local private key. If not provided, a random keypair will be generated.
+    /// Path to the local private key. If not provided, a random keypair will be generated and kept
+    /// in memory.
     pub private_key: Option<PathBuf>,
 
-    /// Relay URL to help in establishing a peer-to-peer connection if one or both peers are behind
-    /// a NAT.
+    /// URL to relay server to help in establishing a peer-to-peer connection if one or both peers
+    /// are behind a NAT or firewall.
     pub relay: Option<RelayUrl>,
 }
 
