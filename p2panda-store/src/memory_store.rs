3b1bc6fb9e9c89ea5d7a22169944fa12e161a543
glyph <glyph@mycelial.technology>
2024-12-04 11:36:11 +0000

    Add one-line module doc for memory implementation

--- p2panda-store/src/memory_store.rs
+++ p2panda-store/src/memory_store.rs
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+//! In-memory persistence for `OperationStore` and `LogStore`.
+
 use std::collections::{BTreeSet, HashMap};
 use std::convert::Infallible;
 use std::fmt::Debug;
