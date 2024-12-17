117eda54b29ee81ea4985d997a219b9a43a0bc9f
adz <x12@adz.garden>
2024-12-17 17:57:12 +0100

    Update comments in fuzz test

--- fuzz/fuzz_targets/header_e2e.rs
+++ fuzz/fuzz_targets/header_e2e.rs
@@ -1,41 +1,37 @@
 #![no_main]
 
+use libfuzzer_sys::fuzz_target;
+use p2panda_core::cbor::{decode_cbor, encode_cbor};
 use p2panda_core::{Header, PrivateKey};
 
-use libfuzzer_sys::fuzz_target;
-
+// Create arbitrary header, sign, serialize and deserialize it.
 fuzz_target!(|header: Header<()>| {
     let private_key = PrivateKey::new();
 
-    // Sign header
     let mut header = header;
     header.sign(&private_key);
     header.verify();
 
-    // Serialize signed header
-    let mut bytes = Vec::new();
-    ciborium::ser::into_writer(&header, &mut bytes).unwrap();
+    let bytes = encode_cbor(&header).expect("header encoding");
+    let result: Result<Header<()>, _> = decode_cbor(&bytes[..]);
 
-    // Deserialize signed header bytes again
-    let result: Result<Header<()>, _> = ciborium::de::from_reader(&bytes[..]);
-
-    // We expect these cases to fail
-    if header.payload_size == 0 && header.payload_hash.is_some() // payload hash not expected when payload size is zero
-        || header.payload_size != 0 && header.payload_hash.is_none() // payload hash expected when payload size non-zero
-        || header.seq_num == 0 && header.backlink.is_some() // backlink not expected when seq number is zero
+    // We expect these cases to fail:
+    //
+    // 1. Payload hash not expected when payload size is zero.
+    // 2. Payload hash expected when payload size non-zero.
+    // 3. Backlink not expected when seq number is zero.
+    // 4. Backlink expected when seq number is non-zero.
+    //
+    // All other cases should successfully deserialize.
+    if header.payload_size == 0 && header.payload_hash.is_some()
+        || header.payload_size != 0 && header.payload_hash.is_none()
+        || header.seq_num == 0 && header.backlink.is_some()
         || header.seq_num != 0 && header.backlink.is_none()
-    // backlink expected when seq number is non-zero
     {
-        // All these cases should error
         assert!(result.is_err())
     } else {
-        // All other cases should successfully deserialize
-        let header_again = result.unwrap();
-
-        // Verify the signed header
+        let header_again = result.expect("header decoding");
         header_again.verify();
-
-        // Assert it matches the original
         assert_eq!(header, header_again)
     }
 });
