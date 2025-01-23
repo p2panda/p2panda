43032f047ba370b4eb580d2777703dde4079b37f
glyph <glyph@mycelial.technology>
2025-01-23 07:49:00 +0000

    Create operations migration

---
+++ p2panda-store/migrations/20250125065817_create-operations.sql
@@ -0,0 +1,20 @@
+-- SPDX-License-Identifier: MIT OR Apache-2.0
+
+CREATE TABLE IF NOT EXISTS operations_v1 (
+    hash                    TEXT            NOT NULL    UNIQUE,
+    log_id                  TEXT            NOT NULL,
+    version                 TEXT            NOT NULL,
+    public_key              TEXT            NOT NULL,
+    signature               TEXT            NULL,
+    payload_size            TEXT            NOT NULL,
+    payload_hash            TEXT            NULL,
+    timestamp               TEXT            NOT NULL,
+    seq_num                 TEXT            NOT NULL,
+    backlink                TEXT            NULL,
+    previous                TEXT            NOT NULL,
+    extensions              BLOB            NULL,
+    body                    BLOB            NULL,
+    header_bytes            TEXT            NOT NULL,
+    -- @TODO(glyph): Should primary key be hash instead?
+    PRIMARY KEY (public_key, log_id, seq_num)
+);
