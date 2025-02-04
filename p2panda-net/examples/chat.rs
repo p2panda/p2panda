7a52b3b40b2e957ea8f7fd9cfac3b9b4cd7e12e8
glyph <glyph@mycelial.technology>
2025-02-04 12:10:52 +0000

    Add clap as dev depencency and parse relay and bootstrap args

--- p2panda-net/examples/chat.rs
+++ p2panda-net/examples/chat.rs
@@ -1,4 +1,5 @@
 use anyhow::{bail, Result};
+use clap::Parser;
 use p2panda_core::{Hash, PrivateKey, PublicKey, Signature};
 use p2panda_discovery::mdns::LocalDiscovery;
 use p2panda_net::network::{FromNetwork, ToNetwork};
@@ -10,11 +11,12 @@ use tokio::sync::mpsc;
 use tracing_subscriber::prelude::*;
 use tracing_subscriber::EnvFilter;
 
-// This is the URL of a relay operated by the p2panda team.
+// Here we have two relay URLs:
 //
-// Alternatively, an iroh staging relay can be used:
-// "https://staging-euw1-1.relay.iroh.network/".
-const RELAY_URL: &str = "https://wasser.liebechaos.org/";
+// One is an iroh staging relay which should be running the latest iroh release version.
+// The other is operated by the p2panda team and may not be running the latest release version.
+const RELAY_URL: &str = "https://staging-euw1-1.relay.iroh.network/";
+//const RELAY_URL: &str = "https://wasser.liebechaos.org/";
 
 pub fn setup_logging() {
     tracing_subscriber::registry()
@@ -22,6 +24,24 @@ pub fn setup_logging() {
         .with(EnvFilter::from_default_env())
         .try_init()
         .ok();
+}
+
+#[derive(Parser)]
+struct Args {
+    // Specify a relay to be registered with the local network builder.
+    //
+    // This is required if you wish to connect to a non-local bootstrap node. The relay will
+    // facilitate a connection with the other node(s); either a direct connection or a proxied
+    // connection if that is not possible.
+    #[arg(short = 'r', long)]
+    use_relay: bool,
+
+    // Supply the public key of another peer to use it as a "bootstrap node" for discovery over
+    // the internet.
+    //
+    // If no value is supplied, peers can only be discovered over your local area network.
+    #[arg(short = 'b', long, value_name = "PUBLIC_KEY")]
+    bootstrap: Option<PublicKey>,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash, Deserialize, Serialize)]
@@ -45,16 +65,27 @@ impl TopicId for ChatTopic {
 async fn main() -> Result<()> {
     setup_logging();
 
+    let args = Args::parse();
+
     let network_id = Hash::new(b"p2panda_chat_example");
     let topic = ChatTopic::new("my_chat");
 
     let private_key = PrivateKey::new();
 
-    let network = NetworkBuilder::new(network_id.into())
-        .discovery(LocalDiscovery::new())
-        .relay(RELAY_URL.parse()?, false, 0)
-        .build()
-        .await?;
+    // Configure the network.
+    let mut network_builder =
+        NetworkBuilder::new(network_id.into()).discovery(LocalDiscovery::new());
+
+    if args.use_relay {
+        println!("using relay: {}", RELAY_URL);
+        network_builder = network_builder.relay(RELAY_URL.parse()?, false, 0);
+    }
+
+    if let Some(node_id) = args.bootstrap {
+        network_builder = network_builder.direct_address(node_id, vec![], None);
+    }
+
+    let network = network_builder.build().await?;
 
     let (tx, mut rx, ready) = network.subscribe(topic).await?;
 
@@ -75,6 +106,8 @@ async fn main() -> Result<()> {
             }
         }
     });
+
+    println!("your public key is: {}", private_key.public_key());
 
     println!(".. waiting for peers to join ..");
     let _ = ready.await;
