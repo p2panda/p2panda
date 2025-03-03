648f4e488315c1911f307c70151325ad20d4537e
Sam Andreae <contact@samandreae.com>
2025-03-03 14:44:18 +0000

    More tests

--- p2panda-stream/src/stream/dependencies.rs
+++ p2panda-stream/src/stream/dependencies.rs
@@ -8,6 +8,7 @@ use crate::dependencies::{
     DependencyChecker as InnerDependencyChecker, DependencyCheckerError, DependencyStore,
 };
 
+/// Dependency checker which handles p2panda operations.
 pub struct DependencyChecker<L, E, OS, DS> {
     operation_store: OS,
     inner: InnerDependencyChecker<Hash, DS>,
@@ -21,6 +22,7 @@ where
     DS: DependencyStore<Hash>,
     E: Extensions,
 {
+    /// Construct a new dependency checker from an operation store and dependency store.
     pub fn new(operation_store: OS, dependency_store: DS) -> Self {
         let inner_dependency_checker = InnerDependencyChecker::new(dependency_store);
         DependencyChecker {
@@ -31,6 +33,7 @@ where
         }
     }
 
+    /// Process a single operation.
     pub async fn process(
         &mut self,
         operation: Operation<E>,
@@ -42,6 +45,7 @@ where
         Ok(())
     }
 
+    /// Take the next ready operation from the queue.
     pub async fn next(&mut self) -> Result<Option<Operation<E>>, OperationDependencyCheckerError> {
         let Some(hash) = self.inner.next() else {
             return Ok(None);
@@ -68,6 +72,7 @@ where
         };
     }
 
+    /// Clear the operation cache.
     pub fn clear_cache(&mut self) {
         self.operation_cache.clear();
     }
@@ -96,15 +101,17 @@ mod tests {
 
     use super::DependencyChecker;
 
-    #[tokio::test]
-    async fn operations_with_previous() {
-        let private_key = PrivateKey::new();
-        let mut operation_store = MemoryStore::<u64>::default();
-
-        // 0P0 <-- 0P1 <--\
-        //     \-- OP2 <-- OP4
-        //      \--OP3 <--/
-
+    /// Create operations which form the following graph with their previous links:
+    ///
+    /// 0P0 <-- 0P1 <--\
+    ///     \-- OP2 <-- OP4
+    ///      \--OP3 <--/
+    ///
+    /// Each operation is inserted into the store in it's own log.
+    async fn setup(
+        private_key: &PrivateKey,
+        operation_store: &mut MemoryStore<u64>,
+    ) -> Vec<Operation> {
         let mut header_0 = Header {
             public_key: private_key.public_key(),
             timestamp: 0,
@@ -194,42 +201,163 @@ mod tests {
             .await
             .unwrap();
 
+        vec![
+            operation_0,
+            operation_1,
+            operation_2,
+            operation_3,
+            operation_4,
+        ]
+    }
+
+    #[tokio::test]
+    async fn operations_with_previous() {
+        let private_key = PrivateKey::new();
+        let mut operation_store = MemoryStore::<u64>::default();
         let dependency_store = DependencyMemoryStore::default();
-        let mut operation_checker = DependencyChecker::new(operation_store, dependency_store);
+        let mut dependency_checker =
+            DependencyChecker::new(operation_store.clone(), dependency_store);
 
-        let result = operation_checker.process(operation_4.clone()).await;
-        assert!(result.is_ok());
-        let result = operation_checker.process(operation_3.clone()).await;
-        assert!(result.is_ok());
-        let result = operation_checker.process(operation_2.clone()).await;
-        assert!(result.is_ok());
-        let result = operation_checker.process(operation_1.clone()).await;
-        assert!(result.is_ok());
-        let result = operation_checker.process(operation_0.clone()).await;
-        assert!(result.is_ok());
+        // Setup test data in the store. They form a dependency graph with the following form:
+        //
+        // 0P0 <-- 0P1 <--\
+        //     \-- OP2 <-- OP4
+        //      \--OP3 <--/
+        //
+        let operations = setup(&private_key, &mut operation_store).await;
 
-        let next = operation_checker.next().await.unwrap();
-        assert_eq!(next.unwrap(), operation_0);
+        // Process each operation out-of-order.
+        let result = dependency_checker.process(operations[4].clone()).await;
+        assert!(result.is_ok());
+        // Each operation gets inserted to the operation cache.
+        assert_eq!(dependency_checker.operation_cache.len(), 1);
+        
+        let result = dependency_checker.process(operations[3].clone()).await;
+        assert!(result.is_ok());
+        assert_eq!(dependency_checker.operation_cache.len(), 2);
+        
+        let result = dependency_checker.process(operations[2].clone()).await;
+        assert!(result.is_ok());
+        assert_eq!(dependency_checker.operation_cache.len(), 3);
+        
+        let result = dependency_checker.process(operations[1].clone()).await;
+        assert!(result.is_ok());
+        assert_eq!(dependency_checker.operation_cache.len(), 4);
+        
+        let result = dependency_checker.process(operations[0].clone()).await;
+        assert!(result.is_ok());
+        assert_eq!(dependency_checker.operation_cache.len(), 5);
+
+        // Calling next should give us the first operation which had all it's dependencies met, in
+        // this case it's the root of the graph OP1. 
+        let next = dependency_checker.next().await.unwrap();
+        assert_eq!(next.unwrap(), operations[0]);
+
+        // Operations OP1, OP2 and OP3 all only depend on operation OP0 and they were created
+        // concurrently, we should see all of these operations next, but we don't know in what order.
+        let mut concurrent_operations =
+            HashSet::from([operations[1].hash, operations[2].hash, operations[3].hash]);
+
+        let next = dependency_checker.next().await.unwrap();
+        assert!(next.is_some());
+        let next = next.unwrap();
+        assert!(concurrent_operations.remove(&next.hash),);
+
+        let next = dependency_checker.next().await.unwrap();
+        assert!(next.is_some());
+        let next = next.unwrap();
+        assert!(concurrent_operations.remove(&next.hash),);
+
+        let next = dependency_checker.next().await.unwrap();
+        assert!(next.is_some());
+        let next = next.unwrap();
+        assert!(concurrent_operations.remove(&next.hash),);
+
+        // We know OP4 will be given last as it depended on OP1, OP2 and OP3.
+        let next = dependency_checker.next().await.unwrap();
+        assert_eq!(next.unwrap(), operations[4]);
+    }
+
+    #[tokio::test]
+    async fn clear_cache() {
+        // Same test as above except we clear the cache before taking operations via the `next` method.
+        let private_key = PrivateKey::new();
+        let mut operation_store = MemoryStore::<u64>::default();
+        let dependency_store = DependencyMemoryStore::default();
+        let mut dependency_checker =
+            DependencyChecker::new(operation_store.clone(), dependency_store);
+
+        let operations = setup(&private_key, &mut operation_store).await;
+
+        let result = dependency_checker.process(operations[4].clone()).await;
+        assert!(result.is_ok());
+        assert_eq!(dependency_checker.operation_cache.len(), 1);
+        let result = dependency_checker.process(operations[3].clone()).await;
+        assert!(result.is_ok());
+        assert_eq!(dependency_checker.operation_cache.len(), 2);
+        let result = dependency_checker.process(operations[2].clone()).await;
+        assert!(result.is_ok());
+        assert_eq!(dependency_checker.operation_cache.len(), 3);
+        let result = dependency_checker.process(operations[1].clone()).await;
+        assert!(result.is_ok());
+        assert_eq!(dependency_checker.operation_cache.len(), 4);
+        let result = dependency_checker.process(operations[0].clone()).await;
+        assert!(result.is_ok());
+        assert_eq!(dependency_checker.operation_cache.len(), 5);
+
+        // Clear the cache!
+        dependency_checker.clear_cache();
+        assert_eq!(dependency_checker.operation_cache.len(), 0);
+
+        let next = dependency_checker.next().await.unwrap();
+        assert_eq!(next.unwrap(), operations[0]);
 
         let mut concurrent_operations =
-            HashSet::from([operation_1.hash, operation_2.hash, operation_3.hash]);
+            HashSet::from([operations[1].hash, operations[2].hash, operations[3].hash]);
 
-        let next = operation_checker.next().await.unwrap();
+        let next = dependency_checker.next().await.unwrap();
         assert!(next.is_some());
         let next = next.unwrap();
         assert!(concurrent_operations.remove(&next.hash),);
 
-        let next = operation_checker.next().await.unwrap();
+        let next = dependency_checker.next().await.unwrap();
         assert!(next.is_some());
         let next = next.unwrap();
         assert!(concurrent_operations.remove(&next.hash),);
 
-        let next = operation_checker.next().await.unwrap();
+        let next = dependency_checker.next().await.unwrap();
         assert!(next.is_some());
         let next = next.unwrap();
         assert!(concurrent_operations.remove(&next.hash),);
 
-        let next = operation_checker.next().await.unwrap();
-        assert_eq!(next.unwrap(), operation_4);
+        let next = dependency_checker.next().await.unwrap();
+        assert_eq!(next.unwrap(), operations[4]);
+    }
+
+    #[tokio::test]
+    async fn missing_dependency() {
+        let private_key = PrivateKey::new();
+        let mut operation_store = MemoryStore::<u64>::default();
+        let dependency_store = DependencyMemoryStore::default();
+        let mut dependency_checker =
+            DependencyChecker::new(operation_store.clone(), dependency_store);
+
+        let operations = setup(&private_key, &mut operation_store).await;
+
+        let result = dependency_checker.process(operations[1].clone()).await;
+        assert!(result.is_ok());
+        assert_eq!(dependency_checker.operation_cache.len(), 1);
+        let result = dependency_checker.process(operations[2].clone()).await;
+        assert!(result.is_ok());
+        assert_eq!(dependency_checker.operation_cache.len(), 2);
+        let result = dependency_checker.process(operations[3].clone()).await;
+        assert!(result.is_ok());
+        assert_eq!(dependency_checker.operation_cache.len(), 3);
+        let result = dependency_checker.process(operations[4].clone()).await;
+        assert!(result.is_ok());
+        assert_eq!(dependency_checker.operation_cache.len(), 4);
+
+        let next = dependency_checker.next().await.unwrap();
+        assert!(next.is_none());
     }
 }
