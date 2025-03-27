29e8cb69a81fb15faa5b6674842a542731422490
adz <x12@adz.garden>
2025-03-27 18:23:24 +0100

    IdentityHandle and OperationId super traits

--- p2panda-group/src/traits/mod.rs
+++ p2panda-group/src/traits/mod.rs
@@ -1,5 +1,8 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
+use std::fmt::Debug;
+use std::hash::Hash as StdHash;
+
 mod key_bundle;
 mod key_manager;
 mod key_registry;
@@ -7,3 +10,13 @@ mod key_registry;
 pub use key_bundle::KeyBundle;
 pub use key_manager::{IdentityManager, PreKeyManager};
 pub use key_registry::{IdentityRegistry, PreKeyRegistry};
+
+pub trait IdentityHandle: Copy + Debug + PartialEq + Eq + StdHash {}
+
+#[cfg(test)]
+impl IdentityHandle for &str {}
+
+pub trait OperationId: Copy + Debug + PartialEq + Eq + StdHash {}
+
+#[cfg(test)]
+impl OperationId for usize {}
