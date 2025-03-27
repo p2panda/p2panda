2f9e17ba0dc3a96b21eb7eb52b9506d3d3e76c4c
adz <x12@adz.garden>
2025-03-27 22:07:04 +0100

    More doc-strings

--- p2panda-group/src/traits/mod.rs
+++ p2panda-group/src/traits/mod.rs
@@ -13,11 +13,20 @@ pub use key_bundle::KeyBundle;
 pub use key_manager::{IdentityManager, PreKeyManager};
 pub use key_registry::{IdentityRegistry, PreKeyRegistry};
 
+/// Handle to identify a group member.
+///
+/// This needs to be unique within a group, can be a username, number or preferably a long byte
+/// string.
 pub trait IdentityHandle: Copy + Debug + PartialEq + Eq + StdHash {}
 
 #[cfg(test)]
 impl IdentityHandle for &str {}
 
+/// Identifier for each group membership operation.
+///
+/// Operations trigger changes of the group state and are usually sent in form of messages over the
+/// network. Each operation needs to be uniquely identifiable, preferably by a collision-resistant
+/// hash.
 pub trait OperationId: Copy + Debug + PartialEq + Eq + StdHash {}
 
 #[cfg(test)]
