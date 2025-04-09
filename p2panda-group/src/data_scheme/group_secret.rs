dcbbadb96f161a8eacc30ee7473b724bc4645314
adz <x12@adz.garden>
2025-04-09 20:12:35 +0200

    Methods to interact with group secrets in bundle

--- p2panda-group/src/data_scheme/group_secret.rs
+++ p2panda-group/src/data_scheme/group_secret.rs
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
 use std::collections::HashMap;
+use std::collections::hash_map::{IntoIter, Iter, Keys, Values};
 use std::fmt;
+use std::hash::Hash as StdHash;
 
 use p2panda_core::cbor::{DecodeError, EncodeError, decode_cbor, encode_cbor};
 use serde::de::{SeqAccess, Visitor};
@@ -18,8 +20,7 @@ pub const GROUP_SECRET_SIZE: usize = 32;
 
 pub type GroupSecretId = [u8; SHA256_DIGEST_SIZE];
 
-#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
-#[cfg_attr(any(test, feature = "test_utils"), derive(Clone))]
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct GroupSecret(Secret<GROUP_SECRET_SIZE>);
 
 impl GroupSecret {
@@ -32,13 +33,14 @@ impl GroupSecret {
         Self(Secret::from_bytes(bytes))
     }
 
-    pub fn try_from_bytes(bytes: &[u8]) -> Result<Self, GroupSecretError> {
+    pub(crate) fn try_from_bytes(bytes: &[u8]) -> Result<Self, GroupSecretError> {
         let bytes: [u8; GROUP_SECRET_SIZE] = bytes
             .try_into()
             .map_err(|_| GroupSecretError::InvalidKeySize)?;
         Ok(Self::from_bytes(bytes))
     }
 
+    /// Returns identifier (SHA256 hash) for this secret.
     pub fn id(&self) -> GroupSecretId {
         sha2_256(&[self.0.as_bytes()])
     }
@@ -48,27 +50,76 @@ impl GroupSecret {
     }
 }
 
-#[derive(Debug, PartialEq, Eq)]
-#[cfg_attr(any(test, feature = "test_utils"), derive(Clone))]
+impl StdHash for GroupSecret {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.id().hash(state);
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub struct GroupSecretBundle(HashMap<GroupSecretId, GroupSecret>);
 
 impl GroupSecretBundle {
-    pub fn try_from_bytes(bytes: &[u8]) -> Result<Self, GroupSecretError> {
-        let bundle: Self = decode_cbor(bytes)?;
-        Ok(bundle)
+    pub fn new() -> Self {
+        Self(HashMap::new())
     }
 
-    pub(crate) fn from_secrets(secrets: Vec<GroupSecret>) -> Self {
-        let mut bundle = HashMap::new();
-        for secret in secrets {
-            bundle.insert(secret.id(), secret);
-        }
-        Self(bundle)
+    pub fn from_secrets(secrets: Vec<GroupSecret>) -> Self {
+        Self(HashMap::from_iter(
+            secrets.into_iter().map(|secret| (secret.id(), secret)),
+        ))
+    }
+
+    pub(crate) fn try_from_bytes(bytes: &[u8]) -> Result<Self, GroupSecretError> {
+        Ok(decode_cbor(bytes)?)
     }
 
     pub(crate) fn to_bytes(&self) -> Result<Vec<u8>, GroupSecretError> {
-        let bytes = encode_cbor(self)?;
-        Ok(bytes)
+        Ok(encode_cbor(self)?)
+    }
+
+    pub fn insert(&mut self, secret: GroupSecret) {
+        self.0.insert(secret.id(), secret);
+    }
+
+    pub fn get(&self, id: &GroupSecretId) -> Option<&GroupSecret> {
+        self.0.get(id)
+    }
+
+    pub fn remove(&mut self, id: &GroupSecretId) -> Option<GroupSecret> {
+        self.0.remove(id)
+    }
+
+    pub fn extend(&mut self, bundle: GroupSecretBundle) {
+        self.0.extend(bundle.0);
+    }
+
+    pub fn contains(&mut self, id: &GroupSecretId) -> bool {
+        self.0.contains_key(id)
+    }
+
+    pub fn len(&self) -> usize {
+        self.0.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
+
+    pub fn iter(&self) -> Iter<'_, GroupSecretId, GroupSecret> {
+        self.0.iter()
+    }
+
+    pub fn into_iter(self) -> IntoIter<GroupSecretId, GroupSecret> {
+        self.0.into_iter()
+    }
+
+    pub fn ids(&self) -> Keys<'_, GroupSecretId, GroupSecret> {
+        self.0.keys()
+    }
+
+    pub fn secrets(&self) -> Values<'_, GroupSecretId, GroupSecret> {
+        self.0.values()
     }
 }
 
@@ -90,13 +141,13 @@ impl<'de> Deserialize<'de> for GroupSecretBundle {
     where
         D: serde::Deserializer<'de>,
     {
-        struct SecretVisitor;
+        struct SecretListVisitor;
 
-        impl<'de> Visitor<'de> for SecretVisitor {
+        impl<'de> Visitor<'de> for SecretListVisitor {
             type Value = Vec<GroupSecret>;
 
             fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-                formatter.write_str("Header encoded as a sequence")
+                formatter.write_str("list of group secrets")
             }
 
             fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
@@ -111,7 +162,7 @@ impl<'de> Deserialize<'de> for GroupSecretBundle {
             }
         }
 
-        let secrets = deserializer.deserialize_seq(SecretVisitor)?;
+        let secrets = deserializer.deserialize_seq(SecretListVisitor)?;
 
         Ok(GroupSecretBundle::from_secrets(secrets))
     }
@@ -130,4 +181,57 @@ pub enum GroupSecretError {
 
     #[error(transparent)]
     Decode(#[from] DecodeError),
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Rng;
+
+    use super::{GroupSecret, GroupSecretBundle};
+
+    #[test]
+    fn group_secret_bundle() {
+        let rng = Rng::from_seed([1; 32]);
+
+        let secret = GroupSecret::from_rng(&rng).unwrap();
+        assert_ne!(secret.as_bytes(), &secret.id());
+
+        let mut bundle_1 = GroupSecretBundle::from_secrets(vec![secret.clone()]);
+        let mut bundle_2 = GroupSecretBundle::from_secrets(vec![secret.clone()]);
+        assert_eq!(bundle_1.len(), 1);
+        assert_eq!(bundle_2.len(), 1);
+
+        assert_eq!(bundle_1.get(&secret.id()), bundle_2.get(&secret.id()));
+        assert!(bundle_1.get(&secret.id()).is_some());
+        assert!(bundle_1.contains(&secret.id()));
+
+        let unknown_secret = GroupSecret::from_rng(&rng).unwrap();
+        assert!(bundle_1.get(&unknown_secret.id()).is_none());
+        assert!(!bundle_1.contains(&unknown_secret.id()));
+
+        let secret_2 = GroupSecret::from_rng(&rng).unwrap();
+        bundle_2.insert(secret_2.clone());
+        assert_eq!(bundle_2.len(), 2);
+
+        bundle_1.extend(bundle_2);
+        assert_eq!(bundle_1.len(), 2);
+
+        assert!(bundle_1.remove(&secret_2.id()).is_some());
+        assert_eq!(bundle_1.len(), 1);
+    }
+
+    #[test]
+    fn serde() {
+        let rng = Rng::from_seed([1; 32]);
+
+        let bundle = GroupSecretBundle::from_secrets(vec![
+            GroupSecret::from_rng(&rng).unwrap(),
+            GroupSecret::from_rng(&rng).unwrap(),
+            GroupSecret::from_rng(&rng).unwrap(),
+            GroupSecret::from_rng(&rng).unwrap(),
+        ]);
+
+        let bytes = bundle.to_bytes().unwrap();
+        assert_eq!(bundle, GroupSecretBundle::try_from_bytes(&bytes).unwrap());
+    }
 }
