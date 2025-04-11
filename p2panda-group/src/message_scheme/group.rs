19fcef814de4acbd15dab4c34387080b12b95f31
adz <x12@adz.garden>
2025-04-11 20:50:39 +0200

    Update group, add or remove members

--- p2panda-group/src/message_scheme/group.rs
+++ p2panda-group/src/message_scheme/group.rs
@@ -5,9 +5,11 @@ use std::marker::PhantomData;
 
 use thiserror::Error;
 
-use crate::crypto::{Rng, Secret};
+use crate::crypto::Rng;
 use crate::key_bundle::OneTimeKeyBundle;
-use crate::message_scheme::dcgka::{ControlMessage, Dcgka, DcgkaError, DcgkaState, DirectMessage};
+use crate::message_scheme::dcgka::{
+    ControlMessage, Dcgka, DcgkaError, DcgkaState, DirectMessage, OperationOutput,
+};
 use crate::message_scheme::ratchet::{DecryptionRatchetState, RatchetSecret, RatchetSecretState};
 use crate::traits::{
     AckedGroupMembership, ForwardSecureOrdering, IdentityHandle, IdentityManager, IdentityRegistry,
@@ -18,14 +20,14 @@ pub struct MessageGroup<ID, OP, PKI, DGM, KMG, ORD> {
     _marker: PhantomData<(ID, OP, PKI, DGM, KMG, ORD)>,
 }
 
-pub struct MessageGroupState<ID, OP, PKI, DGM, KMG, ORD>
+pub struct GroupState<ID, OP, PKI, DGM, KMG, ORD>
 where
     ID: IdentityHandle,
     OP: OperationId,
     PKI: IdentityRegistry<ID, PKI::State> + PreKeyRegistry<ID, OneTimeKeyBundle>,
     DGM: AckedGroupMembership<ID, OP>,
     KMG: IdentityManager<KMG::State> + PreKeyManager,
-    ORD: ForwardSecureOrdering<ID, OP>,
+    ORD: ForwardSecureOrdering<ID, OP, DGM>,
 {
     my_id: ID,
     dcgka: DcgkaState<ID, OP, PKI, DGM, KMG>,
@@ -41,7 +43,7 @@ where
     PKI: IdentityRegistry<ID, PKI::State> + PreKeyRegistry<ID, OneTimeKeyBundle>,
     DGM: AckedGroupMembership<ID, OP>,
     KMG: IdentityManager<KMG::State> + PreKeyManager,
-    ORD: ForwardSecureOrdering<ID, OP>,
+    ORD: ForwardSecureOrdering<ID, OP, DGM>,
 {
     pub fn init(
         my_id: ID,
@@ -49,8 +51,8 @@ where
         pki: PKI::State,
         dgm: DGM::State,
         orderer: ORD::State,
-    ) -> MessageGroupState<ID, OP, PKI, DGM, KMG, ORD> {
-        MessageGroupState {
+    ) -> GroupState<ID, OP, PKI, DGM, KMG, ORD> {
+        GroupState {
             my_id,
             dcgka: Dcgka::init(my_id, my_keys, pki, dgm),
             orderer,
@@ -60,70 +62,127 @@ where
     }
 
     pub fn create(
-        mut y: MessageGroupState<ID, OP, PKI, DGM, KMG, ORD>,
+        mut y: GroupState<ID, OP, PKI, DGM, KMG, ORD>,
         initial_members: Vec<ID>,
         rng: &Rng,
-    ) -> MessageGroupResult<ORD::Message, ID, OP, PKI, DGM, KMG, ORD> {
-        // Make sure the group state is not established yet.
-        assert!(y.ratchet.is_none()); // TODO: Return an error here instead.
+    ) -> GroupResult<ORD::Message, ID, OP, PKI, DGM, KMG, ORD> {
+        // If we have an encryption ratchet we already established a group (either by creating or
+        // processing a "welcome" message in the past).
+        if y.ratchet.is_some() {
+            return Err(MessageGroupError::GroupAlreadyEstablished);
+        }
 
         // Create new group with initial members.
         let (y_dcgka_i, pre) = Dcgka::create(y.dcgka, initial_members, rng)?;
         y.dcgka = y_dcgka_i;
 
-        // Determine parameters for "create" control message.
-        let (y_orderer_i, message) = ORD::next_control_message(y.orderer, &pre.control_message)
-            .map_err(|err| MessageGroupError::Orderer(err))?;
-        y.orderer = y_orderer_i;
+        Ok(Self::process_local(y, pre, rng)?)
+    }
 
-        // Process "create" control message locally.
-        let (y_dcgka_ii, output) = Dcgka::process_local(y.dcgka, message.id(), pre, rng)?;
-        y.dcgka = y_dcgka_ii;
+    pub fn add(
+        mut y: GroupState<ID, OP, PKI, DGM, KMG, ORD>,
+        added: ID,
+        rng: &Rng,
+    ) -> GroupResult<ORD::Message, ID, OP, PKI, DGM, KMG, ORD> {
+        if y.ratchet.is_none() {
+            return Err(MessageGroupError::GroupNotYetEstablished);
+        }
 
-        // Establish our own encryption ratchet for application messages.
-        y.ratchet = Some(RatchetSecret::init(
-            output
-                .me_update_secret
-                .expect("'create' operation always yields an update secret for us")
-                .into(),
-        ));
+        if y.my_id == added {
+            return Err(MessageGroupError::NotAddOurselves);
+        }
 
-        Ok((y, message))
+        // Add a new member to the group.
+        let (y_dcgka_i, pre) = Dcgka::add(y.dcgka, added, rng)?;
+        y.dcgka = y_dcgka_i;
+
+        Ok(Self::process_local(y, pre, rng)?)
+    }
+
+    pub fn remove(
+        mut y: GroupState<ID, OP, PKI, DGM, KMG, ORD>,
+        removed: ID,
+        rng: &Rng,
+    ) -> GroupResult<ORD::Message, ID, OP, PKI, DGM, KMG, ORD> {
+        if y.ratchet.is_none() {
+            return Err(MessageGroupError::GroupNotYetEstablished);
+        }
+
+        // Remove a member from the group.
+        let (y_dcgka_i, pre) = Dcgka::remove(y.dcgka, removed, rng)?;
+        y.dcgka = y_dcgka_i;
+
+        // TODO: Handle removing ourselves.
+
+        Ok(Self::process_local(y, pre, rng)?)
+    }
+
+    pub fn update(
+        mut y: GroupState<ID, OP, PKI, DGM, KMG, ORD>,
+        rng: &Rng,
+    ) -> GroupResult<ORD::Message, ID, OP, PKI, DGM, KMG, ORD> {
+        if y.ratchet.is_none() {
+            return Err(MessageGroupError::GroupNotYetEstablished);
+        }
+
+        // Update the group by generating a new seed.
+        let (y_dcgka_i, pre) = Dcgka::update(y.dcgka, rng)?;
+        y.dcgka = y_dcgka_i;
+
+        Ok(Self::process_local(y, pre, rng)?)
     }
 
     pub fn receive(
-        y: MessageGroupState<ID, OP, PKI, DGM, KMG, ORD>,
+        y: GroupState<ID, OP, PKI, DGM, KMG, ORD>,
         sender: ID,
         seq: OP,
         dependencies: &[OP],
         control_message: ControlMessage<ID, OP>,
         direct_messages: Vec<DirectMessage<ID, OP, DGM>>,
-    ) -> MessageGroupResult<(), ID, OP, PKI, DGM, KMG, ORD> {
+    ) -> GroupResult<(), ID, OP, PKI, DGM, KMG, ORD> {
         todo!()
     }
 
     pub fn send(
-        y: MessageGroupState<ID, OP, PKI, DGM, KMG, ORD>,
+        y: GroupState<ID, OP, PKI, DGM, KMG, ORD>,
         plaintext: &[u8],
-    ) -> MessageGroupResult<(), ID, OP, PKI, DGM, KMG, ORD> {
+    ) -> GroupResult<(), ID, OP, PKI, DGM, KMG, ORD> {
+        if y.ratchet.is_none() {
+            return Err(MessageGroupError::GroupNotYetEstablished);
+        }
+
         todo!()
     }
 
-    pub fn add() {
-        todo!()
-    }
+    fn process_local(
+        mut y: GroupState<ID, OP, PKI, DGM, KMG, ORD>,
+        output: OperationOutput<ID, OP, DGM>,
+        rng: &Rng,
+    ) -> GroupResult<ORD::Message, ID, OP, PKI, DGM, KMG, ORD> {
+        // Determine parameters for to-be-published control message.
+        let (y_orderer_i, message) =
+            ORD::next_control_message(y.orderer, &output.control_message, &output.direct_messages)
+                .map_err(|err| MessageGroupError::Orderer(err))?;
+        y.orderer = y_orderer_i;
 
-    pub fn remove() {
-        todo!()
-    }
+        // Process control message locally to update our state.
+        let (y_dcgka_i, process) = Dcgka::process_local(y.dcgka, message.id(), output, rng)?;
+        y.dcgka = y_dcgka_i;
 
-    pub fn update() {
-        todo!()
+        // Update own encryption ratchet for sending application messages.
+        y.ratchet = Some(RatchetSecret::init(
+            process
+                .me_update_secret
+                .expect("local operation always yields an update secret for us")
+                .into(),
+        ));
+
+        Ok((y, message))
     }
 }
 
-pub type MessageGroupResult<T, ID, OP, PKI, DGM, KMG, ORD> = Result<
-    (MessageGroupState<ID, OP, PKI, DGM, KMG, ORD>, T),
+pub type GroupResult<T, ID, OP, PKI, DGM, KMG, ORD> = Result<
+    (GroupState<ID, OP, PKI, DGM, KMG, ORD>, T),
     MessageGroupError<ID, OP, PKI, DGM, KMG, ORD>,
 >;
 
@@ -133,11 +192,20 @@ where
     PKI: IdentityRegistry<ID, PKI::State> + PreKeyRegistry<ID, OneTimeKeyBundle>,
     DGM: AckedGroupMembership<ID, OP>,
     KMG: PreKeyManager,
-    ORD: ForwardSecureOrdering<ID, OP>,
+    ORD: ForwardSecureOrdering<ID, OP, DGM>,
 {
     #[error(transparent)]
     Dcgka(#[from] DcgkaError<ID, OP, PKI, DGM, KMG>),
 
     #[error(transparent)]
     Orderer(ORD::Error),
+
+    #[error("creating or joining a group is not possible, state is already established")]
+    GroupAlreadyEstablished,
+
+    #[error("state is not ready yet, group needs to be created or joined first")]
+    GroupNotYetEstablished,
+
+    #[error("can not add outselves to the group")]
+    NotAddOurselves,
 }
