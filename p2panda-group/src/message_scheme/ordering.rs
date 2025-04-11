37d8f9c2a590038c3d4ac3f8bb4a76a6ad774b6a
adz <x12@adz.garden>
2025-04-11 16:56:10 +0200

    Write a little bit about ordering

---
+++ p2panda-group/src/message_scheme/ordering.rs
@@ -0,0 +1,91 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+//! Peers need to make sure that messages arrive "in order" to be processed correctly.
+//!
+//! For this we need to define three things:
+//!
+//! 1. Define a way to partially order our messages (for example through a vector clock), like this
+//!    we can sort events "after" or "before" each other, or identify messages which arrived "at
+//!    the same time".
+//! 2. Define a way to declare "dependencies", that is, messages which are required to be processed
+//!    _before_ we can process this message. This is slightly different from a vector clock as we
+//!    do not only declare which message we've observed "before" to help with partial ordering, but
+//!    also point at additional requirements to fullfil the protocol.
+//! 3. Define a set of rules, the "protocol", peers need to follow whenever they publish new
+//!    messages: What information do they need to mention for other peers to correctly order and
+//!    process messages from us?
+//!
+//! When publishing a message peers need to make sure to provide the following informations:
+//!
+//! 1. "create" control messages do not have any dependencies as they are the first messages in a
+//!    group.
+//! 2. When an "add", "update" or "remove" control message is created, the message needs to point
+//!    at a) the last, previously processed control messages (by us and others), b) if any
+//!    application messages were sent by us, the last sent message. This helps with peers
+//!    understanding that they might miss a message when they switch to a new ratchet, they can
+//!    decide to ignore this message "dependency", but will also then potentially loose that
+//!    message. This can be useful to do if messages get lost and peers otherwise get "stuck".
+//! 3. "ack" control messages need to point at the regarding "create", "add", "update" or "remove"
+//!    control message they are acknowledging.
+//! 4. The first application message written during a new "ratchet epoch" needs to point at the
+//!    "ack" or "create", "add", "update" or "remove" message which initiated that epoch.
+//! 5. Every sub-sequent application message needs to point at the previous application message.
+//!
+//! In this example a user "Alice" creates a group with Bob. Both of them send messages into the
+//! group ("Message 1", "Messsage 2" etc.) based on the established ratchet secrets. At some point
+//! Alice decides to renew the group's seed with an "update", and at the same time (concurrently)
+//! Bob "adds" Charlie. After processing all messages in the correct order and meeting all
+//! dependencies Alice and Bob will be able to read all sent messages by each other.
+//!
+//! ```text
+//!
+//!        Alice
+//!       ────────
+//!       ┌──────┐
+//!       │CREATE│
+//!       └──────┘                   Bob
+//!         ▲ ▲ ▲                   ─────
+//!         │ │ │                   ┌───┐
+//!         │ │ └───────────────────┤ACK│
+//!         │ │                     └───┘
+//!         │ │                      ▲ ▲
+//!           │                      │ │
+//! Message 1 │                      │
+//!         ▲ │                      │ Message 1
+//!         │ │                      │ ▲
+//!           │                      │ │
+//! Message 2 │                      │
+//!         ▲ │                      │ Message 2
+//!         │ │                      │ ▲
+//!         │ │                      │ │
+//!        ┌┴─┴───┐                 ┌┴─┴┐
+//!        │UPDATE│   Concurrent!   │ADD│
+//!        └──────┘                 └───┘
+//!         ▲    ▲                   ▲ ▲                   Charlie
+//!         │    │                   │ │                  ─────────
+//!         │ ┌──┼───────────────────┘ ├──────────────────────┐
+//!           │  │                     │                      │
+//! Message 3 │  └───────────────────┐ │                      │
+//!         ▲ │                      │                        │
+//!         │ │                      │ Message 3              │
+//!           │                      │ ▲                      │
+//! Message 4 │                      │ │                      │
+//!           │                      │ │                      │
+//!         ┌─┴─┐                   ┌┴─┴┐                   ┌─┴─┐
+//!         │ACK│                   │ACK│                   │ACK│
+//!         └───┘                   └───┘                   └───┘
+//!```
+//!
+//! When a peer processes a "welcome" message (they got added to a group, like "Charlie" in our
+//! example), then the following steps take place:
+//!
+//! 1. All messages prior to the "welcome" message (the "add" which added us) can be ignored.
+//! 2. All messages after or concurrent to the "welcome" message need to be processed regularily
+//!    like all other messages.
+//! 3. All of this processing needs to be done before we can move on processing future messages.
+//!
+//! In the given example "Charlie" would be added to the group by Bob's "add" control message.
+//! Charlie would process the "welcome", acknowledge it and look at all messages now. They
+//! identified that Alice's "update" happened concurrently to the "add", so they also process this
+//! message. They ignore the "create" as it took place before the "add". They ignore "Message 1"
+//! and "Message 2" of both Alice and Bob, as they would not be able to decrypt them.
