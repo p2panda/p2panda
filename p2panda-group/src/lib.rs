8d12f4281249828c3c4c7128b4e1938867fec435
adz <adzialocha@users.noreply.github.com>
2025-04-16 08:38:33 +0200

    DCGKA for message encryption scheme & identity- and pre-key registry (#734)
    * Introduce Identity- and PreKeyRegistry
    
    * IdentityHandle and OperationId super traits
    
    * Basic KeyRegistry implementation
    
    * Add doc-strings to registry
    
    * Traits for acked group membership
    
    * DCGKA state
    
    * Make state serializable
    
    * Add an ASCII diagram
    
    * Use MGT as generic for key manager
    
    * More doc-strings
    
    * Complete DCGKA implementation
    
    * More doc-strings, make messages serializable
    
    * Doc-strings for DGM
    
    * Correct const value for key sizes
    
    * Improve doc-string for direct messages, add serde derives
    
    * Improve doc-string about paper adjustments
    
    * KMG for key manager generic
    
    * Non-optimal DGM for testing
    
    * Simplify ControlMessage by making it an enum
    
    * Test all group operations
    
    * Make clippy happy
    
    * WIP: General helpers to assert DCGKA group state in tests
    
    * First test checking all group operations
    
    * Fix linter errors in test_utils
    
    * Assertions for key material
    
    * Grammar
    
    * Tests for handling concurrency, fix DCGKA members view
    
    * Test second concurrency adds scenario
    
    * Note on DGM
    
    * Simplify process input, make direct messages part of struct
    
    * Clippy
    
    * Test struct for message id should be unique
    
    * Add test implementation of message id with member
    
    * Update tests to use new MessageId struct
    
    * Clean up unused exports and TODOs
    
    * Revert "Clean up unused exports and TODOs"
    
    This reverts commit 1c3cd52b2c1130ec6e4a8b0bcd6f57db56ee018d.
    
    * Remove TODO
    
    * Fix missing member info in DGM on concurrent removes
    
    * Add a DGM test for concurrency
    
    * DGM test: Check if daphne and charlie are correctly removed
    
    * Apply grammar fixes from code review
    
    Co-authored-by: glyph <gnomad@cryptolab.net>
    
    * Update p2panda-group/src/message_scheme/dcgka.rs
    
    Co-authored-by: glyph <gnomad@cryptolab.net>
    
    * Update p2panda-group/src/message_scheme/dcgka.rs
    
    Co-authored-by: glyph <gnomad@cryptolab.net>
    
    ---------
    
    Co-authored-by: glyph <gnomad@cryptolab.net>

--- p2panda-group/src/lib.rs
+++ p2panda-group/src/lib.rs
@@ -5,6 +5,8 @@
 mod crypto;
 mod key_bundle;
 mod key_manager;
+mod key_registry;
+mod message_scheme;
 pub mod traits;
 mod two_party;
 
@@ -13,6 +15,7 @@ pub use key_bundle::{
     Lifetime, LifetimeError, LongTermKeyBundle, OneTimeKeyBundle, OneTimePreKey, OneTimePreKeyId,
 };
 pub use key_manager::{KeyManager, KeyManagerError, KeyManagerState};
+pub use key_registry::{KeyRegistry, KeyRegistryState};
 pub use two_party::{
     LongTermTwoParty, OneTimeTwoParty, TwoParty, TwoPartyCiphertext, TwoPartyError, TwoPartyMessage,
 };
