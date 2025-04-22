a211ad3c64879a342a9a28a67f0e9887b35dfe70
adz <adzialocha@users.noreply.github.com>
2025-04-22 13:16:37 +0200

    "Message Encryption" Group API & orderer (#741)
    * Write a little bit about ordering
    
    * Add a note on missing concurrent messages
    
    * Start defining traits for orderer, introduce message info trait
    
    * Helper to convert from UpdateSecret to Secret
    
    * Introduce main API for message group encryption
    
    * Update group, add or remove members
    
    * Send encrypted messages with own rachet
    
    * Simplify DCGKA control message enum
    
    * Message traits module, get control and direct messages
    
    * Process remote messages and decrypt them
    
    * Add another TODO
    
    * Queue messages in orderer, and check for ready ones
    
    * WIP: Experimental orderer for tests
    
    * Remember last control messages we've processed by all peers
    
    * Test utils to create a network
    
    * Basic test sending and receiving messages
    
    * Own test_utils module
    
    * Naive implementation handling welcomes
    
    * Delete duplicate file
    
    * Improve doc-strings and names
    
    * Apply grammar fixes from code review
    
    Co-authored-by: glyph <gnomad@cryptolab.net>
    
    ---------
    
    Co-authored-by: glyph <gnomad@cryptolab.net>

--- p2panda-group/src/message_scheme/test_utils/dcgka.rs
+++ p2panda-group/src/message_scheme/test_utils/dcgka.rs
@@ -1,50 +1,16 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
 use std::collections::{HashMap, HashSet};
-use std::fmt::Display;
-
-use serde::{Deserialize, Serialize};
 
 use crate::crypto::x25519::SecretKey;
 use crate::message_scheme::acked_dgm::test_utils::AckedTestDGM;
+use crate::message_scheme::test_utils::{MemberId, MessageId};
 use crate::message_scheme::{
-    AckMessage, AddAckMessage, ControlMessage, Dcgka, DcgkaState, DirectMessage, DirectMessageType,
-    OperationOutput, ProcessOutput, UpdateSecret,
+    ControlMessage, Dcgka, DcgkaState, DirectMessage, DirectMessageType, OperationOutput,
+    ProcessOutput, UpdateSecret,
 };
-use crate::traits::{AckedGroupMembership, OperationId, PreKeyManager};
+use crate::traits::{AckedGroupMembership, PreKeyManager};
 use crate::{KeyManager, KeyRegistry, Lifetime, Rng};
-
-pub type MemberId = usize;
-
-#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
-pub struct MessageId {
-    pub sender: MemberId,
-    pub seq: usize,
-}
-
-impl MessageId {
-    pub fn new(my_id: MemberId) -> Self {
-        Self {
-            sender: my_id,
-            seq: 0,
-        }
-    }
-
-    pub fn inc(&self) -> Self {
-        Self {
-            sender: self.sender,
-            seq: self.seq + 1,
-        }
-    }
-}
-
-impl Display for MessageId {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "[id={}, seq={}]", self.sender, self.seq)
-    }
-}
-
-impl OperationId for MessageId {}
 
 pub type TestDcgkaState = DcgkaState<
     MemberId,
@@ -171,10 +137,13 @@ impl AssertableDcgka {
         // ~~~~~~~~~~~~~~~~
 
         // Group "creator" broadcasts a "create" control message to everyone.
-        let ControlMessage::Create(ref message) = output.control_message else {
+        let ControlMessage::Create {
+            ref initial_members,
+        } = output.control_message
+        else {
             panic!("expected \"create\" control message");
         };
-        assert_eq!(message.initial_members, expected_members.to_vec());
+        assert_eq!(initial_members, expected_members);
 
         // Direct messages
         // ~~~~~~~~~~~~~~~
@@ -259,10 +228,10 @@ impl AssertableDcgka {
         // ~~~~~~~~~~~~~~~~
 
         // "Processing" member of "create" message broadcasts an "ack" control message to everyone.
-        let Some(ControlMessage::Ack(AckMessage {
+        let Some(ControlMessage::Ack {
             ack_sender,
             ack_seq,
-        })) = output.control_message
+        }) = output.control_message
         else {
             panic!("expected \"ack\" control message");
         };
@@ -411,10 +380,10 @@ impl AssertableDcgka {
         // ~~~~~~~~~~~~~~~~
 
         // "Adder" broadcasts an "add" control message to everyone.
-        let ControlMessage::Add(ref message) = output.control_message else {
+        let ControlMessage::Add { added } = output.control_message else {
             panic!("expected \"add\" control message");
         };
-        assert_eq!(message.added, added_id);
+        assert_eq!(added, added_id);
 
         // Direct messages
         // ~~~~~~~~~~~~~~~
@@ -484,10 +453,10 @@ impl AssertableDcgka {
         // ~~~~~~~~~~~~~~~~
 
         // Added broadcasts an "ack" control message to everyone, no direct messages.
-        let Some(ControlMessage::Ack(AckMessage {
+        let Some(ControlMessage::Ack {
             ack_sender,
             ack_seq,
-        })) = output.control_message
+        }) = output.control_message
         else {
             panic!("expected \"ack\" control message");
         };
@@ -573,10 +542,10 @@ impl AssertableDcgka {
         // ~~~~~~~~~~~~~~~~
 
         // Processor broadcasts an "add-ack" control message to everyone.
-        let Some(ControlMessage::AddAck(AddAckMessage {
+        let Some(ControlMessage::AddAck {
             ack_sender,
             ack_seq,
-        })) = output.control_message
+        }) = output.control_message
         else {
             panic!("expected \"add-ack\" control message");
         };
@@ -702,10 +671,10 @@ impl AssertableDcgka {
         // ~~~~~~~~~~~~~~~~
 
         // "Remover" broadcasts a "remove" control message to everyone.
-        let ControlMessage::Remove(ref message) = output.control_message else {
+        let ControlMessage::Remove { removed } = output.control_message else {
             panic!("expected \"remove\" control message");
         };
-        assert_eq!(message.removed, removed_id);
+        assert_eq!(removed, removed_id);
 
         // Direct messages
         // ~~~~~~~~~~~~~~~
@@ -780,10 +749,10 @@ impl AssertableDcgka {
         // ~~~~~~~~~~~~~~~~
 
         // "Processor" of "remove" message broadcasts an "ack" control message to everyone.
-        let Some(ControlMessage::Ack(AckMessage {
+        let Some(ControlMessage::Ack {
             ack_sender,
             ack_seq,
-        })) = output.control_message
+        }) = output.control_message
         else {
             panic!("expected \"ack\" control message");
         };
@@ -856,7 +825,7 @@ impl AssertableDcgka {
         // ~~~~~~~~~~~~~~~~
 
         // "Updater" broadcasts an "update" control message to everyone.
-        let ControlMessage::Update(_) = output.control_message else {
+        let ControlMessage::Update = output.control_message else {
             panic!("expected \"update\" control message");
         };
 
@@ -928,10 +897,10 @@ impl AssertableDcgka {
         // ~~~~~~~~~~~~~~~~
 
         // Processor of "update" message broadcasts an "ack" control message to everyone.
-        let Some(ControlMessage::Ack(AckMessage {
+        let Some(ControlMessage::Ack {
             ack_sender,
             ack_seq,
-        })) = output.control_message
+        }) = output.control_message
         else {
             panic!("expected \"ack\" control message");
         };
