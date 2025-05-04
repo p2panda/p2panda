13c336175a959c32db89d89863a649fc22fd74f2
Sam Andreae <contact@samandreae.com>
2025-05-04 17:19:51 +0100

    Auth graph implementation

--- p2panda-auth/src/traits/mod.rs
+++ p2panda-auth/src/traits/mod.rs
@@ -1,9 +1,25 @@
+use std::fmt::Debug;
+use std::hash::Hash as StdHash;
+
+mod auth_graph;
 mod operation;
 mod ordering;
-mod auth_graph;
 mod resolver;
 
+pub use auth_graph::AuthGraph;
 pub use operation::Operation;
 pub use ordering::Ordering;
-pub use auth_graph::AuthGraph;
 pub use resolver::Resolver;
+
+/// Handle to identify a group member.
+///
+/// Note that this needs to be unique within a group, can be a username, number or preferably a
+/// long byte string.
+pub trait IdentityHandle: Copy + Debug + PartialEq + Eq + StdHash {}
+
+/// Identifier for each group membership operation.
+///
+/// Operations trigger changes of the group state and are usually sent in form of messages over the
+/// network. Each operation needs to be uniquely identifiable, preferably by a collision-resistant
+/// hash.
+pub trait OperationId: Copy + Debug + PartialEq + Eq + StdHash {}
