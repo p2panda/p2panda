c03a5ebaa5efc82787a6c6716e1462b4cddc97f1
Sam Andreae <contact@samandreae.com>
2025-05-04 13:59:44 +0100

    Introduce all core traits

---
+++ p2panda-auth/src/traits/operation.rs
@@ -0,0 +1,18 @@
+/// Interface to express required information from operations processed by any auth graph
+/// implementation.
+///
+/// Applications implementing these traits should authenticate the original sender of each
+/// operation.
+pub trait Operation<ID, OP, P> {
+    /// Id of this operation.
+    fn id(&self) -> OP;
+
+    /// Id of the sender of this operation.
+    fn sender(&self) -> ID;
+
+    /// Other operation dependencies.
+    fn dependencies(&self) -> &Vec<OP>;
+
+    /// Payload of this operation.
+    fn payload(&self) -> &P;
+}
---
+++ p2panda-auth/src/traits/resolver.rs
@@ -0,0 +1,21 @@
+use std::error::Error;
+use std::fmt::Debug;
+
+use serde::{Deserialize, Serialize};
+
+/// Resolver trait used in op-based CRDT for producing operation filters when concurrent
+/// operations cause conflicts which require special handling.
+///
+/// The generic parameter S is the state of the CRDT itself.
+pub trait Resolver<S, MSG> {
+    type Error: Error;
+
+    // Check if this message requires that a full state re-build takes place. This would usually
+    // be due to concurrent operations arriving which require special handling.
+    fn rebuild_required(y: &S, msg: &MSG) -> bool;
+
+    // Process all operations and update internal state as required.
+    //
+    // This could include updating any internal filter object.
+    fn process(y: S) -> Result<S, Self::Error>;
+}
