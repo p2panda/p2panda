df33a91d2c01b43e565ca5adbb6c1ad62a6e8f2f
Sam Andreae <contact@samandreae.com>
2025-05-06 23:41:19 +0100

    Implement test network

--- p2panda-auth/src/group/test_utils/network.rs
+++ p2panda-auth/src/group/test_utils/network.rs
@@ -1,136 +1,172 @@
-// // SPDX-License-Identifier: MIT OR Apache-2.0
-//
-// use std::collections::{HashMap, VecDeque};
-//
-// use crate::{
-//     group::{
-//         Group, GroupAction, GroupControlMessage, GroupMember, GroupState, access::Access,
-//         resolver::GroupResolver,
-//     },
-//     traits::{AuthGraph, IdentityHandle, OperationId},
-// };
-//
-// use super::{TestOperation, TestOrderer, TestOrdererState};
-//
-// type MemberId = char;
-// type MessageId = u32;
-// impl IdentityHandle for MemberId {}
-// impl OperationId for MessageId {}
-//
-// type TestResolver = GroupResolver<MemberId, MessageId, TestOperation<MemberId, MessageId>>;
-// type TestGroup = Group<MemberId, MessageId, TestResolver, TestOrderer>;
-// type TestGroupState = GroupState<MemberId, MessageId, TestOrderer>;
-//
-// pub struct Network {
-//     members: HashMap<MemberId, TestGroupState>,
-//     queue: VecDeque<TestOperation<MemberId, MessageId>>,
-// }
-//
-// impl Network {
-//     pub fn new<const N: usize>(group_id: MemberId, members: [MemberId; N]) -> Self {
-//         Self {
-//             members: HashMap::from_iter(members.into_iter().map(|member_id| {
-//                 (member_id, {
-//                     TestGroupState::new(member_id, group_id, TestOrdererState::new(member_id))
-//                 })
-//             })),
-//             queue: VecDeque::new(),
-//         }
-//     }
-//
-//     pub fn create(
-//         &mut self,
-//         creator: MemberId,
-//         initial_members: Vec<(GroupMember<MemberId>, Access)>,
-//     ) {
-//         let y = self.get_y(&creator);
-//         let control_message = GroupControlMessage::GroupAction {
-//             group_id,
-//             action: GroupAction::Create { initial_members },
-//         };
-//         let (y_i, operation) = TestGroup::prepare(y, &control_message).unwrap();
-//         let y_ii = TestGroup::process(y_i, &operation).unwrap();
-//         self.queue.push_back(operation);
-//         self.set_y(y_ii);
-//     }
-//
-//     pub fn add(&mut self, adder: MemberId, added: MemberId, access: Access) {
-//         let y = self.get_y(&adder);
-//         let operation = GroupOperation::GroupAction(GroupAction::Add {
-//             member: added,
-//             access,
-//         });
-//         let (y_i, message) = TestGroup::prepare(y, operation).unwrap();
-//         let filter = y_i.filter.clone();
-//         let (mut y_ii, filter) = TestGroup::process(y_i, message.clone(), filter).unwrap();
-//         y_ii.filter = filter;
-//         self.queue.push_back(message);
-//         self.set_y(y_ii);
-//     }
-//
-//     pub fn remove(&mut self, remover: MemberId, removed: MemberId) {
-//         let y = self.get_y(&remover);
-//         let operation = GroupOperation::GroupAction(GroupAction::Remove { member: removed });
-//         let (y_i, message) = TestGroup::prepare(y, operation).unwrap();
-//         let filter = y_i.filter.clone();
-//         let (mut y_ii, filter) = TestGroup::process(y_i, message.clone(), filter).unwrap();
-//         y_ii.filter = filter;
-//         self.queue.push_back(message);
-//         self.set_y(y_ii);
-//         self.get_y(&removed);
-//     }
-//
-//     pub fn process(&mut self) {
-//         if self.queue.is_empty() {
-//             return;
-//         }
-//
-//         let member_ids: Vec<MemberId> = self.members.keys().cloned().collect();
-//
-//         while let Some(message) = self.queue.pop_front() {
-//             for id in &member_ids {
-//                 // Do not process our own messages.
-//                 if &message.sender() == id {
-//                     continue;
-//                 }
-//
-//                 let mut y = self.get_y(id);
-//                 let mut filter: GroupFilter<char, u32, Message<char, u32>> = y.filter.clone();
-//
-//                 y.ordering_y = TestOrderer::queue(y.ordering_y.clone(), &message).unwrap();
-//
-//                 loop {
-//                     let (y_orderer_next, result) =
-//                         TestOrderer::next_ready_message(y.ordering_y.clone()).unwrap();
-//                     y.ordering_y = y_orderer_next;
-//
-//                     let Some(message) = result else {
-//                         break;
-//                     };
-//
-//                     (y, filter) = TestGroup::process(y.clone(), message, filter).unwrap();
-//                 }
-//                 y.filter = filter;
-//                 self.set_y(y);
-//             }
-//         }
-//     }
-//
-//     pub fn members(&self, member: &MemberId) -> Vec<(MemberId, Access)> {
-//         let y = self.members.get(member).expect("member exists");
-//         let mut members = Vec::from_iter(TestGroup::members(y));
-//         members.sort();
-//         members
-//     }
-//
-//     fn get_y(&mut self, member: &MemberId) -> TestGroupState {
-//         self.members.remove(member).expect("member exists")
-//     }
-//
-//     fn set_y(&mut self, y: TestGroupState) {
-//         assert!(
-//             self.members.insert(y.my_id, y).is_none(),
-//             "state was removed before insertion"
-//         );
-//     }
-// }
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+use std::collections::{HashMap, VecDeque};
+
+use crate::group::{GroupAction, GroupControlMessage, GroupMember, access::Access};
+use crate::traits::{AuthGraph, Operation, Ordering};
+
+use super::{
+    TestGroup, TestGroupState, TestGroupStateInner, TestGroupStoreState, TestOperation,
+    TestOrderer, TestOrdererState,
+};
+
+type MemberId = char;
+type GroupId = char;
+type MessageId = u32;
+
+pub struct Network {
+    group_store_y: TestGroupStoreState<MemberId, TestGroupStateInner>,
+    members: HashMap<MemberId, HashMap<GroupId, TestGroupState>>,
+    queue: VecDeque<TestOperation<MemberId, MessageId>>,
+}
+
+impl Network {
+    pub fn new<const N: usize>(members: [MemberId; N]) -> Self {
+        Self {
+            group_store_y: TestGroupStoreState::default(),
+            members: HashMap::from_iter(
+                members
+                    .into_iter()
+                    .map(|member_id| (member_id, HashMap::default())),
+            ),
+            queue: VecDeque::new(),
+        }
+    }
+
+    pub fn create(
+        &mut self,
+        group_id: GroupId,
+        creator: MemberId,
+        initial_members: Vec<(GroupMember<MemberId>, Access)>,
+    ) {
+        let y = TestGroupState::new(
+            creator,
+            group_id,
+            self.group_store_y.clone(),
+            TestOrdererState::new(creator),
+        );
+        let control_message = GroupControlMessage::GroupAction {
+            group_id,
+            action: GroupAction::Create { initial_members },
+        };
+        let (y_i, operation) = TestGroup::prepare(y, &control_message).unwrap();
+        let y_ii = TestGroup::process(y_i, &operation).unwrap();
+        self.queue.push_back(operation);
+        self.set_y(y_ii);
+    }
+
+    pub fn add(
+        &mut self,
+        adder: MemberId,
+        added: GroupMember<MemberId>,
+        group_id: GroupId,
+        access: Access,
+    ) {
+        let y = self.get_y(&adder, &group_id);
+        let control_message = GroupControlMessage::GroupAction {
+            group_id,
+            action: GroupAction::Add {
+                member: added,
+                access,
+            },
+        };
+        let (y_i, operation) = TestGroup::prepare(y, &control_message).unwrap();
+        let y_ii = TestGroup::process(y_i, &operation).unwrap();
+        self.queue.push_back(operation);
+        self.set_y(y_ii);
+    }
+
+    pub fn remove(&mut self, remover: MemberId, removed: GroupMember<MemberId>, group_id: GroupId) {
+        let y = self.get_y(&remover, &group_id);
+        let control_message = GroupControlMessage::GroupAction {
+            group_id,
+            action: GroupAction::Remove { member: removed },
+        };
+        let (y_i, operation) = TestGroup::prepare(y, &control_message).unwrap();
+        let y_ii = TestGroup::process(y_i, &operation).unwrap();
+        self.queue.push_back(operation);
+        self.set_y(y_ii);
+    }
+
+    pub fn process(&mut self) {
+        if self.queue.is_empty() {
+            return;
+        }
+
+        let member_ids: Vec<MemberId> = self.members.keys().cloned().collect();
+
+        while let Some(operation) = self.queue.pop_front() {
+            let control_message = operation.payload();
+            let group_id = control_message.group_id();
+
+            for id in &member_ids {
+                // Do not process our own messages.
+                if &operation.sender() == id {
+                    continue;
+                }
+
+                let mut y = self.get_y(id, &group_id);
+                y.orderer_y = TestOrderer::queue(y.orderer_y.clone(), &operation).unwrap();
+
+                loop {
+                    let (y_orderer_next, result) =
+                        TestOrderer::next_ready_message(y.orderer_y.clone()).unwrap();
+                    y.orderer_y = y_orderer_next;
+
+                    let Some(message) = result else {
+                        break;
+                    };
+
+                    y = TestGroup::process(y.clone(), &operation).unwrap();
+                }
+                self.set_y(y);
+            }
+        }
+    }
+
+    pub fn members(
+        &self,
+        member: &MemberId,
+        group_id: &GroupId,
+    ) -> Vec<(GroupMember<MemberId>, Access)> {
+        let y = self
+            .members
+            .get(member)
+            .expect("member exists")
+            .get(group_id)
+            .expect("group exists");
+
+        let mut members = y.members();
+        members.sort();
+        members
+    }
+
+    fn get_y(&mut self, member: &MemberId, group_id: &GroupId) -> TestGroupState {
+        let group_y = self
+            .members
+            .get_mut(member)
+            .expect("member exists")
+            .remove(group_id);
+
+        match group_y {
+            Some(group_y) => group_y,
+            None => TestGroupState::new(
+                *member,
+                *group_id,
+                self.group_store_y.clone(),
+                TestOrdererState::new(*member),
+            ),
+        }
+    }
+
+    fn set_y(&mut self, y: TestGroupState) {
+        assert!(
+            self.members
+                .get_mut(&y.my_id)
+                .expect("member exists")
+                .insert(y.id(), y)
+                .is_none(),
+            "state was removed before insertion"
+        );
+    }
+}
