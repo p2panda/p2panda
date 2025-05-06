2b4e1f7632fca031ab131f65d51fc307236ab153
Sam Andreae <contact@samandreae.com>
2025-05-06 15:59:58 +0100

    Refactor GroupStore to follow established functional approach

--- p2panda-auth/src/group/test_utils/group_store.rs
+++ p2panda-auth/src/group/test_utils/group_store.rs
@@ -1,53 +1,56 @@
-use std::cell::RefCell;
 use std::collections::HashMap;
+use std::fmt::{Debug, Display};
 use std::rc::Rc;
+use std::{cell::RefCell, marker::PhantomData};
 
-use crate::{
-    group::GroupStateInner,
-    traits::{GroupStore, IdentityHandle, OperationId},
-};
+use thiserror::Error;
+
+use crate::traits::{GroupStore, IdentityHandle};
+
+use super::TestGroupStateInner;
+
+#[derive(Debug, Clone)]
+pub struct TestGroupStoreState<ID, G>(Rc<RefCell<HashMap<ID, G>>>)
+where
+    ID: IdentityHandle + Display;
+
+#[derive(Debug, Error)]
+pub enum GroupStoreError {}
 
 #[derive(Clone, Debug)]
-pub struct MemoryStore<ID, OP, MSG>(Rc<RefCell<HashMap<ID, GroupStateInner<ID, OP, MSG>>>>)
-where
-    ID: IdentityHandle,
-    OP: OperationId + Ord,
-    MSG: Clone;
+pub struct TestGroupStore<ID, G> {
+    phantom: PhantomData<(ID, G)>,
+}
 
-impl<ID, OP, MSG> Default for MemoryStore<ID, OP, MSG>
+impl<ID> Default for TestGroupStoreState<ID, TestGroupStateInner>
 where
-    ID: IdentityHandle,
-    OP: OperationId + Ord,
-    MSG: Clone,
+    ID: IdentityHandle + Display,
 {
     fn default() -> Self {
         Self(Rc::new(RefCell::new(HashMap::new())))
     }
 }
 
-impl<ID, OP, MSG> GroupStore<ID, OP, MSG> for MemoryStore<ID, OP, MSG>
-where
-    ID: IdentityHandle,
-    OP: OperationId + Ord,
-    MSG: Clone,
-{
-    fn get(&self, id: &ID) -> Option<GroupStateInner<ID, OP, MSG>> {
-        let store = self.0.borrow();
+impl GroupStore<char, TestGroupStateInner> for TestGroupStore<char, TestGroupStateInner> {
+    type State = TestGroupStoreState<char, TestGroupStateInner>;
+
+    type Error = GroupStoreError;
+
+    fn get(y: &Self::State, id: &char) -> Result<Option<TestGroupStateInner>, Self::Error> {
+        let store = y.0.borrow();
         let inner = store.get(id);
-        inner.cloned()
+        Ok(inner.cloned())
     }
 
     fn insert(
-        &self,
-        id: &ID,
-        group: GroupStateInner<ID, OP, MSG>,
-    ) -> Option<GroupStateInner<ID, OP, MSG>> {
-        let mut store = self.0.borrow_mut();
-        store.insert(*id, group)
-    }
-
-    fn remove(&self, id: &ID) -> Option<GroupStateInner<ID, OP, MSG>> {
-        let mut store = self.0.borrow_mut();
-        store.remove(id)
+        y: Self::State,
+        id: &char,
+        group: &TestGroupStateInner,
+    ) -> Result<Self::State, Self::Error> {
+        {
+            let mut store = y.0.borrow_mut();
+            store.insert(*id, group.clone());
+        }
+        Ok(y)
     }
 }
