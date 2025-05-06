4316ee8dd048be0901abcf1c85337ff8dd05eef6
Sam Andreae <contact@samandreae.com>
2025-05-06 11:09:52 +0100

    WIP: introduce group store

--- p2panda-auth/src/traits/mod.rs
+++ p2panda-auth/src/traits/mod.rs
@@ -2,11 +2,13 @@ use std::fmt::Debug;
 use std::hash::Hash as StdHash;
 
 mod auth_graph;
+mod group_store;
 mod operation;
 mod ordering;
 mod resolver;
 
 pub use auth_graph::AuthGraph;
+pub use group_store::GroupStore;
 pub use operation::Operation;
 pub use ordering::Ordering;
 pub use resolver::Resolver;
