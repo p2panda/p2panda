7f725106496f97b382b08cff8d641565272d688b
glyph <glyph@mycelial.technology>
2025-06-27 12:50:52 +0100

    Add doc strings and initial_member validation

--- p2panda-auth/src/group/dgm.rs
+++ p2panda-auth/src/group/dgm.rs
@@ -25,6 +25,9 @@ where
     #[error(transparent)]
     Group(#[from] GroupError<ID, OP, C, RS, ORD, GS>),
 
+    #[error("group must be created with at least one initial member")]
+    EmptyGroup,
+
     #[error("actor {0} is already a member of group {1}")]
     GroupMember(ID, ID),
 
@@ -38,6 +41,20 @@ where
     SameAccessLevel(ID, Access<C>, ID),
 }
 
+/// Decentralised Group Management (DGM).
+///
+/// The `GroupManager` provides a high-level interface for creating and updating groups. These
+/// groups provide a means for restricting access to application data and resources. Groups are
+/// comprised of members, which may be individuals or groups, and are assigned a user-chosen
+/// identity. Each member is assigned a unique user-chosen identifier and access level. Access
+/// levels are used to enforce restrictions over access to data and the mutation of that data.
+/// They are also used to grant permissions which allow for mutating the group state by adding,
+/// removing and modifying the access level of other members.
+///
+/// Each `GroupManager` method performs internal validation to ensure that the desired group
+/// action is valid in light of the current group state. Attempting to perform an invalid action
+/// results in a `GroupManagerError`. For example, attempting to remove a member who is not
+/// currently part of the group.
 pub struct GroupManager<ID, OP, C, RS, ORD, GS>
 where
     ID: IdentityHandle,
@@ -92,11 +109,24 @@ where
     type Action = GroupControlMessage<ID, OP, C>;
     type Error = GroupManagerError<ID, OP, C, RS, ORD, GS>;
 
+    /// Create a group.
+    ///
+    /// The caller of this method should ensure that the given `group_id` is unique. The set of
+    /// initial members must not be empty; failure to meet these conditions will result in an error.
+    /// Group updates will not be possible if the group is not created with at least one manager,
+    /// since `Manage` access is required to perform any group state modifications.
     fn create(
         &self,
         group_id: ID,
         initial_members: Vec<(GroupMember<ID>, Access<C>)>,
     ) -> Result<(Self::State, ORD::Message), Self::Error> {
+        if initial_members.is_empty() {
+            return Err(GroupManagerError::EmptyGroup);
+        }
+
+        // TODO: Understand exactly what happens if two groups are created with the same
+        // `group_id`. Does an error occur? If so, where and when?
+
         let y = GroupState::new(
             self.my_id,
             group_id,
@@ -115,6 +145,7 @@ where
         Ok((y, operation))
     }
 
+    /// Create a group by processing a remote operation.
     fn create_from_remote(
         &self,
         remote_operation: ORD::Message,
@@ -131,6 +162,10 @@ where
         Ok(y)
     }
 
+    /// Add a group member.
+    ///
+    /// The `adder` must be a manager and the `added` identity must not already be a member of
+    /// the group; failure to meet these conditions will result in an error.
     fn add(
         y: Self::State,
         adder: ID,
@@ -164,6 +199,10 @@ where
         Ok((y, operation))
     }
 
+    /// Remove a group member.
+    ///
+    /// The `remover` must be a manager and the `removed` identity must already be a member of
+    /// the group; failure to meet these conditions will result in an error.
     fn remove(
         y: Self::State,
         remover: ID,
@@ -195,6 +234,12 @@ where
         Ok((y, operation))
     }
 
+    /// Promote a group member to the given access level.
+    ///
+    /// The `promoter` must be a manager and the `promoted` identity must already be a member of
+    /// the group; failure to meet thess conditions will result in an error. A redundant access
+    /// level assignment will also result in an error; for example, if the `promoted` member
+    /// currently has `Read` access and the given access is also `Read`.
     fn promote(
         y: Self::State,
         promoter: ID,
@@ -235,6 +280,12 @@ where
         Ok((y, operation))
     }
 
+    /// Demote a group member to the given access level.
+    ///
+    /// The `demoter` must be a manager and the `demoted` identity must already be a member of
+    /// the group; failure to meet these conditions will result in an error. A redundant access
+    /// level assignment will also result in an error; for example, if the `demoted` member
+    /// currently has `Manage` access and the given access is also `Manage`.
     fn demote(
         y: Self::State,
         demoter: ID,
