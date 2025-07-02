d5de67516c2f54bda0c635f0bddedec73127317a
glyph <glyph@mycelial.technology>
2025-07-02 09:41:08 +0100

    Always return error from query trait methods

--- p2panda-auth/src/traits/query.rs
+++ p2panda-auth/src/traits/query.rs
@@ -11,23 +11,25 @@ pub trait GroupMembershipQuery<ID, OP, C> {
     type Error: Error;
 
     /// Query the current access level of the given member.
+    ///
+    /// The member is expected to be a "stateless" individual, not a "stateful" group.
     fn access(y: &Self::State, member: &ID) -> Result<Access<C>, Self::Error>;
 
     /// Query group membership.
-    fn member_ids(y: &Self::State) -> HashSet<ID>;
+    fn member_ids(y: &Self::State) -> Result<HashSet<ID>, Self::Error>;
 
     /// Return `true` if the given ID is an active member of the group.
-    fn is_member(y: &Self::State, possible_member: &ID) -> bool;
+    fn is_member(y: &Self::State, member: &ID) -> Result<bool, Self::Error>;
 
     /// Return `true` if the given member is currently assigned the `Pull` access level.
-    fn is_puller(y: &Self::State, member: &ID) -> bool;
+    fn is_puller(y: &Self::State, member: &ID) -> Result<bool, Self::Error>;
 
     /// Return `true` if the given member is currently assigned the `Read` access level.
-    fn is_reader(y: &Self::State, member: &ID) -> bool;
+    fn is_reader(y: &Self::State, member: &ID) -> Result<bool, Self::Error>;
 
     /// Return `true` if the given member is currently assigned the `Write` access level.
-    fn is_writer(y: &Self::State, member: &ID) -> bool;
+    fn is_writer(y: &Self::State, member: &ID) -> Result<bool, Self::Error>;
 
     /// Return `true` if the given member is currently assigned the `Manage` access level.
-    fn is_manager(y: &Self::State, member: &ID) -> bool;
+    fn is_manager(y: &Self::State, member: &ID) -> Result<bool, Self::Error>;
 }
