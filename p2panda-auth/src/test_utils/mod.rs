2e00089bb4d88b892fb82c1d87424065be32cb59
Sam Andreae <contact@samandreae.com>
2025-07-04 16:49:41 +0100

    Improve exports from test_utils

--- p2panda-auth/src/test_utils/mod.rs
+++ p2panda-auth/src/test_utils/mod.rs
@@ -5,12 +5,11 @@ mod network;
 mod orderer;
 mod partial_ord;
 
-pub use crate::test_utils::group_store::TestGroupStore;
-pub use network::Network;
-pub use orderer::*;
-pub use partial_ord::*;
+pub(crate) use crate::test_utils::group_store::TestGroupStore;
+pub(crate) use network::Network;
+pub(crate) use orderer::*;
+pub(crate) use partial_ord::*;
 
-use crate::Access;
 use crate::group::resolver::StrongRemove;
 use crate::group::{GroupCrdt, GroupCrdtState};
 use crate::traits::{IdentityHandle, OperationId};
@@ -18,16 +17,17 @@ use crate::traits::{IdentityHandle, OperationId};
 impl IdentityHandle for char {}
 impl OperationId for u32 {}
 
-pub type MemberId = char;
-pub type GroupId = char;
-pub type MessageId = u32;
-pub type Conditions = ();
+pub(crate) type MemberId = char;
+pub(crate) type MessageId = u32;
+pub(crate) type Conditions = ();
 
-pub type GenericTestResolver<ORD, GS> = StrongRemove<MemberId, MessageId, Conditions, ORD, GS>;
-pub type GenericTestGroup<RS, ORD, GS> = GroupCrdt<MemberId, MessageId, Conditions, RS, ORD, GS>;
-pub type GenericTestGroupState<RS, ORD, GS> =
+pub(crate) type GenericTestResolver<ORD, GS> =
+    StrongRemove<MemberId, MessageId, Conditions, ORD, GS>;
+pub(crate) type GenericTestGroup<RS, ORD, GS> =
+    GroupCrdt<MemberId, MessageId, Conditions, RS, ORD, GS>;
+pub(crate) type GenericTestGroupState<RS, ORD, GS> =
     GroupCrdtState<MemberId, MessageId, Conditions, RS, ORD, GS>;
 
-pub type TestResolver = GenericTestResolver<TestOrderer, TestGroupStore>;
-pub type TestGroup = GenericTestGroup<TestResolver, TestOrderer, TestGroupStore>;
-pub type TestGroupState = GenericTestGroupState<TestResolver, TestOrderer, TestGroupStore>;
+pub(crate) type TestResolver = GenericTestResolver<TestOrderer, TestGroupStore>;
+pub(crate) type TestGroup = GenericTestGroup<TestResolver, TestOrderer, TestGroupStore>;
+pub(crate) type TestGroupState = GenericTestGroupState<TestResolver, TestOrderer, TestGroupStore>;
