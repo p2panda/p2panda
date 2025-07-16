3e28f2fe4e223bf2065006249a4f5645dc168ef6
Sam Andreae <contact@samandreae.com>
2025-07-16 11:19:10 +0100

    Use inner mutability pattern for Manager and impl Clone

--- p2panda-spaces/src/manager.rs
+++ p2panda-spaces/src/manager.rs
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
 use std::marker::PhantomData;
+use std::sync::Arc;
+use tokio::sync::RwLock;
 
 use crate::event::Event;
 use crate::group::Group;
@@ -27,6 +29,10 @@ use crate::traits::Forge;
 ///
 /// Is agnostic to current p2panda-streams, networking layer, data type?
 pub struct Manager<S, F, M> {
+    inner: Arc<RwLock<InnerManager<S, F, M>>>,
+}
+
+pub struct InnerManager<S, F, M> {
     forge: F,
     store: S,
     _marker: PhantomData<M>,
@@ -38,10 +44,13 @@ where
     F: Forge<M>,
 {
     pub fn new(store: S, forge: F) -> Self {
-        Self {
+        let inner = InnerManager {
             forge,
             store,
             _marker: PhantomData,
+        };
+        Self {
+            inner: Arc::new(RwLock::new(inner)),
         }
     }
 
@@ -49,8 +58,8 @@ where
         todo!()
     }
 
-    pub fn create_space(&mut self) -> Space<S, F, M> {
-        todo!()
+    pub fn create_space(&self) -> Space<S, F, M> {
+        Space::create(self.clone())
     }
 
     pub fn group(&self) -> Group {
@@ -63,5 +72,16 @@ where
 
     pub fn process(&mut self, _message: &M) -> Vec<Event<S, F, M>> {
         todo!()
+    }
+}
+
+/// Deriving clone on Manager will enforce generics to also impl Clone even though we are wrapping
+/// them in an Arc. See related discussion:
+/// https://stackoverflow.com/questions/72150623/deriveclone-seems-to-wrongfully-enforce-generic-to-be-clone
+impl<S, F, M> Clone for Manager<S, F, M> {
+    fn clone(&self) -> Self {
+        Self {
+            inner: self.inner.clone(),
+        }
     }
 }
