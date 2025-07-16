1bc17bd158a38c06c468b8c2523cbd73e262f43f
Sam Andreae <contact@samandreae.com>
2025-07-16 12:03:13 +0100

    WIP: create method on Space

--- p2panda-spaces/src/space.rs
+++ p2panda-spaces/src/space.rs
@@ -10,6 +10,21 @@ pub struct Space<S, F, M> {
 }
 
 impl<S, F, M> Space<S, F, M> {
+    pub(crate) fn create(manager: Manager<S, F, M>) -> Self {
+
+        // 1. derive a space id
+        //    - generate new key pair
+        //    - use public key for space id
+        //    - use the private key to sign the control message
+        //    - throw away the private key
+        // 2. establish auth group state with create control message
+        // 3. establish encryption group state with create control message
+        // 4. merge and sign control messages in forge (F)
+        // 5. persist new state
+
+        Self { manager }
+    }
+
     pub fn publish(_bytes: &[u8]) {
         todo!()
     }
