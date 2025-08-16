8104da389e7eb8ea7f143d1a5a3b9a31268e6056
Sam Andreae <contact@samandreae.com>
2025-08-16 16:26:22 +0100

    Clippy & fmt

--- p2panda-spaces/src/space.rs
+++ p2panda-spaces/src/space.rs
@@ -260,7 +260,7 @@ where
 
             match member {
                 GroupMember::Individual(id) => {
-                    EncryptionGroup::add(y.encryption_y, id, &mut manager.rng)
+                    EncryptionGroup::add(y.encryption_y, id, &manager.rng)
                         .map_err(SpaceError::EncryptionGroup)?
                 }
                 GroupMember::Group(_) => {
