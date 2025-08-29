e17ff714fc901c32f3fcee4b520c2aec553f178d
glyph <glyph@mycelial.technology>
2025-08-29 07:11:25 +0100

    Introduce addrs module and basic dependencies

--- p2panda-net-next/src/lib.rs
+++ p2panda-net-next/src/lib.rs
@@ -1,14 +1,16 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+mod addrs;
+
+/// Converts an `iroh` public key type to the `p2panda-core` implementation.
+pub(crate) fn to_public_key(key: iroh_base::PublicKey) -> p2panda_core::PublicKey {
+    p2panda_core::PublicKey::from_bytes(key.as_bytes()).expect("already validated public key")
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+/// Converts a `p2panda-core` public key to the "iroh" type.
+pub(crate) fn from_public_key(key: p2panda_core::PublicKey) -> iroh_base::PublicKey {
+    iroh_base::PublicKey::from_bytes(key.as_bytes()).expect("already validated public key")
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+/// Converts a `p2panda-core` private key to the "iroh" type.
+pub(crate) fn from_private_key(key: p2panda_core::PrivateKey) -> iroh_base::SecretKey {
+    iroh_base::SecretKey::from_bytes(key.as_bytes())
 }
