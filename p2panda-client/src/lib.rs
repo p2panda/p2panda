7591f0835b159e77d742c93ac47a5455883f92df
adz <x12@adz.garden>
2025-09-16 00:10:52 +0200

    Move backend module into root

--- p2panda-client/src/lib.rs
+++ p2panda-client/src/lib.rs
@@ -1,11 +1,13 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
+mod backend;
 mod checkpoint;
 mod client;
 mod controller;
 mod subject;
 
 pub use checkpoint::Checkpoint;
+pub use client::{Client, ClientBuilder, ClientError};
 pub use subject::{Subject, SubjectError};
 
 pub type TopicId = [u8; 32];
