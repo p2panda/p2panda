70c7eef2b41872e42b18d5b69f4c2438781702ae
adz <x12@adz.garden>
2025-09-18 18:38:29 +0200

    Introduce basic orderer implementation and traits

--- p2panda-stream-next/src/lib.rs
+++ p2panda-stream-next/src/lib.rs
@@ -1,5 +1,9 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
 mod layer;
+#[cfg(feature = "orderer")]
+mod orderer;
 
 pub use layer::{BufferedLayer, ChainedLayerStream, Layer, LayerExt, LayerStream, StreamChainExt};
+#[cfg(feature = "orderer")]
+pub use orderer::{Orderer, OrdererError, Ordering};
