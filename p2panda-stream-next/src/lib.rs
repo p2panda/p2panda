80d28b8c509550021c8f1ab18a0c8d7d5d0ca9a5
adz <x12@adz.garden>
2025-09-18 18:37:39 +0200

    Remove orderer from this branch

--- p2panda-stream-next/src/lib.rs
+++ p2panda-stream-next/src/lib.rs
@@ -1,9 +1,5 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
 mod layer;
-#[cfg(feature = "orderer")]
-mod orderer;
 
 pub use layer::{BufferedLayer, ChainedLayerStream, Layer, LayerExt, LayerStream, StreamChainExt};
-#[cfg(feature = "orderer")]
-pub use orderer::{Orderer, OrdererError, Ordering};
