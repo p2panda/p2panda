f43e449a51c5c607a09709e7f2c4f6ad674aaa0a
adz <adzialocha@users.noreply.github.com>
2025-09-24 11:19:05 +0200

    New stream API for layer event processors (#804)
    * First experimental approach with processing layers
    
    * Single-threaded approach
    
    * Methods and implementations to work with Stream
    
    * Rename chained -> composed
    
    * A note on single-threaded design
    
    * A note on ugly error types of the buffer layer
    
    * Improve stream design by embedding buffer logic
    
    There was an potential foot-gun when combining an expensive async
    processor with the Stream-based API (you can see a panic there handling
    that situation). This is due to a simplified polling approach where we
    don't park the futures for contiuous polling. The solution is to either
    take care of buffering in the processor implementation directly or wrap
    it around the BufferedProcessor helper.
    
    The solution this commit takes is to have that directly be handled in
    the stream.
    
    Another improvement is removing the `Terminated` error type in
    BufferedProcessor is a bit ugly as technically it will never really
    happen (when the channel is dropped everything else is gone as well).
    
    * Minor clean up
    
    * tokio-stream is a dev dependency
    
    * Correct mod-level doc-string syntax
    
    * Fix typo: seize -> cease
    
    * Apply grammar fixes from code review
    
    Co-authored-by: glyph <gnomad@cryptolab.net>
    
    * One more doc-string improvement
    
    ---------
    
    Co-authored-by: glyph <gnomad@cryptolab.net>

---
+++ p2panda-stream-next/src/lib.rs
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+//! Interfaces to implement and compose event processors on top of data streams with some p2panda
+//! implementations out-of-the-box, wrapping existing p2panda crates for causal message ordering,
+//! log validation, access control and group encryption CRDTs (`p2panda-spaces`) which might come
+//! in handy for many peer-to-peer applications.
+//!
+//! Developers can "stack up" different stream processors on top of each other and layer them based
+//! on the application's needs. Each processor will formulate requirements (in the form of a Rust
+//! trait) to the underlying data type; you either "bring your own" or use our `Operation` data type
+//! which has most requirements implemented.
+//!
+//! In p2panda we clearly separate the "event delivery" layer from the "event processing" one. This
+//! part of the stack doesn't care what transport was used (pidgeon carriers, usb sticks, internet
+//! protocol, LoRa, etc.). Ideally the application should just work fine independent of how the
+//! data was delivered, the only part which really matters is how the event stream was _processed_.
+//!
+//! This stream processor design can be nicely combined with a Pub/Sub system (like `p2panda-net`),
+//! scoped and stateful stream controllers (in `p2panda-client`) and storage backends supporting
+//! atomic transactions (`p2panda-store`).
+//!
+//! ## Single-Threaded Design
+//!
+//! Processors are meant to only be executed within a single thread and do not allow `Send` or
+//! `Sync` types. Users need to make sure to run this code in a "local" tokio runtime.
+mod processors;
+#[cfg(test)]
+mod test_utils;
+
+pub use processors::*;
---
+++ p2panda-stream-next/src/processors/processor.rs
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+/// Interface for implementing data processors.
+///
+/// Processors take in items (input) and yield "processed" or "enriched" versions of them at _some
+/// point_ (output). Please note that these items do _not_ necessarily need to come out in the same
+/// order as they came in. Processors can keep items around until their internal logic decides
+/// they're ready to go, and this can happen in any order required.
+///
+/// Users of processors will use the `process` method to insert "input" items and call `next` to
+/// find out if there's ready processed "output" items to take out.
+///
+/// The `process` method might apply back-pressure, for example when the processor is busy and
+/// can't take in more work.
+///
+/// Processors _never_ terminate. The future returned by `next` will stay in pending state whenever
+/// there's no work to do or the processor can't continue because of it's internal logic. Users can
+/// decide to drop a processor or escalate to a higher-level whenever an error occurs or the
+/// processor is no longer used.
+pub trait Processor<T> {
+    type Output;
+
+    type Error;
+
+    /// Consumes an item for further processing.
+    ///
+    /// Might apply back-pressure when being too busy.
+    fn process(&self, input: T) -> impl Future<Output = Result<(), Self::Error>>;
+
+    /// Returns future with processed output whenever ready.
+    ///
+    /// Please note that processors never terminate and rather return pending state (Poll::Pending)
+    /// in the future when `next` is called and no ready items are available.
+    fn next(&self) -> impl Future<Output = Result<Self::Output, Self::Error>>;
+}
