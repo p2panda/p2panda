590e6d4764250dc70c1184b99d624c09dfc43147
adz <x12@adz.garden>
2025-09-24 12:43:24 +0200

    New p2panda-store-next crate with operation traits

---
+++ p2panda-store-next/src/operations/traits.rs
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+/// Interface for storing, deleting and querying operations.
+///
+/// The concrete type of an "operation" is generic and implementors can use the same interface for
+/// different approaches: sets, append-only logs or hash-graphs etc.
+pub trait OperationStore<T, ID> {
+    type Error;
+
+    /// Insert an operation.
+    ///
+    /// Returns `true` when the insert occurred, or `false` when the operation already existed and
+    /// no insertion occurred.
+    fn insert_operation(
+        &self,
+        id: &ID,
+        operation: T,
+    ) -> impl Future<Output = Result<bool, Self::Error>>;
+
+    /// Get an operation by id.
+    fn get_operation(&self, id: &ID) -> impl Future<Output = Result<T, Self::Error>>;
+
+    /// Query the existence of an operation.
+    ///
+    /// Returns `true` if the operation was found in the store and `false` if not.
+    fn has_operation(&self, id: &ID) -> impl Future<Output = Result<bool, Self::Error>>;
+
+    /// Delete an operation.
+    ///
+    /// Returns `true` when the removal occurred and `false` when the operation was not found in
+    /// the store.
+    fn delete_operation(&mut self, id: &ID) -> impl Future<Output = Result<bool, Self::Error>>;
+}
