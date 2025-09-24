7207fab6394085168a55dce1ff5d13a3ae9aa006
adz <adzialocha@users.noreply.github.com>
2025-09-24 11:19:05 +0200

    New stream API for layer event processors (#804)
    * First experimental approach with processing layers
    
    * Single-threaded approach
    
    * Methods and implementations to work with Stream
    
    * Rename chained -> composed
    
    * A note on single-threaded design
    
    * A note on ugly error types of the buffer layer
    
    * Improve stream design by embedding buffer logic
    
    There was an potential foot-gun when combining an expensive async
    processor with the Stream-based API (you can see a panic there handling
    that situation). This is due to a simplified polling approach where we
    don't park the futures for contiuous polling. The solution is to either
    take care of buffering in the processor implementation directly or wrap
    it around the BufferedProcessor helper.
    
    The solution this commit takes is to have that directly be handled in
    the stream.
    
    Another improvement is removing the `Terminated` error type in
    BufferedProcessor is a bit ugly as technically it will never really
    happen (when the channel is dropped everything else is gone as well).
    
    * Minor clean up
    
    * tokio-stream is a dev dependency
    
    * Correct mod-level doc-string syntax
    
    * Fix typo: seize -> cease
    
    * Apply grammar fixes from code review
    
    Co-authored-by: glyph <gnomad@cryptolab.net>
    
    * One more doc-string improvement
    
    ---------
    
    Co-authored-by: glyph <gnomad@cryptolab.net>

---
+++ p2panda-stream-next/src/processors/composed.rs
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+use crate::processors::Processor;
+
+pub struct ComposedProcessors<P1, P2> {
+    pub first: P1,
+    pub second: P2,
+}
+
+impl<P1, P2, T> Processor<T> for ComposedProcessors<P1, P2>
+where
+    P1: Processor<T>,
+    P2: Processor<P1::Output>,
+{
+    type Output = P2::Output;
+
+    type Error = ComposedError<P1::Error, P2::Error>;
+
+    async fn process(&self, input: T) -> Result<(), Self::Error> {
+        self.first
+            .process(input)
+            .await
+            .map_err(ComposedError::First)?;
+        Ok(())
+    }
+
+    async fn next(&self) -> Result<Self::Output, Self::Error> {
+        loop {
+            tokio::select! {
+                intermediate = self.first.next() => {
+                    match intermediate {
+                        Ok(intermediate) => {
+                            self.second
+                                .process(intermediate)
+                                .await
+                                .map_err(ComposedError::Second)?;
+
+                            // Yield to prevent runtime starvation.
+                            tokio::task::yield_now().await;
+                        },
+                        Err(err) => {
+                            return Err(ComposedError::First(err));
+                        },
+                    }
+                }
+
+                output = self.second.next() => {
+                    return output.map_err(ComposedError::Second);
+                }
+            }
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum ComposedError<P1, P2> {
+    First(P1),
+    Second(P2),
+}
