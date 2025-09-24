9f209a0c07b0fa7d9ec13511224c23f006956f68
adz <adzialocha@users.noreply.github.com>
2025-09-24 11:19:05 +0200

    New stream API for layer event processors (#804)
    * First experimental approach with processing layers
    
    * Single-threaded approach
    
    * Methods and implementations to work with Stream
    
    * Rename chained -> composed
    
    * A note on single-threaded design
    
    * A note on ugly error types of the buffer layer
    
    * Improve stream design by embedding buffer logic
    
    There was an potential foot-gun when combining an expensive async
    processor with the Stream-based API (you can see a panic there handling
    that situation). This is due to a simplified polling approach where we
    don't park the futures for contiuous polling. The solution is to either
    take care of buffering in the processor implementation directly or wrap
    it around the BufferedProcessor helper.
    
    The solution this commit takes is to have that directly be handled in
    the stream.
    
    Another improvement is removing the `Terminated` error type in
    BufferedProcessor is a bit ugly as technically it will never really
    happen (when the channel is dropped everything else is gone as well).
    
    * Minor clean up
    
    * tokio-stream is a dev dependency
    
    * Correct mod-level doc-string syntax
    
    * Fix typo: seize -> cease
    
    * Apply grammar fixes from code review
    
    Co-authored-by: glyph <gnomad@cryptolab.net>
    
    * One more doc-string improvement
    
    ---------
    
    Co-authored-by: glyph <gnomad@cryptolab.net>

---
+++ p2panda-stream-next/src/processors/pipeline.rs
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+use std::marker::PhantomData;
+
+use crate::processors::Processor;
+use crate::processors::composed::ComposedProcessors;
+
+#[derive(Default)]
+pub struct PipelineBuilder<T> {
+    _marker: PhantomData<T>,
+}
+
+impl<T> PipelineBuilder<T> {
+    pub fn new() -> Self {
+        Self {
+            _marker: PhantomData,
+        }
+    }
+
+    pub fn layer<P>(self, processor: P) -> LayeredBuilder<P, T> {
+        LayeredBuilder {
+            processor,
+            _marker: PhantomData,
+        }
+    }
+}
+
+pub struct LayeredBuilder<P, T> {
+    processor: P,
+    _marker: PhantomData<T>,
+}
+
+impl<P, T> LayeredBuilder<P, T> {
+    pub fn layer<P2>(self, processor: P2) -> LayeredBuilder<ComposedProcessors<P, P2>, T>
+    where
+        P: Processor<T>,
+        P2: Processor<P::Output>,
+    {
+        LayeredBuilder {
+            processor: ComposedProcessors {
+                first: self.processor,
+                second: processor,
+            },
+            _marker: PhantomData,
+        }
+    }
+
+    pub fn build(self) -> Pipeline<P> {
+        Pipeline {
+            processor: self.processor,
+        }
+    }
+}
+
+pub struct Pipeline<P> {
+    processor: P,
+}
+
+impl<P, T> Processor<T> for Pipeline<P>
+where
+    P: Processor<T>,
+{
+    type Output = P::Output;
+
+    type Error = P::Error;
+
+    async fn process(&self, input: T) -> Result<(), Self::Error> {
+        self.processor.process(input).await
+    }
+
+    async fn next(&self) -> Result<Self::Output, Self::Error> {
+        self.processor.next().await
+    }
+}
