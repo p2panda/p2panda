f40aea9b78e1b814cdd1058b35fbe3340178cc5b
adz <x12@adz.garden>
2025-09-29 17:22:37 +0200

    Macro to run tests against all stores

---
+++ p2panda-store-next/src/test_utils.rs
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+/// Macro to run the same test logic against all store backend implementations.
+///
+/// This macro takes a closure that will be executed against each store type:
+/// - In-memory store (`MemoryStore`)
+/// - SQLite store (`SqliteStore`)
+///
+/// ## Example
+///
+/// ```rust
+/// assert_all_stores!(|store| async move {
+///     store.mark_ready("test".to_string()).await.unwrap();
+///     assert_eq!(store.ready_len().await, 1);
+/// });
+/// ```
+#[macro_export]
+macro_rules! assert_all_stores {
+    (|$store:ident| $test_body:expr) => {
+        // Test with MemoryStore.
+        {
+            let $store = $crate::memory::MemoryStore::<(), String>::default();
+            $test_body.await;
+        }
+
+        // Test with SqliteStore.
+        {
+            let sqlite_store = $crate::sqlite::SqliteStoreBuilder::new()
+                .random_memory_url()
+                // We're running in a single test thread and can't have more parallel connections.
+                .max_connections(1)
+                .build()
+                .await
+                .unwrap();
+            let permit = sqlite_store.begin().await.unwrap();
+            let $store = sqlite_store.clone();
+            $test_body.await;
+            sqlite_store.commit(permit).await.unwrap();
+        }
+    };
+}
