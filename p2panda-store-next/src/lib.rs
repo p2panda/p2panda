842f140b65e7ba5c383805c1a8e12fc596c62e69
adz <x12@adz.garden>
2025-09-29 14:24:36 +0200

    Memory and sqlite modules behind feature flags

--- p2panda-store-next/src/lib.rs
+++ p2panda-store-next/src/lib.rs
@@ -1,14 +1,6 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+// SPDX-License-Identifier: MIT OR Apache-2.0
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+#[cfg(feature = "memory")]
+pub mod memory;
+#[cfg(feature = "sqlite")]
+pub mod sqlite;
