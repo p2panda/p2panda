9d9e1624a290b926316934c767ade23ac1a49730
glyph <gnomad@cryptolab.net>
2025-09-29 07:08:04 +0100

    Implement supervised gossip actor sub-system (#790)
    * Create new workspace crate
    
    * Introduce addrs module and basic dependencies
    
    * Add dependencies
    
    * Introduce actors module
    
    * Implement gossip receiver actor
    
    * Implement gossip sender actor
    
    * Implement gossip listener actor
    
    * Implement gossip session actor
    
    * Introduce WIP gossip actor implementation
    
    * Introduce actors module
    
    * Introduce network message types
    
    * Introduce network module
    
    * Introduce supervision events to gossip actor
    
    * Handle msgs received from gossip and forward to user
    
    * Fix formatting
    
    * Add SPDX license headers
    
    * Implement iterator improvements
    
    * Ignore unused variables
    
    * Introduce iroh gossip config
    
    * Fix formatting
    
    * Upgrade iroh dependency to latest
    
    * Fix formatting
    
    * Shutdown iroh gossip instance when actor is stopped
    
    * Introduce NetworkId
    
    * Upgrade to latest iroh and tokio versions
    
    * Wait for join in message handler; don't block in pre_start
    
    * Introduce oneshot receiver to only broadcast once gossip has been joined
    
    * Pass oneshot receiver from session actor to sender actor
    
    * Handle join event and add two peer gossip test
    
    * Implement gossip joiner actor
    
    * Expose JoinPeers logic and fix NeighborDown reporting bug
    
    * Clarify code comment and change message variant naming
    
    * Add three peer gossip tests; one with healed partition
    
    * Change gossip channel naming and add supervision TODO
    
    * Track active neighbours for each topic session
    
    * Remove unnecessary drop of receiver
    
    * Add notes about state cleanup and channel handling
    
    * Bump iroh dependencies and clean-up tokio feature flags
    
    * Remove NetworkId type completely
    
    * Remove FromNetwork and ToNetwork types completely
    
    * Remove empty network module
    
    * Remove newline after mod-level doc-strings; fix formatting
    
    * Add TestSupervisor in test_utils module
    
    * Fix typo and add test for gossip actor state

---
+++ p2panda-net-next/src/actors/gossip/listener.rs
@@ -0,0 +1,95 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+//! Listen for messages from the user and forward them to the gossip sender.
+use ractor::{Actor, ActorProcessingErr, ActorRef, Message};
+use tokio::sync::mpsc::Receiver;
+use tracing::warn;
+
+use crate::actors::gossip::sender::ToGossipSender;
+
+pub enum ToGossipListener {
+    /// Wait for a message on the gossip topic channel.
+    WaitForMessage,
+}
+
+impl Message for ToGossipListener {}
+
+pub struct GossipListenerState {
+    receiver: Option<Receiver<Vec<u8>>>,
+}
+
+pub struct GossipListener {
+    sender: ActorRef<ToGossipSender>,
+}
+
+impl GossipListener {
+    pub fn new(sender: ActorRef<ToGossipSender>) -> Self {
+        Self { sender }
+    }
+}
+
+impl Actor for GossipListener {
+    type State = GossipListenerState;
+    type Msg = ToGossipListener;
+    type Arguments = Receiver<Vec<u8>>;
+
+    async fn pre_start(
+        &self,
+        myself: ActorRef<Self::Msg>,
+        receiver: Self::Arguments,
+    ) -> Result<Self::State, ActorProcessingErr> {
+        // Invoke the handler to wait for the first message on the receiver.
+        let _ = myself.cast(ToGossipListener::WaitForMessage);
+
+        let state = GossipListenerState {
+            receiver: Some(receiver),
+        };
+
+        Ok(state)
+    }
+
+    async fn post_start(
+        &self,
+        _myself: ActorRef<Self::Msg>,
+        _state: &mut Self::State,
+    ) -> Result<(), ActorProcessingErr> {
+        Ok(())
+    }
+
+    async fn post_stop(
+        &self,
+        _myself: ActorRef<Self::Msg>,
+        state: &mut Self::State,
+    ) -> Result<(), ActorProcessingErr> {
+        drop(state.receiver.take());
+
+        Ok(())
+    }
+
+    async fn handle(
+        &self,
+        myself: ActorRef<Self::Msg>,
+        _message: Self::Msg,
+        state: &mut Self::State,
+    ) -> Result<(), ActorProcessingErr> {
+        if let Some(receiver) = &mut state.receiver {
+            match receiver.recv().await {
+                Some(bytes) => {
+                    // Forward the message bytes to the gossip sender for broadcast.
+                    let _ = self.sender.cast(ToGossipSender::Broadcast(bytes));
+                }
+                None => {
+                    warn!("gossip listener actor: user dropped sender - channel closed");
+                    myself.stop(Some("receiver channel closed".to_string()));
+
+                    return Ok(());
+                }
+            }
+        }
+
+        // Invoke the handler to wait for the next message on the receiver.
+        let _ = myself.cast(ToGossipListener::WaitForMessage);
+
+        Ok(())
+    }
+}
---
+++ p2panda-net-next/src/actors/gossip/receiver.rs
@@ -0,0 +1,124 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+//! Gossip recevier actor which holds the gossip topic receiver, receives overlay messages and sends
+//! them to the gossip session actor.
+use futures_lite::StreamExt;
+use iroh_gossip::api::GossipReceiver as IrohGossipReceiver;
+use ractor::{Actor, ActorProcessingErr, ActorRef, Message};
+use tracing::error;
+
+use crate::actors::gossip::session::ToGossipSession;
+
+pub enum ToGossipReceiver {
+    /// Wait for an event on the gossip topic receiver.
+    WaitForEvent,
+
+    /// Wait for the first `NeighborUp` event on the receiver, signifying that the gossip overlay
+    /// has been joined.
+    WaitForJoin,
+}
+
+impl Message for ToGossipReceiver {}
+
+pub struct GossipReceiverState {
+    receiver: Option<IrohGossipReceiver>,
+}
+
+pub struct GossipReceiver {
+    session: ActorRef<ToGossipSession>,
+}
+
+impl GossipReceiver {
+    pub fn new(session: ActorRef<ToGossipSession>) -> Self {
+        Self { session }
+    }
+}
+
+impl Actor for GossipReceiver {
+    type State = GossipReceiverState;
+    type Msg = ToGossipReceiver;
+    type Arguments = IrohGossipReceiver;
+
+    async fn pre_start(
+        &self,
+        myself: ActorRef<Self::Msg>,
+        receiver: Self::Arguments,
+    ) -> Result<Self::State, ActorProcessingErr> {
+        // Invoke the handler to wait for the next event on the receiver.
+        let _ = myself.cast(ToGossipReceiver::WaitForJoin);
+
+        let state = GossipReceiverState {
+            receiver: Some(receiver),
+        };
+
+        Ok(state)
+    }
+
+    async fn post_start(
+        &self,
+        _myself: ActorRef<Self::Msg>,
+        _state: &mut Self::State,
+    ) -> Result<(), ActorProcessingErr> {
+        Ok(())
+    }
+
+    async fn post_stop(
+        &self,
+        _myself: ActorRef<Self::Msg>,
+        state: &mut Self::State,
+    ) -> Result<(), ActorProcessingErr> {
+        drop(state.receiver.take());
+
+        Ok(())
+    }
+
+    async fn handle(
+        &self,
+        myself: ActorRef<Self::Msg>,
+        message: Self::Msg,
+        state: &mut Self::State,
+    ) -> Result<(), ActorProcessingErr> {
+        match message {
+            ToGossipReceiver::WaitForJoin => {
+                if let Some(receiver) = &mut state.receiver {
+                    // Wait for the first peer connection.
+                    //
+                    // This will block the actor's message processing queue until the first
+                    // `NeighborUp` event is received. The event is consumed by the call to
+                    // `joined()`.
+                    receiver.joined().await?;
+
+                    // Inform the session actor about our direct neighbors.
+                    let peers = receiver.neighbors().collect();
+                    let _ = self.session.cast(ToGossipSession::ProcessJoined(peers));
+                }
+
+                // Invoke the handler to wait for the next event on the receiver.
+                let _ = myself.cast(ToGossipReceiver::WaitForEvent);
+            }
+            ToGossipReceiver::WaitForEvent => {
+                if let Some(receiver) = &mut state.receiver {
+                    if let Some(received) = receiver.next().await {
+                        match received {
+                            Ok(event) => {
+                                // Send the event up the chain for processing.
+                                let _ = self.session.cast(ToGossipSession::ProcessEvent(event));
+                            }
+                            Err(err) => {
+                                error!("gossip receiver actor: {}", err);
+                                myself.stop(Some("channel closed".to_string()));
+
+                                return Ok(());
+                            }
+                        }
+                    }
+                }
+
+                // Invoke the handler to wait for the next event on the receiver.
+                let _ = myself.cast(ToGossipReceiver::WaitForEvent);
+            }
+        }
+
+        Ok(())
+    }
+}
