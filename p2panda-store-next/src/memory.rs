a51b5a2cc1bf2d73a9972d27f2038295d94c771b
adz <x12@adz.garden>
2025-09-29 14:25:31 +0200

    Fix moving memory store code into correct mod

--- p2panda-store-next/src/memory.rs
+++ p2panda-store-next/src/memory.rs
@@ -1,1 +1,22 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
+
+use std::fmt::Debug;
+
+/// In-memory store.
+///
+/// This does not persist data permamently, all changes are lost when the process ends. Use this
+/// only in development or test contexts.
+#[derive(Debug, Clone)]
+pub struct MemoryStore {}
+
+impl MemoryStore {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl Default for MemoryStore {
+    fn default() -> Self {
+        Self::new()
+    }
+}
