a0792096e5927344227f1350b187dc5547f47f57
glyph <gnomad@cryptolab.net>
2025-09-29 07:08:04 +0100

    Implement supervised gossip actor sub-system (#790)
    * Create new workspace crate
    
    * Introduce addrs module and basic dependencies
    
    * Add dependencies
    
    * Introduce actors module
    
    * Implement gossip receiver actor
    
    * Implement gossip sender actor
    
    * Implement gossip listener actor
    
    * Implement gossip session actor
    
    * Introduce WIP gossip actor implementation
    
    * Introduce actors module
    
    * Introduce network message types
    
    * Introduce network module
    
    * Introduce supervision events to gossip actor
    
    * Handle msgs received from gossip and forward to user
    
    * Fix formatting
    
    * Add SPDX license headers
    
    * Implement iterator improvements
    
    * Ignore unused variables
    
    * Introduce iroh gossip config
    
    * Fix formatting
    
    * Upgrade iroh dependency to latest
    
    * Fix formatting
    
    * Shutdown iroh gossip instance when actor is stopped
    
    * Introduce NetworkId
    
    * Upgrade to latest iroh and tokio versions
    
    * Wait for join in message handler; don't block in pre_start
    
    * Introduce oneshot receiver to only broadcast once gossip has been joined
    
    * Pass oneshot receiver from session actor to sender actor
    
    * Handle join event and add two peer gossip test
    
    * Implement gossip joiner actor
    
    * Expose JoinPeers logic and fix NeighborDown reporting bug
    
    * Clarify code comment and change message variant naming
    
    * Add three peer gossip tests; one with healed partition
    
    * Change gossip channel naming and add supervision TODO
    
    * Track active neighbours for each topic session
    
    * Remove unnecessary drop of receiver
    
    * Add notes about state cleanup and channel handling
    
    * Bump iroh dependencies and clean-up tokio feature flags
    
    * Remove NetworkId type completely
    
    * Remove FromNetwork and ToNetwork types completely
    
    * Remove empty network module
    
    * Remove newline after mod-level doc-strings; fix formatting
    
    * Add TestSupervisor in test_utils module
    
    * Fix typo and add test for gossip actor state

---
+++ p2panda-net-next/src/lib.rs
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+mod actors;
+mod addrs;
+
+pub type TopicId = [u8; 32];
+
+/// Converts an `iroh` public key type to the `p2panda-core` implementation.
+pub(crate) fn to_public_key(key: iroh_base::PublicKey) -> p2panda_core::PublicKey {
+    p2panda_core::PublicKey::from_bytes(key.as_bytes()).expect("already validated public key")
+}
+
+/// Converts a `p2panda-core` public key to the "iroh" type.
+pub(crate) fn from_public_key(key: p2panda_core::PublicKey) -> iroh_base::PublicKey {
+    iroh_base::PublicKey::from_bytes(key.as_bytes()).expect("already validated public key")
+}
+
+/// Converts a `p2panda-core` private key to the "iroh" type.
+pub(crate) fn from_private_key(key: p2panda_core::PrivateKey) -> iroh_base::SecretKey {
+    iroh_base::SecretKey::from_bytes(key.as_bytes())
+}
