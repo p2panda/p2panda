39eb2c7a22fd2c96eb4acfd4f9af0ad947bc9609
glyph <glyph@mycelial.technology>
2025-09-29 07:54:44 +0100

    Introduce actor modules and rand dependency

--- p2panda-net-next/src/actors/mod.rs
+++ p2panda-net-next/src/actors/mod.rs
@@ -1,5 +1,11 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
+mod address_book;
+mod discovery;
+mod events;
 mod gossip;
+mod router;
+mod subscription;
+mod sync;
 #[cfg(test)]
 mod test_utils;
