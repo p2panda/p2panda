3e3dbe4477d219ce252ad6442fb004226f728826
adz <x12@adz.garden>
2025-09-29 16:38:09 +0200

    In-memory implementation for OperationStore

--- p2panda-store-next/src/operations/mod.rs
+++ p2panda-store-next/src/operations/mod.rs
@@ -1,5 +1,9 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
+#[cfg(feature = "memory")]
+mod memory;
 mod traits;
 
+#[cfg(feature = "memory")]
+pub use memory::OperationMemoryStore;
 pub use traits::OperationStore;
