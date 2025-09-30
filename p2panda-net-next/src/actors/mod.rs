77fe0dd44b2ae9b0eb455fd1b0c64e9ff3787dd7
glyph <gnomad@cryptolab.net>
2025-09-30 14:39:06 +0100

    Introduce actor "shells" (#819)
    * Introduce sync actor shells
    
    * Introduce subscription actor shell
    
    * Implement address book actor
    
    * Implement router actor
    
    * Introduce events actor shell
    
    * Introduce discovery actor shell
    
    * Remove NetworkId from address book
    
    * Introduce actor modules and rand dependency
    
    * Introduce endpoint actor shell
    
    * Clean up
    
    * Ignore dead code warnings for now
    
    * Fix formatting
    
    * Ignore unused test result variant for now
    
    * Add SPDX license header
    
    * Remove space after module-level docs-strings

--- p2panda-net-next/src/actors/mod.rs
+++ p2panda-net-next/src/actors/mod.rs
@@ -1,5 +1,12 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
+mod address_book;
+mod discovery;
+mod endpoint;
+mod events;
 mod gossip;
+mod router;
+mod subscription;
+mod sync;
 #[cfg(test)]
 mod test_utils;
