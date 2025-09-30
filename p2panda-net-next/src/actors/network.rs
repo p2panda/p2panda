27e69268bac704943508e74b873da9f26b5022a5
glyph <glyph@mycelial.technology>
2025-09-30 11:18:46 +0100

    Introduce top-level network actor shell

---
+++ p2panda-net-next/src/actors/network.rs
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+//! Network actor.
+//!
+//! This actor forms the root of the entire system supervision tree. It's only role is to spawn and
+//! supervise other actors.
+use ractor::{Actor, ActorProcessingErr, ActorRef, Message, SupervisionEvent};
+
+pub enum ToNetwork {}
+
+impl Message for ToNetwork {}
+
+pub struct NetworkState {}
+
+pub struct Network {}
+
+impl Actor for Network {
+    type State = NetworkState;
+    type Msg = ToNetwork;
+    type Arguments = ();
+
+    async fn pre_start(
+        &self,
+        _myself: ActorRef<Self::Msg>,
+        _args: Self::Arguments,
+    ) -> Result<Self::State, ActorProcessingErr> {
+        Ok(NetworkState {})
+    }
+
+    async fn post_start(
+        &self,
+        _myself: ActorRef<Self::Msg>,
+        _state: &mut Self::State,
+    ) -> Result<(), ActorProcessingErr> {
+        Ok(())
+    }
+
+    async fn post_stop(
+        &self,
+        _myself: ActorRef<Self::Msg>,
+        _state: &mut Self::State,
+    ) -> Result<(), ActorProcessingErr> {
+        Ok(())
+    }
+
+    async fn handle(
+        &self,
+        _myself: ActorRef<Self::Msg>,
+        _message: Self::Msg,
+        _state: &mut Self::State,
+    ) -> Result<(), ActorProcessingErr> {
+        Ok(())
+    }
+
+    async fn handle_supervisor_evt(
+        &self,
+        _myself: ActorRef<Self::Msg>,
+        _message: SupervisionEvent,
+        _state: &mut Self::State,
+    ) -> Result<(), ActorProcessingErr> {
+        Ok(())
+    }
+}
