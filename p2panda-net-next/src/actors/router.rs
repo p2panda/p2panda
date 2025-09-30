f70e8f53624d1fb6ddd46d58486a08abd638565d
glyph <glyph@mycelial.technology>
2025-09-30 09:57:22 +0100

    Clean up

--- p2panda-net-next/src/actors/router.rs
+++ p2panda-net-next/src/actors/router.rs
@@ -9,55 +9,27 @@ use iroh::Endpoint as IrohEndpoint;
 use iroh_gossip::ALPN as GOSSIP_ALPN;
 use ractor::{Actor, ActorProcessingErr, ActorRef, Message};
 
-use crate::actors::gossip::ToGossip;
+pub struct RouterConfig {}
 
-#[derive(Clone)]
-pub struct RouterConfig {
-    pub endpoint: IrohEndpoint,
-    pub gossip: Option<ActorRef<ToGossip>>,
-}
-
-// TODO: Eventually we want to support dynamically adding and removing ALPNs.
 pub enum ToRouter {}
 
 impl Message for ToRouter {}
 
-pub struct RouterState {
-    router: IrohRouter,
-}
+pub struct RouterState {}
 
 pub struct Router;
 
 impl Actor for Router {
     type State = RouterState;
     type Msg = ToRouter;
-    type Arguments = RouterConfig;
+    type Arguments = ();
 
     async fn pre_start(
         &self,
         _myself: ActorRef<Self::Msg>,
-        config: Self::Arguments,
+        _args: Self::Arguments,
     ) -> Result<Self::State, ActorProcessingErr> {
-        let mut router_builder = IrohRouter::builder(config.endpoint);
-
-        // Optionally configure the router to accept gossip connections.
-        if let Some(gossip_actor) = config.gossip {
-            let gossip = gossip_actor
-                .call(ToGossip::Handle, Some(Duration::from_millis(500)))
-                .await
-                // Panics in `pre_start` are caught and wrapped in a `SpawnErr::StartupFailed`
-                // error.
-                .expect("failed to send message to gossip actor")
-                .expect("failed to receive gossip handle from gossip actor");
-
-            router_builder = router_builder.accept(GOSSIP_ALPN, gossip);
-        }
-
-        let router = router_builder.spawn();
-
-        let state = RouterState { router };
-
-        Ok(state)
+        Ok(RouterState {})
     }
 
     async fn post_start(
@@ -71,11 +43,8 @@ impl Actor for Router {
     async fn post_stop(
         &self,
         _myself: ActorRef<Self::Msg>,
-        state: &mut Self::State,
+        _state: &mut Self::State,
     ) -> Result<(), ActorProcessingErr> {
-        // Shutting down the router will call `close()` on the associated iroh endpoint.
-        state.router.shutdown().await?;
-
         Ok(())
     }
 
