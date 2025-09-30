be0722d397635bf35f4d1151c50e75c78b1508c3
glyph <gnomad@cryptolab.net>
2025-09-30 14:39:06 +0100

    Introduce actor "shells" (#819)
    * Introduce sync actor shells
    
    * Introduce subscription actor shell
    
    * Implement address book actor
    
    * Implement router actor
    
    * Introduce events actor shell
    
    * Introduce discovery actor shell
    
    * Remove NetworkId from address book
    
    * Introduce actor modules and rand dependency
    
    * Introduce endpoint actor shell
    
    * Clean up
    
    * Ignore dead code warnings for now
    
    * Fix formatting
    
    * Ignore unused test result variant for now
    
    * Add SPDX license header
    
    * Remove space after module-level docs-strings

---
+++ p2panda-net-next/src/actors/endpoint.rs
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+//! Endpoint actor.
+use ractor::{Actor, ActorProcessingErr, ActorRef, Message, SupervisionEvent};
+
+// TODO: Remove once used.
+#[allow(dead_code)]
+pub struct EndpointConfig {}
+
+pub enum ToEndpoint {}
+
+impl Message for ToEndpoint {}
+
+pub struct EndpointState {}
+
+pub struct Endpoint {}
+
+impl Actor for Endpoint {
+    type State = EndpointState;
+    type Msg = ToEndpoint;
+    type Arguments = ();
+
+    async fn pre_start(
+        &self,
+        _myself: ActorRef<Self::Msg>,
+        _args: Self::Arguments,
+    ) -> Result<Self::State, ActorProcessingErr> {
+        Ok(EndpointState {})
+    }
+
+    async fn post_start(
+        &self,
+        _myself: ActorRef<Self::Msg>,
+        _state: &mut Self::State,
+    ) -> Result<(), ActorProcessingErr> {
+        Ok(())
+    }
+
+    async fn post_stop(
+        &self,
+        _myself: ActorRef<Self::Msg>,
+        _state: &mut Self::State,
+    ) -> Result<(), ActorProcessingErr> {
+        Ok(())
+    }
+
+    async fn handle(
+        &self,
+        _myself: ActorRef<Self::Msg>,
+        _message: Self::Msg,
+        _state: &mut Self::State,
+    ) -> Result<(), ActorProcessingErr> {
+        Ok(())
+    }
+
+    async fn handle_supervisor_evt(
+        &self,
+        _myself: ActorRef<Self::Msg>,
+        _message: SupervisionEvent,
+        _state: &mut Self::State,
+    ) -> Result<(), ActorProcessingErr> {
+        Ok(())
+    }
+}
