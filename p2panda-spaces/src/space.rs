c6c23a21ad900267d5492674e3fee361d71b2d73
Sam Andreae <contact@samandreae.com>
2025-10-03 04:27:46 +0100

    Expect Forge to itself persist all messages
    When calling Forge::forge we expect that the resulting message is persisted to the message store internally by the store. We don't persist any messages ourselves in p2panda-spaces now.

--- p2panda-spaces/src/space.rs
+++ p2panda-spaces/src/space.rs
@@ -197,12 +197,6 @@ where
 
             let mut manager = manager_ref.inner.write().await;
             let message = manager.identity.forge(args).await?;
-            manager
-                .spaces_store
-                .set_message(&message.id(), &message)
-                .await
-                .map_err(SpaceError::MessageStore)?;
-
             message
         };
 
@@ -288,13 +282,6 @@ where
                 space_dependencies,
             };
             let message = manager.identity.forge(args).await?;
-
-            manager
-                .spaces_store
-                .set_message(&message.id(), &message)
-                .await
-                .map_err(SpaceError::MessageStore)?;
-
             space_dependencies = vec![message.id()];
             messages.push(message);
         }
@@ -678,7 +665,6 @@ where
 
         // Forge message.
         let message = manager.identity.forge(args).await?;
-        // @TODO: persist message.
 
         // Update dependencies.
         y.encryption_y
