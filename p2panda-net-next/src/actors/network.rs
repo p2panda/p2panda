8147ad5afef666ccf14463b82e5d492cb8558c89
glyph <gnomad@cryptolab.net>
2025-10-08 10:04:53 +0100

    Introduce actor supervision tree (#822)
    * Introduce subscription actor shell
    
    * Implement address book actor
    
    * Implement router actor
    
    * Clean up
    
    * Ignore dead code warnings for now
    
    * Fix formatting
    
    * Introduce top-level network actor shell
    
    * Spawn events actor and handle supervision events
    
    * Spawn endpoint actor and handle supervision events
    
    * Spawn address book actor and handle supervision events
    
    * Spawn discovery actor and handle supervision events
    
    * Provide names for respawned actors
    
    * Spawn subscription actor and handle supervision events
    
    * Fix events actor naming
    
    * Spawn router actor and handle supervision events
    
    * Introduce sync actor shell
    
    * Spawn gossip and sync actors and handle supervision events
    
    * Move subscription actor spawning to endpoint and use consistent variable naming
    
    * Create iroh gossip config inside gossip actor
    
    * Test to ensure direct children of network actor are started
    
    * Test to ensure direct children of endpoint actor are started
    
    * Test to ensure direct children of subscription actor are started
    
    * Fix formatting
    
    * Run actor readiness tests serially to prevent duplicate actor errors
    
    * Clean up after rebase

---
+++ p2panda-net-next/src/actors/network.rs
@@ -0,0 +1,245 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+//! Network actor.
+//!
+//! The root of the entire system supervision tree; it's only role is to spawn and
+//! supervise other actors.
+use ractor::{Actor, ActorProcessingErr, ActorRef, Message, SupervisionEvent};
+use tracing::{debug, warn};
+
+use crate::actors::address_book::{AddressBook, ToAddressBook};
+use crate::actors::discovery::{Discovery, ToDiscovery};
+use crate::actors::endpoint::{Endpoint, ToEndpoint};
+use crate::actors::events::{Events, ToEvents};
+
+pub enum ToNetwork {}
+
+impl Message for ToNetwork {}
+
+pub struct NetworkState {
+    events_actor: ActorRef<ToEvents>,
+    events_actor_failures: u16,
+    endpoint_actor: ActorRef<ToEndpoint>,
+    endpoint_actor_failures: u16,
+    address_book_actor: ActorRef<ToAddressBook>,
+    address_book_actor_failures: u16,
+    discovery_actor: ActorRef<ToDiscovery>,
+    discovery_actor_failures: u16,
+}
+
+pub struct Network {}
+
+impl Actor for Network {
+    type State = NetworkState;
+    type Msg = ToNetwork;
+    type Arguments = ();
+
+    async fn pre_start(
+        &self,
+        myself: ActorRef<Self::Msg>,
+        _args: Self::Arguments,
+    ) -> Result<Self::State, ActorProcessingErr> {
+        // Spawn the events actor.
+        let (events_actor, _) = Actor::spawn_linked(
+            Some("events".to_string()),
+            Events {},
+            (),
+            myself.clone().into(),
+        )
+        .await?;
+
+        // Spawn the endpoint actor.
+        let (endpoint_actor, _) = Actor::spawn_linked(
+            Some("endpoint".to_string()),
+            Endpoint {},
+            (),
+            myself.clone().into(),
+        )
+        .await?;
+
+        // Spawn the address book actor.
+        let (address_book_actor, _) = Actor::spawn_linked(
+            Some("address book".to_string()),
+            AddressBook {},
+            (),
+            myself.clone().into(),
+        )
+        .await?;
+
+        // Spawn the discovery actor.
+        let (discovery_actor, _) = Actor::spawn_linked(
+            Some("discovery".to_string()),
+            Discovery {},
+            (),
+            myself.clone().into(),
+        )
+        .await?;
+
+        let state = NetworkState {
+            events_actor,
+            events_actor_failures: 0,
+            endpoint_actor,
+            endpoint_actor_failures: 0,
+            address_book_actor,
+            address_book_actor_failures: 0,
+            discovery_actor,
+            discovery_actor_failures: 0,
+        };
+
+        Ok(state)
+    }
+
+    async fn post_start(
+        &self,
+        _myself: ActorRef<Self::Msg>,
+        _state: &mut Self::State,
+    ) -> Result<(), ActorProcessingErr> {
+        Ok(())
+    }
+
+    async fn post_stop(
+        &self,
+        _myself: ActorRef<Self::Msg>,
+        _state: &mut Self::State,
+    ) -> Result<(), ActorProcessingErr> {
+        Ok(())
+    }
+
+    async fn handle(
+        &self,
+        _myself: ActorRef<Self::Msg>,
+        _message: Self::Msg,
+        _state: &mut Self::State,
+    ) -> Result<(), ActorProcessingErr> {
+        Ok(())
+    }
+
+    async fn handle_supervisor_evt(
+        &self,
+        myself: ActorRef<Self::Msg>,
+        message: SupervisionEvent,
+        state: &mut Self::State,
+    ) -> Result<(), ActorProcessingErr> {
+        match message {
+            SupervisionEvent::ActorStarted(actor) => {
+                if let Some(name) = actor.get_name() {
+                    debug!("network actor: received ready from {} actor", name);
+                }
+            }
+            SupervisionEvent::ActorFailed(actor, panic_msg) => {
+                match actor.get_name().as_deref() {
+                    Some("events") => {
+                        warn!("network actor: events actor failed: {}", panic_msg);
+
+                        // Respawn the events actor.
+                        let (events_actor, _) = Actor::spawn_linked(
+                            Some("events".to_string()),
+                            Events {},
+                            (),
+                            myself.clone().into(),
+                        )
+                        .await?;
+
+                        state.events_actor_failures += 1;
+                        state.events_actor = events_actor;
+                    }
+                    Some("endpoint") => {
+                        warn!("network actor: endpoint actor failed: {}", panic_msg);
+
+                        // Respawn the endpoint actor.
+                        let (endpoint_actor, _) = Actor::spawn_linked(
+                            Some("endpoint".to_string()),
+                            Endpoint {},
+                            (),
+                            myself.clone().into(),
+                        )
+                        .await?;
+
+                        state.endpoint_actor_failures += 1;
+                        state.endpoint_actor = endpoint_actor;
+                    }
+                    Some("address book") => {
+                        warn!("network actor: address book actor failed: {}", panic_msg);
+
+                        // Respawn the address book actor.
+                        let (address_book_actor, _) = Actor::spawn_linked(
+                            Some("address book".to_string()),
+                            AddressBook {},
+                            (),
+                            myself.clone().into(),
+                        )
+                        .await?;
+
+                        state.address_book_actor_failures += 1;
+                        state.address_book_actor = address_book_actor;
+                    }
+                    Some("discovery") => {
+                        warn!("network actor: discovery actor failed: {}", panic_msg);
+
+                        // Respawn the discovery actor.
+                        let (discovery_actor, _) = Actor::spawn_linked(
+                            Some("discovery".to_string()),
+                            Discovery {},
+                            (),
+                            myself.clone().into(),
+                        )
+                        .await?;
+
+                        state.discovery_actor_failures += 1;
+                        state.discovery_actor = discovery_actor;
+                    }
+                    _ => (),
+                }
+            }
+            SupervisionEvent::ActorTerminated(actor, _last_state, _reason) => {
+                if let Some(name) = actor.get_name() {
+                    debug!("network actor: {} actor terminated", name);
+                }
+            }
+            _ => (),
+        }
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use ractor::Actor;
+    use serial_test::serial;
+    use tokio::time::{Duration, sleep};
+    use tracing_test::traced_test;
+
+    use super::Network;
+
+    #[tokio::test]
+    #[traced_test]
+    #[serial]
+    async fn network_child_actors_are_started() {
+        let (network_actor, network_actor_handle) =
+            Actor::spawn(Some("network".to_string()), Network {}, ())
+                .await
+                .unwrap();
+
+        // Sleep briefly to allow time for all actors to be ready.
+        sleep(Duration::from_millis(50)).await;
+
+        network_actor.stop(None);
+        network_actor_handle.await.unwrap();
+
+        assert!(logs_contain(
+            "network actor: received ready from events actor"
+        ));
+        assert!(logs_contain(
+            "network actor: received ready from endpoint actor"
+        ));
+        assert!(logs_contain(
+            "network actor: received ready from address book actor"
+        ));
+        assert!(logs_contain(
+            "network actor: received ready from discovery actor"
+        ));
+
+        assert!(!logs_contain("actor failed"));
+    }
+}
