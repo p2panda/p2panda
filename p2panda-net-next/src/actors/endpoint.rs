59974a8fe7193c7ace90f608ada0b59630bb602c
glyph <gnomad@cryptolab.net>
2025-10-08 10:04:53 +0100

    Introduce actor supervision tree (#822)
    * Introduce subscription actor shell
    
    * Implement address book actor
    
    * Implement router actor
    
    * Clean up
    
    * Ignore dead code warnings for now
    
    * Fix formatting
    
    * Introduce top-level network actor shell
    
    * Spawn events actor and handle supervision events
    
    * Spawn endpoint actor and handle supervision events
    
    * Spawn address book actor and handle supervision events
    
    * Spawn discovery actor and handle supervision events
    
    * Provide names for respawned actors
    
    * Spawn subscription actor and handle supervision events
    
    * Fix events actor naming
    
    * Spawn router actor and handle supervision events
    
    * Introduce sync actor shell
    
    * Spawn gossip and sync actors and handle supervision events
    
    * Move subscription actor spawning to endpoint and use consistent variable naming
    
    * Create iroh gossip config inside gossip actor
    
    * Test to ensure direct children of network actor are started
    
    * Test to ensure direct children of endpoint actor are started
    
    * Test to ensure direct children of subscription actor are started
    
    * Fix formatting
    
    * Run actor readiness tests serially to prevent duplicate actor errors
    
    * Clean up after rebase

--- p2panda-net-next/src/actors/endpoint.rs
+++ p2panda-net-next/src/actors/endpoint.rs
@@ -1,7 +1,21 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
 //! Endpoint actor.
+//!
+//! This actor is responsible for creating an iroh `Endpoint` and spawning the router and
+//! subscription actors. It also performs supervision of the spawned actors, restarting them in the
+//! event of failure.
+//!
+//! The router and subscription actors are children of the endpoint actor. This design decision was
+//! made because they both currently rely on an iroh `Endpoint` (for the router, gossip and sync
+//! connections). If something goes wrong with the iroh `Endpoint`, the endpoint actor can be
+//! respawned, recreating all children with their required dependencies.
+use iroh::Endpoint as IrohEndpoint;
 use ractor::{Actor, ActorProcessingErr, ActorRef, Message, SupervisionEvent};
+use tracing::{debug, warn};
+
+use crate::actors::router::{Router, ToRouter};
+use crate::actors::subscription::{Subscription, ToSubscription};
 
 // TODO: Remove once used.
 #[allow(dead_code)]
@@ -11,7 +25,13 @@ pub enum ToEndpoint {}
 
 impl Message for ToEndpoint {}
 
-pub struct EndpointState {}
+pub struct EndpointState {
+    endpoint: IrohEndpoint,
+    subscription_actor: ActorRef<ToSubscription>,
+    subscription_actor_failures: u16,
+    router_actor: ActorRef<ToRouter>,
+    router_actor_failures: u16,
+}
 
 pub struct Endpoint {}
 
@@ -22,10 +42,34 @@ impl Actor for Endpoint {
 
     async fn pre_start(
         &self,
-        _myself: ActorRef<Self::Msg>,
+        myself: ActorRef<Self::Msg>,
         _args: Self::Arguments,
     ) -> Result<Self::State, ActorProcessingErr> {
-        Ok(EndpointState {})
+        // TODO: Build with proper configuration.
+        let endpoint = IrohEndpoint::builder().bind().await?;
+
+        // Spawn the subscription actor.
+        let (subscription_actor, _) = Actor::spawn_linked(
+            Some("subscription".to_string()),
+            Subscription {},
+            endpoint.clone(),
+            myself.clone().into(),
+        )
+        .await?;
+
+        // Spawn the router actor.
+        let (router_actor, _) =
+            Actor::spawn_linked(Some("router".to_string()), Router {}, (), myself.into()).await?;
+
+        let state = EndpointState {
+            endpoint,
+            subscription_actor,
+            subscription_actor_failures: 0,
+            router_actor,
+            router_actor_failures: 0,
+        };
+
+        Ok(state)
     }
 
     async fn post_start(
@@ -55,10 +99,94 @@ impl Actor for Endpoint {
 
     async fn handle_supervisor_evt(
         &self,
-        _myself: ActorRef<Self::Msg>,
-        _message: SupervisionEvent,
-        _state: &mut Self::State,
+        myself: ActorRef<Self::Msg>,
+        message: SupervisionEvent,
+        state: &mut Self::State,
     ) -> Result<(), ActorProcessingErr> {
+        match message {
+            SupervisionEvent::ActorStarted(actor) => {
+                if let Some(name) = actor.get_name() {
+                    debug!("endpoint actor: received ready from {} actor", name);
+                }
+            }
+            SupervisionEvent::ActorFailed(actor, panic_msg) => {
+                match actor.get_name().as_deref() {
+                    Some("subscription") => {
+                        warn!("endpoint actor: subscription actor failed: {}", panic_msg);
+
+                        // Respawn the subscription actor.
+                        let (subscription_actor, _) = Actor::spawn_linked(
+                            Some("subscription".to_string()),
+                            Subscription {},
+                            state.endpoint.clone(),
+                            myself.clone().into(),
+                        )
+                        .await?;
+
+                        state.subscription_actor_failures += 1;
+                        state.subscription_actor = subscription_actor;
+                    }
+                    Some("router") => {
+                        warn!("endpoint actor: router actor failed: {}", panic_msg);
+
+                        // Respawn the router actor.
+                        let (router_actor, _) = Actor::spawn_linked(
+                            Some("router".to_string()),
+                            Router {},
+                            (),
+                            myself.clone().into(),
+                        )
+                        .await?;
+
+                        state.router_actor_failures += 1;
+                        state.router_actor = router_actor;
+                    }
+                    _ => (),
+                }
+            }
+            SupervisionEvent::ActorTerminated(actor, _last_state, _reason) => {
+                if let Some(name) = actor.get_name() {
+                    debug!("endpoint actor: {} actor terminated", name);
+                }
+            }
+            _ => (),
+        }
+
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use ractor::Actor;
+    use serial_test::serial;
+    use tokio::time::{Duration, sleep};
+    use tracing_test::traced_test;
+
+    use super::Endpoint;
+
+    #[tokio::test]
+    #[traced_test]
+    #[serial]
+    async fn endpoint_child_actors_are_started() {
+        let (endpoint_actor, endpoint_actor_handle) =
+            Actor::spawn(Some("endpoint".to_string()), Endpoint {}, ())
+                .await
+                .unwrap();
+
+        // Sleep briefly to allow time for all actors to be ready.
+        sleep(Duration::from_millis(50)).await;
+
+        endpoint_actor.stop(None);
+        endpoint_actor_handle.await.unwrap();
+
+        assert!(logs_contain(
+            "endpoint actor: received ready from subscription actor"
+        ));
+        assert!(logs_contain(
+            "endpoint actor: received ready from router actor"
+        ));
+
+        assert!(!logs_contain("actor failed"));
     }
 }
