e50e6a9b7126ea2a9932f32d820b3e923564c8a7
Sam Andreae <contact@samandreae.com>
2025-10-09 09:57:00 +0100

    Improve top level description

--- p2panda-spaces/src/lib.rs
+++ p2panda-spaces/src/lib.rs
@@ -3,15 +3,25 @@
 // @TODO: Remove this later.
 #![allow(dead_code)]
 
-//! This crate provides access-controlled and encrypted data spaces for dynamic groups of users.
-//! This feature set is achieved through an integration of p2panda-access and p2panda-encryption.
-//! Re-usable groups can be added to one or many spaces, each space has it's own private
-//! identifier and encryption scope. The "Data Encryption" scheme is used which provides
-//! post-compromise security and optional forward secrecy.
+//! This crate provides an api for managing scoped message streams encrypted towards dynamic group of
+//! users. The p2panda-encryption Data Encryption scheme is used for key agreement and group
+//! management is achieved through an integration with p2panda-auth groups.
 //! 
-//! Read more about the underlying [groups
-//! CRDT](https://docs.rs/p2panda-auth/latest/p2panda_auth/) and [encryption
-//! scheme](https://docs.rs/p2panda-encryption/latest/p2panda_encryption/).
+//! ## Features
+//! 
+//! * Decentralized group key agreement with post-compromise security and optional forward secrecy
+//! * Decentralized group management with robust conflict resolution strategies
+//! * Private space identifiers
+//! * Re-use of groups across encryption boundaries
+//! * Nested groups allowing for modelling multi-device profiles
+//! * Generic over underlying data types
+//! 
+//! ## Requirements
+//! 
+//! * Messages must be ordered according to causal relations before processing
+//! 
+//! Read more about the underlying [groups CRDT](https://docs.rs/p2panda-auth/latest/p2panda_auth/)
+//! and [encryption scheme](https://docs.rs/p2panda-encryption/latest/p2panda_encryption/).
 pub mod auth;
 pub mod config;
 pub mod credentials;
