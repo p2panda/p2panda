7cc4964d3ea8aa2b22f1b0a2cc2c33434b2447dc
Sam Andreae <contact@samandreae.com>
2025-10-10 14:33:20 +0100

    Embed README as lib doc string

--- p2panda-spaces/src/lib.rs
+++ p2panda-spaces/src/lib.rs
@@ -1,27 +1,6 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
-//! This crate provides an API for managing scoped message streams encrypted towards a dynamic
-//! group of actors. The p2panda-encryption Data Encryption scheme is used for key agreement and
-//! group management is achieved through an integration with p2panda-auth groups.
-//!
-//! ## Features
-//!
-//! * Decentralized group key agreement with forward secrecy and encrypted messaging with
-//!   post-compromise security
-//! * Decentralized group management with robust conflict resolution strategies
-//! * Private space identifiers
-//! * Re-use of groups across encryption boundaries
-//! * Nested groups allowing for modelling multi-device profiles
-//! * Generic over message type
-//!
-//! ## Requirements
-//!
-//! * Messages must be ordered according to causal relations
-//! * Messages must be signed and verified
-//!
-//! Read more about the underlying [groups
-//! CRDT](https://docs.rs/p2panda-auth/latest/p2panda_auth/) and [encryption
-//! scheme](https://docs.rs/p2panda-encryption/latest/p2panda_encryption/).
+#![doc = include_str!("../README.md")]
 mod auth;
 mod config;
 mod credentials;
