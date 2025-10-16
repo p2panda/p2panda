685ef2e986039fe7ea6ab252b066c2e33546ea5a
adz <x12@adz.garden>
2025-10-16 09:27:32 +0200

    Helper timestamp fn, update lib exports

--- p2panda-net-next/src/lib.rs
+++ p2panda-net-next/src/lib.rs
@@ -1,9 +1,21 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
+use std::time::{SystemTime, UNIX_EPOCH};
+
 mod actors;
 mod addrs;
 
+pub use addrs::{NodeId, NodeInfo, NodeInfoError, NodeTransportInfo, TransportAddress};
+
 pub type TopicId = [u8; 32];
+
+/// Returns current UNIX timestamp from local system time.
+fn current_timestamp() -> u64 {
+    SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .expect("system clock moved backwards")
+        .as_secs()
+}
 
 /// Converts an `iroh` public key type to the `p2panda-core` implementation.
 pub(crate) fn to_public_key(key: iroh_base::PublicKey) -> p2panda_core::PublicKey {
