dfc8fa4ce77829bc38186f68cfe5aaafab180254
glyph <gnomad@cryptolab.net>
2025-10-21 09:23:14 +0100

    Configure endpoint actor and remove router actor (#833)
    * Remove router actor
    
    * Add basic NetworkBuilder and ProtocolMap
    
    * Introduce network and protocols modules
    
    * Instantiate and populate the router inside the endpoint actor
    
    * Fix formatting and make minor type changes
    
    * Don't respawn the endpoint actor on failure; shut down all supervised actors
    
    * Create standalone module for default values
    
    * Introduce endpoint actor config
    
    * Introduce network actor config
    
    * Introduce NetworkBuilder with config
    
    * Introduce a ProtocolId type and implement XOR
    
    * Introduce the NetworkId
    
    * Move the protocol map field onto EndpointConfig
    
    * Add network_id to NetworkBuilder and XOR the protocol id before inserting into map
    
    * Allow protocol ID of any byte length; ensure XOR output is 32 bytes
    
    * Use latest iroh dependency versions
    
    * Move endpoint module and introduce online actor
    
    * Move endpoint module
    
    * Introduce EndpointConnected event message variant
    
    * Clean up
    
    * Use ProtocolId when registering protocol
    
    * Satisfy clippy
    
    * Move online check into endpoint actor
    
    * Replace XOR with blake3 hash for protocol ID
    
    * Add note about using CSPRNG to generate network id
    
    * Use hash function from p2panda_core to avoid blake3 dependency
    
    * Move private key from config to Network struct field
    
    * Remove method underscores and add basic doc-strings
    
    * Cleanup and EndpointConfig doc string
    
    * Remove the protocol registration method from the public API

--- p2panda-net-next/src/actors/events.rs
+++ p2panda-net-next/src/actors/events.rs
@@ -5,14 +5,17 @@
 //! Receives events from other actors, aggregating and enriching them before informing
 //! upstream subscribers.
 use ractor::{Actor, ActorProcessingErr, ActorRef, Message, SupervisionEvent};
+use tracing::debug;
 
-pub enum ToEvents {}
+pub enum ToEvents {
+    ConnectedToRelay,
+}
 
 impl Message for ToEvents {}
 
 pub struct EventsState {}
 
-pub struct Events {}
+pub struct Events;
 
 impl Actor for Events {
     type State = EventsState;
@@ -46,9 +49,15 @@ impl Actor for Events {
     async fn handle(
         &self,
         _myself: ActorRef<Self::Msg>,
-        _message: Self::Msg,
+        message: Self::Msg,
         _state: &mut Self::State,
     ) -> Result<(), ActorProcessingErr> {
+        match message {
+            // TODO: Eventually we want to enrich and forward events to the subscription actor so
+            // they can be passed to any subscribers.
+            ToEvents::ConnectedToRelay => debug!("endpoint connected to relay"),
+        }
+
         Ok(())
     }
 
--- p2panda-net-next/src/actors/gossip/mod.rs
+++ p2panda-net-next/src/actors/gossip/mod.rs
@@ -292,10 +292,10 @@ impl Actor for Gossip {
                 debug!("joined topic {:?} with peers: {:?}", topic_id, peers);
 
                 // Inform the gossip sender actor that the overlay has been joined.
-                if let Some(gossip_joined_tx) = state.gossip_joined_senders.remove(&session_id) {
-                    if gossip_joined_tx.send(1).is_err() {
-                        warn!("oneshot gossip joined receiver dropped")
-                    }
+                if let Some(gossip_joined_tx) = state.gossip_joined_senders.remove(&session_id)
+                    && gossip_joined_tx.send(1).is_err()
+                {
+                    warn!("oneshot gossip joined receiver dropped")
                 }
 
                 let peer_set = HashSet::from_iter(peers);
@@ -307,20 +307,20 @@ impl Actor for Gossip {
             }
             ToGossip::NeighborUp { peer, session_id } => {
                 // Insert the peer into the set of neighbours.
-                if let Some(topic_id) = state.sessions_by_actor_id.get(&session_id) {
-                    if let Some(peer_set) = state.neighbours_by_topic_id.get_mut(topic_id) {
-                        peer_set.insert(peer);
-                    }
+                if let Some(topic_id) = state.sessions_by_actor_id.get(&session_id)
+                    && let Some(peer_set) = state.neighbours_by_topic_id.get_mut(topic_id)
+                {
+                    peer_set.insert(peer);
                 }
 
                 Ok(())
             }
             ToGossip::NeighborDown { peer, session_id } => {
                 // Remove the peer from the set of neighbours.
-                if let Some(topic_id) = state.sessions_by_actor_id.get(&session_id) {
-                    if let Some(peer_set) = state.neighbours_by_topic_id.get_mut(topic_id) {
-                        peer_set.remove(&peer);
-                    }
+                if let Some(topic_id) = state.sessions_by_actor_id.get(&session_id)
+                    && let Some(peer_set) = state.neighbours_by_topic_id.get_mut(topic_id)
+                {
+                    peer_set.remove(&peer);
                 }
 
                 Ok(())
@@ -414,10 +414,10 @@ impl Actor for Gossip {
 mod tests {
     use std::time::Duration;
 
+    use iroh::Endpoint as IrohEndpoint;
     use iroh::discovery::NodeInfo;
     use iroh::discovery::static_provider::StaticProvider;
     use iroh::protocol::Router as IrohRouter;
-    use iroh::{Endpoint as IrohEndpoint, Watcher as _};
     use iroh_gossip::ALPN as GOSSIP_ALPN;
     use p2panda_core::PrivateKey;
     use ractor::{Actor, call};
@@ -482,9 +482,9 @@ mod tests {
             .unwrap();
 
         // Obtain ant's node information including direct addresses.
-        let ant_addrs = ant_endpoint.direct_addresses().initialized().await;
+        let ant_addrs = ant_endpoint.node_addr();
         let ant_node_info = NodeInfo::new(from_public_key(ant_public_key))
-            .with_direct_addresses(ant_addrs.into_iter().map(|direct| direct.addr).collect());
+            .with_direct_addresses(ant_addrs.direct_addresses);
 
         // Bat discovers ant through some out-of-band process.
         bat_discovery.add_node_info(ant_node_info.clone());
@@ -620,9 +620,9 @@ mod tests {
             .unwrap();
 
         // Obtain ant's node information including direct addresses.
-        let ant_addrs = ant_endpoint.direct_addresses().initialized().await;
+        let ant_addrs = ant_endpoint.node_addr();
         let ant_node_info = NodeInfo::new(from_public_key(ant_public_key))
-            .with_direct_addresses(ant_addrs.into_iter().map(|direct| direct.addr).collect());
+            .with_direct_addresses(ant_addrs.direct_addresses);
 
         // Bat discovers ant through some out-of-band process.
         bat_discovery.add_node_info(ant_node_info);
@@ -738,9 +738,9 @@ mod tests {
             .unwrap();
 
         // Obtain ant's node information including direct addresses.
-        let ant_addrs = ant_endpoint.direct_addresses().initialized().await;
+        let ant_addrs = ant_endpoint.node_addr();
         let ant_node_info = NodeInfo::new(from_public_key(ant_public_key))
-            .with_direct_addresses(ant_addrs.into_iter().map(|direct| direct.addr).collect());
+            .with_direct_addresses(ant_addrs.direct_addresses);
 
         // Bat discovers ant through some out-of-band process.
         bat_discovery.add_node_info(ant_node_info);
@@ -785,9 +785,9 @@ mod tests {
             call!(bat_gossip_actor, ToGossip::Subscribe, topic_id, bat_peers).unwrap();
 
         // Obtain bat's node information including direct addresses.
-        let bat_addrs = bat_endpoint.direct_addresses().initialized().await;
+        let bat_addrs = bat_endpoint.node_addr();
         let bat_node_info = NodeInfo::new(from_public_key(bat_public_key))
-            .with_direct_addresses(bat_addrs.into_iter().map(|direct| direct.addr).collect());
+            .with_direct_addresses(bat_addrs.direct_addresses);
 
         // Cat discovers bat through some out-of-band process.
         cat_discovery.add_node_info(bat_node_info);
@@ -893,9 +893,9 @@ mod tests {
             .unwrap();
 
         // Obtain ant's node information including direct addresses.
-        let ant_addrs = ant_endpoint.direct_addresses().initialized().await;
+        let ant_addrs = ant_endpoint.node_addr();
         let ant_node_info = NodeInfo::new(from_public_key(ant_public_key))
-            .with_direct_addresses(ant_addrs.into_iter().map(|direct| direct.addr).collect());
+            .with_direct_addresses(ant_addrs.direct_addresses);
 
         // Bat discovers ant through some out-of-band process.
         bat_discovery.add_node_info(ant_node_info);
@@ -996,9 +996,9 @@ mod tests {
         // topic but cannot "hear" one another.
 
         // Obtain bat's node information including direct addresses.
-        let bat_addrs = bat_endpoint.direct_addresses().initialized().await;
+        let bat_addrs = bat_endpoint.node_addr();
         let bat_node_info = NodeInfo::new(from_public_key(bat_public_key))
-            .with_direct_addresses(bat_addrs.into_iter().map(|direct| direct.addr).collect());
+            .with_direct_addresses(bat_addrs.direct_addresses);
 
         // Cat discovers bat through some out-of-band process.
         cat_discovery.add_node_info(bat_node_info);
