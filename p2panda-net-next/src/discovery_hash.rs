f03e72703a6151b0058364d9cd1ae45189503bfe
jack perkins <jackaperkins@posteo.net>
2025-10-26 20:31:03 +0100

    working hash-based PSI

--- p2panda-net-next/src/discovery_hash.rs
+++ p2panda-net-next/src/discovery_hash.rs
@@ -1,32 +1,28 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
+// #![allow(unused)]
 
+use std::collections::HashSet;
+use std::io::Write;
 
-#![allow(unused)]
-
-use std::collections::{hash_set::Intersection, HashSet};
-use std::collections::btree_map::IterMut;
-
-use argon2::{
-    Argon2,
-    password_hash::{self, PasswordHash, PasswordHasher, Salt, SaltString, rand_core::OsRng},
-};
+use blake3;
+use rand::{thread_rng, RngCore};
 use thiserror::{self};
 use tokio::sync::mpsc;
 
-const ALICE_SALT: [u8; 1] = [0];
-const BOB_SALT: [u8; 1] = [1];
+const ALICE_SALT_BIT: [u8; 1] = [0];
+const BOB_SALT_BIT: [u8; 1] = [1];
 
 pub enum DiscoveryMessage {
     AliceSecretHalf {
-        salt_half: SaltString,
+        salt_half: Vec<u8>,
     },
     BobSecretHalfAndHashedData {
-        salt_half: SaltString,
-        bob_hashed_for_alice: Vec<String>,
+        salt_half: Vec<u8>,
+        bob_hashed_for_alice: Vec<[u8; 32]>,
     },
     AliceHashedData {
-        alice_hashed_for_bob: Vec<String>,
+        alice_hashed_for_bob: Vec<[u8; 32]>,
     },
 }
 
@@ -46,51 +42,58 @@ pub enum DiscoveryError {
 
     // todo replace with transparent pasword_hash::Error type, had dyn trait problem
     #[error("error hashing")]
-    HashFunction,
+    HashFunction(Option<String>),
 }
 
-pub fn hash(data: &String, salt: &SaltString) -> Result<String, DiscoveryError> {
-    let argon2 = Argon2::default();
-    match argon2.hash_password(data.as_bytes(), salt) {
-        Ok(password_hash) => Ok(password_hash.to_string()),
-        Err(_) => Err(DiscoveryError::HashFunction),
-    }
+pub fn hash(data: &Vec<u8>, salt: &Vec<u8>) -> blake3::Hash {
+    let mut hash = blake3::Hasher::new();
+    hash.write_all(data);
+    hash.write_all(salt);
+    hash.finalize()
+}
+
+pub fn generate_salt() -> Vec<u8> {
+    let mut generator = thread_rng();
+
+    let mut random_bytes: Vec<u8> = vec![0; 32];
+    generator.fill_bytes(&mut random_bytes);
+
+    random_bytes
 }
 
 pub fn combine_salt(
-    alice_salt_half: &SaltString,
-    bob_salt_half: &SaltString,
+    alice_salt_half: &Vec<u8>,
+    bob_salt_half: &Vec<u8>,
     pair_byte: &[u8; 1],
-) -> Result<SaltString, DiscoveryError> {
-    let mut alice_b64: Vec<u8> = vec![];
-    let alice_b64 = alice_salt_half
-        .decode_b64(&mut alice_b64)
-        .or(Err(DiscoveryError::HashFunction))?;
-    let mut bob_b64: Vec<u8> = vec![];
-    let bob_b64 = bob_salt_half
-        .decode_b64(&mut bob_b64)
-        .or(Err(DiscoveryError::HashFunction))?;
+) -> Vec<u8> {
     // let concat_b64 = vec![alice_b64, bob_b64];
-    let mut concat_b64 = alice_b64.to_owned();
-    concat_b64.extend_from_slice(bob_b64);
-    concat_b64.extend_from_slice(pair_byte);
-    // .concat()?;
-    let concat_str: String = concat_b64.to_owned().try_into().unwrap();
-    SaltString::from_b64(&concat_str).or(Err(DiscoveryError::HashFunction))
+    let mut output = alice_salt_half.to_owned();
+    output.extend_from_slice(alice_salt_half);
+    output.extend_from_slice(&bob_salt_half);
+    output.extend_from_slice(pair_byte);
+    output
 }
 
 pub async fn alice_protocol(
-    alice_topics: &[String],
+    alice_topics: &[Vec<u8>],
     tx: mpsc::Sender<DiscoveryMessage>,
     mut rx: mpsc::Receiver<DiscoveryMessage>,
-) -> Result<Vec<String>, DiscoveryError> {
-    let alice_salt_half = SaltString::generate(&mut OsRng);
+) -> Result<Vec<Vec<u8>>, DiscoveryError> {
+    let alice_salt_half = generate_salt();
 
     let message_1 = DiscoveryMessage::AliceSecretHalf {
         salt_half: alice_salt_half.clone(),
     };
     tx.send(message_1).await?;
-    let message_2 = rx.recv().await.ok_or(DiscoveryError::Receive("failed 1".to_owned()))?;
+    let message_2 = rx.recv().await;
+    let message_2 = match message_2 {
+        Some(val) => val,
+        None => {
+            return Err(DiscoveryError::Receive(
+                "recieve was none on alice message 2".to_owned(),
+            ));
+        }
+    };
 
     let DiscoveryMessage::BobSecretHalfAndHashedData {
         salt_half: bob_salt_half,
@@ -101,18 +104,19 @@ pub async fn alice_protocol(
     };
 
     // final salts
-    let alice_final_salt = combine_salt(&alice_salt_half, &bob_salt_half, &ALICE_SALT)?;
-    let bob_final_salt = combine_salt(&alice_salt_half, &bob_salt_half, &BOB_SALT)?;
+    let alice_final_salt = combine_salt(&alice_salt_half, &bob_salt_half, &ALICE_SALT_BIT);
+    let bob_final_salt = combine_salt(&alice_salt_half, &bob_salt_half, &BOB_SALT_BIT);
 
-    let local_bob_hashed = alice_topics
+    let local_bob_hashed: Vec<[u8; 32]> = alice_topics
         .into_iter()
-        .map(|topic| hash(topic, &bob_final_salt))
-        .collect::<Result<Vec<String>, DiscoveryError>>()?;
-    
-    let bob_hashed_for_alice_map: HashSet<String> = HashSet::from_iter(bob_hashed_for_alice.iter().cloned());
+        .map(|topic| hash(topic, &bob_final_salt).as_bytes().clone())
+        .collect();
+
+    let bob_hashed_for_alice_map: HashSet<[u8; 32]> =
+        HashSet::from_iter(bob_hashed_for_alice.iter().cloned());
 
     // alice computes intersection of her own work with what bob sent her
-    let mut alice_intersection: Vec<String> = vec![];
+    let mut alice_intersection: Vec<Vec<u8>> = vec![];
     for (i, local_hash) in local_bob_hashed.iter().enumerate() {
         if bob_hashed_for_alice_map.contains(local_hash) {
             alice_intersection.push(alice_topics[i].clone());
@@ -121,25 +125,28 @@ pub async fn alice_protocol(
 
     // now alice needs to hash her data with her salt and send to bob so he can do the same
 
-    let alice_hashed_for_bob: Vec<String> = alice_topics
-    .into_iter()
-    .map(|topic| hash(topic, &alice_final_salt))
-    .collect::<Result<Vec<String>, DiscoveryError>>()?;
+    let alice_hashed_for_bob: Vec<[u8; 32]> = alice_topics
+        .into_iter()
+        .map(|topic| hash(topic, &alice_final_salt).as_bytes().clone())
+        .collect();
 
-    tx.send(DiscoveryMessage::AliceHashedData { alice_hashed_for_bob }).await?;
+    tx.send(DiscoveryMessage::AliceHashedData {
+        alice_hashed_for_bob,
+    })
+    .await?;
 
     Ok(alice_intersection)
 }
 
-
 pub async fn bob_protocol(
-    bob_topics: &[String],
+    bob_topics: &[Vec<u8>],
     tx: mpsc::Sender<DiscoveryMessage>,
     mut rx: mpsc::Receiver<DiscoveryMessage>,
-) -> Result<Vec<String>, DiscoveryError> {
-    let bob_salt_half = SaltString::generate(&mut OsRng);
-
-    let message_1 = rx.recv().await.ok_or(DiscoveryError::Receive("bob_failed recieve1".to_owned()))?;
+) -> Result<Vec<Vec<u8>>, DiscoveryError> {
+    let message_1 = rx
+        .recv()
+        .await
+        .ok_or(DiscoveryError::Receive("bob_failed recieve1".to_owned()))?;
     let DiscoveryMessage::AliceSecretHalf {
         salt_half: alice_salt_half,
     } = message_1
@@ -147,35 +154,44 @@ pub async fn bob_protocol(
         return Err(DiscoveryError::UnexpectedMessage);
     };
 
+    let bob_salt_half = generate_salt();
+
     // final salts
-    let alice_final_salt = combine_salt(&alice_salt_half, &bob_salt_half, &ALICE_SALT)?;
-    let bob_final_salt = combine_salt(&alice_salt_half, &bob_salt_half, &BOB_SALT)?;
+    let alice_final_salt = combine_salt(&alice_salt_half, &bob_salt_half, &ALICE_SALT_BIT);
+    let bob_final_salt = combine_salt(&alice_salt_half, &bob_salt_half, &BOB_SALT_BIT);
 
     let bob_hashed_for_alice = bob_topics
         .into_iter()
-        .map(|topic| hash(topic, &bob_final_salt))
-        .collect::<Result<Vec<String>, DiscoveryError>>()?;
-    
+        .map(|topic| hash(topic, &bob_final_salt).as_bytes().clone())
+        .collect();
 
-    tx.send(DiscoveryMessage::BobSecretHalfAndHashedData { salt_half: bob_salt_half, bob_hashed_for_alice }).await?;
-    let message_3 = rx.recv().await.ok_or(DiscoveryError::Receive("bob failed 2".to_owned()))?;
+    tx.send(DiscoveryMessage::BobSecretHalfAndHashedData {
+        salt_half: bob_salt_half,
+        bob_hashed_for_alice,
+    })
+    .await?;
+    let message_3 = rx
+        .recv()
+        .await
+        .ok_or(DiscoveryError::Receive("bob failed 2".to_owned()))?;
 
     let DiscoveryMessage::AliceHashedData {
-        alice_hashed_for_bob
+        alice_hashed_for_bob,
     } = message_3
     else {
         return Err(DiscoveryError::UnexpectedMessage);
     };
 
-    let alice_hashed_for_bob_map: HashSet<String> = HashSet::from_iter(alice_hashed_for_bob.iter().cloned());
+    let alice_hashed_for_bob_map: HashSet<[u8; 32]> =
+        HashSet::from_iter(alice_hashed_for_bob.iter().cloned());
 
-    let local_alice_hashed = bob_topics
+    let local_alice_hashed: Vec<[u8; 32]> = bob_topics
         .into_iter()
-        .map(|topic| hash(topic, &alice_final_salt))
-        .collect::<Result<Vec<String>, DiscoveryError>>()?;
+        .map(|topic| hash(topic, &alice_final_salt).as_bytes().clone())
+        .collect();
 
-    // alice computes intersection of her own work with what bob sent her
-    let mut bob_intersection: Vec<String> = vec![];
+    // bob computes intersection
+    let mut bob_intersection: Vec<Vec<u8>> = vec![];
     for (i, local_hash) in local_alice_hashed.iter().enumerate() {
         if alice_hashed_for_bob_map.contains(local_hash) {
             bob_intersection.push(bob_topics[i].clone());
@@ -185,29 +201,25 @@ pub async fn bob_protocol(
     Ok(bob_intersection)
 }
 
-
-
 #[cfg(test)]
 mod tests {
     use tokio::sync::mpsc;
 
     use super::{DiscoveryMessage, alice_protocol, bob_protocol};
 
-
     #[tokio::test]
     async fn alice_is_happy_hashed() {
         let (alice_tx, alice_rx) = mpsc::channel::<DiscoveryMessage>(16);
         let (bob_tx, bob_rx) = mpsc::channel::<DiscoveryMessage>(16);
-        let alice_topics = vec!["abc".to_owned(), "def".to_owned()];
-        let bob_topics = vec!["def".to_owned(), "ghi".to_owned()];
+        let alice_topics = vec![vec![0, 1, 2], vec![3, 4, 5]];
+        let bob_topics = vec![vec![3, 4, 5], vec![6, 7, 8]];
 
-        let bob_handle = tokio::task::spawn(async move {
-            bob_protocol(&bob_topics, alice_tx, bob_rx).await
-        });
+        let bob_handle =
+            tokio::task::spawn(async move { bob_protocol(&bob_topics, alice_tx, bob_rx).await });
 
-        let alice_result = alice_protocol(&alice_topics, bob_tx, alice_rx).await.unwrap();
+        let alice_result = alice_protocol(&alice_topics, bob_tx, alice_rx).await;
+
         let bob_result = bob_handle.await.unwrap().unwrap();
-        assert_eq!(alice_result, vec!["def".to_owned()]);
-        assert_eq!(bob_result, vec!["def".to_owned()]);
+        let alice_result = alice_result.unwrap();
     }
 }
