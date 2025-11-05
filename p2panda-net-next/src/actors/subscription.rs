e816ab5b7b9e72aeda99bca6183f36226d55fbe3
glyph <gnomad@cryptolab.net>
2025-11-05 09:47:46 +0000

    Introduce ephemeral messaging (#839)
    * Add basic NetworkBuilder and ProtocolMap
    
    * Instantiate and populate the router inside the endpoint actor
    
    * Don't respawn the endpoint actor on failure; shut down all supervised actors
    
    * Move the protocol map field onto EndpointConfig
    
    * Move endpoint module and introduce online actor
    
    * Move online check into endpoint actor
    
    * Replace XOR with blake3 hash for protocol ID
    
    * Use hash function from p2panda_core to avoid blake3 dependency
    
    * Remove outdated TODO comment
    
    * Introduce first-pass ephemeral topic streams
    
    * Add a subscription message type to create an ephemeral stream
    
    * Begin implementing top-level Network API for streams
    
    * Simplify ToNetwork type and introduce FromNetwork constructors
    
    * Modify gossip actor to always keep a single subscription per topic id
    
    * Handle topic resubscriptions and stream subscriptions
    
    * Introduce TopicStreamError and ephemeral topic stream subscriptions
    
    * Derive PartialEq for FromNetwork
    
    * Update gossip tests to expect broadcast channels
    
    * Rename Network actor to Supervisor
    
    * Clean up tests
    
    * Introduce try_recv method for ephemeral subscription
    
    * Implement build method on Network and add minimal integration test
    
    * Implement unsubscribe logic for gossip actor
    
    * Implement unsubscribe logic for subscription actor
    
    * Add to module docs and introduce unsubscribe method for ephemeral topic stream
    
    * Fix formatting
    
    * Add functions to add and remove public key prefix from actor name
    
    * Export utils module
    
    * Add and remove public key suffix from actor names
    
    * Add a second node to the integration test
    
    * Remove mut on self
    
    * Simplify state drops
    
    * Add ActorNamespace type and rename helper functions
    
    * Update TODO comment
    
    * Rename unsubscribe method
    
    * Rename ReturnEphemeralSubscription variant
    
    * Shorten type names for streams
    
    * Improve naming of sender maps
    
    * Replace txs with senders
    
    * Try to improve grouping of state for gossip sessions
    
    * Clarify module docs around Endpoint requirement
    
    * Improve stream errors
    
    * Another pass at improving errors
    
    * Implement helper to get actor reference
    
    * Remove private type imports
    
    * Apply clippy suggestion

--- p2panda-net-next/src/actors/subscription.rs
+++ p2panda-net-next/src/actors/subscription.rs
@@ -6,16 +6,35 @@
 //! of the spawned actors, restarting them in the event of failure.
 //!
 //! An iroh `Endpoint` is held as part of the internal state of this actor. This allows an
-//! `Endpoint` to be passed into the gossip and sync actors in the event that they need to be
-//! respawned.
+//! `Endpoint` to be passed into the gossip actor in the event that it needs to be respawned (since
+//! the `Endpoint` is needed to instantiate iroh `Gossip`).
+use std::collections::HashMap;
+
 use iroh::Endpoint as IrohEndpoint;
-use ractor::{Actor, ActorProcessingErr, ActorRef, Message, SupervisionEvent};
+use ractor::{
+    Actor, ActorProcessingErr, ActorRef, Message, RpcReplyPort, SupervisionEvent, call, cast,
+};
+use tokio::sync::broadcast::Sender as BroadcastSender;
+use tokio::sync::mpsc::Sender;
 use tracing::{debug, warn};
 
 use crate::actors::gossip::{Gossip, ToGossip};
 use crate::actors::sync::{Sync, ToSync};
+use crate::actors::{generate_actor_namespace, with_namespace, without_namespace};
+use crate::network::{FromNetwork, ToNetwork};
+use crate::topic_streams::{EphemeralStream, EphemeralStreamSubscription};
+use crate::{TopicId, to_public_key};
 
-pub enum ToSubscription {}
+pub enum ToSubscription {
+    /// Subscribe to the topic ID and return a publishing handle.
+    CreateEphemeralStream(TopicId, RpcReplyPort<EphemeralStream>),
+
+    /// Return a subscription handle for the given topic ID.
+    EphemeralSubscription(TopicId, RpcReplyPort<Option<EphemeralStreamSubscription>>),
+
+    /// Unsubscribe from an ephemeral stream for the given topic ID.
+    UnsubscribeEphemeral(TopicId),
+}
 
 impl Message for ToSubscription {}
 
@@ -25,6 +44,8 @@ pub struct SubscriptionState {
     gossip_actor_failures: u16,
     sync_actor: ActorRef<ToSync>,
     sync_actor_failures: u16,
+    to_gossip_senders: HashMap<TopicId, Sender<ToNetwork>>,
+    from_gossip_senders: HashMap<TopicId, BroadcastSender<FromNetwork>>,
 }
 
 pub struct Subscription;
@@ -39,9 +60,11 @@ impl Actor for Subscription {
         myself: ActorRef<Self::Msg>,
         endpoint: Self::Arguments,
     ) -> Result<Self::State, ActorProcessingErr> {
+        let actor_namespace = generate_actor_namespace(&to_public_key(endpoint.node_id()));
+
         // Spawn the gossip actor.
         let (gossip_actor, _) = Actor::spawn_linked(
-            Some("gossip".to_string()),
+            Some(with_namespace("gossip", &actor_namespace)),
             Gossip {},
             endpoint.clone(),
             myself.clone().into(),
@@ -49,8 +72,16 @@ impl Actor for Subscription {
         .await?;
 
         // Spawn the sync actor.
-        let (sync_actor, _) =
-            Actor::spawn_linked(Some("sync".to_string()), Sync {}, (), myself.into()).await?;
+        let (sync_actor, _) = Actor::spawn_linked(
+            Some(with_namespace("sync", &actor_namespace)),
+            Sync {},
+            (),
+            myself.into(),
+        )
+        .await?;
+
+        let to_gossip_senders = HashMap::new();
+        let from_gossip_senders = HashMap::new();
 
         let state = SubscriptionState {
             endpoint,
@@ -58,6 +89,8 @@ impl Actor for Subscription {
             gossip_actor_failures: 0,
             sync_actor,
             sync_actor_failures: 0,
+            to_gossip_senders,
+            from_gossip_senders,
         };
 
         Ok(state)
@@ -82,9 +115,68 @@ impl Actor for Subscription {
     async fn handle(
         &self,
         _myself: ActorRef<Self::Msg>,
-        _message: Self::Msg,
-        _state: &mut Self::State,
+        message: Self::Msg,
+        state: &mut Self::State,
     ) -> Result<(), ActorProcessingErr> {
+        match message {
+            ToSubscription::CreateEphemeralStream(topic_id, reply) => {
+                let actor_namespace =
+                    generate_actor_namespace(&to_public_key(state.endpoint.node_id()));
+
+                // TODO: Ask address book for all peers interested in this topic id.
+                let peers = Vec::new();
+
+                // Check if we're already subscribed.
+                let stream = if let Some(to_gossip_tx) = state.to_gossip_senders.get(&topic_id) {
+                    // Inform the gossip actor about the latest set of peers for this topic id.
+                    cast!(state.gossip_actor, ToGossip::JoinPeers(topic_id, peers))?;
+
+                    EphemeralStream::new(topic_id, to_gossip_tx.clone(), actor_namespace)
+                } else {
+                    // Register a new session with the gossip actor.
+                    let (to_gossip_tx, from_gossip_tx) =
+                        call!(state.gossip_actor, ToGossip::Subscribe, topic_id, peers)?;
+
+                    // Store the gossip sender which is used to publish messages into the
+                    // topic.
+                    state
+                        .to_gossip_senders
+                        .insert(topic_id, to_gossip_tx.clone());
+
+                    // Store the gossip sender. This can be used to create a broadcast receiver
+                    // when the user calls `.subscribe()` on `EphemeralStream`.
+                    state.from_gossip_senders.insert(topic_id, from_gossip_tx);
+
+                    EphemeralStream::new(topic_id, to_gossip_tx, actor_namespace)
+                };
+
+                if !reply.is_closed() {
+                    let _ = reply.send(stream);
+                }
+            }
+            ToSubscription::EphemeralSubscription(topic_id, reply) => {
+                if let Some(from_gossip_tx) = state.from_gossip_senders.get(&topic_id) {
+                    let from_gossip_rx = from_gossip_tx.subscribe();
+
+                    let subscription = EphemeralStreamSubscription::new(topic_id, from_gossip_rx);
+
+                    if !reply.is_closed() {
+                        let _ = reply.send(Some(subscription));
+                    }
+                } else if !reply.is_closed() {
+                    let _ = reply.send(None);
+                }
+            }
+            ToSubscription::UnsubscribeEphemeral(topic_id) => {
+                // Drop all senders associated with the topic id..
+                let _ = state.to_gossip_senders.remove(&topic_id);
+                let _ = state.from_gossip_senders.remove(&topic_id);
+
+                // Tell the gossip actor to unsubscribe from this topic id.
+                cast!(state.gossip_actor, ToGossip::Unsubscribe(topic_id))?;
+            }
+        }
+
         Ok(())
     }
 
@@ -97,17 +189,23 @@ impl Actor for Subscription {
         match message {
             SupervisionEvent::ActorStarted(actor) => {
                 if let Some(name) = actor.get_name() {
-                    debug!("subscription actor: received ready from {} actor", name);
+                    debug!(
+                        "subscription actor: received ready from {} actor",
+                        without_namespace(&name)
+                    );
                 }
             }
             SupervisionEvent::ActorFailed(actor, panic_msg) => {
-                match actor.get_name().as_deref() {
-                    Some("gossip") => {
+                let actor_namespace =
+                    generate_actor_namespace(&to_public_key(state.endpoint.node_id()));
+
+                if let Some(name) = actor.get_name().as_deref() {
+                    if name == with_namespace("gossip", &actor_namespace) {
                         warn!("subscription actor: gossip actor failed: {}", panic_msg);
 
                         // Respawn the gossip actor.
                         let (gossip_actor, _) = Actor::spawn_linked(
-                            Some("gossip".to_string()),
+                            Some(with_namespace("gossip", &actor_namespace)),
                             Gossip {},
                             state.endpoint.clone(),
                             myself.clone().into(),
@@ -116,13 +214,12 @@ impl Actor for Subscription {
 
                         state.gossip_actor_failures += 1;
                         state.gossip_actor = gossip_actor;
-                    }
-                    Some("sync") => {
+                    } else if name == with_namespace("sync", &actor_namespace) {
                         warn!("subscription actor: sync actor failed: {}", panic_msg);
 
                         // Respawn the sync actor.
                         let (sync_actor, _) = Actor::spawn_linked(
-                            Some("sync".to_string()),
+                            Some(with_namespace("sync", &actor_namespace)),
                             Sync {},
                             (),
                             myself.clone().into(),
@@ -132,12 +229,14 @@ impl Actor for Subscription {
                         state.sync_actor_failures += 1;
                         state.sync_actor = sync_actor;
                     }
-                    _ => (),
                 }
             }
             SupervisionEvent::ActorTerminated(actor, _last_state, _reason) => {
                 if let Some(name) = actor.get_name() {
-                    debug!("subscription actor: {} actor terminated", name);
+                    debug!(
+                        "subscription actor: {} actor terminated",
+                        without_namespace(&name)
+                    );
                 }
             }
             _ => (),
