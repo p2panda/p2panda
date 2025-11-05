bd07fb62da0064ce280c982fe38ba9e49ff3ddfd
adz <x12@adz.garden>
2025-11-05 17:23:31 +0100

    tests: Check info insertion

--- p2panda-net-next/src/actors/address_book.rs
+++ p2panda-net-next/src/actors/address_book.rs
@@ -17,6 +17,11 @@ use crate::{NodeId, NodeInfo, TopicId, TransportInfo};
 pub const ADDRESS_BOOK: &str = "net.address_book";
 
 pub enum ToAddressBook {
+    /// Returns information about a node.
+    ///
+    /// Returns `None` if no information was found for this node.
+    NodeInfo(NodeId, RpcReplyPort<Option<NodeInfo>>),
+
     /// Inserts or updates node information into address book. Use this method if adding node
     /// information from a local configuration, trusted, external source, etc.
     ///
@@ -91,6 +96,10 @@ where
         // Note that critical storage failures will return an `ActorProcessingErr` and cause this
         // actor to restart when supervised.
         match message {
+            ToAddressBook::NodeInfo(node_id, reply) => {
+                let result = state.store.node_info(&node_id).await?;
+                let _ = reply.send(result);
+            }
             ToAddressBook::InsertNodeInfo(node_info, reply) => {
                 // Check signature of information. Is it authentic?
                 if let Err(err) = node_info
@@ -116,28 +125,12 @@ where
                 }
 
                 // Is there already an existing entry? Only replace it when information is newer
-                // (it's a simple "last write wins" principle based on a UNIX timestamp).
-                let current = state.store.node_info(&node_id).await?;
-                let current_timestamp = {
-                    let current_transports = current
-                        .as_ref()
-                        .map(|info| info.transports.as_ref())
-                        .flatten();
-                    current_transports.map(|transports| transports.timestamp)
-                };
-
-                let replace_transport_info = match current_timestamp {
-                    Some(current_timestamp) => current_timestamp < transport_info.timestamp,
-                    None => true,
-                };
-
-                if !replace_transport_info {
-                    let _ = reply.send(Ok(false));
-                    return Ok(());
-                }
-
+                // (it's a simple "last write wins" principle based on a UNIX timestamp) handled
+                // inside of `update_transports`.
+                //
                 // If a node info already exists, only update the "transports" aspect of it and
                 // keep any other "local" configuration, otherwise create a new "default" node info.
+                let current = state.store.node_info(&node_id).await?;
                 let node_info = match current {
                     Some(mut current) => current.update_transports(transport_info).map(|_| current),
                     None => Ok(NodeInfo::new(node_id)),
@@ -163,4 +156,132 @@ where
 pub enum AddressBookError {
     #[error(transparent)]
     NodeInfo(crate::addrs::NodeInfoError),
+}
+
+#[cfg(test)]
+mod tests {
+    use p2panda_core::PrivateKey;
+    use p2panda_discovery::address_book::memory::MemoryStore;
+    use ractor::thread_local::{ThreadLocalActor, ThreadLocalActorSpawner};
+    use ractor::{Actor, call};
+    use rand::SeedableRng;
+    use rand_chacha::ChaCha20Rng;
+
+    use crate::actors::{generate_actor_namespace, with_namespace};
+    use crate::addrs::{NodeId, NodeInfo};
+    use crate::args::ApplicationArguments;
+    use crate::{TransportAddress, UnsignedTransportInfo};
+
+    use super::{ADDRESS_BOOK, AddressBook, ToAddressBook};
+
+    type TestTopic = ();
+
+    #[tokio::test]
+    async fn insert_node_and_transport_info() {
+        let private_key = PrivateKey::new();
+        let public_key = private_key.public_key();
+        let actor_namespace = generate_actor_namespace(&public_key);
+        let spawner = ThreadLocalActorSpawner::new();
+        let rng = ChaCha20Rng::from_seed([1; 32]);
+
+        let (actor, handle) = AddressBook::spawn(
+            Some(with_namespace(ADDRESS_BOOK, &actor_namespace)),
+            ApplicationArguments {
+                private_key: private_key.clone(),
+                store: MemoryStore::<ChaCha20Rng, TestTopic, NodeId, NodeInfo>::new(rng),
+            },
+            spawner,
+        )
+        .await
+        .unwrap();
+
+        // Insert new node info.
+        let node_info = NodeInfo::new(public_key);
+        let result = call!(actor, ToAddressBook::InsertNodeInfo, node_info).unwrap();
+        assert!(result.is_ok());
+        assert!(result.unwrap());
+
+        // Overwriting node info should return "false".
+        let mut node_info = NodeInfo::new(public_key);
+        node_info.bootstrap = true;
+        let result = call!(actor, ToAddressBook::InsertNodeInfo, node_info).unwrap();
+        assert!(result.is_ok());
+        assert!(!result.unwrap());
+
+        // Bootstrap should be set to "true", as node info was still overwritten.
+        let result = call!(actor, ToAddressBook::NodeInfo, public_key).unwrap();
+        assert!(result.expect("node info exists in store").bootstrap);
+
+        // Inserting invalid node info should fail.
+        let node_info = {
+            NodeInfo {
+                node_id: public_key,
+                bootstrap: false,
+                transports: Some({
+                    let mut unsigned = UnsignedTransportInfo::new();
+                    unsigned.add_addr(TransportAddress::from_iroh(
+                        public_key,
+                        Some("https://my.relay.net".parse().unwrap()),
+                        [],
+                    ));
+                    let mut transport_info = unsigned.sign(&private_key).unwrap();
+                    transport_info.timestamp = 1234; // Manipulate timestamp to make signature invalid
+                    transport_info
+                }),
+            }
+        };
+        assert!(node_info.verify().is_err());
+        let result = call!(actor, ToAddressBook::InsertNodeInfo, node_info).unwrap();
+        assert!(result.is_err());
+
+        // Inserting transport info should not overwrite "local" data.
+        let mut node_info = NodeInfo::new(public_key);
+        node_info.bootstrap = true;
+        let result = call!(actor, ToAddressBook::InsertNodeInfo, node_info).unwrap();
+        assert!(result.is_ok());
+
+        let transport_info = {
+            let mut unsigned = UnsignedTransportInfo::new();
+            unsigned.add_addr(TransportAddress::from_iroh(
+                public_key,
+                Some("https://my.relay.net".parse().unwrap()),
+                [],
+            ));
+            unsigned.sign(&private_key).unwrap()
+        };
+        let result = call!(
+            actor,
+            ToAddressBook::InsertTransportInfo,
+            public_key,
+            transport_info
+        )
+        .unwrap();
+        assert!(result.is_ok());
+
+        // Even after insertion of new transport info, the "local" bootstrap config is still true.
+        let result = call!(actor, ToAddressBook::NodeInfo, public_key).unwrap();
+        assert!(result.expect("node info exists in store").bootstrap);
+
+        // Inserting invalid transport info should fail.
+        let transport_info = {
+            let mut unsigned = UnsignedTransportInfo::new();
+            unsigned.add_addr(TransportAddress::from_iroh(
+                public_key,
+                Some("https://my.relay.net".parse().unwrap()),
+                [],
+            ));
+            let mut transport_info = unsigned.sign(&private_key).unwrap();
+            transport_info.timestamp = 1234; // Manipulate timestamp to make signature invalid
+            transport_info
+        };
+        assert!(transport_info.verify(&public_key).is_err());
+        let result = call!(
+            actor,
+            ToAddressBook::InsertTransportInfo,
+            public_key,
+            transport_info
+        )
+        .unwrap();
+        assert!(result.is_err());
+    }
 }
