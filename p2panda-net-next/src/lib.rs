92daa5d4b878f5f6a8e4b806e94bb4eb20aee17f
adz <x12@adz.garden>
2025-11-07 14:28:47 +0100

    chore: Introduce utils mod, remove unused endpoint mod

--- p2panda-net-next/src/lib.rs
+++ p2panda-net-next/src/lib.rs
@@ -2,8 +2,6 @@
 
 // @TODO: Remove this.
 #![allow(unused)]
-use std::time::{SystemTime, UNIX_EPOCH};
-
 mod actors;
 mod addrs;
 mod args;
@@ -11,6 +9,7 @@ mod config;
 mod network;
 mod protocols;
 mod topic_streams;
+mod utils;
 
 pub use addrs::{
     NodeId, NodeInfo, NodeInfoError, TransportAddress, TransportInfo, UnsignedTransportInfo,
@@ -37,28 +36,3 @@ pub type TopicId = [u8; 32];
 /// protocols, any communication attempts will fail if they are not using the same network
 /// identifier.
 pub type NetworkId = [u8; 32];
-
-/// Returns current UNIX timestamp from local system time.
-fn current_timestamp() -> u64 {
-    SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .expect("system clock moved backwards")
-        .as_secs()
-}
-
-/// Converts an `iroh` public key type to the `p2panda-core` implementation.
-pub(crate) fn to_public_key(key: iroh_base::PublicKey) -> p2panda_core::PublicKey {
-    p2panda_core::PublicKey::from_bytes(key.as_bytes()).expect("already validated public key")
-}
-
-/// Converts a `p2panda-core` public key to the "iroh" type.
-pub(crate) fn from_public_key(key: p2panda_core::PublicKey) -> iroh_base::PublicKey {
-    iroh_base::PublicKey::from_bytes(key.as_bytes()).expect("already validated public key")
-}
-
-/// Converts a `p2panda-core` private key to the "iroh" type.
-// TODO: Remove once used.
-#[allow(dead_code)]
-pub(crate) fn from_private_key(key: p2panda_core::PrivateKey) -> iroh_base::SecretKey {
-    iroh_base::SecretKey::from_bytes(key.as_bytes())
-}
