76344c2db31d5ab51f6b44ed9ee9c281847a81e1
adz <x12@adz.garden>
2025-11-17 19:00:17 +0100

    Clippy

--- p2panda-sync-next/src/protocols/topic_handshake.rs
+++ p2panda-sync-next/src/protocols/topic_handshake.rs
@@ -196,7 +196,7 @@ pub enum TopicHandshakeError<T> {
 
 impl<T: std::fmt::Debug> Display for TopicHandshakeError<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}", self)
+        write!(f, "{self:?}")
     }
 }
 
