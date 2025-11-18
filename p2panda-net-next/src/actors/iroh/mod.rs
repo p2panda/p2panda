8db13c3fb1c002cf36bb06f3404abb5e818f50a7
adz <x12@adz.garden>
2025-11-18 23:05:39 +0100

    Discovery service connecting address book with iroh

--- p2panda-net-next/src/actors/iroh/mod.rs
+++ p2panda-net-next/src/actors/iroh/mod.rs
@@ -7,21 +7,27 @@ mod mdns;
 #[cfg(test)]
 mod tests;
 
+use std::collections::BTreeSet;
 use std::num::ParseIntError;
+use std::pin::Pin;
 use std::str::FromStr;
 
-use iroh::discovery::UserData;
+use futures_util::Stream;
+use iroh::discovery::{Discovery, EndpointData, UserData};
 use iroh::endpoint_info::MaxLengthExceededError;
 use iroh::protocol::ProtocolHandler;
 use p2panda_core::{IdentityError, Signature};
 use ractor::{ActorRef, call, registry};
 use thiserror::Error;
+use tracing::{debug, error, warn};
 
-use crate::TransportInfo;
 use crate::actors::address_book::{ADDRESS_BOOK, ToAddressBook};
 use crate::actors::iroh::connection::ConnectionActorError;
-use crate::actors::{ActorNamespace, with_namespace};
+use crate::actors::{ActorNamespace, generate_actor_namespace, with_namespace};
 use crate::addrs::NodeId;
+use crate::args::ApplicationArguments;
+use crate::utils::from_public_key;
+use crate::{NodeInfoError, TransportInfo, UnsignedTransportInfo};
 
 pub use endpoint::{IROH_ENDPOINT, IrohEndpoint, ToIrohEndpoint};
 #[cfg(feature = "mdns")]
@@ -194,3 +200,102 @@ pub enum TransportInfoTxtError {
     #[error(transparent)]
     Timestamp(#[from] ParseIntError),
 }
+
+#[derive(Debug)]
+struct AddressBookDiscovery {
+    actor_namespace: ActorNamespace,
+    args: ApplicationArguments,
+}
+
+impl AddressBookDiscovery {
+    pub fn new(args: ApplicationArguments) -> Self {
+        let actor_namespace = generate_actor_namespace(&args.public_key);
+        Self {
+            actor_namespace,
+            args,
+        }
+    }
+
+    async fn address_book_ref(actor_namespace: ActorNamespace) -> Option<ActorRef<ToAddressBook>> {
+        registry::where_is(with_namespace(ADDRESS_BOOK, &actor_namespace))
+            .map(ActorRef::<ToAddressBook>::from)
+    }
+
+    async fn update_address_book(
+        actor_namespace: ActorNamespace,
+        node_id: NodeId,
+        transport_info: TransportInfo,
+    ) -> Result<(), AddressBookDiscoveryError> {
+        let Some(address_book_ref) = Self::address_book_ref(actor_namespace).await else {
+            return Err(AddressBookDiscoveryError::ActorNotAvailable);
+        };
+
+        let _ = call!(
+            address_book_ref,
+            ToAddressBook::InsertTransportInfo,
+            node_id,
+            transport_info
+        )
+        .map_err(|_| AddressBookDiscoveryError::ActorFailed)?;
+
+        Ok(())
+    }
+}
+
+impl Discovery for AddressBookDiscovery {
+    fn publish(&self, data: &EndpointData) {
+        // Create a new transport info with iroh addresses if given. If no iroh address
+        // exists (because we are not reachable) we're explicitly making the address array
+        // empty to inform other nodes about this.
+        let Ok(transport_info) = if data.has_addrs() {
+            UnsignedTransportInfo::from_addrs([iroh::EndpointAddr {
+                id: from_public_key(self.args.public_key),
+                addrs: BTreeSet::from_iter(data.addrs().cloned().into_iter()),
+            }
+            .into()])
+        } else {
+            UnsignedTransportInfo::new()
+        }
+        .sign(&self.args.private_key) else {
+            error!("critically failed signing own transport info");
+            return;
+        };
+
+        let actor_namespace = self.actor_namespace.clone();
+        let public_key = self.args.public_key.clone();
+
+        tokio::task::spawn(async move {
+            // Update entry about ourselves in address book to allow this information to
+            // propagate in other discovery mechanisms or side-channels outside of iroh.
+            if let Err(err) =
+                Self::update_address_book(actor_namespace, public_key, transport_info.clone()).await
+            {
+                warn!("could not update address book with own transport info: {err:#?}");
+            } else {
+                debug!(?transport_info, "updated our iroh endpoint address");
+            }
+        });
+    }
+
+    fn resolve(
+        &self,
+        _endpoint_id: iroh::EndpointId,
+    ) -> Option<BoxStream<Result<iroh::discovery::DiscoveryItem, iroh::discovery::DiscoveryError>>>
+    {
+        None
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum AddressBookDiscoveryError {
+    #[error("address book actor is not available")]
+    ActorNotAvailable,
+
+    #[error("address book actor failed")]
+    ActorFailed,
+
+    #[error("could not update transport information: {0}")]
+    UpdateFailed(#[from] NodeInfoError),
+}
+
+pub type BoxStream<T> = Pin<Box<dyn Stream<Item = T> + Send + 'static>>;
