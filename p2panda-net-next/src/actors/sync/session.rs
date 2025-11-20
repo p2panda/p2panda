b73fc4083e55d1b90c43df6d1b12039ae00d7113
glyph <glyph@mycelial.technology>
2025-11-20 13:33:00 +0000

    Close connection after session ends

--- p2panda-net-next/src/actors/sync/session.rs
+++ p2panda-net-next/src/actors/sync/session.rs
@@ -4,7 +4,7 @@ use std::error::Error as StdError;
 use std::marker::PhantomData;
 
 use futures_channel::mpsc;
-use iroh::endpoint::Connection;
+use iroh::endpoint::{Connection, VarInt};
 use p2panda_sync::topic_handshake::{
     TopicHandshakeEvent, TopicHandshakeInitiator, TopicHandshakeMessage,
 };
@@ -101,6 +101,8 @@ where
                 let mut tx = into_cbor_sink::<P::Message, _>(tx);
                 let mut rx = into_cbor_stream::<P::Message, _>(rx);
                 protocol.run(&mut tx, &mut rx).await?;
+
+                connection.close(VarInt::from_u32(0), b"sync protocol initiate completed");
             }
             SyncSessionMessage::Accept {
                 connection,
@@ -112,6 +114,8 @@ where
                 let mut tx = into_cbor_sink::<P::Message, _>(tx);
                 let mut rx = into_cbor_stream::<P::Message, _>(rx);
                 protocol.run(&mut tx, &mut rx).await?;
+
+                connection.close(VarInt::from_u32(0), b"sync protocol accept completed");
             }
         }
         Ok(())
