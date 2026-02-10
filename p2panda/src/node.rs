8e803597ea6b47e4fa5b4c89f753edded75add74
adz <x12@adz.garden>
2026-02-10 17:07:04 +0100

    Introduce p2panda high-level API skeleton
    Here we are introducing the skeleton of the upcoming `p2panda`
    high-level API.
    
    This is still a draft, minor things might change, but we're setting this
    off to already start working on GObject and FFI.

---
+++ p2panda/src/node.rs
@@ -0,0 +1,109 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+use p2panda_core::{Hash, PrivateKey, PublicKey};
+use p2panda_net::NodeId;
+use p2panda_store::sqlite::{SqliteError, SqliteStore, SqliteStoreBuilder};
+use serde::{Deserialize, Serialize};
+use thiserror::Error;
+
+use crate::Topic;
+pub use crate::builder::NodeBuilder;
+use crate::network::{Network, NetworkConfig, NetworkError};
+use crate::streams::{EphemeralStreamHandle, EventStream, StreamHandle};
+
+// TODO: Can we expose network or will this explode the API surface for GObject unnecessarily?
+pub struct Node {
+    private_key: PrivateKey,
+    public_key: PublicKey,
+    store: SqliteStore<'static>,
+    network: Network,
+}
+
+impl Node {
+    pub fn builder() -> NodeBuilder {
+        NodeBuilder::new()
+    }
+
+    pub async fn spawn() -> Result<Self, NodeError> {
+        // Generates new private key using CSPRNG from system.
+        let private_key = PrivateKey::new();
+
+        // Initialises an in-memory SQLite database.
+        let store = SqliteStoreBuilder::default().build().await?;
+
+        // Use default config, this will _not_ include a bootstrap and relay and reduces the
+        // functionality of p2panda to only work on local-area networks.
+        let config = Config::default();
+
+        Node::spawn_inner(config, private_key, store).await
+    }
+
+    pub(crate) async fn spawn_inner(
+        config: Config,
+        private_key: PrivateKey,
+        store: SqliteStore<'static>,
+    ) -> Result<Self, NodeError> {
+        let public_key = private_key.public_key();
+        let network = Network::spawn(config.network, private_key.clone()).await?;
+
+        Ok(Node {
+            private_key,
+            public_key,
+            store,
+            network,
+        })
+    }
+
+    pub async fn stream<M>(&self, _topic: Topic) -> Result<StreamHandle<M>, NodeError>
+    where
+        M: Serialize + for<'a> Deserialize<'a>,
+    {
+        unimplemented!()
+    }
+
+    pub async fn ephemeral_stream<M>(
+        &self,
+        topic: Topic,
+    ) -> Result<EphemeralStreamHandle<M>, NodeError>
+    where
+        M: Serialize + for<'a> Deserialize<'a>,
+    {
+        unimplemented!()
+    }
+
+    pub async fn events(&self) -> Result<EventStream, NodeError> {
+        unimplemented!()
+    }
+
+    pub fn id(&self) -> NodeId {
+        self.public_key
+    }
+
+    pub fn commit(&self, _message_id: Hash) {
+        unimplemented!()
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Config {
+    pub auto_commit: bool,
+    pub network: NetworkConfig,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            auto_commit: true,
+            network: NetworkConfig::default(),
+        }
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum NodeError {
+    #[error(transparent)]
+    Network(#[from] NetworkError),
+
+    #[error(transparent)]
+    Store(#[from] SqliteError),
+}
---
+++ p2panda/src/streams/mod.rs
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+mod ephemeral_stream;
+mod event_stream;
+mod stream;
+
+pub use ephemeral_stream::{
+    EphemeralMessage, EphemeralStreamError, EphemeralStreamEvent, EphemeralStreamHandle,
+};
+pub use event_stream::{EventStream, SystemEvent};
+pub use stream::{Message, StreamError, StreamEvent, StreamHandle, StreamSubscription};
