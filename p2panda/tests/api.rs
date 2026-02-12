f3f16508ca44b1c0e6fb16681f4c0812f22db3ae
adz <x12@adz.garden>
2026-02-12 19:51:36 +0100

    Ephemeral Streams in p2panda Node
    Implements ephemeral messaging functionality for new p2panda Node API.
    
    This introduces an internally used "wrapper message" format which is
    encoded as CBOR and sent among nodes during gossip. It wraps the
    application's message, providing it with integrity and provenance
    guarantees with a signing scheme and makes sure that messages are unique
    with the help of an hybrid timestamp (microsecond-precision UNIX
    timestamp and logical lamport clock).
    
    The uniqueness is required as iroh-gossip aka p2panda-net ignores
    duplicate messages which shown to be confusing for users.

--- p2panda/tests/api.rs
+++ p2panda/tests/api.rs
@@ -1,33 +1,70 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
-// TODO: Remove this later.
-#![allow(unused)]
-
-use futures_core::Stream;
+use futures_util::StreamExt;
 use p2panda::Topic;
+use p2panda::streams::EphemeralMessage;
 use p2panda_core::PrivateKey;
+use tokio::task::JoinHandle;
 
 #[tokio::test]
-async fn it_works() -> Result<(), Box<dyn std::error::Error>> {
-    let node = p2panda::spawn().await?;
-    println!("{}", node.id());
+async fn build_and_spawn() -> Result<(), Box<dyn std::error::Error>> {
+    // Default & instant setup.
+    let _node = p2panda::spawn().await?;
 
-    let node = p2panda::builder()
+    // Customizable "builder" setup flow.
+    let _node = p2panda::builder()
         .database_url("sqlite::memory:")
         .private_key(PrivateKey::new())
         .spawn()
         .await?;
-    println!("{}", node.id());
-
-    // TODO: All of this is unimplemented:
-    // let channel_id = Topic::new();
-    // let channel = node.ephemeral_stream(channel_id).await?;
-    // println!("{}", channel_id);
-    //
-    // channel.publish("Hello, Panda!".to_string()).await?;
-    //
-    // let rx = channel.subscribe().await?;
-    // while let Some(event) = rx.next().await {}
 
     Ok(())
+}
+
+#[tokio::test]
+async fn ephemeral_messaging() {
+    let chat_id = Topic::new();
+
+    let panda = p2panda::spawn().await.unwrap();
+    let icebear = p2panda::spawn().await.unwrap();
+
+    // Panda joins the chat and sends a message to icebear, then waits for an answer.
+    let panda_task: JoinHandle<EphemeralMessage<String>> = {
+        let chat = panda.ephemeral_stream(chat_id).await.unwrap();
+
+        tokio::spawn(async move {
+            chat.publish("Hello, Icebear!".into()).await.unwrap();
+
+            let mut rx = chat.subscribe().await;
+            let message = rx.next().await.unwrap();
+
+            message
+        })
+    };
+
+    // Icebear joins the chat and waits for a message of panda, to then answer.
+    let icebear_task: JoinHandle<EphemeralMessage<String>> = {
+        let chat = icebear.ephemeral_stream(chat_id).await.unwrap();
+        let mut rx = chat.subscribe().await;
+
+        tokio::spawn(async move {
+            let message = rx.next().await.unwrap();
+            chat.publish("Hi, Panda!".into()).await.unwrap();
+            message
+        })
+    };
+
+    let icebears_received_msg = icebear_task.await.unwrap();
+    let pandas_received_msg = panda_task.await.unwrap();
+
+    // Message authors match the senders.
+    assert_eq!(icebears_received_msg.author(), panda.id());
+    assert_eq!(pandas_received_msg.author(), icebear.id());
+
+    // Everyone received the right messages.
+    assert_eq!(icebears_received_msg.body(), &"Hello, Icebear!".to_string());
+    assert_eq!(pandas_received_msg.body(), &"Hi, Panda!".to_string());
+
+    // Icebear received the message before panda.
+    assert!(icebears_received_msg.timestamp() < pandas_received_msg.timestamp())
 }
