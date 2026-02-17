52694954fcdf5b1da3421143416f66f746427417
Sam Andreae <contact@samandreae.com>
2026-02-17 11:39:52 +0000

    More generic trait definitions

--- p2panda-store-next/src/logs/sqlite.rs
+++ p2panda-store-next/src/logs/sqlite.rs
@@ -2,22 +2,23 @@
 
 use std::collections::HashMap;
 
-use p2panda_core::{Extensions, Hash, Operation};
+use p2panda_core::{Extensions, Hash, Operation, PublicKey};
 
-use crate::logs::traits::SeqNum;
 use crate::logs::{LogId, LogStore};
 use crate::sqlite::{SqliteError, SqliteStore};
 
-impl<'a, L, E> LogStore<Operation<E>, L, Hash> for SqliteStore<'a>
+pub type SeqNum = u64;
+
+impl<'a, L, E> LogStore<Operation<E>, PublicKey, L, SeqNum, Hash> for SqliteStore<'a>
 where
     E: Extensions,
     L: LogId,
 {
     type Error = SqliteError;
 
-    async fn get_log_height(
+    async fn get_latest_entry(
         &self,
-        public_key: &p2panda_core::PublicKey,
+        author: &PublicKey,
         log_id: &L,
     ) -> Result<Option<(Hash, SeqNum)>, Self::Error> {
         todo!()
@@ -25,15 +26,15 @@ where
 
     async fn get_log_heights(
         &self,
-        author: &p2panda_core::PublicKey,
+        author: &PublicKey,
         logs: &[L],
-    ) -> Result<Option<std::collections::HashMap<L, SeqNum>>, Self::Error> {
+    ) -> Result<Option<HashMap<L, SeqNum>>, Self::Error> {
         todo!()
     }
 
     async fn get_log_size(
         &self,
-        public_key: &p2panda_core::PublicKey,
+        author: &PublicKey,
         log_id: &L,
         after: Option<SeqNum>,
         until: Option<SeqNum>,
@@ -43,28 +44,28 @@ where
 
     async fn get_log_entries(
         &self,
-        public_key: &p2panda_core::PublicKey,
+        author: &PublicKey,
         log_id: &L,
         after: Option<SeqNum>,
         until: Option<SeqNum>,
         // @TODO: we actually want a wrapper type here so that we can easily access the serialized
-        // header and body bytes. 
+        // header and body bytes.
     ) -> Result<Option<Vec<Operation<E>>>, Self::Error> {
         todo!()
     }
 
     async fn prune_entries(
         &self,
-        author: &p2panda_core::PublicKey,
+        author: &PublicKey,
         log_id: &L,
         until: &SeqNum,
-    ) -> Result<bool, Self::Error> {
+    ) -> Result<u64, Self::Error> {
         todo!()
     }
 
     async fn get_log_entries_batch(
         &self,
-        public_key: &p2panda_core::PublicKey,
+        author: &PublicKey,
         ranges: &HashMap<L, (Option<SeqNum>, Option<SeqNum>)>,
     ) -> Result<Vec<Operation<E>>, Self::Error> {
         todo!()
@@ -72,7 +73,7 @@ where
 
     async fn get_log_size_batch(
         &self,
-        public_key: &p2panda_core::PublicKey,
+        author: &PublicKey,
         ranges: &HashMap<L, (Option<SeqNum>, Option<SeqNum>)>,
     ) -> Result<(u64, u64), Self::Error> {
         todo!()
--- p2panda-store-next/src/logs/traits.rs
+++ p2panda-store-next/src/logs/traits.rs
@@ -4,8 +4,6 @@ use std::collections::HashMap;
 use std::error::Error;
 use std::fmt::Debug;
 
-use p2panda_core::PublicKey;
-
 /// Uniquely identify a single-author log.
 ///
 /// The `LogId` exists purely to group a set of operations and is intended to be implemented for
@@ -35,25 +33,21 @@ pub trait LogId: Clone + Debug + Eq + std::hash::Hash {}
 
 impl<T> LogId for T where T: Clone + Debug + Eq + std::hash::Hash {}
 
-pub type SeqNum = u64;
-
 /// Store methods for aiding efficient comparison of log-based data types.
 ///
 /// The concrete message type contained on each log "entry" is not known in this API. It is
 /// assumed there is another store for retrieving these during sync (eg. OperationStore).
-pub trait LogStore<T, L, ID> {
+pub trait LogStore<T, A, L, S, ID> {
     type Error: Error;
 
-    /// Get the current height of a log.
-    ///
-    /// Returns the ID and sequence number for the latest entry in a log.
+    /// Get the ID and sequence number of the latest entry in a log.
     ///
     /// Returns None when the author or a log with the requested id was not found.
-    fn get_log_height(
+    fn get_latest_entry(
         &self,
-        public_key: &PublicKey,
+        author: &A,
         log_id: &L,
-    ) -> impl Future<Output = Result<Option<(ID, SeqNum)>, Self::Error>>;
+    ) -> impl Future<Output = Result<Option<(ID, S)>, Self::Error>>;
 
     /// Get current heights for a set of logs.
     ///
@@ -62,19 +56,19 @@ pub trait LogStore<T, L, ID> {
     /// Returns None when the author or a log with the requested id was not found.
     fn get_log_heights(
         &self,
-        author: &PublicKey,
+        author: &A,
         logs: &[L],
-    ) -> impl Future<Output = Result<Option<HashMap<L, SeqNum>>, Self::Error>>;
+    ) -> impl Future<Output = Result<Option<HashMap<L, S>>, Self::Error>>;
 
     /// Get the byte and operation count of the entries in a log.
     /// 
     /// `after` and `until` fields can be provided to select only a range of the log.
     fn get_log_size(
         &self,
-        public_key: &PublicKey,
+        author: &A,
         log_id: &L,
-        after: Option<SeqNum>,
-        until: Option<SeqNum>,
+        after: Option<S>,
+        until: Option<S>,
     ) -> impl Future<Output = Result<Option<(u64, u64)>, Self::Error>>;
 
     /// Get all entries in a log after an optional starting point.
@@ -82,19 +76,21 @@ pub trait LogStore<T, L, ID> {
     /// `after` and `to` fields can be provided to select only a range of the log.
     fn get_log_entries(
         &self,
-        public_key: &PublicKey,
+        author: &A,
         log_id: &L,
-        after: Option<SeqNum>,
-        until: Option<SeqNum>,
+        after: Option<S>,
+        until: Option<S>,
     ) -> impl Future<Output = Result<Option<Vec<T>>, Self::Error>>;
 
     /// Prune all entries in a log until the provided sequence number.
+    /// 
+    /// Returns the number of entries which were pruned.
     fn prune_entries(
         &self,
-        author: &PublicKey,
+        author: &A,
         log_id: &L,
-        until: &SeqNum,
-    ) -> impl Future<Output = Result<bool, Self::Error>>;
+        until: &S,
+    ) -> impl Future<Output = Result<u64, Self::Error>>;
 
     // == OPTIONAL BATCH QUERIES == //
 
@@ -106,8 +102,8 @@ pub trait LogStore<T, L, ID> {
     /// An optional range can be provided for every requested log.
     fn get_log_entries_batch(
         &self,
-        public_key: &PublicKey,
-        ranges: &HashMap<L, (Option<SeqNum>, Option<SeqNum>)>,
+        author: &A,
+        ranges: &HashMap<L, (Option<S>, Option<S>)>,
     ) -> impl Future<Output = Result<Vec<T>, Self::Error>>;
 
     /// Get the byte and operation count of the entries in a set of logs.
@@ -115,7 +111,7 @@ pub trait LogStore<T, L, ID> {
     /// An optional range can be provided for every requested log.
     fn get_log_size_batch(
         &self,
-        public_key: &PublicKey,
-        ranges: &HashMap<L, (Option<SeqNum>, Option<SeqNum>)>,
+        author: &A,
+        ranges: &HashMap<L, (Option<S>, Option<S>)>,
     ) -> impl Future<Output = Result<(u64, u64), Self::Error>>;
 }
