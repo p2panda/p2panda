fcbcea0b81b0a59a7022db9f12ec2edaaed996f8
adz <x12@adz.garden>
2026-02-19 18:13:55 +0100

    Transaction trait for SqliteStore
    Not all places in the p2panda stack know about the concrete database
    type SqliteStore and use trait bounds for certain store APIs instead.
    
    We still want to have database transactions all around. To allow using
    transactions in places where the store is generic, we're introducing an
    `Transaction` trait in p2panda-store-next.

--- p2panda-store-next/src/lib.rs
+++ p2panda-store-next/src/lib.rs
@@ -8,6 +8,8 @@ pub mod orderer;
 #[cfg(feature = "sqlite")]
 pub mod sqlite;
 pub mod topics;
+pub mod traits;
 
 #[cfg(feature = "sqlite")]
 pub use sqlite::{SqliteError, SqliteStore, SqliteStoreBuilder};
+pub use traits::Transaction;
