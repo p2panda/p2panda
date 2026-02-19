2f27d3232a82587bd04b13df8666386a70ab09a6
adz <x12@adz.garden>
2026-02-19 17:19:50 +0100

    Macros & builder improving usage of store in tests
    We're using in-memory SqliteStore A LOT and also transactions. This
    patch introduces an easy way to create a database for tests:
    
    ```rust
    let store = SqliteStore::temporary().await;
    ```
    
    .. and a macro to automatically wrap database operations within a
    transaction (begin & commit) like that:
    
    ```rust
    tx!(store, {
      // Your operations go in here.
    });
    ```
    
    For tests we can use tx_unwrap!() to make error-handling easier.

--- p2panda-store-next/src/lib.rs
+++ p2panda-store-next/src/lib.rs
@@ -1,8 +1,13 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
 pub mod address_book;
+#[cfg(feature = "macros")]
+pub mod macros;
 pub mod operations;
 pub mod orderer;
 #[cfg(feature = "sqlite")]
 pub mod sqlite;
 pub mod topics;
+
+#[cfg(feature = "sqlite")]
+pub use sqlite::{SqliteError, SqliteStore, SqliteStoreBuilder};
