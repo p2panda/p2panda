feab9574426848fef1b6f99414a74bacff2bf2ae
adz <x12@adz.garden>
2026-03-04 23:30:50 +0100

    Fix sqlite feature flag for TopicStore

--- p2panda-store/src/topics/mod.rs
+++ p2panda-store/src/topics/mod.rs
@@ -2,6 +2,7 @@
 
 //! `TopicStore` trait for managing mappings of application data to topics as well as a concrete
 //! `SqliteStore` implementation.
+#[cfg(feature = "sqlite")]
 mod sqlite;
 #[cfg(test)]
 mod tests;
