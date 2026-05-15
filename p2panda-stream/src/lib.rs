c2762c75182c1edceeb720f1e0228c9430ec2226
adz <x12@adz.garden>
2026-05-15 10:04:10 +0200

    README.md and improved docs for p2panda-stream

--- p2panda-stream/src/lib.rs
+++ p2panda-stream/src/lib.rs
@@ -2,8 +2,8 @@
 
 //! Interfaces to implement and compose event processors on top of data streams with some p2panda
 //! implementations out-of-the-box, wrapping existing p2panda crates for causal message ordering,
-//! log validation, access control and group encryption CRDTs (`p2panda-spaces`) which might come
-//! in handy for many peer-to-peer applications.
+//! log validation, access control and group encryption CRDTs which might come in handy for some
+//! peer-to-peer applications.
 //!
 //! Developers can "stack up" different stream processors on top of each other and layer them based
 //! on the application's needs. Each processor will formulate requirements (in the form of a Rust
@@ -12,11 +12,11 @@
 //!
 //! In p2panda we clearly separate the "event delivery" layer from the "event processing" one. This
 //! part of the stack doesn't care what transport was used (pidgeon carriers, usb sticks, internet
-//! protocol, LoRa, etc.). Ideally the application should just work fine independent of how the
-//! data was delivered, the only part which really matters is how the event stream was _processed_.
+//! protocol, LoRa, etc.). Ideally the application should just work fine independent of how the data
+//! was delivered, the only part which really matters is how the event stream was _processed_.
 //!
 //! This stream processor design can be nicely combined with a Pub/Sub system (like `p2panda-net`),
-//! scoped and stateful stream controllers (in `p2panda-client`) and storage backends supporting
+//! scoped and stateful stream controllers (in `p2panda` Node API) and storage backends supporting
 //! atomic transactions (`p2panda-store`).
 //!
 //! ## Single-Threaded Design
