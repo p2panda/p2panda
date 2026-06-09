442d96757ccb65dfba7a55b34af57bab1fb2882c
Matthias Beyer <mail@beyermatthias.de>
2026-06-09 09:45:09 +0200

    encryption: Replace assert!(matches!()) with assert_matches!()
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- p2panda-encryption/src/crypto/xchacha20.rs
+++ p2panda-encryption/src/crypto/xchacha20.rs
@@ -91,21 +91,21 @@ mod tests {
         let invalid_nonce: XAeadNonce = rng.random_array().unwrap();
 
         // Invalid key.
-        assert!(matches!(
+        std::assert_matches!(
             x_aead_decrypt(&invalid_key, &ciphertext, nonce, None),
             Err(XAeadError::Decrypt(chacha20poly1305::Error))
-        ));
+        );
 
         // Invalid nonce.
-        assert!(matches!(
+        std::assert_matches!(
             x_aead_decrypt(&key, &ciphertext, invalid_nonce, None),
             Err(XAeadError::Decrypt(chacha20poly1305::Error))
-        ));
+        );
 
         // Invalid additional data.
-        assert!(matches!(
+        std::assert_matches!(
             x_aead_decrypt(&key, &ciphertext, nonce, Some(b"invalid aad")),
             Err(XAeadError::Decrypt(chacha20poly1305::Error))
-        ));
+        );
     }
 }
