a11ef48697f4f5729884a8816ec3da02484b9e1e
adz <x12@adz.garden>
2026-06-11 19:10:15 +0200

    node: Group and Member API drafts

--- p2panda/src/spaces/mod.rs
+++ p2panda/src/spaces/mod.rs
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
 mod forge;
+mod group;
+mod member;
 pub(crate) mod message;
 mod space;
 pub(crate) mod types;
@@ -10,5 +12,8 @@ pub use p2panda_auth::AccessLevel;
 pub use p2panda_spaces::ActorId;
 pub use p2panda_spaces::manager::ManagerError;
 
+pub use group::{Group, GroupError, GroupFuture};
+pub use member::{GroupActor, Member, MemberError};
+pub(crate) use space::spaces_stream;
 pub use space::{Space, SpaceError, SpaceFuture, SpaceSubscription};
 pub use types::SpacesManagerError;
