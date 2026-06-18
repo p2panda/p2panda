b83aafc9846db32801cb27da64eefe057fae1484
adz <x12@adz.garden>
2026-06-18 10:55:05 +0200

    store: SqliteSpaces with key-stores & id type

--- p2panda-store/src/spaces/traits.rs
+++ p2panda-store/src/spaces/traits.rs
@@ -2,29 +2,33 @@
 
 use std::error::Error;
 
+use p2panda_core::Hash;
+
 use crate::spaces::SpacesMessage;
 
-pub trait SpacesMessageStore<ID, A> {
+pub trait SpacesMessageStore<A> {
     type Error: Error;
 
     fn get_spaces_message(
         &self,
-        id: &ID,
+        id: &Hash,
     ) -> impl Future<Output = Result<Option<SpacesMessage<A>>, Self::Error>>;
 }
 
-pub trait SpacesStore<ID, S> {
+pub trait SpacesStore<S> {
     type Error: Error;
 
-    fn get_space_state_tx(&self, id: &ID) -> impl Future<Output = Result<Option<S>, Self::Error>>;
+    fn get_space_state_tx(&self, id: &Hash)
+    -> impl Future<Output = Result<Option<S>, Self::Error>>;
 
-    fn has_space(&self, id: &ID) -> impl Future<Output = Result<bool, Self::Error>>;
+    fn has_space(&self, id: &Hash) -> impl Future<Output = Result<bool, Self::Error>>;
 
-    fn space_ids(&self) -> impl Future<Output = Result<Vec<ID>, Self::Error>>;
+    fn space_ids(&self) -> impl Future<Output = Result<Vec<Hash>, Self::Error>>;
 }
 
-pub trait SpacesStoreWrite<ID, S> {
+pub trait SpacesStoreWrite<S> {
     type Error: Error;
 
-    fn set_space_state_tx(&self, id: &ID, y: &S) -> impl Future<Output = Result<(), Self::Error>>;
+    fn set_space_state_tx(&self, id: &Hash, y: &S)
+    -> impl Future<Output = Result<(), Self::Error>>;
 }
