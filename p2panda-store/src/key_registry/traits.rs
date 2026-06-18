ea6d87cab94a78cce8bc8e6ee16ec01602774669
adz <x12@adz.garden>
2026-06-18 10:53:05 +0200

    store: Use concrete states type for key stores

--- p2panda-store/src/key_registry/traits.rs
+++ p2panda-store/src/key_registry/traits.rs
@@ -2,11 +2,19 @@
 
 use std::error::Error;
 
+use p2panda_core::VerifyingKey;
+use p2panda_encryption::key_registry::KeyRegistryState;
+
 /// Interface for setting and getting key registry state.
-pub trait KeyRegistryStore<S> {
+pub trait KeyRegistryStore {
     type Error: Error;
 
-    fn get_key_registry(&self) -> impl Future<Output = Result<Option<S>, Self::Error>>;
+    fn get_key_registry(
+        &self,
+    ) -> impl Future<Output = Result<Option<KeyRegistryState<VerifyingKey>>, Self::Error>>;
 
-    fn set_key_registry(&self, state: &S) -> impl Future<Output = Result<(), Self::Error>>;
+    fn set_key_registry(
+        &self,
+        state: &KeyRegistryState<VerifyingKey>,
+    ) -> impl Future<Output = Result<(), Self::Error>>;
 }
