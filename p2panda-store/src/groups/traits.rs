76a2d8e94d04fe0e6c1ee9d9b5bd0fe065c8e732
adz <x12@adz.garden>
2026-06-18 10:52:14 +0200

    store: Use concrete states type for groups store

--- p2panda-store/src/groups/traits.rs
+++ p2panda-store/src/groups/traits.rs
@@ -2,17 +2,30 @@
 
 use std::error::Error;
 
+use p2panda_auth::group::GroupCrdtState;
+use p2panda_auth::traits::{Conditions, Operation as AuthOperation};
+use p2panda_core::{Hash, VerifyingKey};
+
+pub type GroupsStoreState<M, C> = GroupCrdtState<VerifyingKey, Hash, M, C>;
+
 /// Trait describing API for setting and getting groups CRDT state by id.
-pub trait GroupsStore<ID, S> {
+pub trait GroupsStore<M, C>
+where
+    M: AuthOperation<VerifyingKey, Hash, C>,
+    C: Conditions,
+{
     type Error: Error;
 
     /// Set state for a specified groups instance.
     fn set_groups_state_tx(
         &self,
-        id: &ID,
-        state: &S,
+        id: Hash,
+        state: &GroupsStoreState<M, C>,
     ) -> impl Future<Output = Result<(), Self::Error>>;
 
     /// Get state for a specified groups instance.
-    fn get_groups_state_tx(&self, id: &ID) -> impl Future<Output = Result<Option<S>, Self::Error>>;
+    fn get_groups_state_tx(
+        &self,
+        id: Hash,
+    ) -> impl Future<Output = Result<Option<GroupsStoreState<M, C>>, Self::Error>>;
 }
