ac75d549e16f286c2a30c680daef1b66cefe9439
adz <x12@adz.garden>
2026-06-18 10:53:05 +0200

    store: Use concrete states type for key stores

--- p2panda-store/src/key_secrets/traits.rs
+++ p2panda-store/src/key_secrets/traits.rs
@@ -2,12 +2,19 @@
 
 use std::error::Error;
 
+use p2panda_encryption::key_manager::PreKeyBundlesState;
+
 /// Interface for setting and getting pre key secrets.
 // TODO: Naming; should this rather be PreKeyBundlesStore?
-pub trait KeySecretsStore<S> {
+pub trait KeySecretsStore {
     type Error: Error;
 
-    fn get_prekey_secrets(&self) -> impl Future<Output = Result<Option<S>, Self::Error>>;
+    fn get_prekey_secrets(
+        &self,
+    ) -> impl Future<Output = Result<Option<PreKeyBundlesState>, Self::Error>>;
 
-    fn set_prekey_secrets(&self, state: &S) -> impl Future<Output = Result<(), Self::Error>>;
+    fn set_prekey_secrets(
+        &self,
+        state: &PreKeyBundlesState,
+    ) -> impl Future<Output = Result<(), Self::Error>>;
 }
