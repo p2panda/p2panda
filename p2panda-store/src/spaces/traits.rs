8b571205a10d27b0583987cd8c6b1fb78457e3cd
adz <x12@adz.garden>
2026-06-19 15:11:06 +0200

    store: Merge SpacesStoreWrite into SpacesStore
    Users will only work with one unified store (SqliteStore) and not bother
    about fine-grained store trait boundaries.
    
    Additionally we're not distincting between read / writes in other store
    traits either.

--- p2panda-store/src/spaces/traits.rs
+++ p2panda-store/src/spaces/traits.rs
@@ -6,13 +6,13 @@ use p2panda_core::Hash;
 
 use crate::spaces::SpacesMessage;
 
-pub trait SpacesMessageStore<A> {
+pub trait SpacesMessageStore<ARG> {
     type Error: Error;
 
     fn get_spaces_message(
         &self,
         id: &Hash,
-    ) -> impl Future<Output = Result<Option<SpacesMessage<A>>, Self::Error>>;
+    ) -> impl Future<Output = Result<Option<SpacesMessage<ARG>>, Self::Error>>;
 }
 
 pub trait SpacesStore<S> {
@@ -21,14 +21,10 @@ pub trait SpacesStore<S> {
     fn get_space_state_tx(&self, id: &Hash)
     -> impl Future<Output = Result<Option<S>, Self::Error>>;
 
+    fn set_space_state_tx(&self, id: &Hash, y: &S)
+    -> impl Future<Output = Result<(), Self::Error>>;
+
     fn has_space(&self, id: &Hash) -> impl Future<Output = Result<bool, Self::Error>>;
 
     fn space_ids(&self) -> impl Future<Output = Result<Vec<Hash>, Self::Error>>;
-}
-
-pub trait SpacesStoreWrite<S> {
-    type Error: Error;
-
-    fn set_space_state_tx(&self, id: &Hash, y: &S)
-    -> impl Future<Output = Result<(), Self::Error>>;
 }
