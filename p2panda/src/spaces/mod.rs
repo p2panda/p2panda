563f8eed30574f87b4fe6a347bfff5d03e21bc32
Sam Andreae <contact@samandreae.com>
2026-06-25 18:54:26 +0100

    node: create space and add member API e2e

--- p2panda/src/spaces/mod.rs
+++ p2panda/src/spaces/mod.rs
@@ -8,7 +8,7 @@ mod space;
 pub(crate) mod types;
 
 use p2panda_auth::Access;
-use p2panda_core::Topic;
+use p2panda_core::{Topic, VerifyingKey};
 use p2panda_store::SqliteStore;
 
 // Re-export useful types.
@@ -16,6 +16,7 @@ pub use p2panda_auth::AccessLevel;
 pub use p2panda_spaces::manager::ManagerError;
 pub use p2panda_spaces::{ActorId, GroupContext, GroupId, MemberId, SpaceContext, SpaceId};
 
+pub(crate) use forge::KEY_BUNDLE_LOG_ID;
 pub use group::{Group, GroupError, GroupEvent, GroupFuture};
 pub use member::{GroupActor, Member, MemberError};
 pub(crate) use space::spaces_stream;
@@ -24,6 +25,8 @@ pub use types::SpacesManagerError;
 
 use crate::Credentials;
 use crate::forge::OperationForge;
+use crate::operation::LogId;
+use crate::spaces::forge::GROUP_CONTROL_MESSAGE;
 use crate::spaces::types::{AuthCapabilities, SpacesManager, SpacesStore};
 
 pub async fn spaces_manager(
@@ -60,4 +63,16 @@ pub(crate) fn to_initial_members(
             )
         })
         .collect()
+}
+
+pub(crate) fn group_log_id(group_id: VerifyingKey) -> LogId {
+    LogId::digest(&{
+        let mut bytes = Vec::new();
+        bytes.extend_from_slice(group_id.as_bytes());
+        // The group id would be enough to indicate the log id, we hash it here together
+        // with a constant value to prevent possible collisions with logs of same id but
+        // different purpose.
+        bytes.extend_from_slice(GROUP_CONTROL_MESSAGE);
+        bytes
+    })
 }
